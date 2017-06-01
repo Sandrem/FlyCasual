b134104b1dff3966168308c5fe0919ea4d806a3a
Sandrem <oogoeroo@gmail.com>
2017-06-01 20:32:55 +0300

    Add-Remove-Copy

--- Assets/Scripts/MainMenu/MainMenuScript.cs
+++ Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -14,8 +14,7 @@ public class MainMenuScript : MonoBehaviour {
     public GameObject SquadBuilder;
     public GameObject BackgroundImage;
 
-    private Dictionary<string, string> AllShips = new Dictionary<string, string>();
-    private Dictionary<string, string> AllPilots = new Dictionary<string, string>();
+    public GameObject RosterBuilderPrefab;
 
     // Use this for initialization
     void Start () {
@@ -23,13 +22,8 @@ public class MainMenuScript : MonoBehaviour {
         ButtonsHolder.transform.position = new Vector3(Screen.width / 20, Screen.height - Screen.height / 20, 0.0f);
         BackgroundImage.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.height * 16f/9f, Screen.height);
         //BackgroundImage.transform.position = new Vector3(Screen.width - Screen.height, 0, 0);
-        Global.test = "Changed";
     }
 	
-	// Update is called once per frame
-	void Update () {
-	}
-
     public void NewGame()
     {
         OpenSquadronBuilder();
@@ -37,8 +31,7 @@ public class MainMenuScript : MonoBehaviour {
 
     public void StartGame()
     {
-        SetPlayers();
-        GeneratePlayersShipConfigurations();
+        RosterBuilder.PrepareForGameStart();
         SceneManager.LoadScene("Battle");
     }
 
@@ -46,115 +39,7 @@ public class MainMenuScript : MonoBehaviour {
     {
         ButtonsHolder.SetActive(false);
         SquadBuilder.SetActive(true);
-        SetPlayers();
-        SetPlayerFactions();
-        GenerateShipsList();
-        SetShips();
-        SetPilots();
-    }
-
-    private void GenerateShipsList()
-    {
-        IEnumerable<string> namespaceIEnum =
-            from types in Assembly.GetExecutingAssembly().GetTypes()
-            where types.Namespace != null
-            where types.Namespace.StartsWith("Ship.")
-            select types.Namespace;
-
-        List<string> namespaceList = new List<string>();
-        foreach (var ns in namespaceIEnum)
-        {
-            if (!namespaceList.Contains(ns))
-            {
-                namespaceList.Add(ns);
-                Ship.GenericShip newShipTypeContainer = (Ship.GenericShip)System.Activator.CreateInstance(System.Type.GetType(ns + "." + ns.Substring(5)));
-                AllShips.Add(newShipTypeContainer.Type, ns);
-            }
-        }
-
-    }
-
-    private void SetShips()
-    {
-        SetShip(PlayerNo.Player1);
-        SetShip(PlayerNo.Player2);
-    }
-
-    private void SetShip(PlayerNo playerNo)
-    {
-        List<string> results = new List<string>();
-        foreach (var ships in AllShips)
-        {
-            Ship.GenericShip newShip = (Ship.GenericShip) System.Activator.CreateInstance(System.Type.GetType(ships.Value + "." + ships.Value.Substring(5)));
-            if (newShip.faction == Global.GetPlayerFaction(playerNo))
-            {
-                results.Add(ships.Key);
-            }
-        }
-
-        foreach (Transform shipPanel in GameObject.Find("Canvas").transform.Find("Panel").Find("SquadBuilderPanel").Find("ShipsPanel").Find("Player" + Tools.PlayerToInt(playerNo) + "Ships"))
-        {
-            Dropdown shipDropdown = shipPanel.Find("GroupShip").Find("Dropdown").GetComponent<Dropdown>();
-            shipDropdown.ClearOptions();
-            shipDropdown.AddOptions(results);
-        }
-
-    }
-
-    private void SetPilots()
-    {
-        SetPilot(PlayerNo.Player1);
-        SetPilot(PlayerNo.Player2);
-    }
-
-    private void SetPilot(PlayerNo playerNo)
-    {
-        string shipNameFull = GameObject.Find("Canvas").transform.Find("Panel").Find("SquadBuilderPanel").Find("ShipsPanel").Find("Player" + Tools.PlayerToInt(playerNo) + "Ships").Find("ShipPanel1").Find("GroupShip").Find("Dropdown").GetComponent<Dropdown>().captionText.text;
-        string shipNameId = AllShips[shipNameFull];
-
-        List<string> results = GetPilotsList(shipNameId);
-
-        foreach (Transform shipPanel in GameObject.Find("Canvas").transform.Find("Panel").Find("SquadBuilderPanel").Find("ShipsPanel").Find("Player" + Tools.PlayerToInt(playerNo) + "Ships"))
-        {
-            Dropdown pilotDropdown = shipPanel.Find("GroupPilot").Find("Dropdown").GetComponent<Dropdown>();
-            pilotDropdown.ClearOptions();
-            pilotDropdown.AddOptions(results);
-        }
-
-    }
-
-    private List<string> GetPilotsList(string shipName)
-    {
-        List<string> result = new List<string>();
-
-        List<Type> typelist = Assembly.GetExecutingAssembly().GetTypes()
-            .Where(t => String.Equals(t.Namespace, shipName, StringComparison.Ordinal))
-            .ToList();
-
-        foreach (var type in typelist)
-        {
-            Ship.GenericShip newShipContainer = (Ship.GenericShip)System.Activator.CreateInstance(type);
-            if (newShipContainer.PilotName != null)
-            {
-                AllPilots.Add(newShipContainer.PilotName, type.ToString());
-                result.Add(newShipContainer.PilotName);
-            }
-        }
-
-        return result;
-    }
-
-    private Faction GetPlayerFaction(PlayerNo playerNo)
-    {
-        int index = GameObject.Find("Canvas").transform.Find("Panel").Find("SquadBuilderPanel").Find("PlayersPanel").Find("Player" + Tools.PlayerToInt(playerNo) + "Panel").Find("GroupFaction").Find("Dropdown").GetComponent<Dropdown>().value;
-        switch (index)
-        {
-            case 0:
-                return Faction.Rebels;
-            case 1:
-                return Faction.Empire;
-        }
-        return Faction.Empire;
+        RosterBuilder.Initialize();
     }
 
     public void CloseSquadronBuilder()
@@ -168,44 +53,14 @@ public class MainMenuScript : MonoBehaviour {
         Application.Quit();
     }
 
-    private void SetPlayers()
+    public void AddShipPlayer1()
     {
-        Global.RemoveAllPlayers();
-        Global.AddPlayer(GetPlayerType(PlayerNo.Player1));
-        Global.AddPlayer(GetPlayerType(PlayerNo.Player2));
+        RosterBuilder.AddShip(PlayerNo.Player1);
     }
 
-    private void SetPlayerFactions()
+    public void AddShipPlayer2()
     {
-        Global.AddFaction(GetPlayerFaction(PlayerNo.Player1));
-        Global.AddFaction(GetPlayerFaction(PlayerNo.Player2));
+        RosterBuilder.AddShip(PlayerNo.Player2);
     }
 
-    private System.Type GetPlayerType(PlayerNo playerNo)
-    {
-        int index = GameObject.Find("Canvas").transform.Find("Panel").Find("SquadBuilderPanel").Find("PlayersPanel").Find("Player"+ Tools.PlayerToInt(playerNo) + "Panel").Find("GroupPlayer").Find("Dropdown").GetComponent<Dropdown>().value;
-        switch (index)
-        {
-            case 0: return typeof(Players.HumanPlayer);
-            case 1: return typeof(Players.HotacAiPlayer);
-        }
-        return null;
-    }
-
-    private void GeneratePlayersShipConfigurations()
-    {
-        GeneratePlayerShipConfigurations(PlayerNo.Player1);
-        GeneratePlayerShipConfigurations(PlayerNo.Player2);
-    }
-
-    private void GeneratePlayerShipConfigurations(PlayerNo playerNo)
-    {
-        foreach (Transform shipPanel in GameObject.Find("Canvas").transform.Find("Panel").Find("SquadBuilderPanel").Find("ShipsPanel").Find("Player" + Tools.PlayerToInt(playerNo) + "Ships"))
-        {
-            string pilotNameFull = shipPanel.Find("GroupPilot").Find("Dropdown").GetComponent<Dropdown>().captionText.text;
-            string pilotNameId = AllPilots[pilotNameFull];
-
-            Global.AddShip(pilotNameId, new List<string>(), playerNo);
-        }
-    }
 }
---
+++ Assets/Scripts/MainMenu/RosterBuilder.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Reflection;
+using System.Linq;
+using Players;
+
+public static class RosterBuilder {
+
+    private static Dictionary<string, string> AllShips = new Dictionary<string, string>();
+    private static Dictionary<string, string> AllPilots = new Dictionary<string, string>();
+
+    public static void Initialize()
+    {
+        SetPlayers();
+        SetPlayerFactions();
+        GenerateShipsList();
+        AddInitialShips();
+    }
+
+    public static void PrepareForGameStart()
+    {
+        SetPlayers();
+        GeneratePlayersShipConfigurations();
+    }
+
+    private static void AddInitialShips()
+    {
+        AddShip(PlayerNo.Player1);
+        AddShip(PlayerNo.Player2);
+    }
+
+    public static void AddShip(PlayerNo playerNo)
+    {
+        GameObject prefab = GameObject.Find("ScriptHolder").GetComponent<MainMenuScript>().RosterBuilderPrefab;
+        Transform parent = GameObject.Find("Canvas").transform.Find("Panel").Find("SquadBuilderPanel").Find("ShipsPanel").Find("Player" + Tools.PlayerToInt(playerNo) + "Ships");
+
+        GameObject newPanel = MonoBehaviour.Instantiate(prefab, parent);
+        newPanel.transform.localPosition = Vector3.zero;
+        newPanel.transform.Find("Panel").Find("RemoveButton").GetComponent<Button>().onClick.AddListener(delegate { RemoveShip(newPanel); });
+        newPanel.transform.Find("Panel").Find("CopyButton").GetComponent<Button>().onClick.AddListener(delegate { CopyShip(newPanel); });
+
+        SetShip(newPanel, playerNo);
+
+        OrganizeShipsList(playerNo);
+    }
+
+    public static void CopyShip(GameObject panel)
+    {
+        Transform parent = panel.transform.parent;
+        GameObject newPanel = MonoBehaviour.Instantiate(panel, parent);
+        newPanel.transform.Find("Panel").Find("RemoveButton").GetComponent<Button>().onClick.AddListener(delegate { RemoveShip(newPanel); });
+        newPanel.transform.Find("Panel").Find("CopyButton").GetComponent<Button>().onClick.AddListener(delegate { CopyShip(newPanel); });
+
+        OrganizeAllShipsLists();
+    }
+
+    public static void RemoveShip(GameObject panel)
+    {
+        MonoBehaviour.DestroyImmediate(panel);
+        OrganizeAllShipsLists();
+    }
+
+    private static void OrganizeAllShipsLists()
+    {
+        OrganizeShipsList(PlayerNo.Player1);
+        OrganizeShipsList(PlayerNo.Player2);
+    }
+
+    private static void OrganizeShipsList(PlayerNo playerNo)
+    {
+        float offset = 0;
+        Transform addShipPanel = GameObject.Find("Global").transform;
+        foreach (Transform shipPanel in GameObject.Find("Canvas").transform.Find("Panel").Find("SquadBuilderPanel").Find("ShipsPanel").Find("Player" + Tools.PlayerToInt(playerNo) + "Ships"))
+        {
+            if (shipPanel.Find("AddShipButton").gameObject.activeSelf)
+            {
+                addShipPanel = shipPanel;
+            }
+            else
+            {
+                shipPanel.localPosition = new Vector3(shipPanel.localPosition.x, offset, shipPanel.localPosition.z);
+                offset = offset - shipPanel.GetComponent<RectTransform>().sizeDelta.y - 30 - 5;
+            }
+        }
+
+        addShipPanel.localPosition = new Vector3(addShipPanel.localPosition.x, offset, addShipPanel.localPosition.z);
+    }
+
+    private static void GenerateShipsList()
+    {
+        IEnumerable<string> namespaceIEnum =
+            from types in Assembly.GetExecutingAssembly().GetTypes()
+            where types.Namespace != null
+            where types.Namespace.StartsWith("Ship.")
+            select types.Namespace;
+
+        List<string> namespaceList = new List<string>();
+        foreach (var ns in namespaceIEnum)
+        {
+            if (!namespaceList.Contains(ns))
+            {
+                namespaceList.Add(ns);
+                Ship.GenericShip newShipTypeContainer = (Ship.GenericShip)System.Activator.CreateInstance(System.Type.GetType(ns + "." + ns.Substring(5)));
+                AllShips.Add(newShipTypeContainer.Type, ns);
+            }
+        }
+
+    }
+
+    private static void SetShip(GameObject panel, PlayerNo playerNo)
+    {
+        List<string> results = new List<string>();
+        foreach (var ships in AllShips)
+        {
+            Ship.GenericShip newShip = (Ship.GenericShip) System.Activator.CreateInstance(System.Type.GetType(ships.Value + "." + ships.Value.Substring(5)));
+            if (newShip.faction == Global.GetPlayerFaction(playerNo))
+            {
+                results.Add(ships.Key);
+            }
+        }
+
+        Dropdown shipDropdown = panel.transform.Find("GroupShip").Find("Dropdown").GetComponent<Dropdown>();
+        shipDropdown.ClearOptions();
+        shipDropdown.AddOptions(results);
+
+        SetPilot(panel, playerNo);
+    }
+
+    private static void SetPilot(GameObject panel, PlayerNo playerNo)
+    {
+        string shipNameFull = panel.transform.Find("GroupShip").Find("Dropdown").GetComponent<Dropdown>().captionText.text;
+        string shipNameId = AllShips[shipNameFull];
+        List<string> results = GetPilotsList(shipNameId);
+
+        Dropdown pilotDropdown = panel.transform.Find("GroupPilot").Find("Dropdown").GetComponent<Dropdown>();
+        pilotDropdown.ClearOptions();
+        pilotDropdown.AddOptions(results);
+    }
+
+    private static List<string> GetPilotsList(string shipName)
+    {
+        List<string> result = new List<string>();
+
+        List<Type> typelist = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(t => String.Equals(t.Namespace, shipName, StringComparison.Ordinal))
+            .ToList();
+
+        foreach (var type in typelist)
+        {
+            Ship.GenericShip newShipContainer = (Ship.GenericShip)System.Activator.CreateInstance(type);
+            if (newShipContainer.PilotName != null)
+            {
+                if (!AllPilots.ContainsKey(newShipContainer.PilotName))
+                {
+                    AllPilots.Add(newShipContainer.PilotName, type.ToString());
+                }
+                result.Add(newShipContainer.PilotName);
+            }
+        }
+
+        return result;
+    }
+
+    private static Faction GetPlayerFaction(PlayerNo playerNo)
+    {
+        int index = GameObject.Find("Canvas").transform.Find("Panel").Find("SquadBuilderPanel").Find("PlayersPanel").Find("Player" + Tools.PlayerToInt(playerNo) + "Panel").Find("GroupFaction").Find("Dropdown").GetComponent<Dropdown>().value;
+        switch (index)
+        {
+            case 0:
+                return Faction.Rebels;
+            case 1:
+                return Faction.Empire;
+        }
+        return Faction.Empire;
+    }
+
+    private static void SetPlayers()
+    {
+        Global.RemoveAllPlayers();
+        Global.AddPlayer(GetPlayerType(PlayerNo.Player1));
+        Global.AddPlayer(GetPlayerType(PlayerNo.Player2));
+    }
+
+    private static void SetPlayerFactions()
+    {
+        Global.AddFaction(GetPlayerFaction(PlayerNo.Player1));
+        Global.AddFaction(GetPlayerFaction(PlayerNo.Player2));
+    }
+
+    private static System.Type GetPlayerType(PlayerNo playerNo)
+    {
+        int index = GameObject.Find("Canvas").transform.Find("Panel").Find("SquadBuilderPanel").Find("PlayersPanel").Find("Player"+ Tools.PlayerToInt(playerNo) + "Panel").Find("GroupPlayer").Find("Dropdown").GetComponent<Dropdown>().value;
+        switch (index)
+        {
+            case 0: return typeof(Players.HumanPlayer);
+            case 1: return typeof(Players.HotacAiPlayer);
+        }
+        return null;
+    }
+
+    private static void GeneratePlayersShipConfigurations()
+    {
+        GeneratePlayerShipConfigurations(PlayerNo.Player1);
+        GeneratePlayerShipConfigurations(PlayerNo.Player2);
+    }
+
+    private static void GeneratePlayerShipConfigurations(PlayerNo playerNo)
+    {
+        foreach (Transform shipPanel in GameObject.Find("Canvas").transform.Find("Panel").Find("SquadBuilderPanel").Find("ShipsPanel").Find("Player" + Tools.PlayerToInt(playerNo) + "Ships"))
+        {
+            if (shipPanel.name == "AddShipPanel") continue;
+            string pilotNameFull = shipPanel.Find("GroupPilot").Find("Dropdown").GetComponent<Dropdown>().captionText.text;
+            string pilotNameId = AllPilots[pilotNameFull];
+
+            Global.AddShip(pilotNameId, new List<string>(), playerNo);
+        }
+    }
+}

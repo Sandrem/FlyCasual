a18c718c407a96a193cadc15fe3abe91f7099bed
Sandrem <oogoeroo@gmail.com>
2017-06-16 23:04:40 +0300

    Ion Cannon Turret - Basic

---
+++ Assets/Scripts/Model/Upgrades/Turret/IonCannonTurret.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Upgrade;
+
+namespace UpgradesList
+{
+
+    public class IonCannonTurret : GenericSecondaryWeapon
+    {
+        public IonCannonTurret() : base()
+        {
+            Type = UpgradeSlot.Turret;
+
+            Name = "Ion Cannon Turret";
+            ShortName = "Ion Cannon Turret";
+            ImageUrl = "https://vignette4.wikia.nocookie.net/xwing-miniatures/images/f/f3/Ion_Cannon_Turret.png";
+            Cost = 5;
+
+            MinRange = 1;
+            MaxRange = 2;
+            AttackValue = 3;
+        }
+
+        public override void AttachToShip(Ship.GenericShip host)
+        {
+            base.AttachToShip(host);
+        }
+
+        public override bool IsShotAvailable(Ship.GenericShip anotherShip)
+        {
+            bool result = true;
+
+            if (isDiscarded) return false;
+
+            int distance = Actions.GetRange(Host, anotherShip);
+            if (distance < MinRange) return false;
+            if (distance > MaxRange) return false;
+
+            return result;
+        }
+
+    }
+
+}

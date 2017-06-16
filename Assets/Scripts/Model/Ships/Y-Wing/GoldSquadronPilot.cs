80b8c9b5c41cb44c5f2b1a3fa7695c0745640840
Sandrem <oogoeroo@gmail.com>
2017-06-16 20:47:16 +0300

    Y-Wing generic pilots

---
+++ Assets/Scripts/Model/Ships/Y-Wing/GoldSquadronPilot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace YWing
+    {
+        public class GoldSquadronPilot : YWing
+        {
+            public GoldSquadronPilot() : base()
+            {
+                PilotName = "Gold Squadron Pilot";
+                ImageUrl = "https://vignette2.wikia.nocookie.net/xwing-miniatures/images/d/d7/Gold_Squadron_Pilot.jpg";
+                PilotSkill = 2;
+                Cost = 18;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Ships/Y-Wing/YWing.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace YWing
+    {
+        public class YWing : GenericShip
+        {
+
+            public YWing() : base()
+            {
+                Type = "Y-Wing";
+                ManeuversImageUrl = "https://vignette4.wikia.nocookie.net/xwing-miniatures/images/1/18/MR_Y-WING.png";
+
+                Firepower = 2;
+                Agility = 1;
+                MaxHull = 5;
+                MaxShields = 3;
+
+                AddUpgradeSlot(Upgrade.UpgradeSlot.Turret);
+                AddUpgradeSlot(Upgrade.UpgradeSlot.Astromech);
+                AddUpgradeSlot(Upgrade.UpgradeSlot.Torpedoes);
+                AddUpgradeSlot(Upgrade.UpgradeSlot.Torpedoes);
+
+                AssignTemporaryManeuvers();
+                HotacManeuverTable = new XWingTable();
+
+                factions.Add(Faction.Rebels);
+                faction = Faction.Rebels;
+
+                SoundShotsPath = "XWing-Laser";
+                ShotsCount = 2;
+
+                for (int i = 1; i < 3; i++)
+                {
+                    SoundFlyPaths.Add("YWing-Fly" + i);
+                }
+                
+            }
+
+            public override void InitializeShip()
+            {
+                base.InitializeShip();
+                BuiltInActions.Add(new ActionsList.TargetLockAction());
+            }
+
+            private void AssignTemporaryManeuvers()
+            {
+                Maneuvers.Add("1.L.T", ManeuverColor.None);
+                Maneuvers.Add("1.L.B", ManeuverColor.White);
+                Maneuvers.Add("1.F.S", ManeuverColor.Green);
+                Maneuvers.Add("1.R.B", ManeuverColor.White);
+                Maneuvers.Add("1.R.T", ManeuverColor.None);
+                Maneuvers.Add("1.F.R", ManeuverColor.None);
+                Maneuvers.Add("2.L.T", ManeuverColor.White);
+                Maneuvers.Add("2.L.B", ManeuverColor.White);
+                Maneuvers.Add("2.F.S", ManeuverColor.Green);
+                Maneuvers.Add("2.R.B", ManeuverColor.White);
+                Maneuvers.Add("2.R.T", ManeuverColor.White);
+                Maneuvers.Add("2.F.R", ManeuverColor.None);
+                Maneuvers.Add("3.L.T", ManeuverColor.Red);
+                Maneuvers.Add("3.L.B", ManeuverColor.White);
+                Maneuvers.Add("3.F.S", ManeuverColor.White);
+                Maneuvers.Add("3.R.B", ManeuverColor.White);
+                Maneuvers.Add("3.R.T", ManeuverColor.Red);
+                Maneuvers.Add("3.F.R", ManeuverColor.None);
+                Maneuvers.Add("4.F.S", ManeuverColor.Red);
+                Maneuvers.Add("4.F.R", ManeuverColor.Red);
+                Maneuvers.Add("5.F.S", ManeuverColor.None);
+                Maneuvers.Add("5.F.R", ManeuverColor.None);
+            }
+
+        }
+    }
+}

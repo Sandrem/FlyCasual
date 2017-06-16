724b8accfa9451cb7ca28fee5bfb52654c0e39e2
Sandrem <oogoeroo@gmail.com>
2017-06-16 23:11:17 +0300

    TIE Advanced missiles fix

--- Assets/Scripts/Model/Ships/TIE Advanced/TIEAdvanced.cs
+++ Assets/Scripts/Model/Ships/TIE Advanced/TIEAdvanced.cs
@@ -19,6 +19,8 @@ namespace Ship
                 MaxHull = 3;
                 MaxShields = 2;
 
+                AddUpgradeSlot(Upgrade.UpgradeSlot.Missiles);
+
                 AssignTemporaryManeuvers();
                 HotacManeuverTable = new TIEFighterTable();
 

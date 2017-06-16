ce50ec77bc0cc041ee5a884deefb8862ed94d458
Sandrem <oogoeroo@gmail.com>
2017-06-16 22:13:39 +0300

    TIE Advanced generics

---
+++ Assets/Scripts/Model/Ships/TIE Advanced/StormSquadronPilot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace TIEAdvanced
+    {
+        public class StormSquadronPilot : TIEAdvanced
+        {
+            public StormSquadronPilot() : base()
+            {
+                PilotName = "Storm Squadron Pilot";
+                ImageUrl = "https://vignette3.wikia.nocookie.net/xwing-miniatures/images/6/67/Storm_Squadron_Pilot.jpg";
+                PilotSkill = 4;
+                Cost = 23;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Ships/TIE Advanced/TIEAdvanced.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace TIEAdvanced
+    {
+        public class TIEAdvanced : GenericShip
+        {
+
+            public TIEAdvanced() : base()
+            {
+                Type = "TIE Advanced";
+                ManeuversImageUrl = "https://vignette1.wikia.nocookie.net/xwing-miniatures/images/8/85/MI_TIE-ADVANCED.png";
+
+                Firepower = 2;
+                Agility = 3;
+                MaxHull = 3;
+                MaxShields = 2;
+
+                AssignTemporaryManeuvers();
+                HotacManeuverTable = new TIEFighterTable();
+
+                factions.Add(Faction.Empire);
+                faction = Faction.Empire;
+
+                SoundShotsPath = "TIE-Fire";
+                ShotsCount = 2;
+
+                for (int i = 1; i < 8; i++)
+                {
+                    SoundFlyPaths.Add("TIE-Fly" + i);
+                }
+            }
+
+            public override void InitializeShip()
+            {
+                base.InitializeShip();
+                BuiltInActions.Add(new ActionsList.EvadeAction());
+                BuiltInActions.Add(new ActionsList.BarrelRollAction());
+                BuiltInActions.Add(new ActionsList.TargetLockAction());
+            }
+
+            private void AssignTemporaryManeuvers()
+            {
+                Maneuvers.Add("1.L.T", ManeuverColor.None);
+                Maneuvers.Add("1.L.B", ManeuverColor.Green);
+                Maneuvers.Add("1.F.S", ManeuverColor.None);
+                Maneuvers.Add("1.R.B", ManeuverColor.Green);
+                Maneuvers.Add("1.R.T", ManeuverColor.None);
+                Maneuvers.Add("1.F.R", ManeuverColor.None);
+                Maneuvers.Add("2.L.T", ManeuverColor.White);
+                Maneuvers.Add("2.L.B", ManeuverColor.White);
+                Maneuvers.Add("2.F.S", ManeuverColor.Green);
+                Maneuvers.Add("2.R.B", ManeuverColor.White);
+                Maneuvers.Add("2.R.T", ManeuverColor.White);
+                Maneuvers.Add("2.F.R", ManeuverColor.None);
+                Maneuvers.Add("3.L.T", ManeuverColor.White);
+                Maneuvers.Add("3.L.B", ManeuverColor.White);
+                Maneuvers.Add("3.F.S", ManeuverColor.Green);
+                Maneuvers.Add("3.R.B", ManeuverColor.White);
+                Maneuvers.Add("3.R.T", ManeuverColor.White);
+                Maneuvers.Add("3.F.R", ManeuverColor.None);
+                Maneuvers.Add("4.F.S", ManeuverColor.White);
+                Maneuvers.Add("4.F.R", ManeuverColor.Red);
+                Maneuvers.Add("5.F.S", ManeuverColor.White);
+                Maneuvers.Add("5.F.R", ManeuverColor.None);
+            }
+
+        }
+    }
+}

528f24af69ea784ec03ff33d7b4dcc9eb13afb2c
Sandrem <oogoeroo@gmail.com>
2017-08-10 17:05:49 +0300

    Concussion Missiles are hidden

--- Assets/Scripts/Model/Upgrades/Missles/ConcussionMissiles.cs
+++ Assets/Scripts/Model/Upgrades/Missles/ConcussionMissiles.cs
@@ -10,6 +10,8 @@ namespace UpgradesList
     {
         public ConcussionMissiles() : base()
         {
+            IsHidden = true;
+
             Type = UpgradeSlot.Missiles;
 
             Name = "Concussion Missiles";

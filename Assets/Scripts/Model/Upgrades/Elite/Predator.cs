06ab7f9d8a2fda15f9b0cf5b31b56f321a4c7f3b
Sandrem <oogoeroo@gmail.com>
2017-08-12 14:23:45 +0300

    Predator elite upgrade

---
+++ Assets/Scripts/Model/Upgrades/Elite/Predator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Upgrade;
+
+namespace UpgradesList
+{
+
+    public class Predator : GenericUpgrade
+    {
+
+        public Predator() : base()
+        {
+            Type = UpgradeSlot.Elite;
+            Name = ShortName = "Predator";
+            ImageUrl = "https://vignette2.wikia.nocookie.net/xwing-miniatures/images/e/e9/Predator.png";
+            Cost = 3;
+        }
+
+        public override void AttachToShip(Ship.GenericShip host)
+        {
+            base.AttachToShip(host);
+
+            host.AfterGenerateAvailableActionEffectsList += PredatorActionEffect;
+        }
+
+        private void PredatorActionEffect(Ship.GenericShip host)
+        {
+            host.AddAvailableActionEffect(new ActionsList.PredatorActionEffect());
+        }
+
+    }
+}
+
+namespace ActionsList
+{
+
+    public class PredatorActionEffect : GenericAction
+    {
+        private Ship.GenericShip host;
+
+        public PredatorActionEffect()
+        {
+            Name = EffectName = "Predator";
+        }
+
+        public override bool IsActionEffectAvailable()
+        {
+            bool result = false;
+            if (Combat.AttackStep == CombatStep.Attack) result = true;
+            return result;
+        }
+
+        public override void ActionEffect()
+        {
+            DiceRerollManager diceRerollManager = new DiceRerollManager
+            {
+                NumberOfDicesCanBeRerolled = (Combat.Defender.PilotSkill > 2) ? 1 : 2
+            };
+            diceRerollManager.Start();
+        }
+
+    }
+
+}
+

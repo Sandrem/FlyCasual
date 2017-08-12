adad423565b277f0a3648e972d93af7dc01ee128
Sandrem <oogoeroo@gmail.com>
2017-08-12 20:32:37 +0300

    RayCast through dice view window

---
+++ Assets/Scripts/View/Tools/SelectDiceThroughMinimap.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectDiceThroughMinimap : MonoBehaviour {
+
+    private Camera diceCamera;
+
+    // Use this for initialization
+    void Start ()
+    {
+        diceCamera = GameObject.Find("SceneHolder/Board/DiceHolder/DiceCamera").GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = transform.InverseTransformPoint(Input.mousePosition);
+
+            Debug.Log(mousePosition);
+            if (transform.GetComponent<RectTransform>().rect.Contains(mousePosition))
+            {
+                RaycastHit hitInfo = new RaycastHit();
+
+                mousePosition = new Vector3(mousePosition.x, mousePosition.y+255);
+                if (Physics.Raycast(diceCamera.ScreenPointToRay(mousePosition), out hitInfo))
+                {
+                    Debug.Log(hitInfo.collider.name);
+                }
+
+            }
+        }
+	}
+}

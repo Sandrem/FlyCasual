e85ddd071b7e9bbeb99e8a746ceb24e23ada3fd9
Sandrem <oogoeroo@gmail.com>
2017-08-20 23:10:53 +0300

    TIE Interceptor manuevers and AI

--- Assets/Scripts/Model/Ships/TIE Interceptor/TIEInterceptor.cs
+++ Assets/Scripts/Model/Ships/TIE Interceptor/TIEInterceptor.cs
@@ -21,13 +21,13 @@ namespace Ship
                 MaxShields = 0;
 
                 AssignTemporaryManeuvers();
-                HotacManeuverTable = new AI.TIEAdvancedTable();
+                HotacManeuverTable = new AI.TIEInterceptorTable();
 
                 factions.Add(Faction.Empire);
                 faction = Faction.Empire;
 
                 SoundShotsPath = "TIE-Fire";
-                ShotsCount = 2;
+                ShotsCount = 3;
 
                 for (int i = 1; i < 8; i++)
                 {
@@ -45,28 +45,28 @@ namespace Ship
 
             private void AssignTemporaryManeuvers()
             {
-                Maneuvers.Add("1.L.T", ManeuverColor.None);
-                Maneuvers.Add("1.L.B", ManeuverColor.Green);
+                Maneuvers.Add("1.L.T", ManeuverColor.White);
+                Maneuvers.Add("1.L.B", ManeuverColor.None);
                 Maneuvers.Add("1.F.S", ManeuverColor.None);
-                Maneuvers.Add("1.R.B", ManeuverColor.Green);
-                Maneuvers.Add("1.R.T", ManeuverColor.None);
+                Maneuvers.Add("1.R.B", ManeuverColor.None);
+                Maneuvers.Add("1.R.T", ManeuverColor.White);
                 Maneuvers.Add("1.F.R", ManeuverColor.None);
-                Maneuvers.Add("2.L.T", ManeuverColor.White);
-                Maneuvers.Add("2.L.B", ManeuverColor.White);
+                Maneuvers.Add("2.L.T", ManeuverColor.Green);
+                Maneuvers.Add("2.L.B", ManeuverColor.Green);
                 Maneuvers.Add("2.F.S", ManeuverColor.Green);
-                Maneuvers.Add("2.R.B", ManeuverColor.White);
-                Maneuvers.Add("2.R.T", ManeuverColor.White);
+                Maneuvers.Add("2.R.B", ManeuverColor.Green);
+                Maneuvers.Add("2.R.T", ManeuverColor.Green);
                 Maneuvers.Add("2.F.R", ManeuverColor.None);
                 Maneuvers.Add("3.L.T", ManeuverColor.White);
                 Maneuvers.Add("3.L.B", ManeuverColor.White);
                 Maneuvers.Add("3.F.S", ManeuverColor.Green);
                 Maneuvers.Add("3.R.B", ManeuverColor.White);
                 Maneuvers.Add("3.R.T", ManeuverColor.White);
-                Maneuvers.Add("3.F.R", ManeuverColor.None);
-                Maneuvers.Add("4.F.S", ManeuverColor.White);
-                Maneuvers.Add("4.F.R", ManeuverColor.Red);
+                Maneuvers.Add("3.F.R", ManeuverColor.Red);
+                Maneuvers.Add("4.F.S", ManeuverColor.Green);
+                Maneuvers.Add("4.F.R", ManeuverColor.None);
                 Maneuvers.Add("5.F.S", ManeuverColor.White);
-                Maneuvers.Add("5.F.R", ManeuverColor.None);
+                Maneuvers.Add("5.F.R", ManeuverColor.Red);
             }
 
         }

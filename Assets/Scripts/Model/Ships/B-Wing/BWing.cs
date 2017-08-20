3d5763a57af35f99c9f0d92e413d86e69c7faf74
Sandrem <oogoeroo@gmail.com>
2017-08-20 04:43:27 +0300

    B-Wing and E-Wing pilots

---
+++ Assets/Scripts/Model/Ships/B-Wing/BWing.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Movement;
+
+namespace Ship
+{
+    namespace BWing
+    {
+        public class BWing : GenericShip
+        {
+
+            public BWing() : base()
+            {
+                Type = "B-Wing";
+                ManeuversImageUrl = "https://vignette1.wikia.nocookie.net/xwing-miniatures/images/3/3d/MR_T65-X-WING.png";
+
+                Firepower = 3;
+                Agility = 1;
+                MaxHull = 3;
+                MaxShields = 5;
+
+                AddUpgradeSlot(Upgrade.UpgradeSlot.System);
+                AddUpgradeSlot(Upgrade.UpgradeSlot.Cannon);
+                AddUpgradeSlot(Upgrade.UpgradeSlot.Torpedoes);
+                AddUpgradeSlot(Upgrade.UpgradeSlot.Torpedoes);
+
+                AssignTemporaryManeuvers();
+                HotacManeuverTable = new AI.XWingTable();
+
+                factions.Add(Faction.Rebels);
+                faction = Faction.Rebels;
+
+                SoundShotsPath = "XWing-Laser";
+                ShotsCount = 3;
+
+                for (int i = 1; i < 4; i++)
+                {
+                    SoundFlyPaths.Add("XWing-Fly" + i);
+                }
+                
+            }
+
+            public override void InitializeShip()
+            {
+                base.InitializeShip();
+                BuiltInActions.Add(new ActionsList.TargetLockAction());
+                BuiltInActions.Add(new ActionsList.BarrelRollAction());
+            }
+
+            private void AssignTemporaryManeuvers()
+            {
+                Maneuvers.Add("1.L.T", ManeuverColor.None);
+                Maneuvers.Add("1.L.B", ManeuverColor.Green);
+                Maneuvers.Add("1.F.S", ManeuverColor.Green);
+                Maneuvers.Add("1.R.B", ManeuverColor.Green);
+                Maneuvers.Add("1.R.T", ManeuverColor.None);
+                Maneuvers.Add("1.F.R", ManeuverColor.None);
+                Maneuvers.Add("2.L.T", ManeuverColor.White);
+                Maneuvers.Add("2.L.B", ManeuverColor.White);
+                Maneuvers.Add("2.F.S", ManeuverColor.Green);
+                Maneuvers.Add("2.R.B", ManeuverColor.White);
+                Maneuvers.Add("2.R.T", ManeuverColor.White);
+                Maneuvers.Add("2.F.R", ManeuverColor.None);
+                Maneuvers.Add("3.L.T", ManeuverColor.White);
+                Maneuvers.Add("3.L.B", ManeuverColor.White);
+                Maneuvers.Add("3.F.S", ManeuverColor.White);
+                Maneuvers.Add("3.R.B", ManeuverColor.White);
+                Maneuvers.Add("3.R.T", ManeuverColor.White);
+                Maneuvers.Add("3.F.R", ManeuverColor.None);
+                Maneuvers.Add("4.F.S", ManeuverColor.White);
+                Maneuvers.Add("4.F.R", ManeuverColor.Red);
+                Maneuvers.Add("5.F.S", ManeuverColor.None);
+                Maneuvers.Add("5.F.R", ManeuverColor.None);
+            }
+
+        }
+    }
+}

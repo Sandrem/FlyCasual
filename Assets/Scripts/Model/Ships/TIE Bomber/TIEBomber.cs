989af0e4942c7fd62df2ee9a3520af36857cf4c8
Sandrem <oogoeroo@gmail.com>
2017-08-20 23:22:40 +0300

    TIE Bomber Maneuvers and AI

--- Assets/Scripts/Model/Ships/TIE Bomber/TIEBomber.cs
+++ Assets/Scripts/Model/Ships/TIE Bomber/TIEBomber.cs
@@ -27,7 +27,7 @@ namespace Ship
                 AddUpgradeSlot(Upgrade.UpgradeSlot.Bomb);
 
                 AssignTemporaryManeuvers();
-                HotacManeuverTable = new AI.TIEAdvancedTable();
+                HotacManeuverTable = new AI.TIEBomberTable();
 
                 factions.Add(Faction.Empire);
                 faction = Faction.Empire;
@@ -51,16 +51,16 @@ namespace Ship
             private void AssignTemporaryManeuvers()
             {
                 Maneuvers.Add("1.L.T", ManeuverColor.None);
-                Maneuvers.Add("1.L.B", ManeuverColor.Green);
-                Maneuvers.Add("1.F.S", ManeuverColor.None);
-                Maneuvers.Add("1.R.B", ManeuverColor.Green);
+                Maneuvers.Add("1.L.B", ManeuverColor.White);
+                Maneuvers.Add("1.F.S", ManeuverColor.Green);
+                Maneuvers.Add("1.R.B", ManeuverColor.White);
                 Maneuvers.Add("1.R.T", ManeuverColor.None);
                 Maneuvers.Add("1.F.R", ManeuverColor.None);
-                Maneuvers.Add("2.L.T", ManeuverColor.White);
-                Maneuvers.Add("2.L.B", ManeuverColor.White);
+                Maneuvers.Add("2.L.T", ManeuverColor.Red);
+                Maneuvers.Add("2.L.B", ManeuverColor.Green);
                 Maneuvers.Add("2.F.S", ManeuverColor.Green);
-                Maneuvers.Add("2.R.B", ManeuverColor.White);
-                Maneuvers.Add("2.R.T", ManeuverColor.White);
+                Maneuvers.Add("2.R.B", ManeuverColor.Green);
+                Maneuvers.Add("2.R.T", ManeuverColor.Red);
                 Maneuvers.Add("2.F.R", ManeuverColor.None);
                 Maneuvers.Add("3.L.T", ManeuverColor.White);
                 Maneuvers.Add("3.L.B", ManeuverColor.White);
@@ -69,9 +69,9 @@ namespace Ship
                 Maneuvers.Add("3.R.T", ManeuverColor.White);
                 Maneuvers.Add("3.F.R", ManeuverColor.None);
                 Maneuvers.Add("4.F.S", ManeuverColor.White);
-                Maneuvers.Add("4.F.R", ManeuverColor.Red);
-                Maneuvers.Add("5.F.S", ManeuverColor.White);
-                Maneuvers.Add("5.F.R", ManeuverColor.None);
+                Maneuvers.Add("4.F.R", ManeuverColor.None);
+                Maneuvers.Add("5.F.S", ManeuverColor.None);
+                Maneuvers.Add("5.F.R", ManeuverColor.Red);
             }
 
         }

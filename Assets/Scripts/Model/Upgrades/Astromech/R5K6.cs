e0c5ebb6320d26dbea0c8f74a04c7a447814fc06
Sandrem <oogoeroo@gmail.com>
2017-08-22 01:27:01 +0300

    R5-K6 Stub

--- Assets/Scripts/Model/Upgrades/Astromech/R5K6.cs
+++ Assets/Scripts/Model/Upgrades/Astromech/R5K6.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Upgrade;
 
+/*
+ * KNOWN PROBLEMS
+ * 1) Ability during combat -> attack dice results are visible
+ * 2) Triggered on select action instead of select action callback
+ * 3) Pay cost of proton torpedo -> Exception, no subphase callback 
+ */
+
+
 namespace UpgradesList
 {
 
@@ -29,20 +37,59 @@ namespace UpgradesList
 
         private void R5K6Ability(Ship.GenericShip ship, System.Type type)
         {
-            if (type == typeof(Tokens.BlueTargetLockToken))
+            Triggers.RegisterTrigger(new Trigger()
             {
-                // TODO:
-                // Astromech sound
-                // Visual dice throwing
-                int randomValue = Random.Range(0, 8);
-                // Notification about result
-                if (randomValue > 5)
-                {
-                    // Cannot be used now
-                    //Actions.AssignTargetLockToPair(Host, Selection.AnotherShip);
-                }
+                Name = "R5-K6' ability",
+                TriggerOwner = ship.Owner.PlayerNo,
+                TriggerType = TriggerTypes.OnTokenIsSpent,
+                EventHandler = StartSubphaseForR5K6Ability
+            });
+        }
 
+        private void StartSubphaseForR5K6Ability(object sender, System.EventArgs e)
+        {
+            Selection.ActiveShip = Selection.ThisShip;
+            Phases.StartTemporarySubPhase(
+                "R5-K6: Try to re-aquire Target Lock",
+                typeof(SubPhases.R5K6CheckSubPhase),
+                delegate {
+                    Phases.FinishSubPhase(typeof(SubPhases.R5K6CheckSubPhase));
+                    Phases.CurrentSubPhase.CallBack();
+                }
+            );
+        }
+
+    }
+
+}
+
+namespace SubPhases
+{
+
+    public class R5K6CheckSubPhase : DiceRollCheckSubPhase
+    {
+
+        public override void Prepare()
+        {
+            dicesType = DiceKind.Defence;
+            dicesCount = 1;
+
+            finishAction = FinishAction;
+        }
+
+        protected override void FinishAction()
+        {
+            HideDiceResultMenu();
+
+            if (CurrentDiceRoll.DiceList[0].Side == DiceSide.Success)
+            {
+                Actions.AssignTargetLockToPair(Combat.Attacker, Combat.Defender, CallBack, CallBack);
             }
+            else
+            {
+                CallBack();
+            }
+            
         }
 
     }

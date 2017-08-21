d60c4307f0a701741eff532a5704a9e491761dd7
Sandrem <oogoeroo@gmail.com>
2017-08-21 15:37:24 +0300

    Soontir Fell template

---
+++ Assets/Scripts/Model/Ships/TIE Interceptor/SoontirFell.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace TIEInterceptor
+    {
+        public class SoontirFell : TIEInterceptor
+        {
+            public SoontirFell() : base()
+            {
+                PilotName = "Soontir Fell";
+                ImageUrl = "https://vignette4.wikia.nocookie.net/xwing-miniatures/images/c/c2/Alpha_Squadron_Pilot.png";
+                PilotSkill = 9;
+                Cost = 27;
+
+                IsUnique = true;
+                AddUpgradeSlot(Upgrade.UpgradeSlot.Elite);
+            }
+        }
+    }
+}

50421570cf6c4bb9d917174a2e904c8bb4097892
Sandrem <oogoeroo@gmail.com>
2017-08-26 00:02:05 +0300

    ShipPrintedInfo

---
+++ Assets/Scripts/Model/Ships/GenericShip/ShipPrintedInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ship
+{
+    class ShipPrintedInfo
+    {
+        public string ShipType { get; protected set; }
+        public Faction Faction { get; protected set; }
+
+        public string PilotName { get; protected set; }
+        public bool IsUnique { get; protected set; }
+
+        public int PilotSkill { get; protected set; }
+
+        public int Firepower { get; protected set; }
+        public int Agility { get; protected set; }
+        public int MaxHull { get; protected set; }
+        public int MaxShields { get; protected set; }
+
+        // ActionsBar
+
+        // UpgradesBar
+
+        // Maneuvers
+
+        // BaseSize
+
+        // ArctType
+
+        public int Cost { get; protected set; }
+
+    }
+}

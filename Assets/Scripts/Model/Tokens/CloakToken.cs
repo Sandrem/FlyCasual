85590c40b0fbccfbb3a5eb286c488067591609f1
Sandrem <oogoeroo@gmail.com>
2017-08-31 10:07:55 +0300

    New cloak token

--- Assets/Scripts/Model/Tokens/CloakToken.cs
+++ Assets/Scripts/Model/Tokens/CloakToken.cs
@@ -10,6 +10,7 @@ namespace Tokens
         public CloakToken() {
             Name = "Cloak Token";
             Temporary = false;
+            Tooltip = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/reference-cards/CloakAction.png";
         }
 
     }

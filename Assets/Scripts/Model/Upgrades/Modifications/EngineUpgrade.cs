8691d0e2ca75304d518b866eb8f220305efb96d4
Þórður Karl Sigvaldason <thordurk91@gmail.com>
2017-09-04 22:18:38 +0000

    EU, TLT, Ion Cannon, Mangler Cannon
    Typo in Mangler Name fixed

---
+++ Assets/Scripts/Model/Upgrades/Modifications/EngineUpgrade.cs
@@ -0,0 +1,16 @@
+﻿using Upgrade;
+
+namespace UpgradesList
+{ 
+	public class EngineUpgrade : GenericActionBarUpgrade<ActionsList.BoostAction>
+	{
+		public EngineUpgrade() : base()
+		{
+			Type = UpgradeType.Modification;
+			Name = ShortName = "Engine Upgrade";
+			ImageUrl = "https://vignette1.wikia.nocookie.net/xwing-miniatures/images/5/58/Swx53-vectored-thrusters.png";
+			Cost = 4;
+		}        
+	}
+}
+
---
+++ Assets/Scripts/Model/Upgrades/Turret/TwinLaserTurret.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Upgrade;
+
+namespace UpgradesList
+{
+
+    public class TwinLaserTurret : GenericSecondaryWeapon
+    {
+		public TwinLaserTurret() : base()
+        {
+            Type = UpgradeType.Turret;
+
+            Name = "Twin Laser Turret";
+            ShortName = "TLT";
+            ImageUrl = "";
+            Cost = 6;
+
+            MinRange = 2;
+            MaxRange = 3;
+            AttackValue = 3;
+
+			IsTwinAttack = true; 
+			CanShootOutsideArc = true;
+        }
+
+        public override void AttachToShip(Ship.GenericShip host)
+        {
+            base.AttachToShip(host);
+
+			SubscribeOnHit();
+        }
+			
+		private void SubscribeOnHit()
+		{
+			Host.OnAttackHitAsAttacker += RegisterTwinLaserTurretEffect;
+		}
+
+		private void RegisterTwinLaserTurretEffect()
+		{
+			if (Combat.ChosenWeapon == this)
+			{
+				Triggers.RegisterTrigger(new Trigger()
+					{
+						Name = "Twin Laser Turret effect",
+						TriggerType = TriggerTypes.OnAttackHit,
+						TriggerOwner = Combat.Attacker.Owner.PlayerNo,
+						EventHandler = TwinLaserTurretEffect
+					});
+			}
+		}
+		private void TwinLaserTurretEffect(object sender, System.EventArgs e)
+		{
+			Combat.DiceRollAttack.CancelAllResults();
+			Combat.DiceRollAttack.RemoveAllFailures();
+			DefenderSuffersDamage();
+		}
+		private void DefenderSuffersDamage()
+		{
+			Combat.Defender.AssignedDamageDiceroll.AddDice(DiceSide.Success);
+
+			Triggers.RegisterTrigger(new Trigger()
+				{
+					Name = "Suffer damage",
+					TriggerType = TriggerTypes.OnDamageIsDealt,
+					TriggerOwner = Combat.Defender.Owner.PlayerNo,
+					EventHandler = Combat.Defender.SufferDamage,
+					EventArgs = new DamageSourceEventArgs()
+					{
+						Source = Combat.Attacker,
+						DamageType = DamageTypes.ShipAttack
+					},
+					Skippable = true
+				});
+
+			Triggers.ResolveTriggers(TriggerTypes.OnDamageIsDealt, Triggers.FinishTrigger);
+		}
+    }
+
+}

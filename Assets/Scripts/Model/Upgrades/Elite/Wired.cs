6623d494d05dbfe614043df5bd8c30bc4a4ff4b0
rune-b <rune@krokodille.com>
2017-09-10 22:40:11 +0200

    Fixed AI priority for Wired

--- Assets/Scripts/Model/Upgrades/Elite/Wired.cs
+++ Assets/Scripts/Model/Upgrades/Elite/Wired.cs
@@ -44,18 +44,15 @@ namespace ActionsList
         public override int GetActionEffectPriority()
         {
             int result = 0;
-
-            if (!Host.HasToken(typeof(Tokens.FocusToken)))
+                        
+            if (Combat.AttackStep == CombatStep.Defence && Combat.DiceRollAttack.Successes > Combat.DiceRollDefence.Successes && Combat.DiceRollDefence.Focuses > 0)
             {
-                if (Combat.AttackStep == CombatStep.Defence && Combat.DiceRollAttack.Successes > Combat.DiceRollDefence.Successes && Combat.DiceRollDefence.Focuses > 0)
-                {
-                    result = 50;
-                }
+                result = 30;
+            }
 
-                if (Combat.AttackStep == CombatStep.Attack && Combat.DiceRollAttack.Focuses > 0)
-                {
-                    result = 50;                    
-                }
+            if (Combat.AttackStep == CombatStep.Attack && Combat.DiceRollAttack.Focuses > 0)
+            {
+                result = 30;                    
             }
             
             return result;            
@@ -63,7 +60,7 @@ namespace ActionsList
 
         public override bool IsActionEffectAvailable()
         {
-            return (Combat.AttackStep == CombatStep.Attack || Combat.AttackStep == CombatStep.Defence) && Host.HasToken(typeof(Tokens.StressToken));
+            return Host.HasToken(typeof(Tokens.StressToken));
         }
         
         public override void ActionEffect(System.Action callBack)

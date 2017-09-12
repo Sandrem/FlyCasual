0f7081c589eed2703b7b05f9c83c2cf008c8a956
Ben D. Wakefield <ben.d.wakefield@gmail.com>
2017-09-11 22:19:58 -0500

    Stealth Device

---
+++ Assets/Scripts/Model/Upgrades/Modifications/StealthDevice.cs
@@ -0,0 +1,28 @@
+﻿using Ship;
+using Upgrade;
+
+namespace UpgradesList{
+	public class StealthDevice : GenericUpgrade {
+		public StealthDevice() : base() {
+			Type = UpgradeType.Modification;
+			Name = "Stealth Device";
+			ShortName = "Slth Dev";
+			ImageUrl = "https://vignette2.wikia.nocookie.net/xwing-miniatures/images/f/f8/Stealth_Device.png";
+			Cost = 3;
+		}
+
+		public override void AttachToShip(GenericShip host)
+		{
+			base.AttachToShip (host);
+			host.ChangeAgilityBy (1);
+			host.OnAttackHitAsDefender += StealthDeviceCleanup;
+		}
+
+		private void StealthDeviceCleanup()
+		{
+			Messages.ShowError("Hit! Discarding Stealth Device!");
+			Host.ChangeAgilityBy (-1);
+			Discard ();
+		}
+	}
+}

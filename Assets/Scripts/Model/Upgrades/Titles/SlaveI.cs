1ae013f8d4aabc8dfa22f34ef5402495952b4984
rune-b <rune@krokodille.com>
2017-09-14 20:59:20 +0200

    Slave I, Bomb Loadout, B-Wing/E2

---
+++ Assets/Scripts/Model/Upgrades/Titles/SlaveI.cs
@@ -0,0 +1,28 @@
+﻿using Ship;
+using Ship.Firespray31;
+using Upgrade;
+using System.Collections.Generic;
+
+namespace UpgradesList
+{
+    public class SlaveI : GenericUpgradeSlotUpgrade
+    {
+        public SlaveI() : base()
+        {
+            Type = UpgradeType.Title;
+            Name = ShortName = "Slave I";
+            ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/upgrades/Title/slave-i.png";
+            Cost = 0;
+            isUnique = true;
+            AddedSlots = new List<UpgradeSlot>
+            {
+                new UpgradeSlot(UpgradeType.Torpedoes)
+            };
+        }
+
+        public override bool IsAllowedForShip(GenericShip ship)
+        {
+            return ship is Firespray31;
+        }
+    }
+}

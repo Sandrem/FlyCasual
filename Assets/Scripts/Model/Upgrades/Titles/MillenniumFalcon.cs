54657228cdeb07050dfcb818a21ce0f2bb7aeaf7
rune-b <rune@krokodille.com>
2017-09-14 21:43:52 +0200

    Millennium Falcon

---
+++ Assets/Scripts/Model/Upgrades/Titles/MillenniumFalcon.cs
@@ -0,0 +1,23 @@
+﻿using Ship;
+using Ship.YT1300;
+using Upgrade;
+
+namespace UpgradesList
+{ 
+    public class MillenniumFalcon : GenericActionBarUpgrade<ActionsList.EvadeAction>
+    {
+        public MillenniumFalcon() : base()
+        {
+            Type = UpgradeType.Title;
+            Name = ShortName = "Millennium Falcon";
+            ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/upgrades/Title/millennium-falcon.png";
+            Cost = 1;
+            isUnique = true;
+        }
+
+        public override bool IsAllowedForShip(GenericShip ship)
+        {
+            return ship is YT1300;
+        }
+    }
+}

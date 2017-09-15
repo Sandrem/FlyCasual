a752a4b39f60883ba79351cb991c5214ff4f7739
Sandrem <oogoeroo@gmail.com>
2017-09-16 00:14:09 +0300

    K-Wing

---
+++ Assets/Scripts/Model/Ships/K-Wing/GuardianSquadronPilot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace KWing
+    {
+        public class GuardianSquadronPilot : KWing
+        {
+            public GuardianSquadronPilot() : base()
+            {
+                PilotName = "Warden Squadron Pilot";
+                ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/Rebel%20Alliance/K-wing/guardian-squadron-pilot.png";
+                PilotSkill = 4;
+                Cost = 25;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Ships/K-Wing/WardenSquadronPilot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace KWing
+    {
+        public class WardenSquadronPilot : KWing
+        {
+            public WardenSquadronPilot() : base()
+            {
+                PilotName = "Warden Squadron Pilot";
+                ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/Rebel%20Alliance/K-wing/warden-squadron-pilot.png";
+                PilotSkill = 2;
+                Cost = 23;
+            }
+        }
+    }
+}

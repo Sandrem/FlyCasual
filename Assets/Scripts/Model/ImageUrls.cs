bbd024e471fbc528ed92cc1e572251d40db73a9c
rune-b <rune@krokodille.com>
2017-09-15 21:48:45 +0200

    New ImageUrls class for getting image urls for cards from github
    Currently only pilot and upgrade cards supported

---
+++ Assets/Scripts/Model/ImageUrls.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public static class ImageUrls
+{
+    private const string RootURL = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/";
+    private const string UpgradesPath = "upgrades/";
+    private const string PilotsPath = "pilots/";
+
+    public static string GetImageUrl(Upgrade.GenericUpgrade upgrade, string filename = null)
+    {
+        return GetImageUrl(UpgradesPath + upgrade.Type.ToString(), upgrade.Name, filename);
+    }
+
+    public static string GetImageUrl(Ship.GenericShip ship, string filename = null)
+    {
+        return GetImageUrl(PilotsPath + FormatFaction(ship.faction) + "/" + ship.Type, ship.PilotName, filename);
+    }
+
+    private static string GetImageUrl(string subpath, string cardName, string filename)
+    {
+        return RootURL + subpath + "/" + (filename ?? FormatName(cardName) + ".png");
+    }
+
+    private static string FormatFaction(Faction faction)
+    {
+        switch (faction)
+        {
+            case Faction.Rebels:
+                return "Rebel Alliance";
+            case Faction.Empire:
+                return "Galactic Empire";
+            default:
+                throw new NotImplementedException();
+        }
+    }
+
+    private static string FormatName(string name)
+    {
+        return name
+            .ToLower()
+            .Replace(' ', '-')
+            .Replace('/', '-');
+    }
+}
+

317a6f2707c328e6ec007aa5d3990e6fd04605bf
Sandrem <oogoeroo@gmail.com>
2017-09-15 21:42:29 +0300

    StarViper

---
+++ Assets/Scripts/Model/Ships/StarViper/BlackSunAssassin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace StarViper
+    {
+        public class BlackSunAssassin : StarViper
+        {
+            public BlackSunAssassin() : base()
+            {
+                PilotName = "Black Sun Assassin";
+                ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/Scum%20and%20Villainy/StarViper/black-sun-enforcer.png";
+                PilotSkill = 5;
+                Cost = 28;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+
+                SkinName = "Black Sun Assassin";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Ships/StarViper/BlackSunVigo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace StarViper
+    {
+        public class BlackSunVigo : StarViper
+        {
+            public BlackSunVigo() : base()
+            {
+                PilotName = "Black Sun Vigo";
+                ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/Scum%20and%20Villainy/StarViper/black-sun-vigo.png";
+                PilotSkill = 1;
+                Cost = 25;
+
+                SkinName = "Black Sun Vigo";
+            }
+        }
+    }
+}

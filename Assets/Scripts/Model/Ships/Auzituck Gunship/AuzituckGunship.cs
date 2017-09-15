c7c7534519fdc1bef4b0d5ffc7df1898f4d7e087
Sandrem <oogoeroo@gmail.com>
2017-09-16 02:58:17 +0300

    Auzituck Gunship

---
+++ Assets/Scripts/Model/Ships/Auzituck Gunship/AuzituckGunship.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Movement;
+
+namespace Ship
+{
+    namespace AuzituckGunship
+    {
+        public class AuzituckGunship : GenericShip
+        {
+
+            public AuzituckGunship() : base()
+            {
+                Type = "Auzituck Gunship";
+
+                ManeuversImageUrl = "https://vignette.wikia.nocookie.net/xwing-miniatures/images/e/e3/36-36_R-SPACE.PNG";
+
+                Firepower = 3;
+                Agility = 1;
+                MaxHull = 6;
+                MaxShields = 3;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Crew);
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Crew);
+
+                AssignTemporaryManeuvers();
+                HotacManeuverTable = new AI.EWingTable();
+
+                factions.Add(Faction.Rebels);
+                faction = Faction.Rebels;
+
+                SkinName = "Kashyyyk Defender";
+
+                SoundShotsPath = "XWing-Laser";
+                ShotsCount = 3;
+
+                for (int i = 1; i < 4; i++)
+                {
+                    SoundFlyPaths.Add("XWing-Fly" + i);
+                }
+                
+            }
+
+            public override void InitializeShip()
+            {
+                base.InitializeShip();
+
+                // TODO: Reinforce
+            }
+
+            private void AssignTemporaryManeuvers()
+            {
+                Maneuvers.Add("1.L.T", ManeuverColor.None);
+                Maneuvers.Add("1.L.B", ManeuverColor.White);
+                Maneuvers.Add("1.F.S", ManeuverColor.Green);
+                Maneuvers.Add("1.R.B", ManeuverColor.White);
+                Maneuvers.Add("1.R.T", ManeuverColor.None);
+                Maneuvers.Add("1.F.R", ManeuverColor.None);
+                Maneuvers.Add("2.L.T", ManeuverColor.White);
+                Maneuvers.Add("2.L.B", ManeuverColor.Green);
+                Maneuvers.Add("2.F.S", ManeuverColor.Green);
+                Maneuvers.Add("2.R.B", ManeuverColor.Green);
+                Maneuvers.Add("2.R.T", ManeuverColor.White);
+                Maneuvers.Add("2.F.R", ManeuverColor.None);
+                Maneuvers.Add("3.L.T", ManeuverColor.White);
+                Maneuvers.Add("3.L.B", ManeuverColor.White);
+                Maneuvers.Add("3.F.S", ManeuverColor.Green);
+                Maneuvers.Add("3.R.B", ManeuverColor.White);
+                Maneuvers.Add("3.R.T", ManeuverColor.White);
+                Maneuvers.Add("3.F.R", ManeuverColor.Red);
+                Maneuvers.Add("4.F.S", ManeuverColor.White);
+                Maneuvers.Add("4.F.R", ManeuverColor.Red);
+                Maneuvers.Add("5.F.S", ManeuverColor.White);
+                Maneuvers.Add("5.F.R", ManeuverColor.None);
+            }
+
+        }
+    }
+}

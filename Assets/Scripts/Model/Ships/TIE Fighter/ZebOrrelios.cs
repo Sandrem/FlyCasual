27b5d9ea7f3d88396a68df9fc628f458bae9789a
Sandrem <oogoeroo@gmail.com>
2017-09-17 00:30:30 +0300

    Rebel TIE Fighter

---
+++ Assets/Scripts/Model/Ships/TIE Fighter/ZebOrrelios.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace TIEFighter
+    {
+        public class ZebOrrelios : TIEFighter
+        {
+            public ZebOrrelios() : base()
+            {
+                PilotName = "Generic Pilot";
+                ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/Rebel%20Alliance/TIE%20Fighter/zeb-orrelios.png";
+                PilotSkill = 3;
+                Cost = 13;
+
+                faction = Faction.Rebels;
+            }
+        }
+    }
+}

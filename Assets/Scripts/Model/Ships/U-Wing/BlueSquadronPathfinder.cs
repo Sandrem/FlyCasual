c1f313593b342092440198d1fdea464522709c56
Sandrem <oogoeroo@gmail.com>
2017-09-17 02:03:06 +0300

    U-Wing

---
+++ Assets/Scripts/Model/Ships/U-Wing/BlueSquadronPathfinder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace UWing
+    {
+        public class BlueSquadronPathfinder : UWing
+        {
+            public BlueSquadronPathfinder() : base()
+            {
+                PilotName = "Blue Squadron Pathfinder";
+                ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/Rebel%20Alliance/U-wing/blue-squadron-pathfinder.png";
+                PilotSkill = 2;
+                Cost = 23;
+            }
+        }
+    }
+}

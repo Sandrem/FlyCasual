d96c5dbacfb3de81b70a6d6e7c008f18652d478a
Sandrem <oogoeroo@gmail.com>
2017-09-16 03:48:47 +0300

    JumpMaster 5000

---
+++ Assets/Scripts/Model/Ships/JumpMaster 5000/ContractedScout.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace JumpMaster5000
+    {
+        public class ContractedScout : JumpMaster5000
+        {
+            public ContractedScout() : base()
+            {
+                PilotName = "Contracted Scout";
+                ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/Scum%20and%20Villainy/JumpMaster%205000/contracted-scout.png";
+                PilotSkill = 3;
+                Cost = 25;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Ships/JumpMaster 5000/JumpMaster5000.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Movement;
+
+namespace Ship
+{
+    namespace JumpMaster5000
+    {
+        public class JumpMaster5000 : GenericShip
+        {
+
+            public JumpMaster5000() : base()
+            {
+                Type = "JumpMaster 5000";
+                ShipBaseSize = BaseSize.Large;
+                ShipBaseArcsType = Arcs.BaseArcsType.Arc360;
+
+                ManeuversImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/Scum%20and%20Villainy/JumpMaster%205000/contracted-scout.png";
+
+                Firepower = 2;
+                Agility = 2;
+                MaxHull = 5;
+                MaxShields = 4;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Torpedoes);
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Torpedoes);
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Crew);
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.SalvagedAstromechs);
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Illicit);
+
+                AssignTemporaryManeuvers();
+                HotacManeuverTable = new AI.YT2400Table();
+
+                factions.Add(Faction.Scum);
+                faction = Faction.Scum;
+
+                SkinName = "JumpMaster 5000";
+
+                SoundShotsPath = "Falcon-Fire";
+                ShotsCount = 2;
+
+                for (int i = 1; i < 4; i++)
+                {
+                    SoundFlyPaths.Add("Falcon-Fly" + i);
+                }
+
+            }
+
+            public override void InitializeShip()
+            {
+                base.InitializeShip();
+                BuiltInActions.Add(new ActionsList.TargetLockAction());
+                BuiltInActions.Add(new ActionsList.BarrelRollAction());
+            }
+
+            private void AssignTemporaryManeuvers()
+            {
+                Maneuvers.Add("1.L.T", ManeuverColor.White);
+                Maneuvers.Add("1.L.B", ManeuverColor.Green);
+                Maneuvers.Add("1.F.S", ManeuverColor.Green);
+                Maneuvers.Add("1.R.B", ManeuverColor.Green);
+                Maneuvers.Add("1.R.T", ManeuverColor.White);
+                Maneuvers.Add("1.F.R", ManeuverColor.None);
+                Maneuvers.Add("2.L.T", ManeuverColor.White);
+                Maneuvers.Add("2.L.B", ManeuverColor.White);
+                Maneuvers.Add("2.F.S", ManeuverColor.Green);
+                Maneuvers.Add("2.R.B", ManeuverColor.White);
+                Maneuvers.Add("2.R.T", ManeuverColor.White);
+                Maneuvers.Add("2.F.R", ManeuverColor.None);
+                Maneuvers.Add("3.L.T", ManeuverColor.White);
+                Maneuvers.Add("3.L.B", ManeuverColor.White);
+                Maneuvers.Add("3.F.S", ManeuverColor.White);
+                Maneuvers.Add("3.R.B", ManeuverColor.White);
+                Maneuvers.Add("3.R.T", ManeuverColor.White);
+                Maneuvers.Add("3.F.R", ManeuverColor.None);
+                Maneuvers.Add("4.F.S", ManeuverColor.White);
+                Maneuvers.Add("4.F.R", ManeuverColor.Red);
+                Maneuvers.Add("5.F.S", ManeuverColor.None);
+                Maneuvers.Add("5.F.R", ManeuverColor.None);
+            }
+
+        }
+    }
+}

6112029276b5d158a742440010c5cb112be154d2
Sandrem <oogoeroo@gmail.com>
2017-09-16 04:13:08 +0300

    T-70 X-Wing

---
+++ Assets/Scripts/Model/Ships/T-70 X-Wing/BlueSquadronNovice.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace T70XWing
+    {
+        public class BlueSquadronNovice : T70XWing
+        {
+            public BlueSquadronNovice() : base()
+            {
+                PilotName = "Blue Squadron Novice";
+                ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/Resistance/T-70%20X-wing/blue-squadron-novice.png";
+                PilotSkill = 2;
+                Cost = 24;
+            }
+        }
+    }
+}

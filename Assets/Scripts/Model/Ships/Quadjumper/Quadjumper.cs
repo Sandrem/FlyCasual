61d1402f094d9cc19bd68deadf01c7f4fbf12ca7
Sandrem <oogoeroo@gmail.com>
2017-09-17 01:40:52 +0300

    Quadjumper

---
+++ Assets/Scripts/Model/Ships/Quadjumper/Quadjumper.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Movement;
+
+namespace Ship
+{
+    namespace Quadjumper
+    {
+        public class Quadjumper : GenericShip
+        {
+
+            public Quadjumper() : base()
+            {
+                Type = "Quadjumper";
+
+                ManeuversImageUrl = "https://vignette.wikia.nocookie.net/xwing-miniatures/images/4/4d/MS_QUADJUMPER.png";
+
+                Firepower = 2;
+                Agility = 2;
+                MaxHull = 5;
+                MaxShields = 0;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Crew);
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Bomb);
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Tech);
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Illicit);
+
+                AssignTemporaryManeuvers();
+                HotacManeuverTable = new AI.EWingTable();
+
+                factions.Add(Faction.Scum);
+                faction = Faction.Scum;
+
+                SkinName = "Quadjumper";
+
+                SoundShotsPath = "XWing-Laser";
+                ShotsCount = 2;
+
+                for (int i = 1; i < 4; i++)
+                {
+                    SoundFlyPaths.Add("XWing-Fly" + i);
+                }
+                
+            }
+
+            public override void InitializeShip()
+            {
+                base.InitializeShip();
+                BuiltInActions.Add(new ActionsList.BarrelRollAction());
+            }
+
+            private void AssignTemporaryManeuvers()
+            {
+                Maneuvers.Add("1.L.T", ManeuverColor.None);
+                Maneuvers.Add("1.L.B", ManeuverColor.White);
+                Maneuvers.Add("1.F.S", ManeuverColor.Green);
+                Maneuvers.Add("1.R.B", ManeuverColor.White);
+                Maneuvers.Add("1.R.T", ManeuverColor.None);
+                Maneuvers.Add("1.F.R", ManeuverColor.None);
+                Maneuvers.Add("2.L.T", ManeuverColor.White);
+                Maneuvers.Add("2.L.B", ManeuverColor.Green);
+                Maneuvers.Add("2.F.S", ManeuverColor.Green);
+                Maneuvers.Add("2.R.B", ManeuverColor.Green);
+                Maneuvers.Add("2.R.T", ManeuverColor.White);
+                Maneuvers.Add("2.F.R", ManeuverColor.None);
+                Maneuvers.Add("3.L.T", ManeuverColor.White);
+                Maneuvers.Add("3.L.B", ManeuverColor.White);
+                Maneuvers.Add("3.F.S", ManeuverColor.Green);
+                Maneuvers.Add("3.R.B", ManeuverColor.White);
+                Maneuvers.Add("3.R.T", ManeuverColor.White);
+                Maneuvers.Add("3.F.R", ManeuverColor.Red);
+                Maneuvers.Add("4.F.S", ManeuverColor.White);
+                Maneuvers.Add("4.F.R", ManeuverColor.Red);
+                Maneuvers.Add("5.F.S", ManeuverColor.White);
+                Maneuvers.Add("5.F.R", ManeuverColor.None);
+            }
+
+        }
+    }
+}

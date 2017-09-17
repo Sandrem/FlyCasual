d30eaa9b9787a51e86cbb5af6196218c01983491
Sandrem <oogoeroo@gmail.com>
2017-09-17 04:42:24 +0300

    Shadowcaster and Fang

---
+++ Assets/Scripts/Model/Ships/Lancer-class Pursuit Craft/ShadowportHunter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace LancerClassPursuitCraft
+    {
+        public class ShadowportHunter : LancerClassPursuitCraft
+        {
+            public ShadowportHunter() : base()
+            {
+                PilotName = "Shadowport Hunter";
+                ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/Scum%20and%20Villainy/Lancer-class%20Pursuit%20Craft/shadowport-hunter.png";
+                PilotSkill = 2;
+                Cost = 33;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Ships/Protectorate Starfighter/ProtectorateStarfighter.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Movement;
+
+namespace Ship
+{
+    namespace ProtectorateStarfighter
+    {
+        public class ProtectorateStarfighter : GenericShip, TIE
+        {
+
+            public ProtectorateStarfighter() : base()
+            {
+                Type = "Protectorate Starfighter";
+
+                ManeuversImageUrl = "https://vignette.wikia.nocookie.net/xwing-miniatures/images/8/83/MS_PROTECTORATE-STARFIGHTER.png";
+
+                Firepower = 3;
+                Agility = 3;
+                MaxHull = 4;
+                MaxShields = 0;
+
+                AssignTemporaryManeuvers();
+                HotacManeuverTable = new AI.TIEInterceptorTable();
+
+                factions.Add(Faction.Scum);
+                faction = Faction.Scum;
+
+                SkinName = "Protectorate Starfighter";
+
+                SoundShotsPath = "TIE-Fire";
+                ShotsCount = 3;
+
+                for (int i = 1; i < 8; i++)
+                {
+                    SoundFlyPaths.Add("TIE-Fly" + i);
+                }
+            }
+
+            public override void InitializeShip()
+            {
+                base.InitializeShip();
+                BuiltInActions.Add(new ActionsList.TargetLockAction());
+                BuiltInActions.Add(new ActionsList.BarrelRollAction());
+                BuiltInActions.Add(new ActionsList.BoostAction());
+            }
+
+            private void AssignTemporaryManeuvers()
+            {
+                Maneuvers.Add("1.L.T", ManeuverColor.White);
+                Maneuvers.Add("1.L.B", ManeuverColor.None);
+                Maneuvers.Add("1.F.S", ManeuverColor.None);
+                Maneuvers.Add("1.R.B", ManeuverColor.None);
+                Maneuvers.Add("1.R.T", ManeuverColor.White);
+                Maneuvers.Add("1.F.R", ManeuverColor.None);
+                Maneuvers.Add("2.L.T", ManeuverColor.Green);
+                Maneuvers.Add("2.L.B", ManeuverColor.Green);
+                Maneuvers.Add("2.F.S", ManeuverColor.Green);
+                Maneuvers.Add("2.R.B", ManeuverColor.Green);
+                Maneuvers.Add("2.R.T", ManeuverColor.Green);
+                Maneuvers.Add("2.F.R", ManeuverColor.None);
+                Maneuvers.Add("3.L.T", ManeuverColor.White);
+                Maneuvers.Add("3.L.B", ManeuverColor.White);
+                Maneuvers.Add("3.F.S", ManeuverColor.Green);
+                Maneuvers.Add("3.R.B", ManeuverColor.White);
+                Maneuvers.Add("3.R.T", ManeuverColor.White);
+                Maneuvers.Add("3.F.R", ManeuverColor.Red);
+                Maneuvers.Add("4.F.S", ManeuverColor.Green);
+                Maneuvers.Add("4.F.R", ManeuverColor.None);
+                Maneuvers.Add("5.F.S", ManeuverColor.White);
+                Maneuvers.Add("5.F.R", ManeuverColor.Red);
+            }
+
+        }
+    }
+}

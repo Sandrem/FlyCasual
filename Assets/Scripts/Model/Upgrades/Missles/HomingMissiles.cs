036a9d26999a9a711f4a36e2561ad19669576f20
Sandrem <oogoeroo@gmail.com>
2017-09-29 01:13:01 +0300

    Homing Missiles

--- Assets/Scripts/Model/Upgrades/Missles/HomingMissiles.cs
+++ Assets/Scripts/Model/Upgrades/Missles/HomingMissiles.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Ship;
 using UnityEngine;
 using Upgrade;
 
@@ -25,5 +26,62 @@ namespace UpgradesList
 
             IsDiscardedForShot = true;
         }
+
+        public override void AttachToShip(GenericShip host)
+        {
+            base.AttachToShip(host);
+            Host.OnAttack += CheckHomingMissilesAbility;
+        }
+
+        private void CheckHomingMissilesAbility()
+        {
+            if ((Combat.AttackStep == CombatStep.Attack) && (Combat.Attacker == Host))
+            {
+                if (Combat.ChosenWeapon == this)
+                {
+                    ApplyHomingMissilesAbility();
+                }
+            }
+        }
+
+        private void ApplyHomingMissilesAbility()
+        {
+            Combat.Defender.OnTryAddAvailableActionEffect += UseHomingMissilesRestriction;
+            Combat.Defender.AssignToken(new Conditions.HomingMissilesCondition(), delegate { });
+
+            Host.OnAttackPerformed += RemoveHomingMissilesAbility;
+        }
+
+        private void UseHomingMissilesRestriction(ActionsList.GenericAction action, ref bool canBeUsed)
+        {
+            if (action.IsSpendEvade)
+            {
+                Messages.ShowErrorToHuman("Homing Missiles: Cannot spend evade");
+                canBeUsed = false;
+            }
+        }
+
+        private void RemoveHomingMissilesAbility()
+        {
+            Combat.Defender.OnTryAddAvailableActionEffect -= UseHomingMissilesRestriction;
+            Combat.Defender.RemoveToken(typeof(Conditions.HomingMissilesCondition));
+
+            Host.OnAttackPerformed -= RemoveHomingMissilesAbility;
+        }
     }
+}
+
+namespace Conditions
+{
+
+    public class HomingMissilesCondition : Tokens.GenericToken
+    {
+        public HomingMissilesCondition()
+        {
+            Name = "Debuff Token";
+            Temporary = false;
+            Tooltip = new UpgradesList.HomingMissiles().ImageUrl;
+        }
+    }
+
 }

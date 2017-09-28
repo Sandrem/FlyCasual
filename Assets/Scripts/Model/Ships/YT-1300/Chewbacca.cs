e930ed956087b6e19bd5ade07bb1cd9f2bbae6d5
Sandrem <oogoeroo@gmail.com>
2017-09-28 18:28:00 +0300

    Chewbacca's ability

--- Assets/Scripts/Model/Ships/YT-1300/Chewbacca.cs
+++ Assets/Scripts/Model/Ships/YT-1300/Chewbacca.cs
@@ -24,6 +24,22 @@ namespace Ship
                 PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Missile);
                 PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
             }
+
+            public override void InitializePilot()
+            {
+                base.InitializePilot();
+                OnCheckFaceupCrit += FlipCrits;
+            }
+
+            private void FlipCrits(ref bool result)
+            {
+                if (result == true)
+                {
+                    Messages.ShowInfo("Chewbacca: Crit is flipped facedown");
+                    Sounds.PlayShipSound("Chewbacca");
+                    result = false;
+                }
+            }
         }
     }
 }

b4ee96322ed61c5529739745776f7894653bc43f
Sandrem <oogoeroo@gmail.com>
2017-10-05 18:16:13 +0300

    Update notification now is not shown if server shows "The service is unavailable" stub

--- Assets/Scripts/MainMenu/Model/MainMenu.cs
+++ Assets/Scripts/MainMenu/Model/MainMenu.cs
@@ -43,7 +43,7 @@ public partial class MainMenu : MonoBehaviour {
         string[] separator = new string[] { "\r\n" };
         string[] wwwdata = www.text.Split(separator, System.StringSplitOptions.RemoveEmptyEntries);
 
-        if (wwwdata.Length > 0)
+        if (wwwdata.Length > 0 && !wwwdata[0].Contains("DOCTYPE"))
         {
             if (wwwdata.Length == 3)
             {

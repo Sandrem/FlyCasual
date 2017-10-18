063094c926dd32e011565aef237ef571a8acb733
Michael Speth <spethm@landcareresearch.co.nz>
2017-10-18 14:36:27 +1300

    Change to use Triggers.

--- Assets/Scripts/Model/Ships/TIE FO/EpsilonLeader.cs
+++ Assets/Scripts/Model/Ships/TIE FO/EpsilonLeader.cs
@@ -4,50 +4,56 @@ using UnityEngine;
 
 namespace Ship
 {
-    namespace TIEFO
-    {
-        public class EpsilonLeader : TIEFO
-        {
-            public EpsilonLeader() : base()
-            {
-                PilotName = "Epsilon Leader";
-                ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/First%20Order/TIE-fo%20Fighter/epsilon-leader.png";
-                IsUnique = true;
-                PilotSkill = 6;
-                Cost = 19;
-            }
+	namespace TIEFO
+	{
+		public class EpsilonLeader : TIEFO
+		{
+			public EpsilonLeader () : base ()
+			{
+				PilotName = "Epsilon Leader";
+				ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/First%20Order/TIE-fo%20Fighter/epsilon-leader.png";
+				IsUnique = true;
+				PilotSkill = 6;
+				Cost = 19;
+			}
 
-            public override void InitializePilot()
-            {
-                base.InitializePilot();
+			public override void InitializePilot ()
+			{
+				base.InitializePilot ();
 
-                OnCombatPhaseStart += EpsilonLeaderAbility;
-                OnDestroyed += RemoveEpsilonLeaderAbility;
-            }
+				OnCombatPhaseStart += RegisterEpsilonLeaderAbility;
+				//OnDestroyed += RemoveEpsilonLeaderAbility;
+			}
 
-            private void EpsilonLeaderAbility(GenericShip genericShip)
+			private void RegisterEpsilonLeaderAbility (GenericShip genericShip)
+			{
+                Triggers.RegisterTrigger(new Trigger()
+                {
+                    Name = "Epsilon Leader Ability",
+                    TriggerOwner = this.Owner.PlayerNo,
+                    TriggerType = TriggerTypes.OnCombatPhaseStart,
+                    EventHandler = StartSubphaseForEpsilonLeaderAbility
+                });
+			}
+
+            private void StartSubphaseForEpsilonLeaderAbility(object sender, System.EventArgs e)
             {
-                Ship.GenericShip EpsilonLeader = this;
+                Selection.ThisShip = this;
+                Ship.GenericShip EpsilonLeader = Selection.ThisShip;
                 // remove a stress from yourself
-                this.RemoveToken(typeof(Tokens.StressToken));
+                EpsilonLeader.RemoveToken (typeof(Tokens.StressToken));
 
                 // remove a stress from friendly ships at range 1
-                foreach (var friendlyShip in this.Owner.Ships)
-                {
+                foreach (var friendlyShip in EpsilonLeader.Owner.Ships) {
                     Ship.GenericShip friendlyShipDefined = friendlyShip.Value;
-                    Board.ShipDistanceInformation positionInfo = new Board.ShipDistanceInformation(EpsilonLeader, friendlyShipDefined);
-                    if (positionInfo.Range == 1)
-                    {
+                    Board.ShipDistanceInformation positionInfo = new Board.ShipDistanceInformation (EpsilonLeader, friendlyShipDefined);
+                    if (positionInfo.Range == 1) {
                         // remove 1 stress token if it exists
-                        friendlyShipDefined.RemoveToken(typeof(Tokens.StressToken));
+                        friendlyShipDefined.RemoveToken (typeof(Tokens.StressToken));
                     }
                 }
+                Triggers.FinishTrigger ();
             }
-
-            private void RemoveEpsilonLeaderAbility(GenericShip ship)
-            {
-                OnDestroyed -= RemoveEpsilonLeaderAbility;
-            }
-        }
-    }
+		}
+	}
 }

f9854e37a5e063a4feb2ba08b330c6fa28c11d7c
Sandrem <oogoeroo@gmail.com>
2017-10-22 02:18:47 +0300

    Cruise Missiles

---
+++ Assets/Scripts/Model/Upgrades/Missles/CruiseMissiles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Upgrade;
+
+namespace UpgradesList
+{
+
+    public class CruiseMissiles : GenericSecondaryWeapon
+    {
+        public CruiseMissiles() : base()
+        {
+            Type = UpgradeType.Missile;
+
+            Name = "Cruise Missiles";
+            Cost = 3;
+
+            MinRange = 2;
+            MaxRange = 3;
+            AttackValue = 1;
+
+            RequiresTargetLockOnTargetToShoot = true;
+
+            IsDiscardedForShot = true;
+        }
+
+        public override void AttachToShip(Ship.GenericShip host)
+        {
+            base.AttachToShip(host);
+
+            Host.AfterGotNumberOfAttackDice += CruiseMissilesAbility;
+        }
+
+        private void CruiseMissilesAbility(ref int diceCount)
+        {
+            if (Combat.ChosenWeapon == this)
+            {
+                if (Combat.Attacker.AssignedManeuver != null) diceCount += Combat.Attacker.AssignedManeuver.Speed;
+            }
+        }
+
+    }
+
+}

fbd620fa0fb6b13492755345b1228adfb9519bfd
Sandrem <oogoeroo@gmail.com>
2017-10-21 03:25:39 +0300

    Token templates

---
+++ Assets/Scripts/Model/Tokens/WeaponsDisabledToken.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tokens
+{
+
+    public class WeaponsDisabledToken : GenericToken
+    {
+        public WeaponsDisabledToken() {
+            Name = "Weapons Disabled Token";
+            Temporary = true;
+            Tooltip = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/reference-cards/SlamAction.png";
+        }
+
+    }
+
+}

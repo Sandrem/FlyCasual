65f49de02e3d59d1ed0145c4c3292eeecee2752a
rune-b <rune@krokodille.com>
2017-11-01 22:24:46 +0100

    Fire-Control System

---
+++ Assets/Scripts/Model/Upgrades/System/FireControlSystem.cs
@@ -0,0 +1,82 @@
+﻿using Ship;
+using Upgrade;
+
+namespace UpgradesList
+{
+    public class FireControlSystem : GenericUpgrade
+    {
+        public FireControlSystem() : base()
+        {
+            Type = UpgradeType.System;
+            Name = "Fire-Control System";
+            Cost = 2;
+        }
+
+
+        public override void AttachToShip(GenericShip host)
+        {
+            base.AttachToShip(host);
+
+            host.OnAttackPerformed += FireControlSystemAbility;
+        }
+
+        private void FireControlSystemAbility()
+        {
+            if (!Combat.Defender.IsDestroyed)
+            {
+                Triggers.RegisterTrigger(new Trigger()
+                {
+                    Name = "Fire-Control System: Aquire target lock",
+                    TriggerOwner = Host.Owner.PlayerNo,
+                    TriggerType = TriggerTypes.OnAttackPerformed,
+                    EventHandler = AskAquireTargetLock
+                });
+            }
+        }
+
+        private void AskAquireTargetLock(object sender, System.EventArgs e)
+        {
+            Phases.StartTemporarySubPhase(
+                "Fire-Control System's decision",
+                typeof(SubPhases.FireControlSystemDecisionSubPhase),
+                Triggers.FinishTrigger
+            );
+         
+        }
+    }
+}
+
+
+namespace SubPhases
+{
+
+    public class FireControlSystemDecisionSubPhase : DecisionSubPhase
+    {
+
+        public override void Prepare()
+        {
+            infoText = "Fire-Control System: Aquire target lock?";
+
+            AddDecision("Yes", AcquireTargetLock);
+            AddDecision("No", NotAssignToken);
+
+            defaultDecision = "Yes";
+        }
+
+        private void AcquireTargetLock(object sender, System.EventArgs e)
+        {
+            Actions.AssignTargetLockToPair(Combat.Attacker, Combat.Defender, ConfirmDecision, ConfirmDecision);            
+        }
+
+        private void NotAssignToken(object sender, System.EventArgs e)
+        {
+            ConfirmDecision();
+        }
+
+        private void ConfirmDecision()
+        {
+            Phases.FinishSubPhase(this.GetType());
+            CallBack();
+        }
+    }
+}

d9d0ec360b6091599b0610c011edd850168e849d
Michael Speth <spethm@landcareresearch.co.nz>
2017-11-03 10:44:13 +1300

    Working on Omega Ace.

---
+++ Assets/Scripts/Model/Ships/TIE FO/EpsilonAce.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ship
+{
+    namespace TIEFO
+    {
+        public class EpsilonAce : TIEFO,IModifyPilotSkill
+        {
+            private int TruePilotSkill = 4;
+            private int PilotAbilitySkill = 12;
+            public EpsilonAce () : base ()
+            {
+                PilotName = "Epsilon Ace";
+                ImageUrl = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/pilots/First%20Order/TIE-fo%20Fighter/epsilon-ace.png";
+                IsUnique = true;
+                PilotSkill = 4;
+                Cost = 17;
+            }
+
+            public override void InitializePilot ()
+            {
+                base.InitializePilot ();
+
+                // Set Pilot Skill for Pilot Ability
+                PilotSkill = PilotAbilitySkill;
+
+                OnDamageCardIsDealt += RegisterEpsilonAceAbility;
+            }
+
+            private void RegisterEpsilonAceAbility (GenericShip ship)
+            {
+                Triggers.RegisterTrigger(new Trigger()
+                {
+                    Name = "Epsilon Ace Ability",
+                    TriggerOwner = this.Owner.PlayerNo,
+                    TriggerType = TriggerTypes.OnDamageCardIsDealt,
+                    EventHandler = UseEpsilonAceAbility
+                });
+            }
+
+            private void UseEpsilonAceAbility(object sender, System.EventArgs e)
+            {
+                if (Combat.Defender == this && this.Hull != this.MaxHull){
+                    // lose the pilot skill
+                    this.AddPilotSkillModifier(this);
+                    // remove
+                    OnDamageCardIsDealt -= RegisterEpsilonAceAbility;
+                    Triggers.FinishTrigger ();
+                }
+            }
+
+            public void ModifyPilotSkill(ref int pilotSkill)
+            {
+                pilotSkill += this.TruePilotSkill;
+            }
+        }
+    }
+}

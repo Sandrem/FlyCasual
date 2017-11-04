a3cd331ccbeb19a017ef654133817ff0ffa39287
Sandrem <oogoeroo@gmail.com>
2017-11-04 09:31:02 +0200

    Ability of Torani Kulda

--- Assets/Scripts/Model/Ships/M12-L Kimogila/ToraniKulda.cs
+++ Assets/Scripts/Model/Ships/M12-L Kimogila/ToraniKulda.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SubPhases;
+using Ship;
+using Tokens;
 
 namespace Ship
 {
@@ -29,9 +33,102 @@ namespace PilotAbilitiesNamespace
 {
     public class ToraniKuldaAbility : GenericPilotAbility
     {
-        public override void Initialize(Ship.GenericShip host)
+        public override void Initialize(GenericShip host)
         {
             base.Initialize(host);
+
+            Host.OnAttackPerformed += RegisterPilotAbility;
+        }
+
+        private void RegisterPilotAbility()
+        {
+            if (Combat.Attacker.ShipId == Host.ShipId)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnAttackPerformed, TryRegisterAbility);
+            }
+        }
+
+        private void TryRegisterAbility(object sender, System.EventArgs e)
+        {
+            Players.GenericPlayer opponent = Roster.GetPlayer(Roster.AnotherPlayer(Host.Owner.PlayerNo));
+            foreach (var ship in opponent.Ships)
+            {
+                Board.ShipShotDistanceInformation shotInfo = new Board.ShipShotDistanceInformation(Host, ship.Value, Host.PrimaryWeapon);
+                if (shotInfo.InBullseyeArc)
+                {
+                    Triggers.RegisterTrigger(new Trigger() {
+                        Name = ship.Value.ShipId + ": " + Name,
+                        TriggerType = TriggerTypes.OnAbilityDirect,
+                        TriggerOwner = opponent.PlayerNo,
+                        EventHandler = ShowChooseEffect,
+                        Sender = ship.Value
+                    });
+                }
+            }
+
+            Triggers.ResolveTriggers(TriggerTypes.OnAbilityDirect, Triggers.FinishTrigger);
+        }
+
+        private void ShowChooseEffect(object sender, System.EventArgs e)
+        {
+            Selection.ThisShip = (GenericShip) sender;
+
+            Phases.StartTemporarySubPhaseOld(
+                "Select effect of Torani Kulda's ability",
+                typeof(ToraniKuldaAbilityDecisionSubPhase),
+                Triggers.FinishTrigger
+            );
+        }
+    }
+}
+
+namespace SubPhases
+{
+    public class ToraniKuldaAbilityDecisionSubPhase: DecisionSubPhase
+    {
+        public override void PrepareDecision(Action callBack)
+        {
+            InfoText = Selection.ThisShip.ShipId + ": " + "Select effect of Torani Kulda's ability";
+            DecisionOwner = Selection.ThisShip.Owner;
+
+            AddDecision("Suffer 1 damage", SufferDamage);
+            AddDecision("Remove all Focus and Evade tokens", DiscardFocusAndEvadeTokens);
+
+            DefaultDecision = "Remove all Focus and Evade tokens";
+
+            callBack();
+        }
+
+        private void DiscardFocusAndEvadeTokens(object sender, System.EventArgs e)
+        {
+            Selection.ThisShip.RemoveToken(typeof(FocusToken), '*', true);
+            Selection.ThisShip.RemoveToken(typeof(EvadeToken), '*', true);
+
+            ConfirmDecision();
+        }
+
+        private void SufferDamage(object sender, System.EventArgs e)
+        {
+            DiceRoll damage = new DiceRoll(DiceKind.Attack, 0, DiceRollCheckType.Virtual);
+            damage.AddDice(DieSide.Success);
+
+            Selection.ThisShip.AssignedDamageDiceroll.DiceList.AddRange(damage.DiceList);
+
+            Triggers.RegisterTrigger(new Trigger()
+            {
+                Name = "Suffer Torani Kulda's ability damage",
+                TriggerType = TriggerTypes.OnDamageIsDealt,
+                TriggerOwner = Selection.ThisShip.Owner.PlayerNo,
+                EventHandler = Selection.ThisShip.SufferDamage,
+                Skippable = true,
+                EventArgs = new DamageSourceEventArgs()
+                {
+                    Source = "Torani Kulda",
+                    DamageType = DamageTypes.CardAbility
+                }
+            });
+
+            Triggers.ResolveTriggers(TriggerTypes.OnDamageIsDealt, ConfirmDecision);
         }
     }
 }

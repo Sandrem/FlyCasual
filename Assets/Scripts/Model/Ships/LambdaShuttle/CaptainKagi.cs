2d68ad4cb2a87ee9f433aa9a3bca89a4d412e89a
rune-b <rune@krokodille.com>
2017-11-13 20:55:21 +0100

    Update Captain Kagi to new Ability class

--- Assets/Scripts/Model/Ships/LambdaShuttle/CaptainKagi.cs
+++ Assets/Scripts/Model/Ships/LambdaShuttle/CaptainKagi.cs
@@ -1,4 +1,6 @@
-﻿namespace Ship
+﻿using Ship;
+
+namespace Ship
 {
     namespace LambdaShuttle
     {
@@ -10,47 +12,58 @@
                 PilotSkill = 8;
                 Cost = 27;
                 IsUnique = true;
-            }
 
-            public override void InitializePilot()
-            {
-                base.InitializePilot();
-
-                RulesList.TargetLocksRule.OnCheckTargetLockIsAllowed += CanPerformTargetLock;
-                OnDestroyed += RemoveCaptainKagiAbility;
-            }
-
-
-            public void CanPerformTargetLock(ref bool result, GenericShip attacker, GenericShip defender)
-            {
-                bool abilityIsActive = false;
-                if (defender.ShipId != this.ShipId)
-                {
-                    if (defender.Owner.PlayerNo == this.Owner.PlayerNo)
-                    {
-                        Board.ShipDistanceInformation positionInfo = new Board.ShipDistanceInformation(attacker, this);
-                        if (positionInfo.Range >= attacker.TargetLockMinRange && positionInfo.Range <= attacker.TargetLockMaxRange)
-                        {
-                            abilityIsActive = true;                            
-                        }
-                    }
-                }
-
-                if (abilityIsActive)
-                {
-                    if (Roster.GetPlayer(Phases.CurrentPhasePlayer).GetType() == typeof(Players.HumanPlayer))
-                    {
-                        Messages.ShowErrorToHuman("Captain Kagi: You cannot target lock that ship");
-                    }
-                    result = false;
-                }                
-            }
-
-            private void RemoveCaptainKagiAbility(GenericShip ship)
-            {
-                RulesList.TargetLocksRule.OnCheckTargetLockIsAllowed -= CanPerformTargetLock;
-                OnDestroyed -= RemoveCaptainKagiAbility;
+                PilotAbilities.Add(new PilotAbilitiesNamespace.CaptainKagiAbility());
             }
         }
+    }
+}
+
+
+namespace PilotAbilitiesNamespace
+{
+    public class CaptainKagiAbility : GenericPilotAbility
+    {
+
+        public override void Initialize(GenericShip host)
+        {
+            base.Initialize(host);
+
+            RulesList.TargetLocksRule.OnCheckTargetLockIsAllowed += CanPerformTargetLock;
+            host.OnDestroyed += RemoveCaptainKagiAbility;
+        }
+
+
+        public void CanPerformTargetLock(ref bool result, GenericShip attacker, GenericShip defender)
+        {
+            bool abilityIsActive = false;
+            if (defender.ShipId != Host.ShipId)
+            {
+                if (defender.Owner.PlayerNo == Host.Owner.PlayerNo)
+                {
+                    Board.ShipDistanceInformation positionInfo = new Board.ShipDistanceInformation(attacker, Host);
+                    if (positionInfo.Range >= attacker.TargetLockMinRange && positionInfo.Range <= attacker.TargetLockMaxRange)
+                    {
+                        abilityIsActive = true;
+                    }
+                }
+            }
+
+            if (abilityIsActive)
+            {
+                if (Roster.GetPlayer(Phases.CurrentPhasePlayer).GetType() == typeof(Players.HumanPlayer))
+                {
+                    Messages.ShowErrorToHuman("Captain Kagi: You cannot target lock that ship");
+                }
+                result = false;
+            }
+        }
+
+        private void RemoveCaptainKagiAbility(GenericShip ship)
+        {
+            RulesList.TargetLocksRule.OnCheckTargetLockIsAllowed -= CanPerformTargetLock;
+            Host.OnDestroyed -= RemoveCaptainKagiAbility;
+        }
+
     }
 }

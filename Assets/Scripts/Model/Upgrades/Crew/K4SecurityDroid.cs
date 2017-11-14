593f5a76141c882e15f33405678684de298b5e4a
Alex Vaccaro <alexandervaccaro@gmail.com>
2017-11-14 15:04:34 -0800

    Add K4 Security Droid

---
+++ Assets/Scripts/Model/Upgrades/Crew/K4SecurityDroid.cs
@@ -0,0 +1,58 @@
+﻿using Ship;
+using System;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace UpgradesList
+{
+
+    public class K4SecurityDroid : GenericUpgrade
+    {
+        private string ActionName;
+
+        public K4SecurityDroid() : base()
+        {
+            Type = UpgradeType.Crew;
+            ActionName = "K4 Security Droid: Target Lock";
+            Name = "K4 Security Droid";
+            Cost = 4;
+        }
+
+        public override bool IsAllowedForShip(GenericShip ship)
+        {
+            return ship.faction == Faction.Scum;
+        }
+
+        public override void AttachToShip(Ship.GenericShip host)
+        {
+            base.AttachToShip(host);
+
+            host.OnMovementFinish += PlanTargetLock;
+        }
+
+        private void PlanTargetLock(Ship.GenericShip host)
+        {
+            if (host.AssignedManeuver.ColorComplexity == Movement.ManeuverColor.Green)
+            {
+                Triggers.RegisterTrigger(new Trigger() {
+                    Name = ActionName,
+                    TriggerOwner = host.Owner.PlayerNo,
+                    TriggerType = TriggerTypes.OnShipMovementFinish,
+                    EventHandler = PerformTargetLock
+                });
+            }
+        }
+
+        private void PerformTargetLock(object sender, EventArgs e)
+        {
+            ActionsList.TargetLockAction K4SecurityDroidTargetLock = new ActionsList.TargetLockAction()
+            {
+                Name = ActionName,
+                EffectName = ActionName
+            };
+
+            List<ActionsList.GenericAction> actions = new List<ActionsList.GenericAction> { K4SecurityDroidTargetLock };
+            base.Host.AskPerformFreeAction(actions, Triggers.FinishTrigger);
+        }
+    }
+}

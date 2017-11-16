b4bb2500cce283cc676451a542642cfff5a41e0a
Sandrem <oogoeroo@gmail.com>
2017-11-17 01:13:07 +0200

    Chopper pilot

--- Assets/Scripts/Model/Ships/VCX-100/Chopper.cs
+++ Assets/Scripts/Model/Ships/VCX-100/Chopper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ship;
 
 namespace Ship
 {
@@ -15,6 +16,49 @@ namespace Ship
                 Cost = 37;
 
                 IsUnique = true;
+
+                PilotAbilities.Add(new PilotAbilitiesNamespace.ChopperPilotAbility());
+            }
+        }
+    }
+}
+
+namespace PilotAbilitiesNamespace
+{
+    public class ChopperPilotAbility : GenericPilotAbility
+    {
+        private List<GenericShip> shipsToAssignStress;
+
+        public override void Initialize(GenericShip host)
+        {
+            base.Initialize(host);
+
+            Host.OnCombatPhaseStart += RegisterPilotAbility;
+        }
+
+        private void RegisterPilotAbility(GenericShip ship)
+        {
+            RegisterAbilityTrigger(TriggerTypes.OnCombatPhaseStart, AssignStressTokens);
+        }
+
+        private void AssignStressTokens(object sender, System.EventArgs e)
+        {
+            shipsToAssignStress = new List<GenericShip>(Host.ShipsBumped);
+            AssignStressTokenRecursive();
+        }
+
+        private void AssignStressTokenRecursive()
+        {
+            if (shipsToAssignStress.Count > 0)
+            {
+                GenericShip shipToAssignStress = shipsToAssignStress[0];
+                shipsToAssignStress.Remove(shipToAssignStress);
+                Messages.ShowErrorToHuman(shipToAssignStress.PilotName + " is bumped into \"Chopper\" and gets Stress");
+                shipToAssignStress.AssignToken(new Tokens.StressToken(), AssignStressTokenRecursive);
+            }
+            else
+            {
+                Triggers.FinishTrigger();
             }
         }
     }

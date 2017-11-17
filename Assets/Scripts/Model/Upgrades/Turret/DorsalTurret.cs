eb1bb080634620df5eee795af9b2e358b2f829f7
Sandrem <oogoeroo@gmail.com>
2017-11-17 18:07:57 +0200

    Dorsal Turret

---
+++ Assets/Scripts/Model/Upgrades/Turret/DorsalTurret.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace UpgradesList
+{
+
+    public class DorsalTurret : GenericSecondaryWeapon
+    {
+        public DorsalTurret() : base()
+        {
+            Type = UpgradeType.Turret;
+
+            Name = "Dorsal Turret";
+            Cost = 3;
+
+            MinRange = 1;
+            MaxRange = 2;
+            AttackValue = 2;
+
+            CanShootOutsideArc = true;
+        }
+
+        public override void AttachToShip(Ship.GenericShip host)
+        {
+            base.AttachToShip(host);
+
+            Host.AfterGotNumberOfAttackDice += AddDiceAtRangeOne;
+        }
+
+        private void AddDiceAtRangeOne(ref int diceCount)
+        {
+            if (Combat.ChosenWeapon == this && Combat.ShotInfo.Range == 1)
+            {
+                diceCount++;
+            }
+        }
+    }
+}

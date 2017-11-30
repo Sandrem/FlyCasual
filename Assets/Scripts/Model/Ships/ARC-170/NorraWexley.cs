3715ecac050724b720d892733a519c6d142320f4
Sandrem <oogoeroo@gmail.com>
2017-11-30 20:27:33 +0200

    Norra Wexley

---
+++ Assets/Scripts/Model/Ships/ARC-170/NorraWexley.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ship;
+using System.Linq;
+
+namespace Ship
+{
+    namespace ARC170
+    {
+        public class NorraWexley : ARC170
+        {
+            public NorraWexley() : base()
+            {
+                PilotName = "Norra Wexley";
+                PilotSkill = 7;
+                Cost = 29;
+
+                IsUnique = true;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+
+                PilotAbilities.Add(new PilotAbilitiesNamespace.NorraWexleyPilotAbility());
+            }
+        }
+    }
+}
+
+namespace PilotAbilitiesNamespace
+{
+    public class NorraWexleyPilotAbility : GenericPilotAbility
+    {
+        public override void Initialize(GenericShip host)
+        {
+            base.Initialize(host);
+
+            Host.AfterGenerateAvailableActionEffectsList += AddNorraWexleyPilotAbility;
+        }
+
+        private void AddNorraWexleyPilotAbility(GenericShip ship)
+        {
+            NorraWexleyAction newAction = new NorraWexleyAction() { Host = this.Host };
+            ship.AddAvailableActionEffect(newAction);
+        }
+
+        private class NorraWexleyAction : ActionsList.GenericAction
+        {
+            public NorraWexleyAction()
+            {
+                Name = EffectName = "Norra Wexley's ability";
+            }
+
+            public override void ActionEffect(System.Action callBack)
+            {
+                char targetLockPair = GetTargetLockTokenLetterOnAnotherShip();
+                if (targetLockPair != ' ')
+                {
+                    Combat.CurrentDiceRoll.AddDice(DieSide.Focus).ShowWithoutRoll();
+                    Combat.CurrentDiceRoll.OrganizeDicePositions();
+
+                    Host.SpendToken(typeof(Tokens.BlueTargetLockToken), callBack, targetLockPair);
+                }
+                else
+                {
+                    callBack();
+                }
+            }
+
+            private char GetTargetLockTokenLetterOnAnotherShip()
+            {
+                GenericShip anotherShip = null;
+
+                switch (Combat.AttackStep)
+                {
+                    case CombatStep.Attack:
+                        anotherShip = Combat.Defender;
+                        break;
+                    case CombatStep.Defence:
+                        anotherShip = Combat.Attacker;
+                        break;
+                    default:
+                        break;
+                }
+
+                return Actions.GetTargetLocksLetterPair(Host, anotherShip);
+            }
+
+            public override bool IsActionEffectAvailable()
+            {
+                bool result = false;
+                if (GetTargetLockTokenLetterOnAnotherShip() != ' ') result = true;
+                return result;
+            }
+
+            public override int GetActionEffectPriority()
+            {
+                int result = 0;
+
+                if (GetTargetLockTokenLetterOnAnotherShip() != ' ')
+                {
+                    if (Host.GetAvailableActionEffectsList().Count(n => n.IsTurnsAllFocusIntoSuccess) > 0)
+                    {
+                        switch (Combat.AttackStep)
+                        {
+                            case CombatStep.Attack:
+                                if (Host.GetAvailableActionEffectsList().Count(n => n.IsTurnsAllFocusIntoSuccess) > 0)
+                                {
+                                    result = 110;
+                                }
+                                break;
+                            case CombatStep.Defence:
+                                if (Combat.DiceRollAttack.Successes > Combat.DiceRollDefence.Successes + Combat.DiceRollDefence.Focuses)
+                                { 
+                                    result = 110;
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+
+                return result;
+            }
+        }
+
+    }
+}

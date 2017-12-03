2bdf6e4e60d8381ce8f5f17b04f56e70956bca94
Karl Nyborg <galaxy613@gmail.com>
2017-12-03 18:08:31 -0500

    T65B Mod
    Here's my take on how to fix/improve the T65 X-Wing. This provides a
    non-action bar Boost action which stops you from putting auto-thrusters
    on and makes it pointless to put on a T70, while also providing a single
    point reduction.

---
+++ Assets/Scripts/Model/Mods/ModsList/T65B.cs
@@ -0,0 +1,14 @@
+﻿namespace Mods
+{
+    namespace ModsList
+    {
+        public class T65BMod : Mod
+        {
+            public T65BMod()
+            {
+                Name = "T-65B X-Wing";
+                Description = "Adds a cheap new title for T-65 X-Wing";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Upgrades/Titles/T65B.cs
@@ -0,0 +1,75 @@
+﻿using Ship;
+using Ship.XWing;
+using Upgrade;
+using System.Collections.Generic;
+using Mods.ModsList;
+
+namespace UpgradesList
+{
+    public class T65B : GenericUpgradeSlotUpgrade
+    {
+        public T65B() : base()
+        {
+            FromMod = typeof(T65BMod);
+
+            Type = UpgradeType.Title;
+            Name = "T-65B";
+            Cost = -1;
+
+            ImageUrl = "https://media.discordapp.net/attachments/307486197905620993/386997692997435401/T-65B.png?width=195&height=300";
+        }
+
+        public override bool IsAllowedForShip(GenericShip ship)
+        {
+            return (ship is XWing);
+        }
+
+        public override void AttachToShip(Ship.GenericShip host)
+        {
+            base.AttachToShip(host);
+
+            host.AfterGenerateAvailableActionsList += AddT65BAction;
+        }
+
+        private void AddT65BAction(Ship.GenericShip host)
+        {
+            ActionsList.GenericAction newAction = new ActionsList.T65BAction()
+            {
+                ImageUrl = ImageUrl,
+                Host = Host
+            };
+            host.AddAvailableAction(newAction);
+        }
+    }
+}
+
+namespace ActionsList
+{
+
+    public class T65BAction : GenericAction
+    {
+        public T65BAction()
+        {
+            Name = EffectName = "T-65B Boost";
+        }
+
+        public override void ActionTake()
+        {
+            Phases.CurrentSubPhase.Pause();
+            if (!Selection.ThisShip.IsAlreadyExecutedAction(typeof(BoostAction)))
+            {
+                Phases.CurrentSubPhase.Pause();
+                Phases.StartTemporarySubPhaseOld(
+                    "Boost",
+                    typeof(SubPhases.BoostPlanningSubPhase),
+                    Phases.CurrentSubPhase.CallBack
+                );
+            }
+            else
+            {
+                Messages.ShowError("Cannot use T65B: Boost is already executed");
+                Phases.CurrentSubPhase.Resume();
+            }
+        }
+    }
+}

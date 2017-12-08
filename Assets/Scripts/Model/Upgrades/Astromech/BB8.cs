1ec55461a4a93435fb6f3fa730775e9d05777a29
Karl Nyborg <galaxy613@gmail.com>
2017-12-07 20:50:59 -0500

    Initial BB8 Implementation

---
+++ Assets/Scripts/Model/Upgrades/Astromech/BB8.cs
@@ -0,0 +1,43 @@
+﻿using Upgrade;
+using Ship;
+using GameModes;
+using System.Collections.Generic;
+
+namespace UpgradesList
+{
+
+    public class BB8 : GenericUpgrade
+    {
+
+        public BB8() : base()
+        {
+            Type = UpgradeType.Astromech;
+            Name = "BB-8";
+            isUnique = true;
+            Cost = 2;
+        }
+
+        public override void AttachToShip(Ship.GenericShip host)
+        {
+            base.AttachToShip(host);
+
+            host.OnManeuverIsRevealed += AddAction;
+        }
+
+        private void AddAction(Ship.GenericShip host)
+        {
+            Triggers.RegisterTrigger(new Trigger()
+            {
+                Name = "BB-8's ability",
+                TriggerType = TriggerTypes.OnManeuverIsRevealed,
+                TriggerOwner = host.Owner.PlayerNo,
+                EventHandler = AskPerformFreeActions
+            });
+        }
+
+        private void AskPerformFreeActions(object sender, System.EventArgs e)
+        {
+            Host.AskPerformFreeAction(new List<ActionsList.GenericAction>() { new ActionsList.BarrelRollAction() }, Triggers.FinishTrigger);
+        }
+    }
+}

89e379ae061bb54d362e3ebaf58ed5eaacb3c2b7
Sandrem <oogoeroo@gmail.com>
2017-12-08 18:05:44 +0200

    Duplicated message about illegal attack is fixed

--- Assets/Scripts/Model/Rules/RulesList/TargetIsLegalForShotRule.cs
+++ Assets/Scripts/Model/Rules/RulesList/TargetIsLegalForShotRule.cs
@@ -4,15 +4,10 @@ namespace RulesList
 {
     public class TargetIsLegalForShotRule
     {
-        //EVENTS
-        public static event GenericShip.EventHandler2Ships OnCheckTargetIsLegal;
-
         public bool IsLegal(bool isSilent = false)
         {
             bool result = true;
             result = Selection.ThisShip.CallCanPerformAttack(result, null, isSilent);
-
-            if (result) OnCheckTargetIsLegal(ref result, Selection.ThisShip, Selection.AnotherShip);
             return result;
         }
     }

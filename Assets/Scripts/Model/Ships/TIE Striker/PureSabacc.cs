b5e87597fd431e8b02d866c818df3c5d5d43fbb8
Sandrem <oogoeroo@gmail.com>
2017-12-27 02:50:10 +0200

    Pure Sabacc

---
+++ Assets/Scripts/Model/Ships/TIE Striker/PureSabacc.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Abilities;
+
+namespace Ship
+{
+    namespace TIEStriker
+    {
+        public class PureSabacc : TIEStriker
+        {
+            public PureSabacc() : base()
+            {
+                PilotName = "\"Pure Sabacc\"";
+                PilotSkill = 6;
+                Cost = 22;
+
+                IsUnique = true;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+
+                PilotAbilities.Add(new PureSabaccAbility());
+            }
+        }
+    }
+}
+
+namespace Abilities
+{
+    public class PureSabaccAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.AfterGotNumberOfAttackDice += CheckPureSabaccAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.AfterGotNumberOfAttackDice -= CheckPureSabaccAbility;
+        }
+
+        private void CheckPureSabaccAbility(ref int value)
+        {
+            if (HostShip.Hull == HostShip.MaxHull) value++;
+        }
+    }
+}

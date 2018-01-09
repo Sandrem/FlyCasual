e6ccc4c79c271958ba7c53476123f2751a274251
Michael Speth <mspeth@monky-games.com>
2018-01-10 10:17:41 +1300

    Changed to adding dice instead of incr atck val
    Renamed pilot to include "
    Renamed template texture to accomodate "

--- Assets/Scripts/Model/Ships/TIE FO/ZetaLeader.cs
+++ Assets/Scripts/Model/Ships/TIE FO/ZetaLeader.cs
@@ -12,7 +12,7 @@ namespace Ship
         {
             public ZetaLeader () : base ()
             {
-                PilotName = "Zeta Leader";
+                PilotName = "\"Zeta Leader\"";
                 PilotSkill = 7;
                 Cost = 20;
 
@@ -72,7 +72,8 @@ namespace Abilities
             // don't need to check stressed as done already
             // add an attack dice
             IsAbilityUsed = true;
-            HostShip.ChangeFirepowerBy(+1);
+            //HostShip.ChangeFirepowerBy(+1);
+            HostShip.AfterGotNumberOfPrimaryWeaponAttackDice += ZetaLeaderAddAttackDice;
             HostShip.AssignToken(new Tokens.StressToken(), SubPhases.DecisionSubPhase.ConfirmDecision);
         }
 
@@ -81,9 +82,14 @@ namespace Abilities
             // At the end of combat phase, need to remove attack value increase
             if (IsAbilityUsed)
             {
-                HostShip.ChangeFirepowerBy(-1);
+                //HostShip.ChangeFirepowerBy(-1);
+                HostShip.AfterGotNumberOfPrimaryWeaponAttackDice -= ZetaLeaderAddAttackDice;
                 IsAbilityUsed = false;
             }
+        }
+        private void ZetaLeaderAddAttackDice(ref int value)
+        {
+            value++;
         }
     }
 }

855c7709cfc11dcdc16aff6186477e054ff676a2
Sandrem <oogoeroo@gmail.com>
2018-01-13 23:21:32 +0200

    Squad Builder: Initial pozition of used Scroll View is fixed

--- Assets/Scripts/View/SquadBuilder/SquadBuilder.cs
+++ Assets/Scripts/View/SquadBuilder/SquadBuilder.cs
@@ -111,6 +111,7 @@ namespace SquadBuilderNS
 
             Transform contentTransform = GameObject.Find("UI/Panels/SelectPilotPanel/Panel/Scroll View/Viewport/Content").transform;
             DestroyChildren(contentTransform);
+            contentTransform.localPosition = new Vector3(0, contentTransform.localPosition.y, contentTransform.localPosition.z);
             contentTransform.GetComponent<RectTransform>().sizeDelta = new Vector2(pilotsCount*(PILOT_CARD_WIDTH + DISTANCE_MEDIUM) + 2 * DISTANCE_MEDIUM, 0);
 
             foreach (PilotRecord pilot in AllPilotsFiltered)
@@ -499,6 +500,7 @@ namespace SquadBuilderNS
 
             Transform contentTransform = GameObject.Find("UI/Panels/SelectUpgradePanel/Panel/Scroll View/Viewport/Content").transform;
             DestroyChildren(contentTransform);
+            contentTransform.localPosition = new Vector3(0, contentTransform.localPosition.y, contentTransform.localPosition.z);
             contentTransform.GetComponent<RectTransform>().sizeDelta = new Vector2(filteredUpgradesCount * (UPGRADE_CARD_WIDTH + DISTANCE_MEDIUM) + 2 * DISTANCE_MEDIUM, 0);
 
             foreach (UpgradeRecord upgrade in filteredUpgrades)

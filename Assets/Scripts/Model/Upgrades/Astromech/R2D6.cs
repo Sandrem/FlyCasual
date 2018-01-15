3c97c3cb84dffe32f478a81b0b5bd160d8612b6b
Hunter <hkeaton0727@gmail.com>
2018-01-14 20:45:57 -0500

    Added R2-D6 Astromech

---
+++ Assets/Scripts/Model/Upgrades/Astromech/R2D6.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Upgrade;
+using Abilities;
+using Ship;
+using System.Linq;
+
+namespace UpgradesList
+{
+
+    public class R2D6 : GenericUpgradeSlotUpgrade
+    {
+        public R2D6() : base()
+        {
+            Type = UpgradeType.Astromech;
+            Name = "R2-D6";
+            isUnique = true;
+            Cost = 1;
+
+            AddedSlots = new List<UpgradeSlot>
+            {
+                new UpgradeSlot(UpgradeType.Elite)
+            };
+        }        
+
+        public override bool IsAllowedForShip(GenericShip ship)
+        {
+            if (ship.PilotSkill <= 2)
+            {
+                return false;
+            }
+
+            if (ship.PrintedUpgradeIcons.Any(upgrade => upgrade == UpgradeType.Elite))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}

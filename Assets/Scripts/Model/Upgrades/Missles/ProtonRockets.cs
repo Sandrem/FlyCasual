2dec80d0f3b2bb42b5a9e72a3088cfdf25f77798
Evan Lundell <evan.lundell@gmail.com>
2018-01-16 16:11:42 -0600

    Added Proton Rockets

---
+++ Assets/Scripts/Model/Upgrades/Missles/ProtonRockets.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ship;
+using Upgrade;
+
+namespace UpgradesList
+{
+    public class ProtonRockets : GenericSecondaryWeapon
+    {
+        public ProtonRockets() : base()
+        {
+            Type = UpgradeType.Missile;
+
+            Name = "Proton Rockets";
+            Cost = 3;
+
+            MinRange = 1;
+            MaxRange = 1;
+            AttackValue = 2;
+
+            RequiresFocusToShoot = true;
+
+            IsDiscardedForShot = true;
+        }
+
+        public override void AttachToShip(GenericShip host)
+        {
+            base.AttachToShip(host);
+
+            host.OnAttackStartAsAttacker += AddAgilityToAttackValue;
+        }
+
+        private void AddAgilityToAttackValue()
+        {
+            int attackModifier = 3;
+            if (Host.Agility < 3)
+            {
+                attackModifier = Host.Agility;
+            }
+            AttackValue = 2 + attackModifier;
+        }
+    }
+}

7a03bdc682feac155122f93cf5efb73d374f9b8e
sirhaplo <sirhaplo@gmail.com>
2018-01-18 23:15:34 +0100

    Juke - Doesn't work. Calculate results before juke action

--- Assets/Scripts/Model/Upgrades/Elite/Juke.cs
+++ Assets/Scripts/Model/Upgrades/Elite/Juke.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using Upgrade;
 using Ship;
 
+
 namespace UpgradesList
 {
 
@@ -21,17 +22,62 @@ namespace UpgradesList
         public override void AttachToShip(Ship.GenericShip host)
         {
             base.AttachToShip(host);
-            RegisterAbilityTrigger(TriggerTypes., ShowDecision);
-            Host.OnImmediatelyAfterRolling += ChangeCritsToHits;
+            host.AfterGenerateAvailableOppositeActionEffectsList += JukeActionEffect;
             Console.Write("Juke attached");
         }
 
-        private void ChangeCritsToHits(DiceRoll diceroll)
+        private void JukeActionEffect(GenericShip host)
         {
-            Console.Write("Juke executed");
-            diceroll.ChangeAll(DieSide.Blank, DieSide.Crit);
-            diceroll.ChangeAll(DieSide.Focus, DieSide.Crit);
-            diceroll.ChangeAll(DieSide.Success, DieSide.Crit);
+            ActionsList.GenericAction newAction = new ActionsList.JukeActionEffect()
+            {
+                ImageUrl = ImageUrl,
+                Host = host
+            };
+            host.AddAvailableOppositeActionEffect(newAction);
         }
     }
+}
+
+namespace ActionsList
+{
+    public class JukeActionEffect : GenericAction
+    {
+
+        public JukeActionEffect()
+        {
+            Name = EffectName = "Juke";
+            IsOpposite = true;
+        }
+
+        public override int GetActionEffectPriority()
+        {
+            int result = 0;
+
+            result = 100;
+
+            return result;
+        }
+
+        public override bool IsActionEffectAvailable()
+        {
+            bool result = false;
+
+            if (Combat.AttackStep == CombatStep.Defence && 
+                Combat.DiceRollDefence.RegularSuccesses > 0 && 
+                Host.HasToken(typeof(Tokens.EvadeToken)))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public override void ActionEffect(System.Action callBack)
+        {
+            Combat.DiceRollDefence.ChangeOne(DieSide.Success, DieSide.Focus, false);
+            callBack();
+        }
+
+    }
+
 }

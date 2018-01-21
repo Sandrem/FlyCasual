a8e0148523d92e1d0519e625fd89b2841b4a174a
sirhaplo <sirhaplo@gmail.com>
2018-01-21 14:37:28 +0100

    Fix on upgradeSlot checks Havoc Title Smuggling compartment Title

---
+++ Assets/Scripts/Model/Upgrades/Titles/Havoc.cs
@@ -0,0 +1,31 @@
+﻿using Ship;
+using Upgrade;
+using System.Collections.Generic;
+
+
+namespace UpgradesList
+{
+    public class Havoc : GenericUpgradeSlotUpgrade
+    {
+        public Havoc() : base()
+        {
+            Type = UpgradeType.Title;
+            Name = "Havoc";
+            Cost = 0;
+            AddedSlots = new List<UpgradeSlot>
+            {
+                new UpgradeSlot(UpgradeType.System) {  },
+                new UpgradeSlot(UpgradeType.SalvagedAstromech) { MustBeNonUnique = true }
+            };
+            ForbiddenSlots = new List<UpgradeType>
+            {
+                UpgradeType.Crew
+            };
+        }
+
+        public override bool IsAllowedForShip(GenericShip ship)
+        {
+            return ship is Ship.ScurrgH6Bomber.ScurrgH6Bomber;
+        }
+    }
+}

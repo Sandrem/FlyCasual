14c3c2e7befdfd3ff7377bf05413ca42884e886a
sirhaplo <sirhaplo@gmail.com>
2018-01-22 18:04:42 +0100

    SpecialOps - Early tests

--- Assets/Scripts/Model/Upgrades/Titles/SpecialOpsTraining.cs
+++ Assets/Scripts/Model/Upgrades/Titles/SpecialOpsTraining.cs
@@ -1,6 +1,8 @@
 ﻿using Ship;
 using Ship.TIESF;
 using Upgrade;
+using SubPhases;
+using Abilities;
 
 namespace UpgradesList
 {
@@ -9,29 +11,130 @@ namespace UpgradesList
         public SpecialOpsTraining() : base()
         {
             Type = UpgradeType.Title;
-            Name = "Special Ops Training";
+            Name = "Special Ops Training";            
             Cost = 0;            
+
+            UpgradeAbilities.Add(new SpecialOpsTrainingAbility());            
         }
 
         public override bool IsAllowedForShip(GenericShip ship)
         {
             return ship is TIESF;
         }
+        
 
-        public override void AttachToShip(GenericShip host)
+        
+    }
+}
+
+//Special ops
+namespace Abilities
+{
+    public class SpecialOpsTrainingAbility : GenericAbility
+    {        
+        public override void ActivateAbility() {
+            HostShip.OnShotStartAsAttacker += CheckConditions;
+            Phases.OnRoundEnd += ClearAbility;
+        }
+
+        public override void DeactivateAbility() {
+            HostShip.OnShotStartAsAttacker -= CheckConditions;
+            Phases.OnRoundEnd -= ClearAbility;
+        }
+
+        private void CheckConditions()
         {
-            base.AttachToShip(host);
-            host.ChangeFirepowerBy(1);
+            if (IsAbilityUsed) return;
+
+            if (Combat.ChosenWeapon.GetType() != typeof(PrimaryWeaponClass)) return;
+
+            Board.ShipShotDistanceInformation counterAttackInfo = new Board.ShipShotDistanceInformation(Combat.Defender, Combat.Attacker);                        
+            if (!counterAttackInfo.InPrimaryArc) return;            
+
+
+            RegisterAbilityTrigger(TriggerTypes.OnShotStart, StartQuestionSubphase);            
+        }
+
+        private class SpecialOpsTrainingDecisionSubPhase : DecisionSubPhase { }
+        private void StartQuestionSubphase(object sender, System.EventArgs e)
+        {
+            SpecialOpsTrainingDecisionSubPhase selectSpecialOpsTrainingSubPhase = (SpecialOpsTrainingDecisionSubPhase)Phases.StartTemporarySubPhaseNew(
+                Name,
+                typeof(SpecialOpsTrainingDecisionSubPhase),
+                Triggers.FinishTrigger
+            );
+
+            selectSpecialOpsTrainingSubPhase.InfoText = "Use " + Name + "?";
+            selectSpecialOpsTrainingSubPhase.AddDecision("Roll 1 extra die from primary fire arc", RegisterRollExtraDice);
+            selectSpecialOpsTrainingSubPhase.AddTooltip("Roll 1 extra die from primary fire arc", HostShip.ImageUrl);     
+            selectSpecialOpsTrainingSubPhase.AddDecision("Get a second attack from rear arc", RegisterExtraAttack);       
+            selectSpecialOpsTrainingSubPhase.AddTooltip("Get a second attack from rear arc", HostShip.ImageUrl);     
+            selectSpecialOpsTrainingSubPhase.DefaultDecision = GetDefaultDecision();
+            selectSpecialOpsTrainingSubPhase.ShowSkipButton = true;
+            selectSpecialOpsTrainingSubPhase.Start();
+        }
+
+        private string GetDefaultDecision()
+        {
+            string result = "Roll 1 extra die from primary fire arc";
+            //Check if we have targets in both arcs
+            return result;
+        }
+
+        private void RegisterRollExtraDice(object sender, System.EventArgs e)
+        {            
+            if (!IsAbilityUsed) {
+                IsAbilityUsed = true;
+                HostShip.AfterGotNumberOfAttackDice += RollExtraDice;
+                DecisionSubPhase.ConfirmDecision();                
+            }
+        }
+
+        private void RollExtraDice(ref int count)
+        {
+            count++;            
+            Messages.ShowInfo("TieSF rolls extra die from primary arc");
+            HostShip.AfterGotNumberOfAttackDice -= RollExtraDice;
+        }
+
+        private void RegisterExtraAttack(object sender, System.EventArgs e)
+        {
+            if (!IsAbilityUsed)
+            {
+                IsAbilityUsed = true;
+                HostShip.OnAttackFinishAsAttacker += RegisterSpecialOpsExtraAttack;
+                DecisionSubPhase.ConfirmDecision();
+            }
+        }
+
+        private void RegisterSpecialOpsExtraAttack(GenericShip ship)
+        {
+            this.ToggleFrontArc(false);            
+            RegisterAbilityTrigger(TriggerTypes.OnExtraAttack, DoSpecialOpsExtraAttack);
+        }
+
+        private void DoSpecialOpsExtraAttack(object sender, System.EventArgs e)
+        {
+            Messages.ShowInfo(HostShip.PilotName + " can perform second attack from primary weapon");
+
+            Combat.StartAdditionalAttack(
+                HostShip,
+                delegate {
+                    Selection.ThisShip.IsAttackPerformed = true;
+                    ToggleFrontArc(true);
+                    Triggers.FinishTrigger();
+                }
+            );                
         }
 
         //Spegne un arco
-        private void ToggleRearArc(bool isActive)
+        private void ToggleFrontArc(bool isActive)
         {
-            Host.ArcInfo.GetRearArc().ShotPermissions.CanShootPrimaryWeapon = isActive;
+            HostShip.ArcInfo.GetPrimaryArc().ShotPermissions.CanShootPrimaryWeapon = isActive;
         }
 
         //Controlla l'arco
-        private void CheckAssignStress()
+        /*private void CheckAssignStress()
         {
             ShipShotDistanceInformation shotInfo = new ShipShotDistanceInformation(HostShip, TargetShip);
             if (shotInfo.InMobileArc && shotInfo.Range >= 1 && shotInfo.Range <= 2)
@@ -44,116 +147,13 @@ namespace UpgradesList
                 if (!shotInfo.InMobileArc) Messages.ShowError("Target is not inside Mobile Arc");
                 else if (shotInfo.Distance >= 3) Messages.ShowError("Target is outside range 2");
             }
-        }
-    }
-}
-
-//Miranda doni
-namespace Abilities
-{
-    public class MirandaDoniAbility : GenericAbility
-    {
-        public override void ActivateAbility()
-        {
-            HostShip.OnShotStartAsAttacker += CheckConditions;
-            Phases.OnRoundEnd += ClearAbility;
-        }
-
-        public override void DeactivateAbility()
-        {
-            HostShip.OnShotStartAsAttacker -= CheckConditions;
-            Phases.OnRoundEnd -= ClearAbility;
-        }
-
-        private void CheckConditions()
-        {
-            if (!IsAbilityUsed)
-            {
-                RegisterAbilityTrigger(TriggerTypes.OnShotStart, StartQuestionSubphase);
-            }
-        }
-
-        private void StartQuestionSubphase(object sender, System.EventArgs e)
-        {
-            MirandaDoniDecisionSubPhase selectMirandaDoniSubPhase = (MirandaDoniDecisionSubPhase)Phases.StartTemporarySubPhaseNew(
-                Name,
-                typeof(MirandaDoniDecisionSubPhase),
-                Triggers.FinishTrigger
-            );
-
-            selectMirandaDoniSubPhase.InfoText = "Use " + Name + "?";
-
-            if (HostShip.Shields > 0)
-            {
-                selectMirandaDoniSubPhase.AddDecision("Spend 1 shield to roll 1 extra die", RegisterRollExtraDice);
-                selectMirandaDoniSubPhase.AddTooltip("Spend 1 shield to roll 1 extra die", HostShip.ImageUrl);
-            }
-
-            if (HostShip.Shields < HostShip.MaxShields)
-            {
-                selectMirandaDoniSubPhase.AddDecision("Roll 1 fewer die to recover 1 shield", RegisterRegeneration);
-                selectMirandaDoniSubPhase.AddTooltip("Roll 1 fewer die to recover 1 shield", HostShip.ImageUrl);
-            }
-
-            selectMirandaDoniSubPhase.AddDecision("No", delegate { DecisionSubPhase.ConfirmDecision(); });
-
-            selectMirandaDoniSubPhase.DefaultDecision = GetDefaultDecision();
-
-            selectMirandaDoniSubPhase.ShowSkipButton = true;
-
-            selectMirandaDoniSubPhase.Start();
-        }
-
-        private string GetDefaultDecision()
-        {
-            string result = "No";
-
-            if (HostShip.Shields < HostShip.MaxShields) result = "Roll 1 fewer die to recover 1 shield";
-
-            return result;
-        }
-
-        private void RegisterRollExtraDice(object sender, System.EventArgs e)
-        {
-            IsAbilityUsed = true;
-            HostShip.AfterGotNumberOfAttackDice += RollExtraDice;
-
-            DecisionSubPhase.ConfirmDecision();
-        }
-
-        private void RollExtraDice(ref int count)
-        {
-            count++;
-            HostShip.LoseShield();
-
-            Messages.ShowInfo("Miranda Doni spends shield to roll extra die");
-
-            HostShip.AfterGotNumberOfAttackDice -= RollExtraDice;
-        }
-
-        private void RegisterRegeneration(object sender, System.EventArgs e)
-        {
-            IsAbilityUsed = true;
-            HostShip.AfterGotNumberOfAttackDice += RegenerateShield;
-
-            DecisionSubPhase.ConfirmDecision();
-        }
-
-        private void RegenerateShield(ref int count)
-        {
-            count--;
-            HostShip.TryRegenShields();
-
-            Messages.ShowInfo("Miranda Doni rolls 1 fewer die to recover 1 shield");
-
-            HostShip.AfterGotNumberOfAttackDice -= RegenerateShield;
-        }
+        }*/
 
         private void ClearAbility()
         {
             IsAbilityUsed = false;
+            
         }
 
-        private class MirandaDoniDecisionSubPhase : DecisionSubPhase { }
     }
 }

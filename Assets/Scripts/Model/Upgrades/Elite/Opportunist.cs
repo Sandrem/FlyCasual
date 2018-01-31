585f02387d40493258a25ea39e93add44e7d5bd8
Nick <NickyDaB@gmail.com>
2018-01-31 15:11:50 -0500

    Updated 'Opportunist' to use new template

--- Assets/Scripts/Model/Upgrades/Elite/Opportunist.cs
+++ Assets/Scripts/Model/Upgrades/Elite/Opportunist.cs
@@ -1,4 +1,5 @@
-﻿using ActionsList;
+﻿using Abilities;
+using ActionsList;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,30 +17,41 @@ namespace UpgradesList
 			Type = UpgradeType.Elite;
 			Name = "Opportunist";
 			Cost = 4;
+
+			UpgradeAbilities.Add(new OpportunistAbility());
+		}
+	}
+}
+
+namespace Abilities
+{
+	public class OpportunistAbility : GenericAbility
+	{
+		public override void ActivateAbility()
+		{
+			HostShip.OnAttackStartAsAttacker += RegisterOpportunistAbility;
 		}
 
-		public override void AttachToShip(GenericShip HostShip)
+		public override void DeactivateAbility()
 		{
-			base.AttachToShip(HostShip);
+			HostShip.OnAttackStartAsAttacker -= RegisterOpportunistAbility;
+		}
 
-            HostShip.OnAttackStartAsAttacker += OpportunistAbility;
-        }
-		
-		public void OpportunistAbility()
-        {
+		public void RegisterOpportunistAbility()
+		{
 			Triggers.RegisterTrigger(
 				new Trigger()
-                    {
-                        Name = "Opportunist",
-                        TriggerOwner = Combat.Attacker.Owner.PlayerNo,
-                        TriggerType = TriggerTypes.OnAttackStart,
-                        EventHandler = StartOpportunistDecisionSubPhase
-                    }
+				{
+					Name = "Opportunist",
+					TriggerOwner = Combat.Attacker.Owner.PlayerNo,
+					TriggerType = TriggerTypes.OnAttackStart,
+					EventHandler = StartOpportunistDecisionSubPhase
+				}
 			);
-        }
-		
+		}
+
 		private void StartOpportunistDecisionSubPhase(object sender, System.EventArgs e)
-        {
+		{
 			//card constraints say user can't have a stress token, and defender can't have focus or evade tokens
 			if(!Combat.Attacker.Tokens.HasToken(typeof(Tokens.StressToken)) && (!Combat.Defender.Tokens.HasToken(typeof(Tokens.FocusToken)) && !Combat.Defender.Tokens.HasToken(typeof(Tokens.EvadeToken))) )
 			{
@@ -48,7 +60,7 @@ namespace UpgradesList
 					typeof(OpportunistDecisionSubPhase),
 					Triggers.FinishTrigger
 				);
-				
+
 				opportunistDecision.InfoText = "Use Opportunist ability?";
 				opportunistDecision.AddDecision("Yes", UseOpportunistAbility);
 				opportunistDecision.AddDecision("No", DontUseOpportunistAbility);
@@ -56,40 +68,39 @@ namespace UpgradesList
 				opportunistDecision.Start();
 			}
 			else
-            {
-                Triggers.FinishTrigger();
-            }
+			{
+				Triggers.FinishTrigger();
+			}
 		}
-		
+
 		private void UseOpportunistAbility(object sender, System.EventArgs e)
-        {
+		{
 			Combat.Attacker.Tokens.AssignToken(new Tokens.StressToken(Combat.Attacker), AllowRollAdditionalDice);
 		}
-		
-		private void AllowRollAdditionalDice()
-        {
-            Combat.Attacker.AfterGotNumberOfAttackDice += IncreaseByOne;
-            SubPhases.DecisionSubPhase.ConfirmDecision();
-        }
-		
-		private void IncreaseByOne(ref int value)
-        {
-            value++;
-            Combat.Attacker.AfterGotNumberOfAttackDice -= IncreaseByOne;
-        }
-		
-		private void DontUseOpportunistAbility(object sender, System.EventArgs e)
-        {
-            DecisionSubPhase.ConfirmDecision();
-        }
-		
-		private class OpportunistDecisionSubPhase : DecisionSubPhase
-        {
-            public override void SkipButton()
-            {
-                ConfirmDecision();
-            }
-        }
 
-    }
+		private void AllowRollAdditionalDice()
+		{
+			Combat.Attacker.AfterGotNumberOfAttackDice += IncreaseByOne;
+			SubPhases.DecisionSubPhase.ConfirmDecision();
+		}
+
+		private void IncreaseByOne(ref int value)
+		{
+			value++;
+			Combat.Attacker.AfterGotNumberOfAttackDice -= IncreaseByOne;
+		}
+
+		private void DontUseOpportunistAbility(object sender, System.EventArgs e)
+		{
+			DecisionSubPhase.ConfirmDecision();
+		}
+
+		private class OpportunistDecisionSubPhase : DecisionSubPhase
+		{
+			public override void SkipButton()
+			{
+				ConfirmDecision();
+			}
+		}
+	}
 }

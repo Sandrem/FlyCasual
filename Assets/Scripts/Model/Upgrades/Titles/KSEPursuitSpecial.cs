e83dc49adcabe8501a3a681e46604860fb953b54
Sandrem <oogoeroo@gmail.com>
2018-02-04 00:43:06 +0200

    Initial templates

---
+++ Assets/Scripts/Model/Upgrades/Titles/KSEPursuitSpecial.cs
@@ -0,0 +1,34 @@
+﻿using Ship;
+using Ship.Firespray31;
+using Upgrade;
+using Mods.ModsList;
+using System.Collections.Generic;
+
+namespace UpgradesList
+{
+    public class KSEPursuitSpecial : GenericUpgradeSlotUpgrade
+    {
+        public bool IsAlwaysUse;
+
+        public KSEPursuitSpecial() : base()
+        {
+            Type = UpgradeType.Title;
+            Name = "KSE Pursuit Special";
+            Cost = -2;
+
+            ImageUrl = "https://i.imgur.com/TmDkcUR.png";
+
+            AddedSlots = new List<UpgradeSlot>
+            {
+                new UpgradeSlot(UpgradeType.Title) { MustBeDifferent = true }
+            };
+
+            FromMod = typeof(FiresprayFix);
+        }
+
+        public override bool IsAllowedForShip(GenericShip ship)
+        {
+            return ship is Firespray31;
+        }
+    }
+}

c5bd881a5d37f95e69da5748042e3c4dc69b78ae
Sandrem <oogoeroo@gmail.com>
2018-02-06 16:16:27 +0200

    Scum Nym: Obstruction bonus ability

--- Assets/Scripts/Model/Ships/Scurrg H-6 Bomber/CaptainNymScum.cs
+++ Assets/Scripts/Model/Ships/Scurrg H-6 Bomber/CaptainNymScum.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Bombs;
 using Abilities;
+using Ship;
+using ActionsList;
 
 namespace Ship
 {
@@ -34,12 +36,48 @@ namespace Abilities
     {
         public override void ActivateAbility()
         {
-
+            GenericShip.AfterGenerateAvailableActionEffectsListGlobal += CheckObstructionBonus;
         }
 
         public override void DeactivateAbility()
         {
+            GenericShip.AfterGenerateAvailableActionEffectsListGlobal -= CheckObstructionBonus;
+        }
 
+        private void CheckObstructionBonus()
+        {
+            if (Combat.Defender.Owner.PlayerNo != HostShip.Owner.PlayerNo) return;
+
+            if (Combat.AttackStep != CombatStep.Defence) return;
+
+            if (Combat.ShotInfo.IsObstructedByBombToken)
+            {
+                Combat.Defender.AddAvailableActionEffect(new CaptainNymObstructionBonus());
+            }
         }
     }
+}
+
+namespace ActionsList
+{
+
+    public class CaptainNymObstructionBonus : GenericAction
+    {
+        public CaptainNymObstructionBonus()
+        {
+            Name = EffectName = "Captain Nym: Free Evade";
+        }
+
+        public override void ActionEffect(System.Action callBack)
+        {
+            Combat.CurrentDiceRoll.ApplyEvade();
+            callBack();
+        }
+
+        public override int GetActionEffectPriority()
+        {
+            return 110;
+        }
+    }
+
 }

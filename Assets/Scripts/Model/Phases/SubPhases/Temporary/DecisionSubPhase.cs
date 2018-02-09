db682b0531fe6650f9c0c62edc1137a2b543abc4
Sandrem <oogoeroo@gmail.com>
2018-02-09 07:38:21 +0200

    Centered images in decision selection window

--- Assets/Scripts/Model/Phases/SubPhases/Temporary/DecisionSubPhase.cs
+++ Assets/Scripts/Model/Phases/SubPhases/Temporary/DecisionSubPhase.cs
@@ -48,12 +48,13 @@ namespace SubPhases
     public class DecisionSubPhase : GenericSubPhase
     {
         private GameObject decisionPanel;
+        private GameObject buttonsHolder;
         public string InfoText;
         private List<Decision> decisions = new List<Decision>();
         public string DefaultDecisionName;
         protected Players.GenericPlayer DecisionOwner;
         public bool ShowSkipButton;
-        public DecisionViewTypes DecisionViewType = DecisionViewTypes.ImageButtons;
+        public DecisionViewTypes DecisionViewType = DecisionViewTypes.TextButtons;
 
         private const float defaultWindowHeight = 75;
         private const float buttonHeight = 45;
@@ -63,6 +64,7 @@ namespace SubPhases
             IsTemporary = true;
 
             decisionPanel = GameObject.Find("UI").transform.Find("DecisionsPanel").gameObject;
+            buttonsHolder = decisionPanel.transform.Find("Center/DecisionsPanel").gameObject;
 
             PrepareDecision(StartIsFinished);
         }
@@ -116,6 +118,34 @@ namespace SubPhases
             {
                 decisionPanel.transform.Find("InformationPanel").GetComponentInChildren<Text>().text = InfoText;
 
+                switch (DecisionViewType)
+                {
+                    case DecisionViewTypes.TextButtons:
+                        decisionPanel.GetComponent<RectTransform>().sizeDelta = new Vector3(
+                            395,
+                            defaultWindowHeight + ((decisions.Count + 1) / 2) * buttonHeight
+                        );
+                        buttonsHolder.GetComponent<RectTransform>().sizeDelta = new Vector3(
+                            395,
+                            defaultWindowHeight + ((decisions.Count + 1) / 2) * buttonHeight
+                        );
+                        break;
+                    case DecisionViewTypes.ImageButtons:
+                        decisionPanel.GetComponent<RectTransform>().sizeDelta = new Vector3(
+                            Mathf.Max(395, decisions.Count * 194 + (decisions.Count + 1) * 10),
+                            defaultWindowHeight + 300 + 10
+                        );
+                        buttonsHolder.GetComponent<RectTransform>().sizeDelta = new Vector3(
+                            decisions.Count * 194 + (decisions.Count + 1) * 10,
+                            defaultWindowHeight + 300 + 10
+                        );
+                        break;
+                    default:
+                        break;
+                }
+
+                buttonsHolder.transform.localPosition = new Vector2(-buttonsHolder.GetComponent<RectTransform>().sizeDelta.x / 2, -70);
+
                 int i = 0;
                 foreach (var decision in decisions)
                 {
@@ -133,7 +163,6 @@ namespace SubPhases
                             break;
                     }
 
-                    GameObject buttonsHolder = decisionPanel.transform.Find("DecisionsPanel").gameObject;
                     GameObject button = MonoBehaviour.Instantiate(prefab, buttonsHolder.transform);
 
                     switch (DecisionViewType)
@@ -176,24 +205,6 @@ namespace SubPhases
                     i++;
                 }
 
-                switch (DecisionViewType)
-                {
-                    case DecisionViewTypes.TextButtons:
-                        decisionPanel.GetComponent<RectTransform>().sizeDelta = new Vector3(
-                            395,
-                            defaultWindowHeight + ((i + 1) / 2) * buttonHeight
-                        );
-                        break;
-                    case DecisionViewTypes.ImageButtons:
-                        decisionPanel.GetComponent<RectTransform>().sizeDelta = new Vector3(
-                            Mathf.Max(395, (i)*194 + (i+1)*10),
-                            defaultWindowHeight + 300 + 10
-                        );
-                        break;
-                    default:
-                        break;
-                }
-
                 if (DecisionOwner == null) DecisionOwner = Roster.GetPlayer(Phases.CurrentPhasePlayer);
 
                 if (ShowSkipButton) UI.ShowSkipButton();
@@ -224,7 +235,7 @@ namespace SubPhases
         private void HidePanel()
         {
             decisionPanel.gameObject.SetActive(false);
-            foreach (Transform button in decisionPanel.transform.Find("DecisionsPanel"))
+            foreach (Transform button in buttonsHolder.transform)
             {
                 MonoBehaviour.Destroy(button.gameObject);
             }

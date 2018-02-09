b99d264801bb163410545b2c0251fbb1689116cc
Robert Taylor <rats3g@gmail.com>
2018-02-09 18:04:59 -0500

    Implement "Zeta Ace" pilot for TIE/FO Fighter.

---
+++ Assets/Scripts/Model/Ships/TIE FO/ZetaAce.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ship
+{
+    namespace TIEFO
+    {
+        public class ZetaAce : TIEFO
+        {
+            public ZetaAce() : base()
+            {
+                PilotName = "\"Zeta Ace\"";
+                PilotSkill = 5;
+                Cost = 18;
+
+                IsUnique = true;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+
+                PilotAbilities.Add(new Abilities.ZetaAceAbility());
+            }
+        }
+    }
+}
+
+namespace Abilities
+{
+    public class ZetaAceAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnGetAvailableBarrelRollTemplates += ChangeBarrelRollTemplates;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnGetAvailableBarrelRollTemplates -= ChangeBarrelRollTemplates;
+        }
+
+        private void ChangeBarrelRollTemplates(List<Actions.BarrelRollTemplates> availableTemplates)
+        {
+            if (availableTemplates.Contains(Actions.BarrelRollTemplates.Straight1))
+            {
+                availableTemplates.Add(Actions.BarrelRollTemplates.Straight2);
+            }
+        }
+    }
+}

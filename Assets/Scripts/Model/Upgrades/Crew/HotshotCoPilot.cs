055d603e5b77e13c2fa80388936d99b7e6d6e6e8
Sandrem <oogoeroo@gmail.com>
2018-02-19 15:49:00 +0200

    Hotshot Co-pilot condition

--- Assets/Scripts/Model/Upgrades/Crew/HotshotCoPilot.cs
+++ Assets/Scripts/Model/Upgrades/Crew/HotshotCoPilot.cs
@@ -2,6 +2,7 @@
 using GameModes;
 using Ship;
 using Upgrade;
+using Tokens;
 
 namespace UpgradesList
 {
@@ -24,12 +25,63 @@ namespace Abilities
     {
         public override void ActivateAbility()
         {
-
+            HostShip.OnAttackStartAsAttacker += CheckAttackAbility;
+            HostShip.OnAttackStartAsDefender += DefenceAbility;
         }
 
         public override void DeactivateAbility()
         {
+            HostShip.OnAttackStartAsAttacker -= CheckAttackAbility;
+            HostShip.OnAttackStartAsDefender -= DefenceAbility;
+        }
 
+        private void CheckAttackAbility()
+        {
+            if (Combat.ChosenWeapon is PrimaryWeaponClass) AssignCondition(Combat.Defender);
+        }
+
+        private void DefenceAbility()
+        {
+            AssignCondition(Combat.Attacker);
+        }
+
+        private void AssignCondition(GenericShip ship)
+        {
+            Messages.ShowInfo("Hotshot Co-pilot effect is active");
+            ship.Tokens.AssignCondition(new Conditions.HotshotCoPilotCondition(ship));
+            ship.OnTokenIsSpent += CheckRemoveCondition;
+
+            ship.OnAttackFinish += RemoveCondition;
+        }
+
+        private void CheckRemoveCondition(GenericShip ship, System.Type tokenType)
+        {
+            if (tokenType == typeof(FocusToken)) RemoveCondition(ship);
+        }
+
+        private void RemoveCondition(GenericShip ship)
+        {
+            Messages.ShowInfo("Hotshot Co-pilot effect is not active");
+
+            ship.OnAttackFinish -= RemoveCondition;
+            ship.OnTokenIsSpent -= CheckRemoveCondition;
+
+            ship.Tokens.RemoveCondition(typeof(Conditions.HotshotCoPilotCondition));
+        }
+
+        // Check - can finish dice modifications
+    }
+}
+
+namespace Conditions
+{
+    public class HotshotCoPilotCondition : GenericToken
+    {
+        public HotshotCoPilotCondition(GenericShip host) : base(host)
+        {
+            Name = "Debuff Token";
+            Temporary = false;
+            Tooltip = new UpgradesList.HotshotCoPilot().ImageUrl;
         }
     }
 }

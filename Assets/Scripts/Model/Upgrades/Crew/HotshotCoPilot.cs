4d2020275f925da47644fff23cf0dec3f7cdd1ef
Sandrem <oogoeroo@gmail.com>
2018-02-19 15:31:50 +0200

    Hotshot Co-pilot template

---
+++ Assets/Scripts/Model/Upgrades/Crew/HotshotCoPilot.cs
@@ -0,0 +1,35 @@
+﻿using Abilities;
+using GameModes;
+using Ship;
+using Upgrade;
+
+namespace UpgradesList
+{
+    public class HotshotCoPilot : GenericUpgrade
+    {
+        public HotshotCoPilot() : base()
+        {
+            Type = UpgradeType.Crew;
+            Name = "Hotshot Co-pilot";
+            Cost = 4;
+
+            UpgradeAbilities.Add(new HotshotCoPilotAbility());
+        }
+    }
+}
+
+namespace Abilities
+{
+    public class HotshotCoPilotAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+
+        }
+
+        public override void DeactivateAbility()
+        {
+
+        }
+    }
+}

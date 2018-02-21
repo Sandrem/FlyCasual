53d7bc02ece6a214e7056f9b1ec6f5d0ed583180
Sandrem <oogoeroo@gmail.com>
2018-02-21 22:59:28 +0200

    Maarek Stele (TIE Defender) is added

---
+++ Assets/Scripts/Model/Ships/TIE Defender/MaarekSteleDefender.cs
@@ -0,0 +1,21 @@
+﻿namespace Ship
+{
+    namespace TIEDefender
+    {
+        public class MaarekSteleDefender : TIEDefender
+        {
+            public MaarekSteleDefender() : base()
+            {
+                PilotName = "Maarek Stele";
+                PilotSkill = 7;
+                Cost = 27;
+
+                IsUnique = true;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+
+                PilotAbilities.Add(new Abilities.MaarekSteleAbility());
+            }
+        }
+    }
+}

42f21fe6387d62d083d35033adc148332167a63a
Sandrem <oogoeroo@gmail.com>
2018-02-22 21:42:07 +0200

    Mod "Elite Y-Wing Pilots"

---
+++ Assets/Scripts/Model/Ships/Y-Wing/HortonSalmElite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ship;
+using System;
+using Mods.ModsList;
+
+namespace Ship
+{
+    namespace YWing
+    {
+        public class HortonSalmElite : HortonSalm
+        {
+            public HortonSalmElite() : base()
+            {
+                PilotName = "Horton Salm (Elite)";
+
+                ImageUrl = "https://i.imgur.com/iOArz8A.png";
+
+                UpgradeBar.AddSlot(Upgrade.UpgradeType.Elite);
+
+                FromMod = typeof(EliteYWingPilotsMod);
+            }
+        }
+    }
+}

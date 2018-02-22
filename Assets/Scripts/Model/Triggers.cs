4ad5797006c7877f958a413f7033b103ca8e0ebb
Sandrem <oogoeroo@gmail.com>
2018-02-22 16:05:58 +0200

    Targeting Scrambler is added

--- Assets/Scripts/Model/Triggers.cs
+++ Assets/Scripts/Model/Triggers.cs
@@ -13,6 +13,7 @@ public enum TriggerTypes
     OnSetupPhaseStart,
     OnBeforePlaceForces,
     OnRoundStart,
+    OnPlanningSubPhaseStart,
     OnActionSubPhaseStart,
     OnActionDecisionSubPhaseEnd,
     OnActivationPhaseStart,

6643aaff4b58fb7dff1b90c04d6bbff6eb596ff7
Sandrem <oogoeroo@gmail.com>
2018-02-22 13:53:48 +0200

    Mod "My other ride is..." with Maarek Stele (Gunboat)

---
+++ Assets/Scripts/Model/Mods/ModsList/MyOtherRideIs.cs
@@ -0,0 +1,14 @@
+﻿namespace Mods
+{
+    namespace ModsList
+    {
+        public class MyOtherRideIsMod : Mod
+        {
+            public MyOtherRideIsMod()
+            {
+                Name = "My other ride is...";
+                Description = "Maarek Stele as Alpha-class Star Wing pilot";
+            }
+        }
+    }
+}

7eb75bdf1e71896039e9dbd0078e1053932d90e1
Sandrem <oogoeroo@gmail.com>
2018-02-27 19:20:33 +0200

    Updater is fixed

--- Assets/Scripts/MainMenu/Model/MainMenu.cs
+++ Assets/Scripts/MainMenu/Model/MainMenu.cs
@@ -30,7 +30,7 @@ public partial class MainMenu : MonoBehaviour {
         ModsManager.Initialize();
         Options.ReadOptions();
         Options.UpdateVolume();
-        StartCoroutine(CheckUpdates());
+        CheckUpdates();
     }
 
     public void StartBattle()
@@ -61,43 +61,15 @@ public partial class MainMenu : MonoBehaviour {
         Application.OpenURL(NewVersionUrl);
     }
 
-    private IEnumerator CheckUpdates()
+    private void CheckUpdates()
     {
-        WWW www = new WWW(Options.CheckVersionUrl);
-        yield return www;
-
-        string[] separator = new string[] { "\r\n" };
-        string[] wwwdata = www.text.Split(separator, System.StringSplitOptions.RemoveEmptyEntries);
-
-        if (wwwdata.Length > 0 && !wwwdata[0].Contains("DOCTYPE"))
+        string latestVersion = RemoteSettings.GetString("UpdateLatestVersion", Global.CurrentVersion);
+        if (latestVersion != Global.CurrentVersion)
         {
-            if (wwwdata.Length == 3)
-            {
-                Options.SetCheckVersionUrl(wwwdata[2]);
-                StartCoroutine(CheckUpdates());
-            }
-            else
-            {
-                if (wwwdata.Length == 2)
-                {
-                    if (wwwdata[0] != Global.CurrentVersion)
-                    {
-                        ShowNewVersionIsAvailable(wwwdata[0], wwwdata[1]);
-                    }
-                }
-            }
+            string updateLink = RemoteSettings.GetString("UpdateLink");
+            ShowNewVersionIsAvailable(latestVersion, updateLink);
         }
     }
-
-    /*public void ImportSquadList()
-    {
-        RosterBuilder.ImportSquadList();
-    }
-
-    public void ExportSquadList()
-    {
-        RosterBuilder.ExportSquadList(Players.PlayerNo.Player1);
-    }*/
 
     // 0.3.2 UI
 

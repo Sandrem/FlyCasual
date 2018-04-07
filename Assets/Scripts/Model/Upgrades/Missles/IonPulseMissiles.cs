6403179e4be73d9cb528e698c22a371f016521c1
Stephen Wangner <steve@wangner.net>
2018-04-06 21:13:51 -0400

    Update IonPulseMissiles.cs
    took out unnecessary calls to Game.Wait and extraneous comments

--- Assets/Scripts/Model/Upgrades/Missles/IonPulseMissiles.cs
+++ Assets/Scripts/Model/Upgrades/Missles/IonPulseMissiles.cs
@@ -42,31 +42,11 @@ namespace Abilities
 
 		public override void DeactivateAbility()
 		{
-			//If the below line is uncommented, it appears that the ability
-			//	gets deactivated before it fires.
 
-			//Symptom is that when the missiles hit, no Ion Tokens are assigned
-			//	and the actual damage rolled is applied, instead of a single damage.
-
-			//I have tested this with various messages displayed to the user at 
-			//	important points - when this happens, "RegisterIonPulseMissileHit()" is never called.
-
-			//HostShip.OnShotHitAsAttacker -= RegisterIonPulseMissleHit;
-
-			//Moved the deregister to after the single damage is applied, see below.
-			//Order of events is:
-			//	1. Cancel and Remove all hits from attack dice
-			//	2. Assign first Ion Token
-			//	3. Assign second Ion Token
-			//	4. Apply single damage
-			//	5. Deactivate ability with HostShip.OnShotHitAsAttacker -= RegisterIonPulseMissleHit;
 		}
 
 		private void RegisterIonPulseMissleHit()
 		{
-			//Note: the old syntax of "if (Combat.ChosenWeapon == this)" no longer works 
-			//	since "this" is now the ability, not the weapon.
-			//	"this.HostUpgrade" gets the weapon, which can then be compared to "Combat.ChosenWeapon"
 			if (Combat.ChosenWeapon == this.HostUpgrade) {
 				Triggers.RegisterTrigger (new Trigger () {
 					Name = "Ion Pulse Missile Hit",
@@ -90,8 +70,7 @@ namespace Abilities
 			Combat.Defender.Tokens.AssignToken(
 				new Tokens.IonToken(Combat.Defender),
 				delegate {
-					GameManagerScript Game = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
-					Game.Wait(0, AddSecondIonToken);
+					AddSecondIonToken();
 				}
 			);
 		}
@@ -101,8 +80,7 @@ namespace Abilities
 			Combat.Defender.Tokens.AssignToken(
 				new Tokens.IonToken(Combat.Defender),
 				delegate {
-					GameManagerScript Game = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
-					Game.Wait(0, DefenderSuffersDamage);
+					DefenderSuffersDamage();
 				}
 			);
 		}

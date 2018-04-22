de214daf472ee472567902cd680b1d6b0dcb3e1f
Sandrem <oogoeroo@gmail.com>
2018-04-22 16:52:49 +0300

    0.4.4 Hotfix 2

--- Assets/Scripts/Model/Global.cs
+++ Assets/Scripts/Model/Global.cs
@@ -11,8 +11,8 @@ public class Global : MonoBehaviour {
 
     public static string test = "I am accessible from every scene";
 
-    public static string CurrentVersion = "0.4.4 Hotfix 1";
-    public static int CurrentVersionInt = 100040401;
+    public static string CurrentVersion = "0.4.4 Hotfix 2";
+    public static int CurrentVersionInt = 100040402;
 
     void Awake()
     {

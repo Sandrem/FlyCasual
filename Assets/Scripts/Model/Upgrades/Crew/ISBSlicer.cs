70605c060503ac698678178d423ab69361c14cd2
Azrapse <azrapse@gmail.com>
2018-04-30 23:14:12 +0300

    ISB Slicer finished

--- Assets/Scripts/Model/Upgrades/Crew/ISBSlicer.cs
+++ Assets/Scripts/Model/Upgrades/Crew/ISBSlicer.cs
@@ -4,6 +4,9 @@ using Upgrade;
 using Abilities;
 using Ship;
 using Tokens;
+using SubPhases;
+using System.Linq;
+using System;
 
 namespace UpgradesList
 {
@@ -52,22 +55,54 @@ namespace Abilities
         {            
             if (action is JamAction && jamTarget != null)
             {
-                HostShip.OnActionDecisionSubphaseEnd += RegisterTrigger;
+                RegisterAbilityTrigger(TriggerTypes.OnActionIsPerformed, ISBSlicerEffect);
             }
         }
 
-        private void RegisterTrigger(Ship.GenericShip ship)
+        private void ISBSlicerEffect(object sender, EventArgs e)
         {
-            HostShip.OnActionDecisionSubphaseEnd -= RegisterTrigger;
-            RegisterAbilityTrigger(TriggerTypes.OnActionDecisionSubPhaseEnd, DoISBSlicerEffect);            
+            SelectTargetForAbility(
+                  AssignExtraJamToken,
+                  IsShipWithoutJamAtRangeOneOfTarget,
+                  GetAiJamPriority,
+                  HostShip.Owner.PlayerNo,
+                  true,
+                  null,
+                  HostUpgrade.Name,
+                  "Choose ship at range 1 of the target that isn't jammed.\nIt gets a Jam token.",
+                  HostUpgrade.ImageUrl
+              );
         }
 
-        private void DoISBSlicerEffect(object sender, System.EventArgs e)
+        private void AssignExtraJamToken()
         {
-            if (jamTarget.Tokens.CountTokensByType<JamToken>() == 0)
-            {
-                jamTarget.Tokens.AssignToken(new JamToken(jamTarget), Triggers.FinishTrigger);
-            }
+            TargetShip.Tokens.AssignToken(new JamToken(TargetShip), SelectShipSubPhase.FinishSelection);
+        }
+        
+        private bool IsShipWithoutJamAtRangeOneOfTarget(GenericShip ship)
+        {
+            var filters = new[] { TargetTypes.OtherFriendly, TargetTypes.This, TargetTypes.Enemy }
+                .ToList();
+            var match = FilterByTargetType(ship, filters) && Board.BoardManager.GetRangeOfShips(jamTarget, ship) <= 1;
+            return match;
+        }
+
+        private int GetAiJamPriority(GenericShip ship)
+        {
+            int result = 0;
+
+            result += HasTokenPriority(ship);
+            result += ship.Cost + ship.UpgradeBar.GetUpgradesOnlyFaceup().Sum(n => n.Cost);
+
+            return result;
+        }
+
+        private int HasTokenPriority(GenericShip ship)
+        {
+            if (ship.Tokens.HasToken(typeof(Tokens.FocusToken))) return 100;
+            if (ship.PrintedActions.Any(n => n.GetType() == typeof(ActionsList.EvadeAction)) || ship.Tokens.HasToken(typeof(Tokens.EvadeToken))) return 50;
+            if (ship.PrintedActions.Any(n => n.GetType() == typeof(ActionsList.TargetLockAction)) || ship.Tokens.HasToken(typeof(Tokens.BlueTargetLockToken), '*')) return 50;
+            return 0;
         }
     }
 }

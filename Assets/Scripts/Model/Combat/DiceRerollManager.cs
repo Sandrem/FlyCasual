3f4ab73f98b2a996d5524afeb6738790ef27218c
Sandrem <oogoeroo@gmail.com>
2018-05-02 07:55:25 +0300

    DiceRerollManager doesn't generate list of available dice modification second time

--- Assets/Scripts/Model/Combat/DiceRerollManager.cs
+++ Assets/Scripts/Model/Combat/DiceRerollManager.cs
@@ -188,22 +188,7 @@ public partial class DiceRerollManager
     {
         GameObject.Find("UI/CombatDiceResultsPanel").transform.Find("DiceModificationsPanel").gameObject.SetActive(isActive);
 
-        if (isActive)
-        {
-            Combat.ToggleConfirmDiceResultsButton(true);
-            if (!IsOpposite)
-            {
-                Combat.ShowDiceModificationButtons();
-            }
-            else
-            {
-                Combat.ShowOppositeDiceModificationButtons();
-            }
-        }
-        else
-        {
-            Combat.HideDiceModificationButtons();
-        }
+        if (!isActive) Combat.HideDiceModificationButtons();
     }
 
     private void ToggleDiceRerollsPanel(bool isActive)

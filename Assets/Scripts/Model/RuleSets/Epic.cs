0091ec4a5dfee752a77370894af205115ea17d24
Sandrem <oogoeroo@gmail.com>
2018-05-07 18:51:36 +0300

    Epic rules

---
+++ Assets/Scripts/Model/RuleSets/Epic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RuleSets
+{
+    public class Epic : FirstEdition
+    {
+        public override string Name { get { return "Epic"; } }
+
+        public override int MaxPoints { get { return 300; } }}
+    }
+}

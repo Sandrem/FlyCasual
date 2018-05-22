450ff407220bab3a80cd649d193d33b45571b862
Sandrem <oogoeroo@gmail.com>
2018-05-22 23:38:15 +0300

    Templates for SE upgrades

---
+++ Assets/Scripts/Model/Upgrades/Elite/Outmaneuver.cs
@@ -0,0 +1,20 @@
+﻿using Ship;
+using UnityEngine;
+using Upgrade;
+using Abilities;
+using RuleSets;
+
+namespace UpgradesList
+{
+    public class Outmaneuver : GenericUpgrade
+    {
+        public Outmaneuver() : base()
+        {
+            Types.Add(UpgradeType.Elite);
+            Name = "Outmaneuver";
+            Cost = 10;
+
+            UpgradeRuleType = typeof(SecondEdition);
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Upgrades/Elite/Selfless.cs
@@ -0,0 +1,23 @@
+﻿using Upgrade;
+using UnityEngine;
+using Ship;
+using System.Collections.Generic;
+using SubPhases;
+using System;
+using Abilities;
+using RuleSets;
+
+namespace UpgradesList
+{
+    public class Selfless : GenericUpgrade
+    {
+        public Selfless() : base()
+        {
+            Types.Add(UpgradeType.Elite);
+            Name = "Selfless";
+            Cost = 1;
+
+            UpgradeRuleType = typeof(SecondEdition);
+        }
+    }
+}

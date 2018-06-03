fdc29268fd6bebf15f3d336f8a587739462c18e8
Joon Yee Chuah <jchuah@tacc.utexas.edu>
2018-06-03 13:32:21 -0500

    Implemented Sensor Cluster Tech

---
+++ Assets/Scripts/Model/Upgrades/Tech/SensorCluster.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Upgrade;
+using Abilities;
+using Ship;
+using Tokens;
+
+namespace UpgradesList
+{
+
+    public class SensorCluster : GenericUpgrade
+    {
+        public SensorCluster() : base()
+        {
+            Types.Add(UpgradeType.Tech);
+            Name = "Sensor Cluster";
+            Cost = 2;
+
+            UpgradeAbilities.Add(new SensorClusterAbility());
+        }
+    }
+
+}
+
+namespace Abilities
+{
+    public class SensorClusterAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.AfterGenerateAvailableActionEffectsList += SensorClusterActionEffect;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.AfterGenerateAvailableActionEffectsList -= SensorClusterActionEffect;
+        }
+
+        private void SensorClusterActionEffect(GenericShip host)
+        {
+            ActionsList.GenericAction newAction = new ActionsList.SensorClusterActionEffect()
+            {
+                ImageUrl = HostUpgrade.ImageUrl,
+                Host = host
+            };
+            host.AddAvailableActionEffect(newAction);
+        }
+    }
+}
+
+namespace ActionsList
+{
+    public class SensorClusterActionEffect : GenericAction
+    {
+
+        public SensorClusterActionEffect()
+        {
+            Name = EffectName = "Sensor Cluster";
+            DiceModificationTiming = DiceModificationTimingType.Normal;
+        }
+
+        public override int GetActionEffectPriority()
+        {
+            int result = 0;
+
+            result = 100;
+
+            return result;
+        }
+
+        public override bool IsActionEffectAvailable()
+        {
+            bool result = false;
+
+            if (Combat.AttackStep == CombatStep.Defence &&
+                Combat.DiceRollDefence.Blanks > 0 &&
+                Host.Tokens.HasToken(typeof(FocusToken)))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public override void ActionEffect(System.Action callBack)
+        {
+            Host.Tokens.RemoveToken(typeof(FocusToken), delegate
+            {
+                Combat.DiceRollDefence.ChangeOne(DieSide.Blank, DieSide.Success, false);
+                callBack();
+            });
+
+        }
+
+    }
+
+}

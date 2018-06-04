6b7c3dfa7a432cb840bf3e643e2a2f7602a924ef
Joon Yee Chuah <jchuah@tacc.utexas.edu>
2018-06-04 18:56:33 -0500

    Snap Shot activates

--- Assets/Scripts/Model/Upgrades/Elite/SnapShot.cs
+++ Assets/Scripts/Model/Upgrades/Elite/SnapShot.cs
@@ -5,15 +5,22 @@ using System.Collections.Generic;
 using Tokens;
 using Abilities;
 using BoardTools;
+using SubPhases;
+using UpgradesList;
+using ActionsList;
+using System;
 
 namespace UpgradesList
 {
-    public class SnapShot : GenericUpgrade
+    public class SnapShot : GenericSecondaryWeapon
     {
         public SnapShot() : base()
         {
-            Types.Add(UpgradeType.Elite);
+            Types.Add(UpgradeType.Cannon);
             Name = "Snap Shot";
+            MinRange = 1;
+            MaxRange = 1;
+            AttackValue = 2;
             Cost = 2;
 
             UpgradeAbilities.Add(new SnapShotAbility());
@@ -26,63 +33,123 @@ namespace Abilities
 {
     public class SnapShotAbility : GenericAbility
     {
-        private GenericShip snapShotTarget;
+        private GenericShip snapShotTarget = null;
 
         public override void ActivateAbility()
         {
-            // mix Kanan Jarrus, TIE Defender x7, Quickdraw and Fenn Rau
+            HostShip.OnAttackStartAsAttacker += AddSnapShotRestriction;
             GenericShip.OnMovementFinishGlobal += CheckSnapShotAbility;
             Phases.OnActivationPhaseEnd_Triggers += CleanUpSnapShotAbility;
         }
 
         public override void DeactivateAbility()
         {
+            HostShip.OnAttackStartAsAttacker += AddSnapShotRestriction;
             GenericShip.OnMovementFinishGlobal -= CheckSnapShotAbility;
             Phases.OnActivationPhaseEnd_Triggers -= CleanUpSnapShotAbility;
         }
 
+        private void AddSnapShotRestriction() {
+            GenericShip ship;
+            if (Combat.Attacker.ShipId == HostShip.ShipId && snapShotTarget != null) {
+                ship = Combat.Attacker;
+
+                ship.OnTryAddAvailableActionEffect += UseSnapShotRestriction;
+                ship.OnAttackFinish += RemoveSnapShotRestriction;
+            }
+        }
+
+        private void UseSnapShotRestriction(GenericShip ship, ActionsList.GenericAction action, ref bool canBeUsed)
+        {
+            Messages.ShowErrorToHuman("SnapShot: Unable to modify dice.");
+            canBeUsed = false;
+        }
+
+        private void RemoveSnapShotRestriction(GenericShip ship)
+        {
+            ship.OnTryAddAvailableActionEffect -= UseSnapShotRestriction;
+            ship.OnAttackFinish -= RemoveSnapShotRestriction;
+        }
+
+        private void CleanUpSnapShotAbility()
+        {
+            ClearIsAbilityUsedFlag();
+            HostShip.IsAttackPerformed = false;
+            HostShip.IsCannotAttackSecondTime = false;
+        }
+
+        public void AfterSnapShotAttackSubPhase()
+        {
+            HostShip.IsCannotAttackSecondTime = true;
+            snapShotTarget = null;
+            // Check this
+            HostShip.OnAttackFinishAsAttacker -= SetIsAbilityIsUsed;
+            Phases.FinishSubPhase(Phases.CurrentSubPhase.GetType());
+            Triggers.FinishTrigger();
+        }
+
+
+        //Based on Dengar counterattack
         private void CheckSnapShotAbility(GenericShip ship)
         {
-            if (ship.Owner != HostShip.Owner && !IsAbilityUsed)
+            if (!IsAbilityUsed && ship.Owner.PlayerNo != HostShip.Owner.PlayerNo)
             {
-                ShotInfo shotInfo = new ShotInfo(HostShip, ship, HostShip.PrimaryWeapon);
-                if (shotInfo.InArc && shotInfo.Range <= 1)
-                {
-                    snapShotTarget = ship;
-                    AskToUseAbility(NeverUseByDefault, PerformSnapShotAbility);
-                }
+                snapShotTarget = ship;
+                RegisterAbilityTrigger(TriggerTypes.OnShipMovementFinish, AskSnapShotAbility);
+            }
+        }
 
-            } else {
+        private void AskSnapShotAbility(object sender, System.EventArgs e)
+        {
+            ShotInfo shotInfo = new ShotInfo(HostShip, snapShotTarget, HostShip.PrimaryWeapon);
+
+            if (shotInfo.InArc && shotInfo.Range <= 1)
+            {
+                AskToUseAbility(AlwaysUseByDefault, PerformSnapShot);
+            }
+            else
+            {
                 Triggers.FinishTrigger();
             }
         }
 
-        private void PerformSnapShotAbility(object sender, System.EventArgs e)
+        private bool SnapShotAttackFilter(GenericShip defender, IShipWeapon weapon)
+        {
+            bool result = true;
+            if (defender != snapShotTarget || !(weapon is UpgradesList.SnapShot))
+            {
+                Messages.ShowErrorToHuman(
+                    string.Format("Snap Shot target must be {0}, using Snap Shot weapon", snapShotTarget.PilotName));
+                result = false;
+            }
+
+            return result;
+        }
+
+        public void SnapShotSetUsed()
         {
             IsAbilityUsed = true;
-            Combat.StartAdditionalAttack(
-                HostShip, 
-                AfterSnapShotAttackSubPhase, 
-                SnapShotAttackFilter, 
-                HostUpgrade.Name,
-                "You may perform an attack",
-                HostUpgrade.ImageUrl
-            );
         }
 
-        private bool SnapShotAttackFilter(GenericShip defender, IShipWeapon weapon) {
-            // Stuck here. Snapshot weapon?
-            return defender == snapShotTarget && weapon == HostShip.PrimaryWeapon;
+        private void PerformSnapShot(object sender, EventArgs e)
+        {
+            if (!HostShip.IsCannotAttackSecondTime)
+            {
+                HostShip.OnAttackFinishAsAttacker += SetIsAbilityIsUsed;
+                Combat.StartAdditionalAttack(
+                    HostShip,
+                    AfterSnapShotAttackSubPhase,
+                    SnapShotAttackFilter,
+                    HostShip.PilotName,
+                    "You may perform an additional attack against " + snapShotTarget.PilotName + ".",
+                    HostShip.ImageUrl
+                );
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
         }
-
-        private void CleanUpSnapShotAbility() {
-            ClearIsAbilityUsedFlag();
-            HostShip.IsCannotAttackSecondTime = false;
-        }
-
-        private void AfterSnapShotAttackSubPhase() {
-            HostShip.IsCannotAttackSecondTime = true;
-            Triggers.FinishTrigger();
-        }
+       
     }
 }

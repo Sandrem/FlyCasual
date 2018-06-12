b0f55871cf88e1154abe31b8b95b1361546d77b5
Sandrem <oogoeroo@gmail.com>
2018-06-12 21:42:23 +0300

    FE failed BarrelRoll/Boost/TL is fixed

--- Assets/Scripts/Model/RuleSets/FirstEdition.cs
+++ Assets/Scripts/Model/RuleSets/FirstEdition.cs
@@ -83,6 +83,8 @@ namespace RuleSets
         public override void ActionIsFailed(GenericShip ship, Type actionType)
         {
             ship.RemoveAlreadyExecutedAction(actionType);
+            SubPhases.DecisionSubPhase decisionSubphase = Phases.CurrentSubPhase.PreviousSubPhase as SubPhases.DecisionSubPhase;
+            if (decisionSubphase != null) decisionSubphase.DecisionWasPreparedAndShown = false;
             Phases.CurrentSubPhase.PreviousSubPhase.Resume();
         }
 

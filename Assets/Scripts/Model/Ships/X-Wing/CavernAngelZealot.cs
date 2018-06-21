e99c140da6bbd74b29ab2ab727928e8bc686f8fc
Sandrem <oogoeroo@gmail.com>
2018-06-21 05:40:42 +0300

    Cavern Angel Zealot is added

---
+++ Assets/Scripts/Model/Ships/X-Wing/CavernAngelZealot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Upgrade;
+
+namespace Ship
+{
+    namespace XWing
+    {
+        public class CavernAngelZealot : XWing
+        {
+            public CavernAngelZealot() : base()
+            {
+                PilotName = "Cavern Angel Zealot";
+                PilotSkill = 1;
+                Cost = 22;
+
+                SkinName = "Partisan";
+
+                PrintedUpgradeIcons.Add(UpgradeType.Elite);
+            }
+        }
+    }
+}

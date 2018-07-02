61688e79a07c5c776cf5af163bc71e3abb15320a
Sandrem <oogoeroo@gmail.com>
2018-07-02 21:42:54 +0300

    R4 Astromech (testing is required)

--- Assets/Scripts/Model/Upgrades/Astromech/R4Astromech.cs
+++ Assets/Scripts/Model/Upgrades/Astromech/R4Astromech.cs
@@ -1,26 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Upgrade;
-using Abilities;
 using RuleSets;
 using Ship;
-using System;
+using Movement;
 
 namespace UpgradesList
 {
 
     public class R4Astromech : GenericUpgrade
     {
-
         public R4Astromech() : base()
         {
             Types.Add(UpgradeType.Astromech);
             Name = "R4 Astromech";
             Cost = 10;
 
+            ImageUrl = "https://i.imgur.com/ziyiDMr.png";
+
             UpgradeRuleType = typeof(SecondEdition);
+
+            UpgradeAbilities.Add(new Abilities.SecondEdition.R4AstromechAbilitySE());
         }
     }
 
+}
+
+namespace Abilities
+{
+    namespace SecondEdition
+    {
+        public class R4AstromechAbilitySE : GenericAbility
+        {
+            public override void ActivateAbility()
+            {
+                HostShip.AfterGetManeuverColorDecreaseComplexity += ApplyAbility;
+            }
+
+            public override void DeactivateAbility()
+            {
+                HostShip.AfterGetManeuverColorDecreaseComplexity -= ApplyAbility;
+            }
+
+            private void ApplyAbility(GenericShip ship, ref MovementStruct movement)
+            {
+                if (movement.Speed == ManeuverSpeed.Speed1 || movement.Speed == ManeuverSpeed.Speed2)
+                {
+                    if (movement.Bearing == ManeuverBearing.Straight || movement.Bearing == ManeuverBearing.Bank || movement.Bearing == ManeuverBearing.Turn)
+                    {
+                        movement.ColorComplexity = ReduceComplexity(movement.ColorComplexity);
+                    }
+                }
+            }
+
+            private MovementComplexity ReduceComplexity(MovementComplexity complexity)
+            {
+                switch (complexity)
+                {
+                    case MovementComplexity.Normal:
+                        complexity = MovementComplexity.Easy;
+                        break;
+                    case MovementComplexity.Complex:
+                        complexity = MovementComplexity.Normal;
+                        break;
+                }
+
+                return complexity;
+            }
+        }
+    }
 }

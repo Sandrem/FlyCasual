33a4cae46ca4d877e396b07dffd5b9709cef364f
jychuah <jychuah@gmail.com>
2018-07-08 22:49:19 -0500

    Shara Bey implemented for attack payment and target lock rerolls

---
+++ Assets/Scripts/Model/Ships/ARC-170/SharaBey.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ship;
+using System.Linq;
+using Tokens;
+using BoardTools;
+
+namespace Ship
+{
+    namespace ARC170
+    {
+        public class SharaBey : ARC170
+        {
+            public SharaBey() : base()
+            {
+                PilotName = "Shara Bey";
+                PilotSkill = 6;
+                Cost = 28;
+
+                IsUnique = true;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+
+                PilotAbilities.Add(new Abilities.SharaBeyPilotAbility());
+            }
+        }
+    }
+}
+
+namespace Abilities
+{
+    public class SharaBeyPilotAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnGameStart += AddSharaBeyPilotAbility;
+
+        }
+
+        public void AddSharaBeyPilotAbility()
+        {
+            foreach (KeyValuePair<string, GenericShip> entry in HostShip.Owner.Ships)
+            {
+                if (entry.Value != HostShip)
+                {
+                    entry.Value.OnGenerateAvailableAttackPaymentList += AddSharaBeyPilotPayment;
+                    entry.Value.AfterGenerateAvailableActionEffectsList += AddSharaBeyActionEffect;
+                }
+            }
+        }
+
+        public override void DeactivateAbility()
+        {
+            foreach (KeyValuePair<string, GenericShip> entry in HostShip.Owner.Ships)
+            {
+                if (entry.Value != HostShip)
+                {
+                    entry.Value.OnGenerateAvailableAttackPaymentList -= AddSharaBeyPilotPayment;
+                }
+            }
+        }
+
+        private void AddSharaBeyPilotPayment(List<GenericToken> waysToPay)
+        {
+            //char targetLockLetter = Actions.GetTargetLocksLetterPair(HostShip, Combat.Defender);
+            if (!HostShip.Tokens.HasToken(typeof(BlueTargetLockToken), '*')) return;
+            ShotInfo shotInfo = new ShotInfo(Selection.ActiveShip, HostShip, HostShip.PrimaryWeapon);
+            if (shotInfo.Range <= 2)
+            {
+                GenericToken sharaToken = HostShip.Tokens.GetToken(typeof(BlueTargetLockToken), '*');
+                waysToPay.Add(sharaToken);
+            }
+        }
+
+        private void AddSharaBeyActionEffect(Ship.GenericShip ship)
+        {
+            ship.AddAvailableActionEffect(new ActionsList.SharaBeyAction()
+            {
+                Host = HostShip
+            });
+        }
+    }
+}
+
+namespace ActionsList
+{
+    public class SharaBeyAction : GenericAction
+    {
+        private char sharaLockLetter;
+
+        public SharaBeyAction()
+        {
+            Name = EffectName = "Shara Bey";
+        }
+
+        public override bool IsActionEffectAvailable()
+        {
+            ShotInfo shotInfo = new ShotInfo(Host, Combat.Attacker, Host.PrimaryWeapon);
+            if (shotInfo.Range > 2) return false;
+            sharaLockLetter = Actions.GetTargetLocksLetterPair(Host, Combat.Defender);
+            if (sharaLockLetter == ' ') return false;
+            GenericToken sharaToken = Host.Tokens.GetToken(typeof(BlueTargetLockToken), sharaLockLetter);
+            if (sharaToken == null) return false;
+            return Combat.AttackStep == CombatStep.Attack;
+        }
+
+        public override int GetActionEffectPriority()
+        {
+            int result = 0;
+
+            if (Combat.AttackStep == CombatStep.Attack)
+            {
+                if (Combat.DiceRollAttack.Blanks > 0)
+                {
+                    result = 90;
+                }
+                else if (Combat.DiceRollAttack.Focuses > 0 && Combat.Attacker.GetAvailableActionEffectsList().Count(n => n.IsTurnsAllFocusIntoSuccess) == 0)
+                {
+                    result = 90;
+                }
+                else if (Combat.DiceRollAttack.Focuses > 0)
+                {
+                    result = 30;
+                }
+            }
+
+            return result;
+        }
+
+        public override void ActionEffect(System.Action callBack)
+        {
+            
+            Host.Tokens.SpendToken(typeof(BlueTargetLockToken), 
+                                   delegate {
+                                        DiceRerollManager diceRerollManager = new DiceRerollManager
+                                        {
+                                            CallBack = callBack
+                                        };
+                                        diceRerollManager.Start();
+                                   }, sharaLockLetter);
+
+        }
+
+    }
+}

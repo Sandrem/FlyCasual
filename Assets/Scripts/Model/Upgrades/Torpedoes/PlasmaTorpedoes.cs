3b25f99a6a8d4129183d6f73d3f17d4efa4a87fc
tetrarchangel <35899916+tetrarchangel@users.noreply.github.com>
2018-07-14 10:52:34 +0100

    Plasma Torpedoes
    Plasma Torpedoes which mostly work but cause lag and occasional crashes

--- Assets/Scripts/Model/Upgrades/Torpedoes/PlasmaTorpedoes.cs
+++ Assets/Scripts/Model/Upgrades/Torpedoes/PlasmaTorpedoes.cs
@@ -3,6 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Upgrade;
+using Abilities;
+using Ship;
+using Conditions;
+using System.Linq;
+using SubPhases;
+using ActionsList;
+using UpgradesList;
 
 namespace UpgradesList
 {
@@ -10,7 +17,7 @@ namespace UpgradesList
     {
         public PlasmaTorpedoes() : base()
         {
-            IsHidden = true;
+            
 
             Types.Add(UpgradeType.Torpedo);
 
@@ -25,7 +32,71 @@ namespace UpgradesList
             
             SpendsTargetLockOnTargetToShoot = true;
             IsDiscardedForShot = true;
+
+            UpgradeAbilities.Add(new PlasmaTorpedoAbility());
         }
     }
 }
 
+namespace Abilities
+{
+    public class PlasmaTorpedoAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnShotHitAsAttacker += PlanPlasmaTorpedoes;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnCombatDeactivation += DeactivateAbilityPlanned;
+        }
+
+        private void DeactivateAbilityPlanned(GenericShip ship)
+        {
+            HostShip.OnCombatDeactivation -= DeactivateAbilityPlanned;
+
+            HostShip.OnCombatDeactivation -= PlanShieldRemove;
+
+            HostShip.OnShotHitAsAttacker -= PlanPlasmaTorpedoes;
+        }
+
+        private void PlanPlasmaTorpedoes()
+        {
+            if (Combat.ChosenWeapon is PlasmaTorpedoes)
+            {
+                HostShip.OnAttackFinishAsAttacker += PlanShieldRemove;
+
+                HostShip.OnShotHitAsAttacker -= PlanPlasmaTorpedoes;
+            }
+        }
+
+        private void PlanShieldRemove(GenericShip hostShip)
+        {
+            if (Combat.Defender.Shields != 0)
+            { ShieldRemove(); }
+        }
+    
+        private void ShieldRemove()
+        {
+            {
+                Combat.Defender.AssignedDamageDiceroll.AddDice(DieSide.Success);
+                Messages.ShowInfoToHuman(string.Format("{0} had a Shield removed by Plasma Torpedo", Combat.Defender.PilotName));
+                Triggers.RegisterTrigger(
+                    new Trigger()
+                    {
+                        Name = "Damage from Plasma Torpodoes",
+                        TriggerType = TriggerTypes.OnDamageIsDealt,
+                        TriggerOwner = Combat.Defender.Owner.PlayerNo,
+                        EventHandler = Combat.Defender.SufferDamage,
+                        EventArgs = new DamageSourceEventArgs()
+                        {
+                            Source = Combat.Attacker,
+                            DamageType = DamageTypes.CardAbility
+                        },
+                    });
+                Triggers.ResolveTriggers(TriggerTypes.OnDamageIsDealt);
+            }
+        }
+    }
+}

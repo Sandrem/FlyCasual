b8446a6cf6548d6ee07578aee2f29a2068d94ba1
Sandrem <oogoeroo@gmail.com>
2018-08-01 01:53:32 +0300

    Ved Foslo is added

---
+++ Assets/Scripts/Model/Ships/TIE Advanced/VedFoslo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ship;
+using Movement;
+using GameModes;
+using RuleSets;
+
+namespace Ship
+{
+    namespace TIEAdvanced
+    {
+        public class VedFoslo : TIEAdvanced, ISecondEditionPilot
+        {
+            public VedFoslo() : base()
+            {
+                PilotName = "Ved Foslo";
+                PilotSkill = 4;
+                Cost = 47;
+
+                IsUnique = true;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+
+                PilotRuleType = typeof(SecondEdition);
+
+                PilotAbilities.Add(new Abilities.JunoEclipseAbility());
+            }
+
+            public void AdaptPilotToSecondEdition()
+            {
+                //Not required
+            }
+        }
+    }
+}

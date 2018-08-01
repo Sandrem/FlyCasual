883ff31e10480ab00aa8c864c4f4bf0318b43763
Matthew Blanchard <matthewblanc@msn.com>
2018-08-01 14:01:36 -0400

    Added Torkil Mux SE.

--- Assets/Scripts/Model/Ships/HWK-290/RoarkGarnet.cs
+++ Assets/Scripts/Model/Ships/HWK-290/RoarkGarnet.cs
@@ -58,7 +58,7 @@ namespace Abilities
 
         protected virtual string GenerateAbilityMessage()
         {
-            return "Choose other friendly ship.\nUntil the end of the phase, treat that ship's pilot skill value as \"12\".";
+            return "Choose another friendly ship in arc.\nUntil the end of the phase, treat that ship's pilot skill value as \"12\".";
         }
 
         private void Ability(object sender, EventArgs e)

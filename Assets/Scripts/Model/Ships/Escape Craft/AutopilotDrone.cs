ef8f17ed885ef36ea2d5de3055e5569d3bbba203
Sandrem <oogoeroo@gmail.com>
2018-08-03 23:32:19 +0300

    Scum Falcon

---
+++ Assets/Scripts/Model/Ships/Escape Craft/AutopilotDrone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ship;
+using RuleSets;
+
+namespace Ship
+{
+    namespace EscapeCraft
+    {
+        public class AutopilotDrone : EscapeCraft, ISecondEditionPilot
+        {
+            public AutopilotDrone() : base()
+            {
+                PilotName = "Autopilot Drone";
+                PilotSkill = 1;
+                Cost = 12;
+
+                IsUnique = true;
+
+                // TODO
+                // PilotAbilities.Add(new Abilities.ZebOrreliosPilotAbility());
+            }
+
+            public void AdaptPilotToSecondEdition()
+            {
+                // Not required
+            }
+        }
+    }
+}

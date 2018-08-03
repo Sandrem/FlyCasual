cde537ca611337ad3fd53a08378656c0e38bc74f
Matthew Blanchard <matthewblanc@msn.com>
2018-08-02 23:06:37 -0400

    Added Laetin A'shera SE.

---
+++ Assets/Scripts/Model/Ships/M3-A Interceptor/LaetinAshera.cs
@@ -0,0 +1,59 @@
+﻿using Abilities.SecondEdition;
+using RuleSets;
+using System.Collections;
+using System.Collections.Generic;
+using Tokens;
+
+namespace Ship
+{
+    namespace M3AScyk
+    {
+        public class LaetinAshera : M3AScyk, ISecondEditionPilot
+        {
+            public LaetinAshera() : base()
+            {
+                PilotName = "Laetin A'shera";
+                PilotSkill = 3;
+                Cost = 35;
+
+                IsUnique = true;
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+
+                PilotRuleType = typeof(SecondEdition);
+
+                PilotAbilities.Add(new LaetinAsheraSE());
+            }
+
+            public void AdaptPilotToSecondEdition()
+            {
+                // Not required
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class LaetinAsheraSE : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnAttackMissedAsAttacker += RegisterTrigger;
+            HostShip.OnAttackMissedAsDefender += RegisterTrigger;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnAttackMissedAsAttacker -= RegisterTrigger;
+            HostShip.OnAttackMissedAsDefender -= RegisterTrigger;
+        }
+
+        private void RegisterTrigger()
+        {
+            RegisterAbilityTrigger(TriggerTypes.OnAttackMissed, delegate 
+            {
+                HostShip.Tokens.AssignToken(typeof(EvadeToken), Triggers.FinishTrigger);
+            });
+        }
+    }
+}

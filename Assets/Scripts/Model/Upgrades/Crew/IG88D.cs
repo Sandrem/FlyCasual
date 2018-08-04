53ad03aefd7c425b0336f0612b819f2ad22f250e
jtg15 <john.geddes@aggiemail.usu.edu>
2018-08-04 17:03:39 -0600

    SE IG-88D Crew (#1118)
    * Fix SE Aggressor Dial, Add SE IG-88A & IG-2000
    
    * SE IG-88D Crew Card

--- Assets/Scripts/Model/Upgrades/Crew/IG88D.cs
+++ Assets/Scripts/Model/Upgrades/Crew/IG88D.cs
@@ -1,11 +1,14 @@
 ﻿using Abilities;
+using Abilities.SecondEdition;
+using ActionsList;
+using RuleSets;
 using Ship;
 using UnityEngine;
 using Upgrade;
 
 namespace UpgradesList
 {
-    public class IG88D : GenericUpgrade
+    public class IG88D : GenericUpgrade, ISecondEditionUpgrade
     {
         public IG88D() : base()
         {
@@ -20,9 +23,52 @@ namespace UpgradesList
             UpgradeAbilities.Add(new Ig2000Ability());
         }
 
+        public void AdaptUpgradeToSecondEdition()
+        {
+            Cost = 4;
+            UpgradeAbilities.Add(new Ig88DCrewAbility());
+        }
+
         public override bool IsAllowedForShip(GenericShip ship)
         {
             return ship.faction == Faction.Scum;
+        }
+    }
+}
+
+namespace Abilities
+{
+    public class Ig88DCrewAbility : GenericAbility
+    {
+        bool addedAction = false;
+        bool addedAbility = false;
+
+        public override void ActivateAbility() { }
+        public override void ActivateAbilityForSquadBuilder()
+        {
+            if (!HostShip.ActionBar.HasAction(typeof(CalculateAction)))
+            {
+                HostShip.ActionBar.AddGrantedAction(new CalculateAction(), HostUpgrade);
+                addedAction = true;
+            }
+            if (HostShip.PilotAbilities.Find(ability => ability is AdvancedDroidBrain) == null)
+            {
+                HostShip.PilotAbilities.Add(new AdvancedDroidBrain());
+                addedAbility = true;
+            }
+        }
+
+        public override void DeactivateAbility() { }
+        public override void DeactivateAbilityForSquadBuilder()
+        {
+            if (addedAction)
+            {
+                HostShip.ActionBar.RemoveGrantedAction(typeof(CalculateAction), HostUpgrade);
+            }
+            if (addedAbility)
+            {
+                HostShip.PilotAbilities.RemoveAll(ability => ability is AdvancedDroidBrain);
+            }
         }
     }
 }

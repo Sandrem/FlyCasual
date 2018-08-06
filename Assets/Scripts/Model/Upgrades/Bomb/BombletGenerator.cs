568252ad044e313647c01401517cbc92c52b3c14
vladamex <vladamex@hotmail.com>
2018-08-07 01:01:03 +0200

    WIP Bomblet Generator

--- Assets/Scripts/Model/Upgrades/Bomb/BombletGenerator.cs
+++ Assets/Scripts/Model/Upgrades/Bomb/BombletGenerator.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using Upgrade;
 using Ship;
-using System.Linq;
+using RuleSets;
+using SubPhases;
 
 namespace UpgradesList
 {
 
-    public class BombletGenerator : GenericTimedBomb
+    public class BombletGenerator : GenericTimedBomb, ISecondEditionUpgrade
     {
 
         public BombletGenerator() : base()
@@ -21,6 +22,17 @@ namespace UpgradesList
             isUnique = true;
 
             bombPrefabPath = "Prefabs/Bombs/Bomblet";
+        }
+
+        public void AdaptUpgradeToSecondEdition()
+        {
+            IsDiscardedAfterDropped = false;
+            UsesCharges = true;
+
+            UpgradeAbilities.Add(new Abilities.SecondEdition.BombletGeneratorAbilitySE());
+
+            MaxCharges = 2;
+            Cost = 5;
         }
 
         public override void ExplosionEffect(GenericShip ship, Action callBack)
@@ -97,5 +109,68 @@ namespace SubPhases
             CallBack();
         }
     }
+}
 
+namespace Abilities.SecondEdition
+{
+    public class BombletGeneratorAbilitySE : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+                Phases.Events.OnActivationPhaseStart += RegisterAskToRecoverBombletGeneratorCharges;
+        }
+
+        public override void DeactivateAbility()
+        {
+            Phases.Events.OnActivationPhaseStart -= RegisterAskToRecoverBombletGeneratorCharges;
+        }
+
+        private void RegisterAskToRecoverBombletGeneratorCharges()
+        {
+            RegisterAbilityTrigger(TriggerTypes.OnActivationPhaseStart, StartQuestionSubphase);
+        }
+
+        protected void StartQuestionSubphase(object sender, System.EventArgs e)
+        {
+            if (HostUpgrade.Charges < HostUpgrade.MaxCharges && HostShip.Shields > 0)
+            {
+                BombletGeneratorDecisionSubPhase reloadBombletGeneratorSubPhase = (BombletGeneratorDecisionSubPhase) Phases.StartTemporarySubPhaseNew(
+                    Name,
+                    typeof(BombletGeneratorDecisionSubPhase),
+                    Triggers.FinishTrigger
+                );
+
+                reloadBombletGeneratorSubPhase.InfoText = "Use " + Name + "?";
+
+                reloadBombletGeneratorSubPhase.AddDecision("Spend 1 shield to recover two charges", RegisterRecoverTwoCharges);
+                reloadBombletGeneratorSubPhase.AddTooltip("Spend 1 shield to recover two charges", HostShip.ImageUrl);
+
+                reloadBombletGeneratorSubPhase.AddDecision("No", delegate { DecisionSubPhase.ConfirmDecision(); });
+
+                reloadBombletGeneratorSubPhase.DefaultDecisionName = GetDefaultDecision();
+
+                reloadBombletGeneratorSubPhase.ShowSkipButton = true;
+
+                reloadBombletGeneratorSubPhase.Start();
+            }
+        }
+
+        protected void RegisterRecoverTwoCharges(object sender, System.EventArgs e)
+        {
+            IsAbilityUsed = true;
+            HostUpgrade.SetChargesToMax();
+            HostShip.LoseShield();
+
+            DecisionSubPhase.ConfirmDecision();
+        }
+
+        protected string GetDefaultDecision()
+        {
+            string result = "No";
+
+            return result;
+        }
+
+        protected class BombletGeneratorDecisionSubPhase : DecisionSubPhase { }
+    }
 }

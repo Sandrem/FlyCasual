1fae94322b22cfac69b65bd0b013156196583f02
Matthew Blanchard <matthewblanc@msn.com>
2018-08-12 11:16:00 -0400

    Added an EPT to Valen Rudor.

--- Assets/Scripts/Model/Ships/TIE Fighter/ValenRudor.cs
+++ Assets/Scripts/Model/Ships/TIE Fighter/ValenRudor.cs
@@ -16,6 +16,8 @@ namespace Ship
 
                 IsUnique = true;
 
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+
                 PilotRuleType = typeof(SecondEdition);
                 PilotAbilities.Add(new Abilities.SecondEdition.ValenRudorAbilitySE());
             }

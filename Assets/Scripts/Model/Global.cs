0633d910892fbacd90623e8f2384c947bd7dce6a
Sandrem <oogoeroo@gmail.com>
2018-08-31 19:22:18 +0300

    0.6.1

--- Assets/Scripts/Model/Global.cs
+++ Assets/Scripts/Model/Global.cs
@@ -10,8 +10,8 @@ public class Global : MonoBehaviour {
 
     private static bool isAlreadyInitialized;
 
-    public static string CurrentVersion = "0.6.0";
-    public static int CurrentVersionInt = 100060000;
+    public static string CurrentVersion = "0.6.1";
+    public static int CurrentVersionInt = 100060100;
 
     void Awake()
     {

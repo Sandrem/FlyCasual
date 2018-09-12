7af40a968ae9684ac3722dfdd63f0734268e3929
rune-b <rune@krokodille.com>
2018-09-12 21:20:45 +0200

    SE Redline. Fixed typos.

--- Assets/Scripts/Model/Ships/TIE Punisher/Redline.cs
+++ Assets/Scripts/Model/Ships/TIE Punisher/Redline.cs
@@ -1,10 +1,11 @@
-﻿using RuleSets;
+﻿using ActionsList;
+using RuleSets;
 using Ship;
 using SubPhases;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Tokens;
-using UnityEngine;
 
 namespace Ship
 {
@@ -27,6 +28,9 @@ namespace Ship
             {
                 PilotSkill = 5;
                 Cost = 44;
+
+                PilotAbilities.RemoveAll(a => a is Abilities.RedlineAbility);
+                PilotAbilities.Add(new Abilities.SecondEdition.RedlineAbility());
             }
         }
     }
@@ -34,6 +38,7 @@ namespace Ship
 
 namespace Abilities
 {
+    //You may maintain 2 target locks on the same ship. When you acquire a target lock, you may acquire a second lock on that same ship.
     public class RedlineAbility : GenericAbility
     {
         public override void ActivateAbility()
@@ -77,6 +82,39 @@ namespace Abilities
         {
             IsAbilityUsed = false;
             Triggers.FinishTrigger();
+        }
+    }
+
+    namespace SecondEdition
+    {
+        //You can maintain 2 locks. After you perform an action, you may acquire a lock.
+        public class RedlineAbility : GenericAbility
+        {
+            public override void ActivateAbility()
+            {
+                HostShip.TwoTargetLocksOnSameTargetsAreAllowed.Add(HostShip);
+                HostShip.OnActionIsPerformed += RegisterAbility;
+            }
+
+            public override void DeactivateAbility()
+            {
+                HostShip.TwoTargetLocksOnSameTargetsAreAllowed.Remove(HostShip);
+                HostShip.OnActionIsPerformed -= RegisterAbility;
+            }
+
+            private void RegisterAbility(GenericAction action)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnActionIsPerformed, AcquireSecondTargetLock);
+            }
+
+            private void AcquireSecondTargetLock(object sender, EventArgs e)
+            {
+                HostShip.ChooseTargetToAcquireTargetLock(
+                    Triggers.FinishTrigger,
+                    "You may acquire a lock",
+                    HostShip.ImageUrl
+                );
+            }
         }
     }
 }

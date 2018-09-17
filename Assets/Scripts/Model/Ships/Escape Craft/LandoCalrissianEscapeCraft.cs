f965b1bebe99ff8f4943326a7bac83116ed2919c
Sandrem <oogoeroo@gmail.com>
2018-09-17 14:26:31 +0300

    Lando Calrissian Scum both pilots + fix

---
+++ Assets/Scripts/Model/Ships/Escape Craft/LandoCalrissianEscapeCraft.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ship;
+using RuleSets;
+
+namespace Ship
+{
+    namespace EscapeCraft
+    {
+        public class LandoCalrissianEscapeCraft : EscapeCraft, ISecondEditionPilot
+        {
+            public LandoCalrissianEscapeCraft() : base()
+            {
+                PilotName = "Lando Calrissian";
+                PilotSkill = 4;
+                Cost = 26;
+
+                IsUnique = true;
+
+                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+
+                PilotAbilities.Add(new Abilities.SecondEdition.LandoCalrissianScumPilotAbilitySE());
+
+                SEImageNumber = 226;
+            }
+
+            public void AdaptPilotToSecondEdition()
+            {
+                // Not required
+            }
+        }
+    }
+}

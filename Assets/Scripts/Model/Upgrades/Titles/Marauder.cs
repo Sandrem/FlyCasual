8e64e6b4c727ca2e2608165d3628c2666c5e7dd6
Sandrem <oogoeroo@gmail.com>
2018-10-02 19:37:13 +0300

    Cost of Marauder title is fixed

--- Assets/Scripts/Model/Upgrades/Titles/Marauder.cs
+++ Assets/Scripts/Model/Upgrades/Titles/Marauder.cs
@@ -15,7 +15,7 @@ namespace UpgradesList
         {
             Types.Add(UpgradeType.Title);
             Name = "Marauder";
-            Cost = 2;
+            Cost = 3;
             UpgradeRuleType = typeof(SecondEdition);
             isUnique = true;
             AddedSlots = new List<UpgradeSlot>

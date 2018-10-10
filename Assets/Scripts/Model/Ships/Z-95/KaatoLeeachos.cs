de0f1e3fa9ee7053612470ec8a7fa5605ff3f830
Kyle D Simpson <kyleacct@gmail.com>
2018-10-10 18:32:00 -0400

    Bugfix.  Kaa'to ability didn't work.  The pilot now matches the already-working Palob Godalhi with minor changes to the ability.

--- Assets/Scripts/Model/Ships/Z-95/KaatoLeeachos.cs
+++ Assets/Scripts/Model/Ships/Z-95/KaatoLeeachos.cs
@@ -1,137 +1,243 @@
-﻿using Abilities.SecondEdition;
-using RuleSets;
+﻿using System;
+using System.Collections.Generic;
 using Ship;
 using SubPhases;
-using System;
-using System.Collections.Generic;
+using UnityEngine;
+using Upgrade;
+using Players;
+using System.Linq;
 using Tokens;
+using RuleSets;
+using BoardTools;
+using Arcs;
 
 namespace Ship
 {
-    namespace Z95
-    {
-        public class KaatoLeeachos : Z95, ISecondEditionPilot
-        {
-            public KaatoLeeachos() : base()
-            {
-                PilotName = "Kaa'to Leeachos";
-                PilotSkill = 3;
-                Cost = 29;
+	namespace Z95
+	{
+		public class KaatoLeeachos : Z95, ISecondEditionPilot
+		{
+			public KaatoLeeachos() : base()
+			{
+				PilotName = "Kaa'to Leeachos";
+				PilotSkill = 5;
+				Cost = 15;
 
-                IsUnique = true;
+				IsUnique = true;
 
-                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Illicit);
-                PrintedUpgradeIcons.Add(Upgrade.UpgradeType.Elite);
+				PrintedUpgradeIcons.Add(UpgradeType.Elite);
+				PrintedUpgradeIcons.Add(UpgradeType.Illicit);
 
-                PilotRuleType = typeof(SecondEdition);
+				faction = Faction.Scum;
 
-                faction = Faction.Scum;
-
-                PilotAbilities.Add(new KaatoLeeachosAbilitySE());
-
-                SEImageNumber = 170;
+				PilotAbilities.Add(new Abilities.KaatoLeeachos());
             }
 
             public void AdaptPilotToSecondEdition()
             {
-               // nah
+                PilotSkill = 3;
+                Cost = 29;
+
+                PilotAbilities.RemoveAll(ability => ability is Abilities.KaatoLeeachos);
+                PilotAbilities.Add(new Abilities.SecondEdition.KaatoLeeachosSE());
+
+                SEImageNumber = 170;
             }
         }
-    }
+	}
+}
+
+namespace Abilities
+{
+	public class KaatoLeeachos : GenericAbility
+	{
+		public override void ActivateAbility()
+		{
+			Phases.Events.OnCombatPhaseStart_Triggers += RegisterAbility;
+		}
+
+		public override void DeactivateAbility()
+		{
+			Phases.Events.OnCombatPhaseStart_Triggers -= RegisterAbility;
+		}
+
+		private void RegisterAbility()
+		{
+			RegisterAbilityTrigger(TriggerTypes.OnCombatPhaseStart, Ability);
+		}
+
+		private void Ability(object sender, EventArgs e)
+		{
+			if (TargetsForAbilityExist (FilterAbilityTarget)) {
+				Messages.ShowInfoToHuman ("Kaa'to Leeachos: Select a ship to remove Focus/Evade token from");
+
+				SelectTargetForAbility (
+					SelectAbilityTarget,
+					FilterAbilityTarget,
+					GetAiAbilityPriority,
+					HostShip.Owner.PlayerNo,
+                    true,
+                    null,
+                    HostShip.PilotName,
+                    "Choose a ship to remove 1 focus or evade token from it and assign this token to yourself.",
+                    HostShip.ImageUrl
+                );
+			} else {
+					Triggers.FinishTrigger();
+			}
+		}
+
+		protected virtual bool FilterAbilityTarget(GenericShip ship)
+		{
+			return FilterByTargetType(ship, new List<TargetTypes>() { TargetTypes.OtherFriendly }) && FilterTargetsByRange(ship, 1, 2) && FilterTargetWithTokens(ship);
+		}
+
+		protected bool FilterTargetWithTokens(GenericShip ship)
+		{
+			return (ship.Tokens.HasToken(typeof(Tokens.FocusToken)) || ship.Tokens.HasToken(typeof(Tokens.EvadeToken)));
+		}
+
+		private int GetAiAbilityPriority(GenericShip ship)
+		{
+            // This is the same AI logic used on PalobGodalhi and it might be good enough as-is
+			int result = 0;
+
+			int shipFocusTokens = ship.Tokens.CountTokensByType(typeof(Tokens.FocusToken));
+			int shipEvadeTokens = ship.Tokens.CountTokensByType(typeof(Tokens.EvadeToken));
+
+			result += ship.Cost + ship.UpgradeBar.GetUpgradesOnlyFaceup().Sum(n => n.Cost);
+			if (shipFocusTokens > 0)
+				result += 50;
+			if (shipFocusTokens == 1) 
+				result += 100;
+			if (shipEvadeTokens > 0)
+				result += 25;
+
+			return result;
+		}
+
+		private void SelectAbilityTarget()
+		{
+			GenericShip thisship = TargetShip;
+			int numfocustokens = thisship.Tokens.CountTokensByType (typeof(Tokens.FocusToken));
+			int numevadetokens = thisship.Tokens.CountTokensByType (typeof(Tokens.EvadeToken));
+
+			if (numfocustokens > 0 && numevadetokens == 0) {
+				TakeFocus ();
+			} else {
+				if (numfocustokens == 0 && numevadetokens > 0) {
+					takeEvade ();
+				} else {
+					if (numfocustokens > 0 && numevadetokens > 0) {
+						AskWhichTokenToTake (takeFocusEventHandler, takeEvadeEventHandler);
+					} else {
+						SelectShipSubPhase.FinishSelection ();
+					}
+				}
+			}
+		}	
+
+		private void TakeFocus() {
+			TargetShip.Tokens.RemoveToken (
+				typeof(FocusToken),
+				delegate {
+					HostShip.Tokens.AssignToken (
+                        typeof(FocusToken),
+						delegate {
+							SelectShipSubPhase.FinishSelection();
+						}
+					);
+				}
+			);
+		}
+
+		private void takeEvade() {
+			TargetShip.Tokens.RemoveToken (
+				typeof(EvadeToken),
+				delegate {
+					HostShip.Tokens.AssignToken (
+                        typeof(EvadeToken),
+						delegate {
+							SelectShipSubPhase.FinishSelection();
+						}
+					);
+				}
+			);
+		}
+
+		private void takeFocusEventHandler(object sender, EventArgs e) {
+			TargetShip.Tokens.RemoveToken (
+				typeof(FocusToken),
+				delegate {
+					HostShip.Tokens.AssignToken (
+                        typeof(FocusToken),
+						delegate {
+							WhichTokenDecisionSubphase.ConfirmDecisionNoCallback();
+							SelectShipSubPhase.FinishSelection();
+						}
+					);
+				}
+			);
+		}
+
+		private void takeEvadeEventHandler(object sender, EventArgs e) {
+			TargetShip.Tokens.RemoveToken (
+				typeof(EvadeToken),
+				delegate {
+					HostShip.Tokens.AssignToken (
+                        typeof(EvadeToken),
+						delegate {
+							WhichTokenDecisionSubphase.ConfirmDecisionNoCallback();
+							SelectShipSubPhase.FinishSelection();
+						}
+					);
+				}
+			);
+		}
+
+		private void AskWhichTokenToTake(EventHandler takeFocusHandler, EventHandler takeEvadeHandler, Action callback = null)
+		{
+			if (callback == null)
+				callback = Triggers.FinishTrigger;
+
+			if (HostShip.Owner.Type == PlayerType.Ai) {
+				TakeFocus ();
+			} else {
+
+				DecisionSubPhase whichToken = (DecisionSubPhase)Phases.StartTemporarySubPhaseNew (
+					                             Name,
+					                             typeof(WhichTokenDecisionSubphase),
+					                             callback
+				                             );
+
+				whichToken.InfoText = "Take which type of Token?";
+
+				whichToken.RequiredPlayer = HostShip.Owner.PlayerNo;
+
+				whichToken.AddDecision ("Focus", takeFocusHandler);
+				whichToken.AddDecision ("Evade", takeEvadeHandler);
+
+				whichToken.ShowSkipButton = false;
+
+				whichToken.Start ();
+			}
+		}
+
+		private class WhichTokenDecisionSubphase : DecisionSubPhase { }
+
+	}
 }
 
 namespace Abilities.SecondEdition
 {
-    public class KaatoLeeachosAbilitySE : GenericAbility
+    public class KaatoLeeachosSE : KaatoLeeachos
     {
-        public override void ActivateAbility()
+        protected override bool FilterAbilityTarget(GenericShip ship)
         {
-            Phases.Events.OnCombatPhaseStart_Triggers += RegisterAbility;
-        }
-
-        public override void DeactivateAbility()
-        {
-            Phases.Events.OnCombatPhaseStart_Triggers -= RegisterAbility;
-        }
-
-        private void RegisterAbility()
-        {
-            if(TargetsForAbilityExist(FilterAbilityTarget))
-                RegisterAbilityTrigger(TriggerTypes.OnCombatPhaseStart, Ability);
-        }
-
-        protected virtual string GenerateAbilityMessage()
-        {
-            return "Choose another friendly ship to transfer focus or evade from.";
-        }
-
-        private void Ability(object sender, EventArgs e)
-        {
-            SelectTargetForAbility(
-                SelectAbilityTarget,
-                FilterAbilityTarget,
-                GetAiAbilityPriority,
-                HostShip.Owner.PlayerNo,
-                true,
-                null,
-                HostShip.PilotName,
-                GenerateAbilityMessage(),
-                HostShip.ImageUrl
-            );
-        }
-
-        protected virtual bool FilterAbilityTarget(GenericShip ship)
-        {
-            return FilterByTargetType(ship, new List<TargetTypes>() { TargetTypes.OtherFriendly, TargetTypes.This })
-                && FilterTargetsByRange(ship, 1, 2)
-                && ship.Tokens.HasToken(typeof(FocusToken));
-        }
-
-        private int GetAiAbilityPriority(GenericShip ship)
-        {
-            int result = 0;
-            return result;
-        }
-
-        private void SelectAbilityTarget()
-        {
-            SelectShipSubPhase.FinishSelectionNoCallback();
-
-            KaatoLeeachosDecisionSubPhaseSE subphase = (KaatoLeeachosDecisionSubPhaseSE)
-            Phases.StartTemporarySubPhaseNew(
-                "Select token to transfer to Kaa'to Leeachos.",
-                typeof(KaatoLeeachosDecisionSubPhaseSE),
-                Triggers.FinishTrigger
-            );
-
-            subphase.Start();
-
-        }
-    
-
-        public class KaatoLeeachosDecisionSubPhaseSE : DecisionSubPhase
-        {
-            public GenericShip target;
-            Type tokenType = null;
-
-            public override void PrepareDecision(Action callBack)
-            {
-                InfoText = Selection.AnotherShip.PilotName + ": " + "Select token to transfer to Kaato.";
-                DecisionOwner = Selection.AnotherShip.Owner;
-
-                if (Selection.AnotherShip.Tokens.HasToken(typeof(FocusToken)))
-                    AddDecision("Transfer focus token.", delegate { tokenType = typeof(FocusToken); Selection.AnotherShip.Tokens.RemoveToken(tokenType, AddTokenToKaato); });
-
-                if (Selection.AnotherShip.Tokens.HasToken(typeof(EvadeToken)))
-                    AddDecision("Transfer evade token.", delegate { tokenType = typeof(EvadeToken); Selection.AnotherShip.Tokens.RemoveToken(tokenType, AddTokenToKaato); });
-
-                callBack();
-            }
-
-            private void AddTokenToKaato()
-            {
-                Selection.ThisShip.Tokens.AssignToken(tokenType, DecisionSubPhase.ConfirmDecision);
-            }
+            return 
+                FilterByTargetType(ship, new List<TargetTypes>() { TargetTypes.OtherFriendly }) &&
+                FilterTargetsByRange(ship, 1, 2) &&
+                FilterTargetWithTokens(ship);
         }
     }
 }

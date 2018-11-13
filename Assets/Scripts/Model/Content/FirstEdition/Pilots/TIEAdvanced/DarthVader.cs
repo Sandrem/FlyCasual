d4c571fe4239649eacb81279d3d21ec5d4c6ab6a
Sandrem <oogoeroo@gmail.com>
2018-11-13 21:22:41 +0200

    TIE Advanced X1 + Vader

---
+++ Assets/Scripts/Model/Content/FirstEdition/Pilots/TIEAdvanced/DarthVader.cs
@@ -0,0 +1,59 @@
+﻿using Abilities.FirstEdition;
+using ActionsList;
+using Ship;
+using System.Collections.Generic;
+
+namespace Ship
+{
+    namespace FirstEdition.TIEAdvanced
+    {
+        public class DarthVader : TIEAdvanced
+        {
+            public DarthVader() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Darth Vader",
+                    9,
+                    29,
+                    limited: 1,
+                    abilityType: typeof(DarthVaderAbility)
+                );
+
+                ShipInfo.UpgradeIcons.Upgrades.Add(Upgrade.UpgradeType.Elite);
+
+                ModelInfo.SkinName = "Blue";
+            }
+        }
+    }
+}
+
+namespace Abilities.FirstEdition
+{
+    public class DarthVaderAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnActionDecisionSubphaseEnd += DoSecondAction;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActionDecisionSubphaseEnd -= DoSecondAction;
+        }
+
+        private void DoSecondAction(GenericShip ship)
+        {
+            if (!HostShip.Tokens.HasToken(typeof(Tokens.StressToken)) && Phases.CurrentSubPhase.GetType() == typeof(SubPhases.ActionDecisonSubPhase))
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnFreeActionPlanned, PerformFreeAction);
+            }
+        }
+
+        private void PerformFreeAction(object sender, System.EventArgs e)
+        {
+            List<GenericAction> actions = Selection.ThisShip.GetAvailableActions();
+
+            HostShip.AskPerformFreeAction(actions, Triggers.FinishTrigger);
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEAdvancedX1/DarthVader.cs
@@ -0,0 +1,86 @@
+﻿using Abilities.FirstEdition;
+using ActionsList;
+using Ship;
+using System.Collections.Generic;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEAdvancedX1
+    {
+        public class DarthVader : TIEAdvancedX1
+        {
+            public DarthVader() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Darth Vader",
+                    6,
+                    70,
+                    limited: 1,
+                    abilityType: typeof(DarthVaderAbility),
+                    force: 3
+                );
+
+                ShipInfo.UpgradeIcons.Upgrades.Add(Upgrade.UpgradeType.Force);
+
+                ModelInfo.SkinName = "Blue";
+
+                SEImageNumber = 93;
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //After you perform an action, you may spend 1 force to perform an action.
+    public class DarthVaderAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnActionIsPerformed += CheckConditions;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActionIsPerformed -= CheckConditions;
+        }
+
+        private void CheckConditions(GenericAction action)
+        {
+            if (HostShip.State.Force > 0)
+            {
+                HostShip.OnActionDecisionSubphaseEnd += DoAnotherAction;
+            }
+        }
+
+        private void DoAnotherAction(GenericShip ship)
+        {
+            HostShip.OnActionDecisionSubphaseEnd -= DoAnotherAction;
+
+            RegisterAbilityTrigger(TriggerTypes.OnFreeAction, PerformAction);
+        }
+
+        private void PerformAction(object sender, System.EventArgs e)
+        {
+            Messages.ShowInfoToHuman("Darth Vader: you may spend 1 force to perform an action");
+
+            HostShip.BeforeFreeActionIsPerformed += PayForceCost;
+
+            List<GenericAction> actions = Selection.ThisShip.GetAvailableActions();
+            HostShip.AskPerformFreeAction(actions, CleanUp);
+        }
+
+        private void PayForceCost(GenericAction action)
+        {
+            HostShip.State.Force--;
+            HostShip.BeforeFreeActionIsPerformed -= PayForceCost;
+        }
+
+        private void CleanUp()
+        {
+            HostShip.BeforeFreeActionIsPerformed -= PayForceCost;
+            Triggers.FinishTrigger();
+        }
+
+    }
+}

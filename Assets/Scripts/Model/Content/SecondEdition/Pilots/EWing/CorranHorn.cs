8daeabfceab956435c9ed8db76cb150f6d895795
Sandrem <oogoeroo@gmail.com>
2018-11-21 17:35:26 +0200

    E-Wing

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/EWing/CorranHorn.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/EWing/CorranHorn.cs
@@ -1,9 +1,7 @@
 ﻿using BoardTools;
 using Ship;
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Tokens;
 
 namespace Ship
 {
@@ -31,115 +29,43 @@ namespace Ship
     }
 }
 
-namespace Abilities
+namespace Abilities.SecondEdition
 {
-    public abstract class CorranHornBaseAbility : GenericAbility
+    //At initiative 0, you may perform a bonus primary attack against an enemy ship in your bullseye firing arc. 
+    //If you do, at the start of the next Planning Phase, gain 1 disarm token.
+    public class CorranHornAbility : CorranHornBaseAbility
     {
-        protected TriggerTypes TriggerType;
-        protected string Description;
-        protected Func<GenericShip, IShipWeapon, bool, bool> ExtraAttackFilter;
-
-        protected void RegisterCorranHornAbility()
+        public CorranHornAbility()
         {
-            if (!HostShip.Tokens.HasToken(typeof(WeaponsDisabledToken)))
-            {
-                RegisterAbilityTrigger(TriggerType, UseCorranHornAbility);
-            }
+            TriggerType = TriggerTypes.OnCombatPhaseEnd;
+            Description = "You may perform a bonus bullseye primary attack\nGain 1 disarm token next round";
+            ExtraAttackFilter = IsBullsEyePrimary;
         }
 
-        protected void UseCorranHornAbility(object sender, System.EventArgs e)
+        public override void ActivateAbility()
         {
-            Combat.StartAdditionalAttack(
-                HostShip,
-                AfterExtraAttackSubPhase,
-                ExtraAttackFilter,
-                HostShip.PilotName,
-                Description,
-                HostShip
-            );
+            //This is technically not the correct timing, but works for now. The combat phase should be rewritten to allow 
+            //for abilities to add extra activations 
+            Phases.Events.OnCombatPhaseEnd_Triggers += RegisterCorranHornAbility;
         }
 
-        private void AfterExtraAttackSubPhase()
+        public override void DeactivateAbility()
         {
-            // "Weapons disabled" token is assigned only if attack was successfully performed
-            if (HostShip.IsAttackPerformed) Phases.Events.OnRoundStart += RegisterAssignWeaponsDisabledTrigger;
-
-            Triggers.FinishTrigger();
+            Phases.Events.OnCombatPhaseEnd_Triggers -= RegisterCorranHornAbility;
         }
 
-        private void RegisterAssignWeaponsDisabledTrigger()
+        private bool IsBullsEyePrimary(GenericShip defender, IShipWeapon weapon, bool isSilent)
         {
-            Phases.Events.OnRoundStart -= RegisterAssignWeaponsDisabledTrigger;
-            RegisterAbilityTrigger(TriggerTypes.OnRoundStart, AssignWeaponsDisabledTrigger);
-        }
-
-        private void AssignWeaponsDisabledTrigger(object sender, System.EventArgs e)
-        {
-            HostShip.Tokens.AssignToken(typeof(WeaponsDisabledToken), Triggers.FinishTrigger);
-        }
-    }
-
-    namespace FirstEdition
-    {
-        //At the start of the End phase, you may perform one attack. You cannot attack during the next round.
-        public class CorranHornAbility : CorranHornBaseAbility
-        {
-            public CorranHornAbility()
+            bool result = false;
+            if (weapon is PrimaryWeaponClass && new ShotInfo(HostShip, defender, weapon).InArcByType(Arcs.ArcTypes.Bullseye))
             {
-                TriggerType = TriggerTypes.OnEndPhaseStart;
-                Description = "You may perform an additional attack.\nYou cannot attack during next round.";
+                result = true;
             }
-
-            public override void ActivateAbility()
+            else
             {
-                Phases.Events.OnEndPhaseStart_Triggers += RegisterCorranHornAbility;
+                if (!isSilent) Messages.ShowError("Attack must be performed with primary weapon in bullseye arc");
             }
-
-            public override void DeactivateAbility()
-            {
-                Phases.Events.OnEndPhaseStart_Triggers -= RegisterCorranHornAbility;
-            }
-        }
-    }
-
-    namespace SecondEdition
-    {
-        //At initiative 0, you may perform a bonus primary attack against an enemy ship in your bullseye firing arc. 
-        //If you do, at the start of the next Planning Phase, gain 1 disarm token.
-        public class CorranHornAbility : CorranHornBaseAbility
-        {
-            public CorranHornAbility()
-            {
-                TriggerType = TriggerTypes.OnCombatPhaseEnd;
-                Description = "You may perform a bonus bullseye primary attack\nGain 1 disarm token next round";
-                ExtraAttackFilter = IsBullsEyePrimary;
-            }
-
-            public override void ActivateAbility()
-            {
-                //This is technically not the correct timing, but works for now. The combat phase should be rewritten to allow 
-                //for abilities to add extra activations 
-                Phases.Events.OnCombatPhaseEnd_Triggers += RegisterCorranHornAbility;
-            }
-
-            public override void DeactivateAbility()
-            {
-                Phases.Events.OnCombatPhaseEnd_Triggers -= RegisterCorranHornAbility;
-            }
-
-            private bool IsBullsEyePrimary(GenericShip defender, IShipWeapon weapon, bool isSilent)
-            {
-                bool result = false;
-                if (weapon is PrimaryWeaponClass && new ShotInfo(HostShip, defender, weapon).InArcByType(Arcs.ArcTypes.Bullseye))
-                {
-                    result = true;
-                }
-                else
-                {
-                    if (!isSilent) Messages.ShowError("Attack must be performed with primary weapon in bullseye arc");
-                }
-                return result;
-            }
+            return result;
         }
     }
 }

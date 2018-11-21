28b490e684937a63084eac2ebfe694da80093292
Sandrem <oogoeroo@gmail.com>
2018-11-21 17:39:34 +0200

    FE G1-A

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/G1AStarfighter/Zuckuss.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/G1AStarfighter/Zuckuss.cs
@@ -1,7 +1,4 @@
-﻿using Actions;
-using ActionsList;
-using Ship;
-using Upgrade;
+﻿using Upgrade;
 
 namespace Ship
 {
@@ -23,59 +20,6 @@ namespace Ship
 
                 SEImageNumber = 202;
             }
-        }
-    }
-}
-
-namespace Abilities.FirstEdition
-{
-    public class ZuckussAbility : GenericAbility
-    {
-        public override void ActivateAbility()
-        {
-            HostShip.OnAttackStartAsAttacker += RegisterZuckussAbility;
-            HostShip.OnAttackFinishAsAttacker += RemoveZuckussAbility;
-        }
-
-        public override void DeactivateAbility()
-        {
-            HostShip.OnAttackStartAsAttacker -= RegisterZuckussAbility;
-            HostShip.OnAttackFinishAsAttacker -= RemoveZuckussAbility;
-        }
-
-        private void RegisterZuckussAbility()
-        {
-            RegisterAbilityTrigger(TriggerTypes.OnAttackStart, ShowDecision);
-        }
-
-        private void ShowDecision(object sender, System.EventArgs e)
-        {
-            AskToUseAbility(AlwaysUseByDefault, UseAbility);
-        }
-
-        private void UseAbility(object sender, System.EventArgs e)
-        {
-            IsAbilityUsed = true;
-
-            HostShip.AfterGotNumberOfPrimaryWeaponAttackDice += ZuckussAddAttackDice;
-            Combat.Defender.AfterGotNumberOfDefenceDice += ZuckussAddAttackDice;
-
-            SubPhases.DecisionSubPhase.ConfirmDecision();
-        }
-
-        private void RemoveZuckussAbility(GenericShip genericShip)
-        {
-            // At the end of combat phase, need to remove attack value increase
-            if (IsAbilityUsed)
-            {
-                HostShip.AfterGotNumberOfPrimaryWeaponAttackDice -= ZuckussAddAttackDice;
-                Combat.Defender.AfterGotNumberOfDefenceDice -= ZuckussAddAttackDice;
-                IsAbilityUsed = false;
-            }
-        }
-        private void ZuckussAddAttackDice(ref int value)
-        {
-            value++;
         }
     }
 }

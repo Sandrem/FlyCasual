af849f04d2a2f5cd48e22e9bb43be8cf71aa6685
Sandrem <oogoeroo@gmail.com>
2018-11-21 22:37:32 +0200

    FE Hera Syndulla U-Wing

---
+++ Assets/Scripts/Model/Content/FirstEdition/Pilots/UWing/HeraSyndulla.cs
@@ -0,0 +1,26 @@
+﻿using Mods.ModsList;
+using Upgrade;
+
+namespace Ship
+{
+    namespace FirstEdition.UWing
+    {
+        public class HeraSyndulla : UWing
+        {
+            public HeraSyndulla() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Hera Syndulla",
+                    7,
+                    28,
+                    limited: 1,
+                    abilityType: typeof(Abilities.FirstEdition.HeraSyndullaAbility)
+                );
+
+                ShipInfo.UpgradeIcons.Upgrades.Add(UpgradeType.Elite);
+
+                RequiredMods.Add(typeof(PhoenixSquadronMod));
+            }
+        }
+    }
+}

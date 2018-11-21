0282a77e0d8e5df19f6814cde411293df79cf701
Sandrem <oogoeroo@gmail.com>
2018-11-21 16:42:23 +0200

    Tycho Mod

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/RZ1AWing/TychoCelchu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Ship
+{
+    namespace SecondEdition.RZ1AWing
+    {
+        public class TychoCelchu : RZ1AWing
+        {
+            public TychoCelchu() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Tycho Celchu",
+                    5,
+                    42,
+                    limited: 1,
+                    abilityType: typeof(Abilities.FirstEdition.TychoCelchuAbility)
+                );
+
+                ShipInfo.UpgradeIcons.Upgrades.Add(Upgrade.UpgradeType.Elite);
+
+                ImageUrl = "https://i.imgur.com/lzgv9da.png";
+
+                RequiredMods = new List<System.Type>() { typeof(Mods.ModsList.FirstEditionPilotsMod) };
+            }
+        }
+    }
+}

88606befee01b0bc92d98947420c842b0e290125
Sandrem <oogoeroo@gmail.com>
2018-11-21 18:40:51 +0200

    FE YT-2400

---
+++ Assets/Scripts/Model/Content/FirstEdition/Pilots/YT2400/DashRendar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace FirstEdition.YT2400
+    {
+        public class DashRendar : YT2400
+        {
+            public DashRendar() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Dash Rendar",
+                    7,
+                    36,
+                    limited: 1,
+                    abilityType: typeof(Abilities.FirstEdition.DashRendarAbility)
+                );
+
+                ShipInfo.UpgradeIcons.Upgrades.Add(UpgradeType.Elite);
+            }
+        }
+    }
+}
+
+namespace Abilities.FirstEdition
+{
+    public class DashRendarAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            Phases.Events.OnActivationPhaseStart += ActivateDashRendarAbility;
+            Phases.Events.OnCombatPhaseStart_NoTriggers += DeactivateDashRendarAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            Phases.Events.OnActivationPhaseStart -= ActivateDashRendarAbility;
+            Phases.Events.OnCombatPhaseStart_NoTriggers -= DeactivateDashRendarAbility;
+        }
+
+        private void ActivateDashRendarAbility()
+        {
+            HostShip.IsIgnoreObstacles = true;
+        }
+
+        private void DeactivateDashRendarAbility()
+        {
+            HostShip.IsIgnoreObstacles = false;
+        }
+
+    }
+}

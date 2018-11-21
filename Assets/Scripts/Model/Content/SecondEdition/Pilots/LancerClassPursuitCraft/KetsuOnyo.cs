3cbb4500831371d0feb247fc2c075541dbf8bb8b
Sandrem <oogoeroo@gmail.com>
2018-11-21 18:05:07 +0200

    FE Lancer

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/LancerClassPursuitCraft/KetsuOnyo.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/LancerClassPursuitCraft/KetsuOnyo.cs
@@ -1,10 +1,4 @@
-﻿using ActionsList;
-using Arcs;
-using BoardTools;
-using Ship;
-using SubPhases;
-using System.Collections.Generic;
-using Upgrade;
+﻿using Upgrade;
 
 namespace Ship
 {
@@ -27,83 +21,5 @@ namespace Ship
                 SEImageNumber = 218;
             }
         }
-    }
-}
-
-namespace Abilities.FirstEdition
-{
-    public class KetsuOnyoPilotAbility : GenericAbility
-    {
-
-        public override void ActivateAbility()
-        {
-            Phases.Events.OnCombatPhaseStart_Triggers += TryRegisterKetsuOnyoPilotAbility;
-        }
-
-        public override void DeactivateAbility()
-        {
-            Phases.Events.OnCombatPhaseStart_Triggers -= TryRegisterKetsuOnyoPilotAbility;
-        }
-
-        private void TryRegisterKetsuOnyoPilotAbility()
-        {
-            if (TargetsForAbilityExist(FilterTargetsOfAbility))
-            {
-                RegisterAbilityTrigger(TriggerTypes.OnCombatPhaseStart, AskSelectShip);
-            }
-        }
-
-        private void AskSelectShip(object sender, System.EventArgs e)
-        {
-            SelectTargetForAbility(
-                CheckAssignTractorBeam,
-                FilterTargetsOfAbility,
-                GetAiPriorityOfTarget,
-                HostShip.Owner.PlayerNo,
-                HostShip.PilotName,
-                "Choose a ship inside your primary and mobile firing arcs to assign 1 Tractor Beam token to it.",
-                HostShip
-            );
-        }
-
-        private bool FilterTargetsOfAbility(GenericShip ship)
-        {
-            ShotInfo shotInfo = new ShotInfo(HostShip, ship, HostShip.PrimaryWeapon);
-
-            return FilterByTargetType(ship, new List<TargetTypes>() { TargetTypes.Enemy, TargetTypes.OtherFriendly })
-                && FilterTargetsByRange(ship, 0, 1)
-                && shotInfo.InArcByType(ArcTypes.Mobile)
-                && shotInfo.InPrimaryArc;
-        }
-
-        private int GetAiPriorityOfTarget(GenericShip ship)
-        {
-            return 50;
-        }
-
-        private void CheckAssignTractorBeam()
-        {
-            SelectShipSubPhase.FinishSelectionNoCallback();
-
-            ShotInfo shotInfo = new ShotInfo(HostShip, TargetShip, HostShip.PrimaryWeapon);
-            if (shotInfo.InArcByType(ArcTypes.Mobile) && shotInfo.InPrimaryArc && shotInfo.Range == 1)
-            {
-                Messages.ShowError(HostShip.PilotName + " assigns Tractor Beam Token\nto " + TargetShip.PilotName);
-                Tokens.TractorBeamToken token = new Tokens.TractorBeamToken(TargetShip, HostShip.Owner);
-                TargetShip.Tokens.AssignToken(token, Triggers.FinishTrigger);
-            }
-            else
-            {
-                if (!shotInfo.InArcByType(ArcTypes.Mobile) || !shotInfo.InPrimaryArc)
-                {
-                    Messages.ShowError("Target is not inside Mobile Arc and Primary Arc");
-                }
-                else if (shotInfo.Range > 1)
-                {
-                    Messages.ShowError("Target is outside range 1");
-                }
-            }
-        }
-
     }
 }
--- Assets/Scripts/Model/Content/SecondEdition/Pilots/LancerClassPursuitCraft/SabineWren.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/LancerClassPursuitCraft/SabineWren.cs
@@ -1,8 +1,4 @@
-﻿using ActionsList;
-using Arcs;
-using BoardTools;
-using Ship;
-using Upgrade;
+﻿using Upgrade;
 
 namespace Ship
 {
@@ -25,59 +21,5 @@ namespace Ship
                 SEImageNumber = 220;
             }
         }
-    }
-}
-
-namespace Abilities.FirstEdition
-{
-    public class SabineWrenLancerPilotAbility : GenericAbility
-    {
-
-        public override void ActivateAbility()
-        {
-            HostShip.OnGenerateDiceModifications += AddSabinebility;
-        }
-
-        public override void DeactivateAbility()
-        {
-            HostShip.OnGenerateDiceModifications -= AddSabinebility;
-        }
-
-        private void AddSabinebility(GenericShip ship)
-        {
-            ship.AddAvailableDiceModification(new SabineWrenDiceModification());
-        }
-
-        private class SabineWrenDiceModification : GenericAction
-        {
-            public SabineWrenDiceModification()
-            {
-                Name = DiceModificationName = "Sabine Wren's ability";
-            }
-
-            public override void ActionEffect(System.Action callBack)
-            {
-                Combat.CurrentDiceRoll.AddDice(DieSide.Focus).ShowWithoutRoll();
-                Combat.CurrentDiceRoll.OrganizeDicePositions();
-                callBack();
-            }
-
-            public override bool IsDiceModificationAvailable()
-            {
-                bool result = false;
-                if (Combat.AttackStep == CombatStep.Defence)
-                {
-                    ShotInfo shotInfo = new ShotInfo(Combat.Defender, Combat.Attacker, Combat.Defender.PrimaryWeapon);
-                    if (shotInfo.InArcByType(ArcTypes.Mobile)) result = true;
-                }
-                return result;
-            }
-
-            public override int GetDiceModificationPriority()
-            {
-                return 110;
-            }
-        }
-
     }
 }

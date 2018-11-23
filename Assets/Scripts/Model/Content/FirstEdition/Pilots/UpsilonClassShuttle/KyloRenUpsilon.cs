d980024163866c51206712d33f7d19ee9f8aa872
Sandrem <oogoeroo@gmail.com>
2018-11-23 04:01:13 +0200

    FE Upsilon-class Shuttle

---
+++ Assets/Scripts/Model/Content/FirstEdition/Pilots/UpsilonClassShuttle/KyloRenUpsilon.cs
@@ -0,0 +1,194 @@
+﻿using Conditions;
+using Ship;
+using SubPhases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tokens;
+using Upgrade;
+
+namespace Ship
+{
+    namespace FirstEdition.UpsilonClassShuttle
+    {
+        public class KyloRenUpsilon : UpsilonClassShuttle
+        {
+            public KyloRenUpsilon() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Kylo Ren",
+                    6,
+                    34,
+                    limited: 1,
+                    abilityType: typeof(Abilities.FirstEdition.KyloRenPilotAbility)
+                );
+
+                ShipInfo.UpgradeIcons.Upgrades.Add(UpgradeType.Elite);
+            }
+        }
+    }
+}
+
+namespace Abilities.FirstEdition
+{
+    public class KyloRenPilotAbility : GenericAbility
+    {
+        public GenericDamageCard AssignedDamageCard;
+        public GenericShip ShipWithCondition;
+
+        public override void ActivateAbility()
+        {
+            HostShip.OnAttackHitAsDefender += CheckKyloRenAbility;
+            Phases.Events.OnRoundEnd += ClearIsAbilityUsedFlag;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnAttackHitAsDefender -= CheckKyloRenAbility;
+            Phases.Events.OnRoundEnd -= ClearIsAbilityUsedFlag;
+        }
+
+        private void CheckKyloRenAbility()
+        {
+            if (!IsAbilityUsed)
+            {
+                IsAbilityUsed = true;
+                RegisterAbilityTrigger(TriggerTypes.OnAttackHit, AssignConditionToAttacker);
+            }
+        }
+
+        private void AssignConditionToAttacker(object sender, EventArgs e)
+        {
+            Sounds.PlayShipSound("Ill-Show-You-The-Dark-Side");
+
+            if (AssignedDamageCard == null)
+            {
+                // If condition is not in play - select card to assign
+                ShowPilotCrits();
+            }
+            else
+            {
+                // If condition is in play - reassing only
+                RemoveConditions(ShipWithCondition);
+                AssignConditions(Combat.Attacker);
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private void ShowPilotCrits()
+        {
+            SelectPilotCritDecision selectPilotCritSubphase = (SelectPilotCritDecision)Phases.StartTemporarySubPhaseNew(
+                "Select Damage Card",
+                typeof(SelectPilotCritDecision),
+                Triggers.FinishTrigger
+            );
+
+            List<GenericDamageCard> opponentDeck = DamageDecks.GetDamageDeck(Roster.AnotherPlayer(HostShip.Owner.PlayerNo)).Deck;
+            foreach (var card in opponentDeck.Where(n => n.Type == CriticalCardType.Pilot))
+            {
+                Decision existingDecision = selectPilotCritSubphase.GetDecisions().Find(n => n.Name == card.Name);
+                if (existingDecision == null)
+                {
+                    selectPilotCritSubphase.AddDecision(card.Name, delegate { SelectDamageCard(card); }, card.ImageUrl, 1);
+                }
+                else
+                {
+                    existingDecision.SetCount(existingDecision.Count + 1);
+                }
+            }
+
+            selectPilotCritSubphase.DecisionViewType = DecisionViewTypes.ImagesDamageCard;
+
+            selectPilotCritSubphase.DefaultDecisionName = selectPilotCritSubphase.GetDecisions().First().Name;
+
+            selectPilotCritSubphase.InfoText = "Kylo Ren: Select Damage Card";
+
+            selectPilotCritSubphase.RequiredPlayer = HostShip.Owner.PlayerNo;
+
+            selectPilotCritSubphase.Start();
+        }
+
+        private void SelectDamageCard(GenericDamageCard damageCard)
+        {
+            Messages.ShowInfo("Card is selected: " + damageCard.Name);
+
+            AssignedDamageCard = damageCard;
+            AssignedDamageCard.IsFaceup = true;
+            DamageDeck opponentDamageDeck = DamageDecks.GetDamageDeck(Combat.Attacker.Owner.PlayerNo);
+            opponentDamageDeck.RemoveFromDamageDeck(damageCard);
+            opponentDamageDeck.ReShuffleDeck();
+            AssignConditions(Combat.Attacker);
+
+            DecisionSubPhase.ConfirmDecision();
+        }
+
+        private void AssignConditions(GenericShip ship)
+        {
+            ShipWithCondition = ship;
+
+            ship.Tokens.AssignCondition(typeof(IllShowYouTheDarkSide));
+            ship.Tokens.AssignCondition(new IllShowYouTheDarkSideDamageCard(ship) { Tooltip = AssignedDamageCard.ImageUrl });
+
+            ship.OnSufferCriticalDamage += SufferAssignedCardInstead;
+            ship.OnShipIsDestroyed += RemoveConditionsOnDestroyed;
+        }
+
+        private void RemoveConditions(GenericShip ship)
+        {
+            ShipWithCondition = null;
+            ship.Tokens.RemoveCondition(typeof(IllShowYouTheDarkSide));
+            ship.Tokens.RemoveCondition(typeof(IllShowYouTheDarkSideDamageCard));
+
+            ship.OnSufferCriticalDamage -= SufferAssignedCardInstead;
+            ship.OnShipIsDestroyed -= RemoveConditionsOnDestroyed;
+        }
+
+        private void SufferAssignedCardInstead(object sender, EventArgs e, ref bool isSkipSufferDamage)
+        {
+            if ((e as DamageSourceEventArgs).DamageType == DamageTypes.ShipAttack)
+            {
+                Messages.ShowInfo("Kylo Ren: Assigned card is dealt instead");
+
+                isSkipSufferDamage = true;
+
+                GenericShip ship = ShipWithCondition;
+                Combat.CurrentCriticalHitCard = AssignedDamageCard;
+
+                AssignedDamageCard = null;
+                RemoveConditions(ship);
+
+                ship.ProcessDrawnDamageCard(e);
+            }
+        }
+
+        private void RemoveConditionsOnDestroyed(GenericShip ship, bool isFled)
+        {
+            AssignedDamageCard = null;
+            RemoveConditions(ship);
+        }
+
+        private class SelectPilotCritDecision : DecisionSubPhase { };
+    }
+}
+
+namespace Conditions
+{
+    public class IllShowYouTheDarkSide : GenericToken
+    {
+        public IllShowYouTheDarkSide(GenericShip host) : base(host)
+        {
+            Name = "I'll Show You The Dark Side Condition";
+            Temporary = false;
+            Tooltip = "https://raw.githubusercontent.com/guidokessels/xwing-data/master/images/conditions/ill-show-you-the-dark-side.png";
+        }
+    }
+
+    public class IllShowYouTheDarkSideDamageCard : GenericToken
+    {
+        public IllShowYouTheDarkSideDamageCard(GenericShip host) : base(host)
+        {
+            Name = "I'll Show You The Dark Side Damage Card Condition";
+            Temporary = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/FirstEdition/Ships/UpsilonClassShuttle.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Movement;
+using ActionsList;
+using Actions;
+using Arcs;
+using Upgrade;
+
+namespace Ship
+{
+    namespace FirstEdition.UpsilonClassShuttle
+    {
+        public class UpsilonClassShuttle : GenericShip
+        {
+            public UpsilonClassShuttle() : base()
+            {
+                ShipInfo = new ShipCardInfo
+                (
+                    "Upsilon-class Shuttle",
+                    BaseSize.Large,
+                    Faction.Imperial,
+                    new ShipArcsInfo(ArcTypes.Primary, 4), 1, 6, 6,
+                    new ShipActionsInfo(
+                        new ActionInfo(typeof(FocusAction)),
+                        new ActionInfo(typeof(TargetLockAction)),
+                        new ActionInfo(typeof(CoordinateAction))
+                    ),
+                    new ShipUpgradesInfo(
+                        UpgradeType.Title,
+                        UpgradeType.Modification,
+                        UpgradeType.System,
+                        UpgradeType.Crew,
+                        UpgradeType.Crew,
+                        UpgradeType.Tech,
+                        UpgradeType.Tech
+                    ),
+                    subFaction: SubFaction.FirstOrder
+                );
+
+                IconicPilots = new Dictionary<Faction, System.Type> {
+                    { Faction.Imperial, typeof(KyloRenUpsilon) }
+                };
+
+                ModelInfo = new ShipModelInfo(
+                    "Upsilon-class Shuttle",
+                    "Upsilon-class Shuttle"
+                );
+
+                DialInfo = new ShipDialInfo(
+                    new ManeuverInfo(ManeuverSpeed.Speed0, ManeuverDirection.Stationary, ManeuverBearing.Stationary, MovementComplexity.Complex),
+
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Complex),
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Complex),
+
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Easy),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Easy),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Normal),
+
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Complex),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Complex)
+                );
+
+                SoundInfo = new ShipSoundInfo(
+                    new List<string>()
+                    {
+                        "Slave1-Fly1",
+                        "Slave1-Fly2"
+                    },
+                    "Slave1-Fire", 4
+                );
+
+                // ManeuversImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/fe/d9/fed9939b-8331-462b-a3b8-d8359d1342bd/swx75_a3_dial.png";
+
+                /* HotacManeuverTable = new AI.TIEFighterTable(); */
+            }
+        }
+    }
+}

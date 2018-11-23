761b3bfdbe842c734d109064b7d293a12626f0dc
Sandrem <oogoeroo@gmail.com>
2018-11-23 04:42:58 +0200

    FE TIE/SF

---
+++ Assets/Scripts/Model/Content/FirstEdition/Pilots/TIESfFighter/Backdraft.cs
@@ -0,0 +1,82 @@
+﻿using ActionsList;
+using Ship;
+using Upgrade;
+
+namespace Ship
+{
+    namespace FirstEdition.TIESfFighter
+    {
+        public class Backdraft : TIESfFighter
+        {
+            public Backdraft() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Backdraft\"",
+                    7,
+                    27,
+                    limited: 1,
+                    abilityType: typeof(Abilities.FirstEdition.BackdraftAbility)
+                );
+
+                ShipInfo.UpgradeIcons.Upgrades.Add(UpgradeType.Elite);
+            }
+        }
+    }
+}
+
+namespace Abilities.FirstEdition
+{
+    public class BackdraftAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnGenerateDiceModifications += AddBackdraftAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnGenerateDiceModifications -= AddBackdraftAbility;
+        }
+
+        private void AddBackdraftAbility(GenericShip ship)
+        {
+            ship.AddAvailableDiceModification(new BackdraftAbilityAction());
+        }
+    }
+}
+
+namespace ActionsList
+{
+    public class BackdraftAbilityAction : GenericAction
+    {
+        public BackdraftAbilityAction()
+        {
+            Name = DiceModificationName = "\"Backdraft\" ability";
+        }
+
+        public override int GetDiceModificationPriority()
+        {
+            int result = 0;
+            result = 110;
+            return result;
+        }
+
+        public override bool IsDiceModificationAvailable()
+        {
+            bool result = false;
+            if (Combat.AttackStep == CombatStep.Attack && !Combat.ShotInfo.InPrimaryArc)
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        public override void ActionEffect(System.Action callBack)
+        {
+            Combat.CurrentDiceRoll.AddDice(DieSide.Crit).ShowWithoutRoll();
+            Combat.CurrentDiceRoll.OrganizeDicePositions();
+            callBack();
+        }
+    }
+
+}

45ba8a680fdf2bf44a458ef1fe2410c7ec5b96aa
Sandrem <oogoeroo@gmail.com>
2018-11-24 08:33:46 +0200

    Salvaged Astromechs

---
+++ Assets/Scripts/Model/Content/FirstEdition/Upgrade/SalvagedAstromech/Genius.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Upgrade;
+using System;
+using Ship;
+using SubPhases;
+using Bombs;
+using System.Linq;
+
+namespace UpgradesList.FirstEdition
+{
+    public class Genius : GenericUpgrade
+    {
+        public Genius() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "\"Genius\"",
+                UpgradeType.SalvagedAstromech,
+                cost: 0,
+                isLimited: true,
+                abilityType: typeof(Abilities.FirstEdition.GeniusAbility)
+            );
+        }
+    }
+}
+
+namespace Abilities.FirstEdition
+{
+    public class GeniusAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnMovementFinish += CheckGeniusAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnMovementFinish -= CheckGeniusAbility;
+        }
+
+        private void CheckGeniusAbility(GenericShip ship)
+        {
+            if (HostShip.IsBumped) return;
+            if (HostShip.IsBombAlreadyDropped) return;
+            if (!BombsManager.HasTimedBombs(ship)) return;
+            if (BoardTools.Board.IsOffTheBoard(ship)) return;
+
+            RegisterAbilityTrigger(TriggerTypes.OnMovementActivation, AskUseGeniusAbility);
+        }
+
+        private void AskUseGeniusAbility(object sender, EventArgs e)
+        {
+            AskToUseAbility(NeverUseByDefault, UseGeniusAbility);
+        }
+
+        private void UseGeniusAbility(object sender, EventArgs e)
+        {
+            List<GenericUpgrade> timedBombsInstalled = BombsManager.GetTimedBombsInstalled(HostShip);
+            DecisionSubPhase.ConfirmDecisionNoCallback();
+
+            if (timedBombsInstalled.Count == 1)
+            {
+                BombsManager.CurrentBomb = timedBombsInstalled[0] as GenericBomb;
+                StartDropBombSubphase();
+            }
+            else
+            {
+                AskToSelectTimedBomb(StartDropBombSubphase);
+            }
+        }
+
+        private void AskToSelectTimedBomb(Action callback)
+        {
+            GeniusBombDecisionSubPhase selectBombToDrop = (GeniusBombDecisionSubPhase)Phases.StartTemporarySubPhaseNew(
+                "Select bomb to drop",
+                typeof(GeniusBombDecisionSubPhase),
+                callback
+            );
+
+            foreach (var timedBombInstalled in BombsManager.GetTimedBombsInstalled(HostShip))
+            {
+                selectBombToDrop.AddDecision(
+                    timedBombInstalled.UpgradeInfo.Name,
+                    delegate { SelectBomb(timedBombInstalled); }
+                );
+            }
+
+            selectBombToDrop.InfoText = "Select bomb to drop";
+
+            selectBombToDrop.DefaultDecisionName = BombsManager.GetTimedBombsInstalled(HostShip).First().UpgradeInfo.Name;
+
+            selectBombToDrop.RequiredPlayer = HostShip.Owner.PlayerNo;
+
+            selectBombToDrop.Start();
+        }
+
+        private void SelectBomb(GenericUpgrade timedBombUpgrade)
+        {
+            BombsManager.CurrentBomb = timedBombUpgrade as GenericTimedBomb;
+            DecisionSubPhase.ConfirmDecision();
+        }
+
+        private class GeniusBombDecisionSubPhase : DecisionSubPhase { }
+
+        private void StartDropBombSubphase()
+        {
+            Phases.StartTemporarySubPhaseOld(
+                "Bomb drop planning",
+                typeof(BombDropPlanningSubPhase),
+                CheckThatBombIsDiscarded
+            );
+        }
+
+        private void CheckThatBombIsDiscarded()
+        {
+            if (BombsManager.CurrentBomb == null || BombsManager.CurrentBomb.IsDiscardedAfterDropped)
+            {
+                Triggers.FinishTrigger();
+            }
+            else
+            {
+                BombsManager.CurrentBomb.TryDiscard(Triggers.FinishTrigger);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/FirstEdition/Upgrade/SalvagedAstromech/R4Agromech.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Upgrade;
+using Tokens;
+using System;
+using Ship;
+using SubPhases;
+
+namespace UpgradesList.FirstEdition
+{
+    public class R4Agromech : GenericUpgrade
+    {
+        public R4Agromech() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "R4 Agromech",
+                UpgradeType.SalvagedAstromech,
+                cost: 2,
+                abilityType: typeof(Abilities.FirstEdition.R4AgromechAbility)
+            );
+        }
+    }
+}
+
+namespace Abilities.FirstEdition
+{
+    public class R4AgromechAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnTokenIsSpent += CheckAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnTokenIsSpent -= CheckAbility;
+        }
+
+        private void CheckAbility(GenericShip ship, Type type)
+        {
+            if (Combat.AttackStep == CombatStep.Attack && type == typeof(FocusToken) && Combat.Attacker == HostShip && Combat.Defender != null)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnTokenIsSpent, AskAcquireTargetLock);
+            }
+        }
+
+        private void AskAcquireTargetLock(object sender, EventArgs e)
+        {
+            AskToUseAbility(AlwaysUseByDefault, AcquireTargetLock, null, null, true);
+        }
+
+        private void AcquireTargetLock(object sender, EventArgs e)
+        {
+            ActionsHolder.AcquireTargetLock(Combat.Attacker, Combat.Defender, DecisionSubPhase.ConfirmDecision, DecisionSubPhase.ConfirmDecision);
+        }
+    }
+}

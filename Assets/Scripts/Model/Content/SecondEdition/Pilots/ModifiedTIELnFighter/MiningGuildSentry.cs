a8d1f59e71920ce33bbd39df10a6bfb9dd7631c4
Sandrem <oogoeroo@gmail.com>
2018-12-02 23:35:02 +0200

    Modified TIE/ln Fighter - template

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/ModifiedTIELnFighter/MiningGuildSentry.cs
@@ -0,0 +1,22 @@
+﻿namespace Ship
+{
+    namespace SecondEdition.ModifiedTIELnFighter
+    {
+        public class MiningGuildSentry : ModifiedTIELnFighter
+        {
+            public MiningGuildSentry() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Mining Guild Sentry",
+                    1,
+                    24 //,
+                    // seImageNumber: 92
+                );
+
+                // Ability
+
+                ImageUrl = "https://vignette.wikia.nocookie.net/xwing-miniatures-second-edition/images/4/44/Swz23_mining-guild-sentry.png";
+            }
+        }
+    }
+}

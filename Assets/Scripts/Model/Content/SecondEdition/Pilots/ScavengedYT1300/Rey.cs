0806ab7e710af717e5105cee2a168568666aa95c
Sandrem <oogoeroo@gmail.com>
2018-12-02 22:20:41 +0200

    Templates for Resistance ships

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/ScavengedYT1300/Rey.cs
@@ -0,0 +1,31 @@
+﻿using BoardTools;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.ScavengedYT1300
+    {
+        public class Rey : ScavengedYT1300
+        {
+            public Rey() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Rey",
+                    5,
+                    80,
+                    isLimited: true,
+                    //abilityType: typeof(Abilities.SecondEdition.HanSoloRebelPilotAbility),
+                    //charges: 1,
+                    //regensCharges: true,
+                    extraUpgradeIcon: UpgradeType.Talent //,
+                    //seImageNumber: 69
+                );
+
+                ImageUrl = "https://vignette.wikia.nocookie.net/xwing-miniatures-second-edition/images/5/58/Swz19_a2_reys-pilot.png";
+            }
+        }
+    }
+}

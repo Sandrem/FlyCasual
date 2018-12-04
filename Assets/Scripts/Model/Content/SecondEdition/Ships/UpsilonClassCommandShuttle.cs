4d5603daf74ff6d69527540b96d011150f573c3e
Sandrem <oogoeroo@gmail.com>
2018-12-04 16:51:29 +0200

    SE LinkedBattery ship ability

--- Assets/Scripts/Model/Content/SecondEdition/Ships/UpsilonClassCommandShuttle.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/UpsilonClassCommandShuttle.cs
@@ -1,13 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Movement;
 using ActionsList;
 using Actions;
 using Upgrade;
 using Ship;
-using System;
-using SubPhases;
-using Abilities.FirstEdition;
 
 namespace Ship
 {
@@ -30,8 +26,31 @@ namespace Ship
 
                 IconicPilots[Faction.FirstOrder] = typeof(LieutenantDormitz);
 
+                ShipAbilities.Add(new Abilities.SecondEdition.LinkedBattery());
+
                 // ManeuversImageUrl = "https://vignette.wikia.nocookie.net/xwing-miniatures-second-edition/images/d/d4/Maneuver_lambda_shuttle.png";
             }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class LinkedBattery : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.AfterGotNumberOfAttackDice += CheckAddDice;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.AfterGotNumberOfAttackDice -= CheckAddDice;
+        }
+
+        private void CheckAddDice(ref int count)
+        {
+            if (Combat.ChosenWeapon is GenericSpecialWeapon && (Combat.ChosenWeapon as GenericSpecialWeapon).HasType(UpgradeType.Cannon)) count++;
         }
     }
 }

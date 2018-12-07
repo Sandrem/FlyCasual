3a35b9b89dfa9d4067ce80dd05759927fe5d2d91
Azrapse <azrapse@gmail.com>
2018-12-08 01:08:42 +0200

    Adapting "Genius" to Second Edition

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Astromech/Genius.cs
@@ -0,0 +1,42 @@
+﻿using Upgrade;
+using System;
+using Ship;
+using SubPhases;
+using Bombs;
+using System.Linq;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace UpgradesList.SecondEdition
+{
+    public class Genius : GenericUpgrade
+    {
+        public Genius() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "\"Genius\"",
+                UpgradeType.Astromech,
+                cost: 0,
+                isLimited: true,
+                abilityType: typeof(Abilities.SecondEdition.GeniusAbility),
+                restriction: new FactionRestriction(Faction.Scum),                
+                seImageNumber: 143
+            );
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class GeniusAbility : Abilities.FirstEdition.GeniusAbility
+    {
+        protected override void StartDropBombSubphase()
+        {
+            Phases.StartTemporarySubPhaseOld(
+                "Bomb drop planning",
+                typeof(BombDropPlanningSubPhase),
+                Triggers.FinishTrigger
+            );
+        }
+    }
+}

70524db3617ac9427bdf6f6e4281578adaa47d4d
GeneralVryth <45475694+GeneralVryth@users.noreply.github.com>
2018-12-07 21:59:43 -0800

    Adapting Pilots
    "Midnight"
    "Scorch"
    "Static"
    Joph Seastriker

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEFoFighter/Midnight.cs
@@ -0,0 +1,25 @@
+﻿using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEFoFighter
+    {
+        public class Midnight : TIEFoFighter
+        {
+            public Midnight() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Midnight\"",
+                    6,
+                    44,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.FirstEdition.OmegaLeaderAbility),
+                    extraUpgradeIcon: UpgradeType.Talent //,
+                                                         //seImageNumber: 120
+                );
+
+                ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/56/94/56940164-d919-4b04-8303-f39357555fad/swz18_a1_midnight.png";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEFoFighter/Scorch.cs
@@ -0,0 +1,25 @@
+﻿using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEFoFighter
+    {
+        public class Scorch : TIEFoFighter
+        {
+            public Scorch() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Scorch\"",
+                    4,
+                    35,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.FirstEdition.ZetaLeaderAbility),
+                    extraUpgradeIcon: UpgradeType.Talent //,
+                                                         //seImageNumber: 120
+                );
+
+                ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/80/bb/80bbabbb-3b30-448e-a896-ddcfc05082bd/swz26_a1_scorch.png";
+            }
+        }
+    }
+}

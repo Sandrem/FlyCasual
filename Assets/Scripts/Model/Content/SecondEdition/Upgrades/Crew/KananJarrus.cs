bc49638d663c1c5414a381edd3ef69035d755e08
Azrapse <azrapse@gmail.com>
2018-12-08 14:07:48 +0200

    SE Kanan Jarrus (Crew) Fix: Ships are at range 0 of themselves in Second Edition (instead of 1).

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/KananJarrus.cs
@@ -0,0 +1,82 @@
+﻿using Ship;
+using Upgrade;
+using UnityEngine;
+using System;
+using Tokens;
+using SubPhases;
+
+namespace UpgradesList.SecondEdition
+{
+    public class KananJarrus : GenericUpgrade
+    {
+        public KananJarrus() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Kanan Jarrus",
+                UpgradeType.Crew,
+                cost: 14,
+                isLimited: true,
+                restriction: new FactionRestriction(Faction.Rebel),
+                abilityType: typeof(Abilities.SecondEdition.KananJarrusCrewAbility),
+                addForce: 1,
+                seImageNumber: 86
+            );
+        }        
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class KananJarrusCrewAbility : GenericAbility
+    {
+        private GenericShip ShipToRemoveStress;
+
+        public override void ActivateAbility()
+        {
+            GenericShip.OnMovementFinishGlobal += CheckAbility;            
+        }
+
+        public override void DeactivateAbility()
+        {
+            GenericShip.OnMovementFinishGlobal -= CheckAbility;            
+        }
+
+        private void CheckAbility(GenericShip ship)
+        {
+            if (ship.IsBumped || BoardTools.Board.IsOffTheBoard(ship) || !HostShip.Tokens.HasToken<ForceToken>())
+            {
+                return;
+            }
+            if (ship.Owner.PlayerNo == HostShip.Owner.PlayerNo && ship.AssignedManeuver.ColorComplexity == Movement.MovementComplexity.Normal)
+            {
+                BoardTools.DistanceInfo distanceInfo = new BoardTools.DistanceInfo(HostShip, ship);
+                if (distanceInfo.Range < 3)
+                {
+                    ShipToRemoveStress = ship;
+                    RegisterAbilityTrigger(TriggerTypes.OnMovementFinish, AskKananJarrusAbility);
+                }
+            }
+        }
+
+        private void AskKananJarrusAbility(object sender, System.EventArgs e)
+        {
+            if (ShipToRemoveStress.Tokens.HasToken(typeof(StressToken)) && HostShip.Tokens.HasToken<ForceToken>())
+            {
+                AskToUseAbility(AlwaysUseByDefault, RemoveStress, null, null, true);
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private void RemoveStress(object sender, EventArgs e)
+        {
+            HostShip.Tokens.SpendToken(typeof(ForceToken), () => 
+                ShipToRemoveStress.Tokens.RemoveToken(
+                    typeof(StressToken),
+                    DecisionSubPhase.ConfirmDecision
+                ));            
+        }                
+    }
+}

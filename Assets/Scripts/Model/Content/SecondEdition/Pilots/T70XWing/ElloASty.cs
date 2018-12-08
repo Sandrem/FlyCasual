0ed4064d408d87bc0951a5acdb0ba513375d2954
GeneralVryth <45475694+GeneralVryth@users.noreply.github.com>
2018-12-07 18:53:39 -0800

    Adapting 5 Pilots
    "Blackout"
    Nien Nunb
    Ello Asty
    Kare Kun
    Temmin Wexley

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/T70XWing/ElloASty.cs
@@ -0,0 +1,60 @@
+﻿using Movement;
+using Ship;
+using Tokens;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.T70XWing
+    {
+        public class ElloASty : T70XWing
+        {
+            public ElloASty() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Ello Asty",
+                    5,
+                    56,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.ElloAstyAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                //seImageNumber: 93
+                );
+
+                //ModelInfo.SkinName = "Black One";
+
+                ImageUrl = "http://infinitearenas.com/xw2browse/images/resistance/ello-asty.jpg";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class ElloAstyAbility : Abilities.FirstEdition.ElloAstyAbility
+    {
+        //private const string LEFT_TALON_ROLL = "3.L.E";
+        //private const string RIGHT_TALON_ROLL = "3.R.E";
+
+        public override void ActivateAbility()
+        {
+            HostShip.AfterGetManeuverColorDecreaseComplexity += CheckElloAstyAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.AfterGetManeuverColorDecreaseComplexity -= CheckElloAstyAbility;
+        }
+
+        private void CheckElloAstyAbility(GenericShip ship, ref ManeuverHolder movement)
+        {
+            if (movement.Bearing == ManeuverBearing.TallonRoll)
+            {
+                if (HostShip.Tokens.CountTokensByType(typeof(StressToken)) <= 2)
+                {
+                    movement.ColorComplexity = MovementComplexity.Normal;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEVnSilencer/Blackout.cs
@@ -0,0 +1,25 @@
+﻿using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEVnSilencer
+    {
+        public class Blackout : TIEVnSilencer
+        {
+            public Blackout() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Blackout\"",
+                    5,
+                    70,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.FirstEdition.TestPilotBlackoutAbility),
+                    extraUpgradeIcon: UpgradeType.Talent //,
+                                                         //seImageNumber: 120
+                );
+
+                ImageUrl = "http://infinitearenas.com/xw2browse/images/first-order/blackout.jpg";
+            }
+        }
+    }
+}

5fa0ffaf9e253515a34441370cd9ad8e8409766e
GeneralVryth <45475694+GeneralVryth@users.noreply.github.com>
2018-12-11 22:23:53 -0800

    Adding Rey, Heroic, and Fanatical

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/ScavengedYT1300/Rey.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/ScavengedYT1300/Rey.cs
@@ -1,7 +1,8 @@
 ﻿using BoardTools;
-using System;
+using Ship;
 using System.Collections;
 using System.Collections.Generic;
+using Tokens;
 using Upgrade;
 
 namespace Ship
@@ -17,15 +18,96 @@ namespace Ship
                     5,
                     80,
                     isLimited: true,
-                    //abilityType: typeof(Abilities.SecondEdition.HanSoloRebelPilotAbility),
-                    //charges: 1,
-                    //regensCharges: true,
-                    extraUpgradeIcon: UpgradeType.Talent //,
-                    //seImageNumber: 69
+                    abilityType: typeof(Abilities.SecondEdition.ReyPilotAbility),
+                    force: 2,
+                    extraUpgradeIcon: UpgradeType.Force //,
+                                                        //seImageNumber: 69
                 );
 
-                ImageUrl = "https://vignette.wikia.nocookie.net/xwing-miniatures-second-edition/images/5/58/Swz19_a2_reys-pilot.png";
+                ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/f9/27/f9276fd7-52fd-4b05-8599-05039fa479e6/swz19_a2_reys-pilot.png";
             }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class ReyPilotAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnGenerateDiceModifications += AddDiceModification;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnGenerateDiceModifications -= AddDiceModification;
+        }
+
+        public void AddDiceModification(GenericShip ship)
+        {
+            ship.AddAvailableDiceModification(new ReyAction(ship));
+        }
+
+        private class ReyAction : ActionsList.GenericAction
+        {
+            public ReyAction(GenericShip ship)
+            {
+                Name = DiceModificationName = "Rey's Ability";
+                TokensSpend.Add(typeof(ForceToken));
+                HostShip = ship;
+            }
+
+            public override void ActionEffect(System.Action callBack)
+            {
+                Combat.CurrentDiceRoll.ChangeOne(DieSide.Blank, DieSide.Success);
+                Combat.CurrentDiceRoll.OrganizeDicePositions();
+
+                HostShip.Tokens.SpendToken(
+                    typeof(ForceToken),
+                    callBack
+                );
+            }
+
+            public override bool IsDiceModificationAvailable()
+            {
+                bool result = false;
+
+                switch (Combat.AttackStep)
+                {
+                    case CombatStep.Attack:
+                        if (Combat.ShotInfo.InArc && HostShip.State.Force > 0 && Combat.CurrentDiceRoll.Blanks > 0) result = true;
+                        break;
+                    case CombatStep.Defence:
+                        ShotInfo shotInfo = new ShotInfo(Combat.Defender, Combat.Attacker, Combat.Defender.PrimaryWeapon);
+                        if (shotInfo.InArc && HostShip.State.Force > 0 && Combat.CurrentDiceRoll.Blanks > 0) result = true;
+                        break;
+                    default:
+                        break;
+                }
+
+                return result;
+            }
+
+            public override int GetDiceModificationPriority()
+            {
+                int result = 0;
+                DiceRoll diceValues = Combat.CurrentDiceRoll;
+                if (diceValues.Blanks > 0)
+                {
+                    if (diceValues.Focuses == 0 && diceValues.Blanks == 1)
+                    {
+                        result = 81;
+                    }
+                    else
+                    {
+                        result = 40;
+                    }
+                }
+
+                return result;
+            }
+
         }
     }
 }

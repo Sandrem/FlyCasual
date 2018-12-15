0ffb1a8cf66c13906c41c91c349aa22a0338226c
Sandrem <oogoeroo@gmail.com>
2018-12-15 05:52:18 +0200

    Resistance YT-1300 model

--- Assets/Scripts/Model/Content/FirstEdition/Pilots/YT1300/ResistanceSympathizer.cs
+++ Assets/Scripts/Model/Content/FirstEdition/Pilots/YT1300/ResistanceSympathizer.cs
@@ -20,6 +20,7 @@ namespace Ship
                 ShipInfo.ArcInfo.Arcs.ForEach(a => a.Firepower = 3);
                 ShipInfo.Hull = 8;
                 ShipInfo.Shields = 5;
+                ModelInfo = new ShipModelInfo("Scavenged YT-1300", "YT-1300");
 
                 ShipInfo.SubFaction = Faction.Resistance;
             }

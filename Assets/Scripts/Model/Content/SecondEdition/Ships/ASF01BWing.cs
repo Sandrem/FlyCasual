3b01264ef3fa7834f1de3521474fea9c10e35acc
rune-b <rune@krokodille.com>
2018-12-17 20:40:49 +0100

    Fixed B-Wing linked action

--- Assets/Scripts/Model/Content/SecondEdition/Ships/ASF01BWing.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/ASF01BWing.cs
@@ -20,7 +20,7 @@ namespace Ship
                 ShipInfo.UpgradeIcons.Upgrades.Add(UpgradeType.Cannon);
                 ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Torpedo);
 
-                ShipInfo.ActionIcons.AddActions(new ActionInfo(typeof(BarrelRollAction), ActionColor.Red));
+                ShipInfo.ActionIcons.AddLinkedAction(new LinkedActionInfo(typeof(FocusAction), typeof(BarrelRollAction)));
 
                 DialInfo.AddManeuver(new ManeuverHolder(ManeuverSpeed.Speed1, ManeuverDirection.Left, ManeuverBearing.TallonRoll), MovementComplexity.Complex);
                 DialInfo.AddManeuver(new ManeuverHolder(ManeuverSpeed.Speed1, ManeuverDirection.Right, ManeuverBearing.TallonRoll), MovementComplexity.Complex);

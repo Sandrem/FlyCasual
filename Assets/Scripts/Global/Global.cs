19a0f904123ce91660692a2a740575eab5a04b64
Sandrem <oogoeroo@gmail.com>
2018-12-17 18:18:10 +0200

    Credits, Version is changed to 0.7.0

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -10,7 +10,7 @@ public class Global : MonoBehaviour {
 
     private static bool isAlreadyInitialized;
 
-    public static string CurrentVersion = "0.7.0 DEV";
+    public static string CurrentVersion = "0.7.0";
     public static int CurrentVersionInt = 100070000;
 
     void Awake()

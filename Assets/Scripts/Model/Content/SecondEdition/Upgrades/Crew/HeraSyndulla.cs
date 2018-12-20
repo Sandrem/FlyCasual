d764200ba51c8793af897f0ede4b77ebd47bc494
Sandrem <oogoeroo@gmail.com>
2018-12-20 04:23:49 +0200

    Hera Syndulla (Rebel Crew)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/HeraSyndulla.cs
@@ -0,0 +1,69 @@
+﻿using Upgrade;
+using Ship;
+using Tokens;
+
+namespace UpgradesList.SecondEdition
+{
+    public class HeraSyndulla : GenericUpgrade
+    {
+        public HeraSyndulla() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Hera Syndulla",
+                UpgradeType.Crew,
+                cost: 4,
+                isLimited: true,
+                restriction: new FactionRestriction(Faction.Rebel),
+                abilityType: typeof(Abilities.SecondEdition.HeraSyndullaCrewAbility),
+                seImageNumber: 84
+            );
+        }        
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class HeraSyndullaCrewAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.CanPerformRedManeuversWhileStressed = true;
+            HostShip.OnMovementFinishSuccessfully += CheckAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.CanPerformRedManeuversWhileStressed = false;
+            HostShip.OnMovementFinishSuccessfully -= CheckAbility;
+        }
+
+        private void CheckAbility(GenericShip ship)
+        {
+            RegisterAbilityTrigger(TriggerTypes.OnMovementFinish, CheckStressRemoval);
+        }
+
+        private void CheckStressRemoval(object sender, System.EventArgs e)
+        {
+            if (HostShip.Tokens.CountTokensByType<StressToken>() >= 3)
+            {
+                Messages.ShowInfo(HostUpgrade.UpgradeInfo.Name + ": Stress token is removed, damage is suffered");
+                HostShip.Tokens.RemoveToken(typeof(StressToken), SufferDamage);
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private void SufferDamage()
+        {
+            DamageSourceEventArgs damageArgs = new DamageSourceEventArgs()
+            {
+                DamageType = DamageTypes.CardAbility,
+                Source = HostUpgrade
+            };
+
+            HostShip.Damage.TryResolveDamage(1, damageArgs, Triggers.FinishTrigger);
+        }
+    }
+}

5b3d805f13e769db308a12937e138fe8e2cb07da
Sandrem <oogoeroo@gmail.com>
2018-12-21 05:44:41 +0200

    SE Dauntless Title

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Title/Dauntless.cs
@@ -0,0 +1,55 @@
+﻿using Arcs;
+using Ship;
+using System.Collections.Generic;
+using System.Linq;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class Dauntless : GenericUpgrade
+    {
+        public Dauntless() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Dauntless",
+                UpgradeType.Title,
+                cost: 6,
+                isLimited: true,
+                restriction: new ShipRestriction(typeof(Ship.SecondEdition.VT49Decimator.VT49Decimator)),
+                abilityType: typeof(Abilities.SecondEdition.DauntlessAbility),
+                seImageNumber: 123
+            );
+        }        
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class DauntlessAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnMovementFinishUnsuccessfully += RegisterAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnMovementFinishUnsuccessfully += RegisterAbility;
+        }
+
+        private void RegisterAbility(GenericShip ship)
+        {
+            RegisterAbilityTrigger(TriggerTypes.OnMovementFinish, AskToPerformWhiteActionAsRed);
+        }
+
+        private void AskToPerformWhiteActionAsRed(object sender, System.EventArgs e)
+        {
+            Messages.ShowInfoToHuman(HostUpgrade.UpgradeInfo.Name + ": You may perform an action");
+
+            HostShip.AskPerformFreeAction(
+                HostShip.GetAvailableWhiteActionsAsRed(),
+                Triggers.FinishTrigger
+            );
+        }
+    }
+}

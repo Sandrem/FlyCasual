7750eb1b9fb9c1f186261e419bb85ab50d464f21
rune-b <rune@krokodille.com>
2019-01-06 18:29:56 +0100

    Lando rebel crew

---
+++ Assets/Scripts/Model/Content/FirstEdition/Upgrades/Crew/LandoCalrissian.cs
@@ -0,0 +1,68 @@
+﻿using Upgrade;
+
+namespace UpgradesList.FirstEdition
+{
+    public class LandoCalrissian : GenericUpgrade
+    {
+        public LandoCalrissian() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Lando Calrissian",
+                UpgradeType.Crew,
+                cost: 3,
+                isLimited: true,
+                restriction: new FactionRestriction(Faction.Rebel),
+                abilityType: typeof(Abilities.FirstEdition.LandoCalrissianCrewAbility)
+            );
+        }
+    }
+}
+
+namespace Abilities.FirstEdition
+{
+    //ACTION: Roll 2 defense dice. For each focus result, assign 1 focus token to your ship. For each evade result, assign 1 evade token to your ship.
+    public class LandoCalrissianCrewAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnGenerateActions += AddAction;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnGenerateActions -= AddAction;
+        }
+
+        protected void AddAction(Ship.GenericShip ship)
+        {
+            ship.AddAvailableAction(new ActionsList.GenericAction()
+            {
+                ImageUrl = HostUpgrade.ImageUrl,
+                HostShip = HostShip,
+                Source = HostUpgrade,
+                DoAction = DoAction,
+                Name = HostName
+            });
+        }
+
+        protected void DoAction()
+        {
+            PerformDiceCheck(
+                HostName,
+                DiceKind.Defence,
+                2,
+                DiceCheckFinished,
+                Phases.CurrentSubPhase.CallBack);
+        }
+
+        protected virtual void DiceCheckFinished()
+        {
+            HostShip.Tokens.AssignTokens(() => new Tokens.FocusToken(HostShip), DiceCheckRoll.Focuses, () => {
+                HostShip.Tokens.AssignTokens(() => new Tokens.EvadeToken(HostShip), DiceCheckRoll.Successes, () =>
+                {
+                    AbilityDiceCheck.ConfirmCheck();
+                });
+            });
+        }
+    }
+}

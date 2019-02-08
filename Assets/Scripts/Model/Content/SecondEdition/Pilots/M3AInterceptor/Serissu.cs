3f4f1e10ede9226961e237f419717422ff67f6a4
Sandrem <oogoeroo@gmail.com>
2019-02-08 15:42:19 +0200

    SE Serissu ability is fixed

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/M3AInterceptor/Serissu.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/M3AInterceptor/Serissu.cs
@@ -1,4 +1,6 @@
-﻿using Upgrade;
+﻿using ActionsList;
+using Ship;
+using Upgrade;
 
 namespace Ship
 {
@@ -13,10 +15,40 @@ namespace Ship
                     5,
                     40,
                     isLimited: true,
-                    abilityType: typeof(Abilities.FirstEdition.SerissuAbility),
+                    abilityType: typeof(Abilities.SecondEdition.SerissuAbility),
                     extraUpgradeIcon: UpgradeType.Talent,
                     seImageNumber: 183
                 );
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    // When another friendly ship at Range 0-1 is defending, it may reroll 1 defense die.
+    public class SerissuAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            GenericShip.OnGenerateDiceModificationsGlobal += AddSerissuAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            GenericShip.OnGenerateDiceModificationsGlobal -= AddSerissuAbility;
+        }
+
+        private void AddSerissuAbility(GenericShip ship)
+        {
+            Combat.Defender.AddAvailableDiceModification(new SerissuAction() { HostShip = this.HostShip });
+        }
+
+        protected class SerissuAction : FriendlyRerollAction
+        {
+            public SerissuAction() : base(1, 1, true, RerollTypeEnum.DefenseDice)
+            {
+                Name = DiceModificationName = "Serissu's ability";
             }
         }
     }

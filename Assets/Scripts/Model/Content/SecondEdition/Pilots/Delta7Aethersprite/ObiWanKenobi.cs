fd540e375a5be78190980e3d9ac3de2aea820c79
rune-b <rune@krokodille.com>
2019-02-08 21:22:27 +0100

    Obi-Wan

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Delta7Aethersprite/ObiWanKenobi.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Delta7Aethersprite/ObiWanKenobi.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ship;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Upgrade;
@@ -26,16 +27,43 @@ namespace Ship.SecondEdition.Delta7Aethersprite
 
 namespace Abilities.SecondEdition
 {
+    //After a friendly ship at range 0-2 spends a focus token, you may spend force. If you do, that ship gains 1 focus token.
     public class ObiWanKenobiAbility : GenericAbility
     {
         public override void ActivateAbility()
         {
-            // TODO
+            GenericShip.OnTokenIsSpentGlobal += RegisterAbility;
         }
 
         public override void DeactivateAbility()
         {
-            // TODO
+            GenericShip.OnTokenIsSpentGlobal -= RegisterAbility;
+        }
+
+        private void RegisterAbility(GenericShip ship, Type tokenType)
+        {
+            if (HostShip.State.Force > 0 
+                && ship.Owner == HostShip.Owner 
+                && tokenType == typeof(Tokens.FocusToken) 
+                && new BoardTools.DistanceInfo(ship, HostShip).Range < 3)
+            {
+                TargetShip = ship;
+                RegisterAbilityTrigger(TriggerTypes.OnTokenIsSpent, AskToUseObiWanAbility);
+            }
+        }
+
+        private void AskToUseObiWanAbility(object sender, EventArgs e)
+        {
+            AskToUseAbility(
+                AlwaysUseByDefault,
+                UseAbility
+            );
+        }
+
+        private void UseAbility(object sender, EventArgs e)
+        {
+            HostShip.State.Force--;
+            TargetShip.Tokens.AssignToken(new Tokens.FocusToken(TargetShip), SubPhases.DecisionSubPhase.ConfirmDecision);
         }
     }
 }

7e591b3bac062ccd71c009736d1a0a62eb75978a
rune-b <rune@krokodille.com>
2019-02-08 22:14:42 +0100

    Saesee Tiin

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Delta7Aethersprite/SaeseeTiin.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Delta7Aethersprite/SaeseeTiin.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GameModes;
+using Movement;
+using Ship;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Upgrade;
@@ -28,16 +31,49 @@ namespace Ship.SecondEdition.Delta7Aethersprite
 
 namespace Abilities.SecondEdition
 {
+    //After a friendly ship at range 0-2 reveals its dial, you may spend 1 force. 
+    //If you do, set its dial to another maneuver of the same speed and difficulty.
     public class SaeseeTiinAbility : GenericAbility
     {
         public override void ActivateAbility()
         {
-            // TODO
+            GenericShip.OnManeuverIsRevealedGlobal += RegisterAskChangeManeuver;
         }
 
         public override void DeactivateAbility()
         {
-            // TODO
+            GenericShip.OnManeuverIsRevealedGlobal -= RegisterAskChangeManeuver;
+        }
+
+        private void RegisterAskChangeManeuver(GenericShip ship)
+        {
+            if (HostShip.State.Force > 0
+                && ship.Owner == HostShip.Owner
+                && new BoardTools.DistanceInfo(ship, HostShip).Range < 3)
+            {
+                TargetShip = ship;
+                RegisterAbilityTrigger(TriggerTypes.OnManeuverIsRevealed, AskChangeManeuver);
+            }
+        }
+
+        private void AskChangeManeuver(object sender, System.EventArgs e)
+        {
+            Messages.ShowInfoToHuman(HostName + ": You may change the maneuver");
+            TargetShip.Owner.ChangeManeuver(ManeuverSelected, IsSameComplexityAndSpeed);
+        }
+
+        private void ManeuverSelected(string maneuverString)
+        {
+            if (maneuverString != TargetShip.AssignedManeuver.ToString()) HostShip.State.Force--;
+            GameMode.CurrentGameMode.AssignManeuver(maneuverString);
+        }
+
+        private bool IsSameComplexityAndSpeed(string maneuverString)
+        {
+            ManeuverHolder movementStruct = new ManeuverHolder(maneuverString);
+
+            return movementStruct.ColorComplexity == TargetShip.AssignedManeuver.ColorComplexity
+                && movementStruct.SpeedInt == TargetShip.AssignedManeuver.Speed;
         }
     }
 }

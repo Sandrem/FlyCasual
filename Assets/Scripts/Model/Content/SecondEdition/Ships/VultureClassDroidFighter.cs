496bcf7d89a186af93362a269403d3323c353a75
rune-b <rune@krokodille.com>
2019-02-10 21:15:59 +0100

    Droid fighter ability

--- Assets/Scripts/Model/Content/SecondEdition/Ships/VultureClassDroidFighter.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/VultureClassDroidFighter.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Actions;
 using ActionsList;
 using Arcs;
 using Movement;
+using Ship;
+using SubPhases;
 using Upgrade;
 
 namespace Ship.SecondEdition.VultureClassDroidFighter
@@ -28,6 +32,7 @@ namespace Ship.SecondEdition.VultureClassDroidFighter
                     UpgradeType.Modification
                 )
             );
+            ShipAbilities.Add(new Abilities.SecondEdition.NetworkedCalculationsAbility());
 
             ShipInfo.ActionIcons.AddLinkedAction(new LinkedActionInfo(typeof(BarrelRollAction), typeof(CalculateAction)));
 
@@ -81,6 +86,96 @@ namespace Ship.SecondEdition.VultureClassDroidFighter
             HotacManeuverTable = new AI.EscapeCraftTable();
 
             // ManeuversImageUrl = "https://vignette.wikia.nocookie.net/xwing-miniatures-second-edition/images/2/20/Maneuver_escape_craft.png";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //While you defend or perform an attack. you may spend 1 calculate token from a friendly ship 
+    //at range 0-1 to change 1 focus result to an evade or hit result.
+    public class NetworkedCalculationsAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            AddDiceModification(
+                "Networked Calculations",
+                IsDiceModificationAvailable,
+                GetDiceModificationAiPriority,
+                DiceModificationType.Change,
+                1,
+                new List<DieSide> {DieSide.Focus},
+                DieSide.Success,
+                payAbilityCost: PayAbilityCost
+            );
+        }
+
+        public override void DeactivateAbility()
+        {
+            RemoveDiceModification();
+        }
+
+        private bool IsDiceModificationAvailable()
+        {
+            if ((Combat.AttackStep == CombatStep.Attack) || (Combat.AttackStep == CombatStep.Defence))
+            {
+                return BoardTools.Board
+                    .GetShipsAtRange(HostShip, new UnityEngine.Vector2(0, 1), Team.Type.Friendly)
+                    .Where(ship => ship.Tokens.HasToken<Tokens.CalculateToken>())
+                    .Any();
+            }
+            return false;
+        }
+        
+        private int GetDiceModificationAiPriority()
+        {
+            return 40;
+        }
+
+        private void PayAbilityCost(Action<bool> callback)
+        {
+            SelectTargetForAbility(
+                () => SpendToken(callback),
+                FilterTargets,
+                GetAiPriority,
+                HostShip.Owner.PlayerNo,
+                "Networked Calculations",
+                "Choose a friendly ship to spend a calculate token",
+                HostUpgrade
+            );
+        }
+
+        private void SpendToken(Action<bool> callback)
+        {
+            SelectShipSubPhase.FinishSelectionNoCallback();
+
+            if (TargetShip.Tokens.HasToken<Tokens.CalculateToken>())
+            {
+                TargetShip.Tokens.SpendToken(typeof(Tokens.CalculateToken), () => callback(true));
+            }
+            else
+            {
+                callback(false);
+            }
+        }
+
+        private bool FilterTargets(GenericShip ship)
+        {
+            return FilterByTargetType(ship, TargetTypes.This, TargetTypes.OtherFriendly) && FilterTargetsByRange(ship, 0, 1);
+        }
+
+        private int GetAiPriority(GenericShip ship)
+        {
+            //prioritize cheap ships and ships with multiple calculate tokens
+            //a more advanced function could take into account which ships have already attacked, and which are likely to be attacked
+
+            int result = 0;
+
+            result += ship.Tokens.CountTokensByType<Tokens.CalculateToken>() * 100;
+
+            result += 200 - ship.PilotInfo.Cost;
+
+            return result;
         }
     }
 }

f8388585b1e21e7b13e55cb660ea7253ef1e3595
rune-b <rune-b@users.noreply.github.com>
2019-02-13 23:09:19 +0100

    Darth Maul pilot (#1562)
    * wip
    
    * Fix

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/SithInfiltrator/DarthMaul.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/SithInfiltrator/DarthMaul.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using Ship;
 using Upgrade;
 
 namespace Ship.SecondEdition.SithInfiltrator
@@ -27,16 +26,112 @@ namespace Ship.SecondEdition.SithInfiltrator
 
 namespace Abilities.SecondEdition
 {
+    //After you perform an attack, you may spend 2 force to perform a bonus primary attack against a different target. 
+    //If your attack missed, you may perform that bonus primary attack against the same target instead.
     public class DarthMaulPilotAbility : GenericAbility
     {
+        private bool FirstAttackMissed;
+        private GenericShip OriginalDefender;
+
         public override void ActivateAbility()
         {
-            // TODO
+            HostShip.OnAttackStartAsAttacker += SaveOriginalDefender;
+            HostShip.OnAttackHitAsAttacker += RegisterHitAbility;
+            HostShip.OnAttackMissedAsAttacker += RegisterMissedAbility;
         }
 
         public override void DeactivateAbility()
         {
-            // TODO
+            HostShip.OnAttackStartAsAttacker -= SaveOriginalDefender;
+            HostShip.OnAttackHitAsAttacker -= RegisterHitAbility;
+            HostShip.OnAttackMissedAsAttacker -= RegisterMissedAbility;
+        }
+
+        private void SaveOriginalDefender()
+        {
+            OriginalDefender = Combat.Defender;
+        }
+
+        private void RegisterMissedAbility()
+        {
+            FirstAttackMissed = true;
+            StartSecondAttack();
+        }
+
+        private void RegisterHitAbility()
+        {
+            FirstAttackMissed = false;
+            StartSecondAttack();
+        }
+
+        private void StartSecondAttack()
+        {
+            if (HostShip.State.Force >= 2)
+            {
+                HostShip.OnCombatCheckExtraAttack += RegisterSecondAttackTrigger;
+            }
+        }
+
+        private void RegisterSecondAttackTrigger(GenericShip ship)
+        {
+            HostShip.OnCombatCheckExtraAttack -= RegisterSecondAttackTrigger;
+
+            RegisterAbilityTrigger(TriggerTypes.OnCombatCheckExtraAttack, UseAbility);
+        }
+
+        private void UseAbility(object sender, EventArgs e)
+        {
+            if (!HostShip.IsCannotAttackSecondTime)
+            {
+                HostShip.IsCannotAttackSecondTime = true;
+
+                Combat.StartAdditionalAttack(
+                    HostShip,
+                    FinishAdditionalAttack,
+                    IsAllowedAttack,
+                    HostName,
+                    "You may spend 2 force to perform a bonus primary attack" + (FirstAttackMissed ? "" : " against a different target"),
+                    HostUpgrade
+                );
+            }
+            else
+            {
+                Messages.ShowErrorToHuman(string.Format("{0} cannot attack one more time", HostShip.PilotInfo.PilotName));
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private bool IsAllowedAttack(GenericShip defender, IShipWeapon weapon, bool isSilent)
+        {
+            if (weapon.WeaponType != WeaponTypes.PrimaryWeapon)
+            {
+                if (!isSilent) Messages.ShowError("Bonus attack must be a primary attack");
+                return false;
+            }
+
+            if (!FirstAttackMissed && defender == OriginalDefender)
+            {
+
+                if (!isSilent) Messages.ShowError("Bonus attack must be against a different target");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void FinishAdditionalAttack()
+        {
+            if (HostShip.IsAttackPerformed)
+            {
+                HostShip.State.Force -= 2;
+            }
+            else
+            {
+                // If attack is skipped, set this flag, otherwise regular attack can be performed second time
+                HostShip.IsAttackPerformed = true;
+            }
+
+            Triggers.FinishTrigger();
         }
     }
 }

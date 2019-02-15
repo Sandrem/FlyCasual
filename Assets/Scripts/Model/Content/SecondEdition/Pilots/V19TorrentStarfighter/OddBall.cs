11dbafa611b93e48969e5faa8633f6622512b034
rune-b <rune-b@users.noreply.github.com>
2019-02-15 07:39:01 +0100

    Kickback, Oddball and Swoop (#1573)
    * Kickback and Oddball
    
    * Swoop

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/V19TorrentStarfighter/OddBall.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/V19TorrentStarfighter/OddBall.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using ActionsList;
+using BoardTools;
+using Movement;
+using Ship;
+using SubPhases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Upgrade;
@@ -25,16 +30,86 @@ namespace Ship.SecondEdition.V19TorrentStarfighter
 
 namespace Abilities.SecondEdition
 {
+    //After you fully execute a red maneuver or perform a red action, if there is an enemy ship in your bullseye arc, 
+    //you may acquire a lock on that ship.
     public class OddBallAbility : GenericAbility
     {
         public override void ActivateAbility()
         {
-            // TODO
+            HostShip.OnActionIsPerformed += CheckConditions;
+            HostShip.OnMovementFinishSuccessfully += RegisterMovementTrigger;
         }
 
         public override void DeactivateAbility()
         {
-            // TODO
+            HostShip.OnActionIsPerformed -= CheckConditions;
+            HostShip.OnMovementFinishSuccessfully -= RegisterMovementTrigger;
+        }
+
+        private void CheckConditions(GenericAction action)
+        {
+            if (action.IsRed && Board.GetShipsInBullseyeArc(HostShip, Team.Type.Enemy).Any())
+            {
+                HostShip.OnActionDecisionSubphaseEnd += RegisterActionTrigger;
+            }
+        }
+
+        private void RegisterActionTrigger(GenericShip ship)
+        {
+            HostShip.OnActionDecisionSubphaseEnd -= RegisterActionTrigger;
+
+            RegisterAbilityTrigger(TriggerTypes.OnFreeAction, AskAbility);
+        }
+
+        private void RegisterMovementTrigger(GenericShip ship)
+        {
+            if (HostShip.GetLastManeuverColor() == MovementComplexity.Complex && Board.GetShipsInBullseyeArc(HostShip, Team.Type.Enemy).Any())
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnMovementFinish, AskAbility);
+            }
+        }
+
+        private void AskAbility(object sender, System.EventArgs e)
+        {
+            SelectTargetForAbility(
+                    GrantFreeTargetLock,
+                    FilterAbilityTargets,
+                    GetAiAbilityPriority,
+                    HostShip.Owner.PlayerNo,
+                    HostName,
+                    "You may aquire a lock on an enemy in your bullseye arc",
+                    HostShip
+                );
+        }
+
+        private int GetAiAbilityPriority(GenericShip ship)
+        {
+            var result = 0;
+
+            var range = new BoardTools.DistanceInfo(HostShip, ship).Range;
+
+            result += (3 - range) * 100;
+
+            result += ship.PilotInfo.Cost;
+
+            return result;
+        }
+
+        private bool FilterAbilityTargets(GenericShip ship)
+        {
+            return ship.Owner != HostShip.Owner && HostShip.SectorsInfo.IsShipInSector(ship, Arcs.ArcType.Bullseye);
+        }
+
+        private void GrantFreeTargetLock()
+        {
+            if (TargetShip != null)
+            {
+                ActionsHolder.AcquireTargetLock(HostShip, TargetShip, SelectShipSubPhase.FinishSelection, SelectShipSubPhase.FinishSelection);
+            }
+            else
+            {
+                SelectShipSubPhase.FinishSelection();
+            }
         }
     }
 }
--- Assets/Scripts/Model/Content/SecondEdition/Pilots/V19TorrentStarfighter/Swoop.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/V19TorrentStarfighter/Swoop.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using ActionsList;
+using Ship;
 using System.Collections.Generic;
-using System.Linq;
 using Upgrade;
 
 namespace Ship.SecondEdition.V19TorrentStarfighter
@@ -25,16 +25,39 @@ namespace Ship.SecondEdition.V19TorrentStarfighter
 
 namespace Abilities.SecondEdition
 {
+    //After a friendly small or medium ship fully executes a speed 3-4 maneuver, if it is at range 0-1, it may perform a red boost action.
     public class SwoopAbility : GenericAbility
     {
         public override void ActivateAbility()
         {
-            // TODO
+            GenericShip.OnMovementFinishSuccessfullyGlobal += RegisterTrigger;
         }
 
         public override void DeactivateAbility()
         {
-            // TODO
+            GenericShip.OnMovementFinishSuccessfullyGlobal -= RegisterTrigger;
+        }
+
+        private void RegisterTrigger(GenericShip ship)
+        {
+            if (ship.Owner == HostShip.Owner
+                && (ship.ShipInfo.BaseSize == BaseSize.Small || ship.ShipInfo.BaseSize == BaseSize.Medium)
+                && (ship.AssignedManeuver.Speed == 3 || ship.AssignedManeuver.Speed == 4)
+                && new BoardTools.DistanceInfo(ship, HostShip).Range <= 1)
+            {
+                TargetShip = ship;
+                RegisterAbilityTrigger(TriggerTypes.OnMovementFinish, AskPerformBoostAction);
+            }
+        }
+
+        private void AskPerformBoostAction(object sender, System.EventArgs e)
+        {
+            Messages.ShowInfoToHuman(HostName + ": you may perform a red boost action");
+
+            TargetShip.AskPerformFreeAction(
+                new List<GenericAction>() { new BoostAction() { IsRed = true } },
+                Triggers.FinishTrigger
+            );
         }
     }
 }

61c5ce70708aef57b3e9816f6c349d936d465ffe
Sandrem <oogoeroo@gmail.com>
2019-02-26 17:47:38 +0200

    Dark Courier

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/SithInfiltrator/DarkCourier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Ship;
+using Upgrade;
+
+namespace Ship.SecondEdition.SithInfiltrator
+{
+    public class DarkCourier : SithInfiltrator
+    {
+        public DarkCourier()
+        {
+            PilotInfo = new PilotCardInfo(
+                "Dark Courier",
+                2,
+                50
+            );
+
+            RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
+
+            ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/e2/02/e20245ab-47e9-41f9-abdf-62f571246faf/swz30_dark-courier.png";
+        }
+    }
+}

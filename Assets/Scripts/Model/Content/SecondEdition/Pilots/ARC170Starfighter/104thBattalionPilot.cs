a59db3132138262759da4413b6e12e37e504449c
rune-b <rune@krokodille.com>
2019-03-16 21:02:40 +0100

    Cards from the ARC-170 preview
    104th Battalion Pilot
    Squad Seven Veteran
    Oddball
    R4-P44
    Jag

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/ARC170Starfighter/104thBattalionPilot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.ARC170Starfighter
+    {
+        public class P104thBattalionPilot : ARC170Starfighter
+        {
+            public P104thBattalionPilot() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "104th Battalion Pilot",
+                    2,
+                    45,
+                    factionOverride: Faction.Republic
+                );
+
+                RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
+
+                ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/0d/16/0d16748c-6591-4e97-96ee-8db6c89abca5/swz33_battalion-pilot.png";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/ARC170Starfighter/Jag.cs
@@ -0,0 +1,67 @@
+﻿using Ship;
+using System;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.ARC170Starfighter
+    {
+        public class Jag : ARC170Starfighter
+        {
+            public Jag() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Jag\"",
+                    3,
+                    50,
+                    isLimited: true,
+                    factionOverride: Faction.Republic,
+                    abilityType: typeof(Abilities.SecondEdition.JagAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+
+                RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
+
+                ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/06/ec/06ecf59a-74bb-425b-9b5e-0d90a76d3261/swz33_jag.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //After a friendly ship at range 1-2 in your left or right arc defends, you may acquire a lock on the attacker.
+    public class JagAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            GenericShip.OnAttackFinishGlobal += CheckAbility;
+        }
+        public override void DeactivateAbility()
+        {
+            GenericShip.OnAttackFinishGlobal -= CheckAbility;
+        }
+
+        private void CheckAbility(GenericShip ship)
+    {
+            var range = new BoardTools.DistanceInfo(HostShip, Combat.Defender).Range;
+
+            if (Combat.Defender.Owner == HostShip.Owner
+                && (HostShip.SectorsInfo.IsShipInSector(Combat.Attacker, Arcs.ArcType.Left) || HostShip.SectorsInfo.IsShipInSector(Combat.Attacker, Arcs.ArcType.Right))
+                && range >= 1 && range <= 2)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnAttackFinish, delegate
+                {
+                    AskToUseAbility(AlwaysUseByDefault, AcquireLock);
+                });                
+            }
+        }
+
+        private void AcquireLock(object sender, EventArgs e)
+        {
+            Messages.ShowInfo(HostName + ": Acquires lock on " + Combat.Attacker.PilotName);
+            ActionsHolder.AcquireTargetLock(HostShip, Combat.Attacker, SubPhases.DecisionSubPhase.ConfirmDecision, SubPhases.DecisionSubPhase.ConfirmDecision);
+        }
+    }
+}

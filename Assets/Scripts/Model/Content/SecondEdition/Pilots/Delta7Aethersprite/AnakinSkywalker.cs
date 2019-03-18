a2a3cb83de0c384571bb12682a5b302042384fd6
tdalious <thundertoe.marley@gmail.com>
2019-03-18 13:12:02 -0400

    Anakin Skywalker
    anakin skywalker leaks

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Delta7Aethersprite/AnakinSkywalker.cs
@@ -0,0 +1,99 @@
+using Ship;
+using System;
+using SubPhases;
+using System.Collections.Generic;
+using System.Linq;
+using Upgrade;
+using Team;
+using Tokens;
+using Movement;
+using BoardTools;
+using UnityEngine;
+
+namespace Ship.SecondEdition.Delta7Aethersprite
+{
+    public class AnakinSkywalker : Delta7Aethersprite
+    {
+        public AnakinSkywalker()
+        {
+            PilotInfo = new PilotCardInfo(
+                "Anakin Skywalker",
+                6,
+                68,
+                true,
+                force: 3,
+                abilityType: typeof(Abilities.SecondEdition.AnakinSkywalkerAbility),
+                extraUpgradeIcon: UpgradeType.Force
+            );
+
+            RequiredMods = new List<System.Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
+
+            ImageUrl = "https://i.imgur.com/MTqp3tr.jpg";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    // After you fully execute a maneuver, if you have an enemy ship in your primary arc at
+    // range 0-1 or in your bulls-eye arc, you may spend 1 force token to remove stress token.
+    public class AnakinSkywalkerAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnMovementFinishSuccessfully += RegisterCheckAnakinAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnMovementFinishSuccessfully -= RegisterCheckAnakinAbility;
+        }
+
+        private void RegisterCheckAnakinAbility(GenericShip ship)
+        {
+            if (isAnakinAbilityAvailable(ship)) {
+                RegisterAbilityTrigger(TriggerTypes.OnMovementFinish, AskToUseAnakin);
+            }
+        }
+
+        private void AskToUseAnakin(object sender, System.EventArgs e)
+        {
+            if (isAnakinAbilityAvailable(HostShip)) {
+                AskToUseAbility(AlwaysUseByDefault, UseAnakinAbility);
+            } else {
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private void UseAnakinAbility(object sender, System.EventArgs e) 
+        {
+            HostShip.Tokens.SpendToken(typeof(ForceToken), () => 
+                HostShip.Tokens.RemoveToken(
+                    typeof(StressToken),
+                    DecisionSubPhase.ConfirmDecision
+                ));
+        }
+
+        private bool isAnakinAbilityAvailable(GenericShip ship)
+        {
+            if (ship.AssignedManeuver.ColorComplexity == MovementComplexity.Complex
+                && ship.State.Force > 0 
+                && ship.Owner == HostShip.Owner)
+            {
+                int enemies = 0;
+                enemies += Board.GetShipsInBullseyeArc(HostShip, Team.Type.Enemy).Count;
+                foreach(var s in Board.GetShipsAtRange(HostShip, new Vector2(0,1), Team.Type.Enemy)) {
+                    if (Board.IsShipInArc(HostShip,s)) {
+                        enemies++;
+                    }
+                }
+                
+                if (enemies > 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+    }
+}

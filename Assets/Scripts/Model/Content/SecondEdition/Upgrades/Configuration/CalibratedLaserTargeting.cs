958b47b2c3b1dd190f55283eee9c46d3308c1498
rune-b <rune@krokodille.com>
2019-03-20 23:04:25 +0100

    Variable cost for Calibrated Laser Targeting

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Configuration/CalibratedLaserTargeting.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Configuration/CalibratedLaserTargeting.cs
@@ -19,7 +19,23 @@ namespace UpgradesList.SecondEdition
             );
             
             ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/4a/32/4a32d934-9d57-433c-8fb6-ce6c1cb52224/swz34_calibrated-laser-targeting.png";
-        }        
+        }
+
+        public void UpdateCost(GenericShip ship)
+        {
+            Dictionary<int, int> initiativeToCost = new Dictionary<int, int>()
+            {
+                {0, 0},
+                {1, 0},
+                {2, 2},
+                {3, 4},
+                {4, 6},
+                {5, 8},
+                {6, 10}
+            };
+
+            UpgradeInfo.Cost = initiativeToCost[ship.PilotInfo.Initiative];
+        }
     }
 }
 

88c316e646af8a844c6b64eeaa6cb239b30622da
rune-b <rune-b@users.noreply.github.com>
2019-03-23 00:15:14 +0100

    Some cards from the ARC-170 preview + Barriss, Luminara & Ahsoka (#1669)
    * Cards from the ARC-170 preview
    
    104th Battalion Pilot
    Squad Seven Veteran
    Oddball
    R4-P44
    Jag
    
    * Allow multiple dice modifications in one ability
    
    * Wolffe
    
    * Updated points and slots
    
    * Luminara and Barriss
    
    * Ahsoka Tano. Fixed bug with R4-P17

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/ARC170Starfighter/104thBattalionPilot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.ARC170Starfighter
+    {
+        public class P104thBattalionPilot : ARC170Starfighter
+        {
+            public P104thBattalionPilot() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "104th Battalion Pilot",
+                    2,
+                    42,
+                    factionOverride: Faction.Republic
+                );
+
+                ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/0d/16/0d16748c-6591-4e97-96ee-8db6c89abca5/swz33_battalion-pilot.png";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Astromech/R4P44.cs
@@ -0,0 +1,57 @@
+﻿using Upgrade;
+using Ship;
+using Movement;
+using System;
+using BoardTools;
+using System.Linq;
+
+namespace UpgradesList.SecondEdition
+{
+    public class R4P44 : GenericUpgrade
+    {
+        public R4P44() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "R4-P44",
+                UpgradeType.Astromech,
+                cost: 5, 
+                abilityType: typeof(Abilities.SecondEdition.R4P44Ability),
+                restriction: new FactionRestriction(Faction.Republic)
+                //seImageNumber: ?
+            );
+            ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/71/af/71afc8f6-022b-48f7-9694-83e6322782b2/swz33_r4-p44-upgrade.png";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //After you fully execute a red maneuver, if there is an enemy ship in your bullseye arc, gain 1 calculate token.
+    public class R4P44Ability : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnMovementFinishSuccessfully += RegisterMovementTrigger;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnMovementFinishSuccessfully -= RegisterMovementTrigger;
+        }
+
+
+        private void RegisterMovementTrigger(GenericShip ship)
+        {
+            if (HostShip.GetLastManeuverColor() == MovementComplexity.Complex && Board.GetShipsInBullseyeArc(HostShip, Team.Type.Enemy).Any())
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnMovementFinish, GainCalculateToken);
+            }
+        }
+
+        private void GainCalculateToken(object sender, EventArgs e)
+        {
+            Messages.ShowInfo(HostName + ": " + HostShip + " gains 1 calculate token");
+            HostShip.Tokens.AssignToken(typeof(Tokens.CalculateToken), Triggers.FinishTrigger);
+        }
+    }
+}

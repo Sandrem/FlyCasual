e8373f0e46823e1a268748fdff0d1450cb966162
Sandrem <oogoeroo@gmail.com>
2019-04-16 01:06:55 +0300

    BTLB-B Y-wing and Shadow Squadron Veteran

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/BTLBYWing/ShadowSquadronVeteran.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.BTLBYWing
+    {
+        public class ShadowSquadronVeteran : BTLBYWing
+        {
+            public ShadowSquadronVeteran() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Shadow Squadron Veteran",
+                    3,
+                    33,
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+
+                ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/b6/ca/b6cad4cc-120c-4637-8d49-3c9dba68f6ed/swz_w5_pilot-shadow-sqd-vet.png";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/BTLBYWing.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Movement;
+using ActionsList;
+using Upgrade;
+using Actions;
+using Arcs;
+using BoardTools;
+
+namespace Ship.SecondEdition.BTLBYWing
+{
+    public class BTLBYWing : GenericShip
+    {
+        public BTLBYWing() : base()
+        {
+            ShipInfo = new ShipCardInfo
+            (
+                "BTL-B Y-wing",
+                BaseSize.Small,
+                Faction.Republic,
+                new ShipArcsInfo(ArcType.Front, 2), 1, 5, 3,
+                new ShipActionsInfo(
+                    new ActionInfo(typeof(FocusAction)),
+                    new ActionInfo(typeof(TargetLockAction)),
+                    new ActionInfo(typeof(BarrelRollAction), ActionColor.Red),
+                    new ActionInfo(typeof(ReloadAction), ActionColor.Red)
+                ),
+                new ShipUpgradesInfo(
+                    UpgradeType.Title,
+                    UpgradeType.Modification,
+                    UpgradeType.Turret,
+                    UpgradeType.Torpedo,
+                    UpgradeType.Gunner,
+                    UpgradeType.Bomb,
+                    UpgradeType.Astromech
+                )
+            );
+
+            IconicPilots = new Dictionary<Faction, System.Type> {
+                { Faction.Republic, typeof(ShadowSquadronVeteran) }
+            };
+
+            ModelInfo = new ShipModelInfo(
+                "Y-wing",
+                "Yellow"
+            );
+
+            DialInfo = new ShipDialInfo(
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Normal),
+
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Complex),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Complex),
+
+                new ManeuverInfo(ManeuverSpeed.Speed4, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Complex),
+                new ManeuverInfo(ManeuverSpeed.Speed4, ManeuverDirection.Forward, ManeuverBearing.Reverse, MovementComplexity.Complex)
+            );
+
+            SoundInfo = new ShipSoundInfo(
+                new List<string>()
+                {
+                    "YWing-Fly1",
+                    "YWing-Fly2"
+                },
+                "XWing-Laser", 3
+            );
+
+            //ManeuversImageUrl
+
+            OldShipTypeName = "Y-wing";
+        }
+    }
+}

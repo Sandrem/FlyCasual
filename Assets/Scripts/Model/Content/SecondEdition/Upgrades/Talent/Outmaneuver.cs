b8a65361ea4a210b63c6a0de5a212c1ce56ef7e7
Benny Tsai <benny.tsai@gmail.com>
2019-04-17 09:57:44 -0700

    Outmaneuver should work only if attack uses front sector

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/Outmaneuver.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/Outmaneuver.cs
@@ -25,7 +25,7 @@ namespace Abilities.SecondEdition
     {
         protected override bool AbilityCanBeUsed()
         {
-            if (!Combat.ShotInfo.InArcByType(Arcs.ArcType.Front)) return false;
+            if (!Combat.ChosenWeapon.WeaponInfo.ArcRestrictions.Contains(Arcs.ArcType.Front)) return false;
 
             BoardTools.ShotInfo reverseShotInfo = new BoardTools.ShotInfo(Combat.Defender, Combat.Attacker, Combat.Defender.PrimaryWeapons);
             if (reverseShotInfo.InArc) return false;

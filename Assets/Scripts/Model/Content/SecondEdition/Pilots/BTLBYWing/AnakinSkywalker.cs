4689f7afc4db64c05af71ef3ee8d24d0b5dc2f1e
Sandrem <oogoeroo@gmail.com>
2019-05-03 21:04:19 +0300

    BTL-B Y-wing: Anakin and Odd Ball

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/BTLBYWing/AnakinSkywalker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.BTLBYWing
+    {
+        public class AnakinSkywalker : BTLBYWing
+        {
+            public AnakinSkywalker() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Anakin Skywalker",
+                    6,
+                    40,
+                    isLimited: true,
+                    extraUpgradeIcon: UpgradeType.Force,
+                    force: 3,
+                    abilityText: "After you fully execute a maneuver, if there is an enemy ship in your standard front arc at range 0-1 or in your bullseye arc, you may spend 1 force to remove 1 stress token.",
+                    abilityType: typeof(Abilities.SecondEdition.AnakinSkywalkerAbility)
+                );
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/BTLBYWing/OddBall.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.BTLBYWing
+    {
+        public class OddBall : BTLBYWing
+        {
+            public OddBall() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Odd Ball\"",
+                    5,
+                    35,
+                    isLimited: true,
+                    extraUpgradeIcon: UpgradeType.Talent,
+                    abilityText: "After you fully execute a red maneuver or perform a red action, if there is an enemy ship in your bullseye arc, you may acquire a lock on that ship.",
+                    abilityType: typeof(Abilities.SecondEdition.OddBallAbility)
+                );
+            }
+        }
+    }
+}

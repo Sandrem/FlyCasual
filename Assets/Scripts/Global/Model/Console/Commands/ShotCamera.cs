665af79c5eec85593fb4d9b5d4d6bb9b237b1031
Sandrem <oogoeroo@gmail.com>
2019-05-08 12:00:39 +0300

    Position of Dynamic Camera is fixed

--- Assets/Scripts/Global/Model/Console/Commands/ShotCamera.cs
+++ Assets/Scripts/Global/Model/Console/Commands/ShotCamera.cs
@@ -61,18 +61,19 @@ namespace CommandsList
 
         public static void ShowShotCamera(GenericShip attacker, GenericShip defender)
         {
-            Vector3 position = attacker.GetPosition();
+            Vector3 position = attacker.GetCenter();
             Transform directionTransform = defender.GetModelTransform();
-            Vector3 direction = defender.GetPosition();
+            Vector3 direction = defender.GetCenter();
 
-            float shiftBackSize = (attacker.ShipBase.Size == BaseSize.Large) ? 2f : 1.5f;
-            float shiftRightSize = (attacker.ShipBase.Size == BaseSize.Large) ? -1.5f : -1f;
+            float shiftBackSize = (attacker.ShipBase.Size == BaseSize.Large) ? 1f : 0.75f;
+            float shiftRightSize = (attacker.ShipBase.Size == BaseSize.Large) ? 1f : 0.75f;
             float shiftUpSize = (attacker.ShipBase.Size == BaseSize.Large) ? 1.5f : 1f;
 
-            Vector3 vectorToTarget = direction - position;
-            Vector3 vectorLeft = new Vector3(-vectorToTarget.z, 0, -vectorToTarget.x);
-            Vector3 shift = vectorToTarget.normalized * -1 * shiftBackSize
-                + vectorLeft.normalized * shiftRightSize
+            Vector3 vectorToTarget = (direction - position).normalized;
+            Vector3 vectorLeft = new Vector3(vectorToTarget.z, 0, -vectorToTarget.x);
+
+            Vector3 shift = vectorToTarget * -1 * shiftBackSize
+                + vectorLeft * shiftRightSize
                 + new Vector3(0, shiftUpSize, 0);
             CameraScript.AnimateChangePosition(position + shift, directionTransform);
         }

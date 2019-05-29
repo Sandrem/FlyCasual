921f5fd0cf3756a6bfc0146257e8b1533f7ce7a6
Sandrem <oogoeroo@gmail.com>
2019-05-29 03:03:06 +0300

    Qi'Ra

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/QiRa.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Obstacles;
+using Ship;
+using Tokens;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class QiRa : GenericUpgrade
+    {
+        public QiRa()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+            "Qi'Ra",
+            UpgradeType.Crew,
+            cost: 2,
+            abilityType: typeof(Abilities.SecondEdition.QiRaAbility),
+            seImageNumber: 161,
+            isLimited: true,
+            restriction: new FactionRestriction(Faction.Scum));
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class QiRaAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnTargetLockIsAcquired += CheckAbility;
+            HostShip.OnBeforeTokenIsRemoved += CheckRemoveAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnTargetLockIsAcquired -= CheckAbility;
+            HostShip.OnBeforeTokenIsRemoved -= CheckRemoveAbility;
+        }
+
+        private void CheckAbility(ITargetLockable target)
+        {
+            if (target is GenericObstacle)
+            {
+                Messages.ShowInfo("Qi'Ra: While you move or perform attack, you ignore this obstacle");
+                HostShip.IgnoreObstaclesList.Add(target as GenericObstacle);
+            }
+        }
+
+        private void CheckRemoveAbility(GenericShip ship, GenericToken token, ref bool data)
+        {
+            BlueTargetLockToken lockToken = token as BlueTargetLockToken;
+            if (lockToken != null)
+            {
+                GenericObstacle lockedObstacle = lockToken.OtherTargetLockTokenOwner as GenericObstacle;
+                if (lockedObstacle != null)
+                {
+                    if (HostShip.Tokens.GetTargetLockLetterPairsOn(lockedObstacle).Count == 1)
+                    {
+                        Messages.ShowInfo("Qi'Ra: You don't ignore obstacle anymore");
+                        HostShip.IgnoreObstaclesList.Remove(lockedObstacle);
+                    }
+                }
+            }
+        }
+    }
+}

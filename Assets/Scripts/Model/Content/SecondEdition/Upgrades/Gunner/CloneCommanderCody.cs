354a2f56e410c8f2fdb06136f1498373f58a8887
Benny Tsai <benny.tsai@gmail.com>
2019-07-04 02:16:22 -0700

    Cmdr. Cody

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Gunner/CloneCommanderCody.cs
@@ -0,0 +1,68 @@
+﻿using Ship;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class CloneCommanderCody : GenericUpgrade
+    {
+        public CloneCommanderCody() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+				"Clone Commander Cody",
+                UpgradeType.Gunner,
+                cost: 3,
+                isLimited: true,
+                restriction: new FactionRestriction(Faction.Republic),
+                abilityType: typeof(Abilities.SecondEdition.CloneCommanderCodyAbility)
+            );
+
+            ImageUrl = "https://vignette.wikia.nocookie.net/xwing-miniatures-second-edition/images/d/d7/Swz33_cody-upgrade.png";
+        }        
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //After you perform an attack that missed, if 1 or more hit/crit results were neutralized, the defender gains 1 strain token.
+    public class CloneCommanderCodyAbility : GenericAbility
+    {
+        int hitOrCritResults;
+
+        public override void ActivateAbility()
+        {
+            HostShip.OnDefenceStartAsAttacker += SaveHitOrCritResults;
+            HostShip.OnAttackMissedAsAttacker += RegisterAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnDefenceStartAsAttacker -= SaveHitOrCritResults;
+            HostShip.OnAttackMissedAsAttacker -= RegisterAbility;
+        }
+
+        private void SaveHitOrCritResults()
+        {
+            hitOrCritResults = Combat.DiceRollAttack.Successes;
+        }
+
+        private void RegisterAbility()
+        {
+            if (hitOrCritResults > 0)
+            {
+                HostShip.OnCombatDeactivation += RegisterTrigger;
+            }
+        }
+
+        private void RegisterTrigger(GenericShip ship)
+        {
+            HostShip.OnCombatDeactivation -= RegisterTrigger;
+            RegisterAbilityTrigger(TriggerTypes.OnCombatDeactivation, AssignStrainToDefender);
+        }
+
+        private void AssignStrainToDefender(object sender, System.EventArgs e)
+        {
+            Messages.ShowInfo(HostUpgrade.UpgradeInfo.Name + " assigned a strain token to " + Combat.Defender.PilotInfo.PilotName);
+            Combat.Defender.Tokens.AssignToken(typeof(Tokens.StrainToken), Triggers.FinishTrigger);
+        }
+    }
+}

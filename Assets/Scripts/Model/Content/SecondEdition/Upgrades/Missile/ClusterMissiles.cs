8c08065d0add5293e55db9bf82e003caadccd489
Sandrem <oogoeroo@gmail.com>
2019-07-15 21:11:06 +0300

    Cluster missiles saves distance between original defender and other ships

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Missile/ClusterMissiles.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Missile/ClusterMissiles.cs
@@ -1,5 +1,6 @@
 ﻿using BoardTools;
 using Ship;
+using System.Collections.Generic;
 using System.Linq;
 using Tokens;
 using Upgrade;
@@ -33,6 +34,7 @@ namespace Abilities.SecondEdition
     public class CluseterMissilesAbility : GenericAbility
     {
         private GenericShip OriginalDefender;
+        private List<GenericShip> TargetsInRange;
 
         public override void ActivateAbility()
         {
@@ -51,6 +53,13 @@ namespace Abilities.SecondEdition
         private void SaveOriginalDefender()
         {
             OriginalDefender = Combat.Defender;
+            TargetsInRange = new List<GenericShip>();
+
+            foreach (GenericShip ship in Roster.AllShips.Values)
+            {
+                DistanceInfo distInfo = new DistanceInfo(OriginalDefender, ship);
+                if (distInfo.Range < 2) TargetsInRange.Add(ship);
+            }
         }
 
         private void CheckClusterMissilesAbility(GenericShip ship)
@@ -74,13 +83,8 @@ namespace Abilities.SecondEdition
         {
             bool result = false;
 
-            foreach (var ship in OriginalDefender.Owner.Ships.Values)
+            foreach (var ship in TargetsInRange)
             {
-                if (ship.ShipId == OriginalDefender.ShipId) continue;
-
-                DistanceInfo distInfo = new DistanceInfo(ship, OriginalDefender);
-                if (distInfo.Range > 1) continue;
-
                 ShotInfo shotInfo = new ShotInfo(HostShip, ship, HostUpgrade as IShipWeapon);
                 if (shotInfo.IsShotAvailable) return true;
             }
@@ -112,8 +116,7 @@ namespace Abilities.SecondEdition
             {
                 if (defender.ShipId != OriginalDefender.ShipId)
                 {
-                    DistanceInfo distInfo = new DistanceInfo(OriginalDefender, defender);
-                    if (distInfo.Range < 2)
+                    if (TargetsInRange.Contains(defender))
                     {
                         result = true;
                     }

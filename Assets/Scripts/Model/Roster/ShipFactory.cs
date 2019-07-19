2cb1dcef18f84ffdf25f8cb4b997e9dd1c0c9b34
Benny Tsai <benny.tsai@gmail.com>
2019-07-19 14:48:09 -0700

    remove last reference to BonusAttack rule

--- Assets/Scripts/Model/Roster/ShipFactory.cs
+++ Assets/Scripts/Model/Roster/ShipFactory.cs
@@ -43,7 +43,6 @@ public static class ShipFactory {
         newShipContainer.OnGenerateDiceModifications += Rules.Force.AddForceAction;
         newShipContainer.OnRoundEnd += Rules.Force.RegenerateForce;
         newShipContainer.OnRoundEnd += Rules.Charge.RegenerateCharge;
-        newShipContainer.OnRoundEnd += Rules.BonusAttack.ResetCanBonusAttack;
         newShipContainer.OnShipIsRemoved += Rules.Destruction.WhenShipIsRemoved;
         newShipContainer.OnActionIsPerformed_System += Rules.Actions.ActionColorCheck;
         newShipContainer.OnActionIsPerformed += Rules.Actions.CheckLinkedAction;

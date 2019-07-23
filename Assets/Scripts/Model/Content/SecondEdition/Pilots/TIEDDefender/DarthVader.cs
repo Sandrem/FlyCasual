cb547e3cbe0e8b482d77d0d23b84b116dcc8c2b3
rune-b <rune@krokodille.com>
2019-07-23 09:49:44 +0200

    Mod: Vader in defender

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEDDefender/DarthVader.cs
@@ -0,0 +1,29 @@
+﻿using Mods.ModsList;
+using System;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEDDefender
+    {
+        public class DarthVader : TIEDDefender
+        {
+            public DarthVader() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Darth Vader",
+                    6,
+                    125,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.DarthVaderAbility),
+                    force: 3,
+                    extraUpgradeIcon: UpgradeType.Force
+                );
+
+                RequiredMods = new List<Type>() { typeof(VaderInDefender) };
+                ImageUrl = "https://i.imgur.com/1YLmbYQ.png";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Mods/ModsList/VaderInDefender.cs
@@ -0,0 +1,15 @@
+﻿namespace Mods
+{
+    namespace ModsList
+    {
+        public class VaderInDefender : Mod
+        {
+            public VaderInDefender()
+            {
+                Name = "Vader in Defender";
+                Description = "Darth Vader as a TIE Defender pilot";
+                EditionType = typeof(Editions.SecondEdition);
+            }
+        }
+    }
+}

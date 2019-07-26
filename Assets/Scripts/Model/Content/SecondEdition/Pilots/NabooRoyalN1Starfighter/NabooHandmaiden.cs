89af0999cc81f45855b09fbfc07899624d5a9df1
Sandrem <oogoeroo@gmail.com>
2019-07-26 08:40:43 +0300

    Names of triggers are updated

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/NabooRoyalN1Starfighter/NabooHandmaiden.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/NabooRoyalN1Starfighter/NabooHandmaiden.cs
@@ -56,7 +56,7 @@ namespace Abilities.SecondEdition
         {
             Triggers.RegisterTrigger(new Trigger()
             {
-                Name = "Naboo Handmaiden decision",
+                Name = HostShip.ShipId + ": Assign \"Decoyed\" condition",
                 TriggerType = TriggerTypes.OnSetupEnd,
                 TriggerOwner = HostShip.Owner.PlayerNo,
                 EventHandler = SelectNabooHandmaidenTarget,
@@ -116,8 +116,7 @@ namespace Conditions
 
         public Decoyed(GenericShip host) : base(host)
         {
-            Name = "Decoyed Condition";
-            ImageName = "Buff Token";
+            Name = ImageName = "Decoyed Condition";
             Temporary = false;
             Tooltip = "https://images-cdn.fantasyflightgames.com/filer_public/7e/38/7e38aca8-b0ea-4ddc-8ec4-64efca1544c8/swz40_decoyed.png";
         }

3d91715e86006fcf68f388de65b8c097c6f41233
Benny Tsai <benny.tsai@gmail.com>
2019-07-26 11:53:46 -0700

    fix Nantex stats

--- Assets/Scripts/Model/Content/SecondEdition/Ships/NantexClassStarfighter.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/NantexClassStarfighter.cs
@@ -24,7 +24,7 @@ namespace Ship.SecondEdition.NantexClassStarfighter
                 "Nantex-class Starfighter",
                 BaseSize.Small,
                 Faction.Separatists,
-                new ShipArcsInfo(ArcType.Bullseye, 3), 2, 5, 0,
+                new ShipArcsInfo(ArcType.Bullseye, 3), 3, 4, 0,
                 new ShipActionsInfo(
                     new ActionInfo(typeof(FocusAction)),
                     new ActionInfo(typeof(EvadeAction))

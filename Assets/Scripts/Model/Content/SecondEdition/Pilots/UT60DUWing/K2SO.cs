2bce7b7599b4cc8aa80d6bd58f5eecacdf14e9e7
Sandrem <oogoeroo@gmail.com>
2019-08-01 23:09:32 +0300

    K-2SO (U-Wing)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/UT60DUWing/K2SO.cs
@@ -0,0 +1,65 @@
+﻿using Abilities.SecondEdition;
+using ActionsList;
+using Ship;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Tokens;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.UT60DUWing
+    {
+        public class K2SO : UT60DUWing
+        {
+            public K2SO() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "K-2SO",
+                    3,
+                    47,
+                    isLimited: true,
+                    abilityType: typeof(K2SOPilotAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+
+                ShipInfo.ActionIcons.SwitchToDroidActions();
+
+                ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/56/cb/56cb9ec8-0eff-4c6f-acda-f54413baadc7/swz66_k-2so.png";
+
+                RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class K2SOPilotAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnTokenIsAssigned += RegisterSoontirFelAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnTokenIsAssigned -= RegisterSoontirFelAbility;
+        }
+
+        private void RegisterSoontirFelAbility(GenericShip ship, Type tokenType)
+        {
+            if (tokenType == typeof(StressToken))
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnTokenIsAssigned, AssignToken);
+            }
+        }
+
+        private void AssignToken(object sender, EventArgs e)
+        {
+            Messages.ShowInfo("K-2SO: Calculate Token is assigned");
+            HostShip.Tokens.AssignToken(typeof(CalculateToken), Triggers.FinishTrigger);
+        }
+    }
+}

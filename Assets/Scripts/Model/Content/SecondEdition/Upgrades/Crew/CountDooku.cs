1f1857c73e4d7d2f929dc568ff2756ac191a8b9e
rune-b <rune-b@users.noreply.github.com>
2019-08-05 07:23:08 +0200

    Count Dooku crew (#2155)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/CountDooku.cs
@@ -0,0 +1,152 @@
+﻿using Actions;
+using Ship;
+using SubPhases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class CountDooku : GenericUpgrade
+    {
+        public CountDooku() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Count Dooku",
+                UpgradeType.Crew,
+                cost: 10,
+                isLimited: true,
+                addForce: 1,
+                restriction: new FactionRestriction(Faction.Separatists),
+                abilityType: typeof(Abilities.SecondEdition.CountDookuCrewAbility)
+            );
+
+            ImageUrl = "https://sb-cdn.fantasyflightgames.com/card_images/en/a1920988dd8634d9780c6363e3f36f6e.png";
+        }
+    }
+}
+
+
+namespace Abilities.SecondEdition
+{
+    //Before a ship at range 0-2 rolls attack or defense dice, if all of your [Force] are active, you may spend 1 [Force] and name a result. 
+    //If the roll does not contain the named result, the ship must change 1 die to that result.
+    public class CountDookuCrewAbility : GenericAbility
+    {
+        private DieSide NamedResult;
+
+        public override void ActivateAbility()
+        {
+            GenericShip.OnDiceAboutToBeRolled += CheckDookuAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            GenericShip.OnDiceAboutToBeRolled -= CheckDookuAbility;
+        }
+        
+        private void CheckDookuAbility()
+        {
+            GenericShip targetShip = null;
+            if (Combat.AttackStep == CombatStep.Attack) targetShip = Combat.Attacker;
+            else if (Combat.AttackStep == CombatStep.Defence) targetShip = Combat.Defender;
+
+            if (targetShip != null && HostShip.GetRangeToShip(targetShip) <= 2 && HostShip.State.Force == HostShip.State.MaxForce)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnDiceAboutToBeRolled, StartQuestionSubphase);
+                TargetShip = targetShip;
+            }
+        }
+
+        private void StartQuestionSubphase(object sender, EventArgs e)
+        {
+            if (HostShip.State.Force == HostShip.State.MaxForce)
+            {
+                var newSubPhase = (DecisionSubPhase)Phases.StartTemporarySubPhaseNew(
+                    Name,
+                    typeof(DecisionSubPhase),
+                    Triggers.FinishTrigger
+                );
+
+                newSubPhase.DescriptionShort = HostName;
+                newSubPhase.DescriptionLong = "You may spend 1 force to name a result. If the roll does not contain the named result, the ship must change 1 die to that result";
+                newSubPhase.ImageSource = HostShip;
+
+                newSubPhase.RequiredPlayer = HostShip.Owner.PlayerNo;
+                newSubPhase.ShowSkipButton = true;
+
+                if (Combat.AttackStep == CombatStep.Attack)
+                {
+                    newSubPhase.AddDecision("Critical Hit", delegate { NameResult(DieSide.Crit, "Critical Hit"); });
+                    newSubPhase.AddDecision("Hit", delegate { NameResult(DieSide.Success, "Hit"); });
+                }
+                else if (Combat.AttackStep == CombatStep.Defence)
+                {
+                    newSubPhase.AddDecision("Evade", delegate { NameResult(DieSide.Success, "Evade"); });
+                }
+
+                newSubPhase.AddDecision("Focus", delegate { NameResult(DieSide.Focus, "Focus"); });
+                newSubPhase.AddDecision("Blank", delegate { NameResult(DieSide.Blank, "Blank"); });
+
+                newSubPhase.DefaultDecisionName = GetDefaultDecision();
+                newSubPhase.OnSkipButtonIsPressed = Skip;
+                newSubPhase.Start();
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private void Skip()
+        {
+            NamedResult = DieSide.Unknown;
+        }
+
+        private string GetDefaultDecision()
+        {
+            //Very simple AI decision code.
+            
+            if (TargetShip.Owner == HostShip.Owner)
+            {
+                if (Combat.AttackStep == CombatStep.Attack)
+                {
+                    return "Critical Hit";
+                }
+                else
+                {
+                    return "Evade";
+                }
+            }
+            else
+            {
+                return "Blank";
+            }
+        }
+
+        private void NameResult(DieSide dieSide, string dieDescription)
+        {
+            HostShip.State.Force--;
+            NamedResult = dieSide;
+            Messages.ShowInfo($"{HostName} chooses '{dieDescription}'.");
+            TargetShip.OnImmediatelyAfterRolling += HandleDiceChange;
+            DecisionSubPhase.ConfirmDecision();
+        }
+
+
+        private void HandleDiceChange(DiceRoll diceroll)
+        {
+            TargetShip.OnImmediatelyAfterRolling -= HandleDiceChange;
+            if (NamedResult != DieSide.Unknown && !diceroll.HasResult(NamedResult))
+            {
+                diceroll.ChangeWorstResultTo(NamedResult);
+                
+                Messages.ShowInfo($"{HostName} changes one result to {NamedResult}");
+            }
+            NamedResult = DieSide.Unknown;
+        }
+    }
+}

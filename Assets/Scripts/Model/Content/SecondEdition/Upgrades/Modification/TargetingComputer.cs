923275738a08a45911f29fa1c9218441db41c915
Sandrem <oogoeroo@gmail.com>
2019-08-13 23:22:08 +0300

    Targeting Computer

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Modification/TargetingComputer.cs
@@ -0,0 +1,25 @@
+﻿using Actions;
+using ActionsList;
+using Ship;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class TargetingComputer : GenericUpgrade
+    {
+        public TargetingComputer() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Targeting Computer",
+                UpgradeType.Modification,
+                cost: 2,
+                addAction: new ActionInfo(typeof(TargetLockAction))
+            );
+
+            ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/0b/d7/0bd7d42f-4401-4f58-9f9e-a5856e6c94f1/swz47_upgrade-targeting-computer.png";
+
+            FromMod = typeof(Mods.ModsList.UnreleasedContentMod);
+        }
+    }
+}

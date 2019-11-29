ae7feed399f004952a7732459bd666256bcaf302
Benny Tsai <benny.tsai@gmail.com>
2019-11-28 18:57:11 -0800

    Y-wing Oddball ability doesn't need to fully execute maneuver (#2336)

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/BTLBYWing/OddBall.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/BTLBYWing/OddBall.cs
@@ -15,12 +15,32 @@ namespace Ship
                     44,
                     isLimited: true,
                     extraUpgradeIcons: new List<UpgradeType> { UpgradeType.Talent, UpgradeType.Astromech },
-                    abilityText: "After you fully execute a red maneuver or perform a red action, if there is an enemy ship in your bullseye arc, you may acquire a lock on that ship.",
-                    abilityType: typeof(Abilities.SecondEdition.OddBallAbility)
+                    abilityText: "After you execute a red maneuver or perform a red action, if there is an enemy ship in your bullseye arc, you may acquire a lock on that ship.",
+                    abilityType: typeof(Abilities.SecondEdition.OddBallBTLBYwingAbility)
                 );
 
                 ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/99/a7/99a78a22-4e8c-4197-a7fb-2163746daa90/swz48_pilot-odd-ball.png";
             }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //After you execute a red maneuver or perform a red action, if there is an enemy ship in your bullseye arc,
+    //you may acquire a lock on that ship.
+    public class OddBallBTLBYwingAbility : OddBallAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnActionIsPerformed += CheckConditions;
+            HostShip.OnMovementFinish += RegisterMovementTrigger;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActionIsPerformed -= CheckConditions;
+            HostShip.OnMovementFinish -= RegisterMovementTrigger;
         }
     }
 }

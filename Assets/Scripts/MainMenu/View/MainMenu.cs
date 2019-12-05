65f115b6b4b80db067dbe0762c1e5c633bed94a3
Sandrem <oogoeroo@gmail.com>
2019-12-05 17:23:39 +0200

    Dice Stats Size Ui Fixer

--- Assets/Scripts/MainMenu/View/MainMenu.cs
+++ Assets/Scripts/MainMenu/View/MainMenu.cs
@@ -65,8 +65,10 @@ public partial class MainMenu : MonoBehaviour {
                 UpdatePlayerInfo();
                 break;
             case "OptionsPanel":
-                //Options.InitializePanel();
                 OptionsUI.Instance.InitializeOptionsPanel();
+                break;
+            case "StatsPanel":
+                StatsUI.Instance.InitializeStatsPanel();
                 break;
             case "ModsPanel":
                 ModsManager.InitializePanel();

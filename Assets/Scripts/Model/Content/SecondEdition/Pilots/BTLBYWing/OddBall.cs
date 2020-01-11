3e60d5c2fce94111df950122e30f9cbd6def7666
rune-b <rune@krokodille.com>
2020-01-11 16:40:46 +0100

    Static Disccharge Vanes now cause damage

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/BTLBYWing/OddBall.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/BTLBYWing/OddBall.cs
@@ -15,32 +15,13 @@ namespace Ship
                     44,
                     isLimited: true,
                     extraUpgradeIcons: new List<UpgradeType> { UpgradeType.Talent, UpgradeType.Astromech },
-                    abilityText: "After you execute a red maneuver or perform a red action, if there is an enemy ship in your bullseye arc, you may acquire a lock on that ship.",
-                    abilityType: typeof(Abilities.SecondEdition.OddBallBTLBYwingAbility)
+                    //January 2020 errata: Should read: "After you fully execute...":
+                    abilityText: "After you fully execute a red maneuver or perform a red action, if there is an enemy ship in your bullseye arc, you may acquire a lock on that ship.",
+                    abilityType: typeof(Abilities.SecondEdition.OddBallAbility)
                 );
 
                 ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/99/a7/99a78a22-4e8c-4197-a7fb-2163746daa90/swz48_pilot-odd-ball.png";
             }
-        }
-    }
-}
-
-namespace Abilities.SecondEdition
-{
-    //After you execute a red maneuver or perform a red action, if there is an enemy ship in your bullseye arc,
-    //you may acquire a lock on that ship.
-    public class OddBallBTLBYwingAbility : OddBallAbility
-    {
-        public override void ActivateAbility()
-        {
-            HostShip.OnActionIsPerformed += CheckConditions;
-            HostShip.OnMovementFinish += RegisterMovementTrigger;
-        }
-
-        public override void DeactivateAbility()
-        {
-            HostShip.OnActionIsPerformed -= CheckConditions;
-            HostShip.OnMovementFinish -= RegisterMovementTrigger;
         }
     }
 }

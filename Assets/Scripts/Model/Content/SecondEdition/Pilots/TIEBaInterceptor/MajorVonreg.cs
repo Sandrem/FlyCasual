ab6199ceaa73adb971d8d979d94b6d1dbc6c2a95
Sandrem <oogoeroo@gmail.com>
2020-01-21 18:10:55 +0200

    Cost of Major Vonreg is fixed

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEBaInterceptor/MajorVonreg.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEBaInterceptor/MajorVonreg.cs
@@ -17,7 +17,7 @@ namespace Ship
                 PilotInfo = new PilotCardInfo(
                     "Major Vonreg",
                     6,
-                    55,
+                    57,
                     isLimited: true,
                     abilityType: typeof(Abilities.SecondEdition.MajorVonregAbility)
                 );

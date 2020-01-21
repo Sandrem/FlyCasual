a8a5537e5566c3eded5ddf326d5ca261d1850393
Sandrem <oogoeroo@gmail.com>
2020-01-21 18:09:00 +0200

    Version is changed to 0.9.7

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -10,9 +10,9 @@ public class Global : MonoBehaviour {
 
     private static bool isAlreadyInitialized;
 
-    public static string CurrentVersion = "1.0.0";
-    public static int CurrentVersionInt = 101000000;
-    public static int LatestVersionInt = 101000000;
+    public static string CurrentVersion = "0.9.7";
+    public static int CurrentVersionInt = 100090700;
+    public static int LatestVersionInt = 100090700;
 
     void Awake()
     {

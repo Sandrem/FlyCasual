d294c65ec1a16a60a9caea04c59cb6c5a03ac249
Sandrem <oogoeroo@gmail.com>
2020-02-20 21:21:33 +0200

    "Rush"

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEVnSilencer/Rush.cs
@@ -0,0 +1,55 @@
+﻿using ActionsList;
+using Ship;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEVnSilencer
+    {
+        public class Rush : TIEVnSilencer
+        {
+            public Rush() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Rush\"",
+                    2,
+                    57,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.RushAbility)
+                );
+
+                ImageUrl = "https://sb-cdn.fantasyflightgames.com/card_images/en/886d715885da65bdf10ad7c68e4d0a93.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class RushAbility : GenericAbility, IModifyPilotSkill
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnDamageCardIsDealt += ApplyInitiative;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnDamageCardIsDealt -= ApplyInitiative;
+            HostShip.State.RemovePilotSkillModifier(this);
+        }
+
+        private void ApplyInitiative(GenericShip ship)
+        {
+            HostShip.OnDamageCardIsDealt -= ApplyInitiative;
+            HostShip.State.AddPilotSkillModifier(this);
+        }
+
+        public void ModifyPilotSkill(ref int pilotSkill)
+        {
+            Messages.ShowInfo(HostShip.PilotInfo.PilotName + ": Iniative is set to 6");
+            pilotSkill = 6;
+        }
+    }
+}

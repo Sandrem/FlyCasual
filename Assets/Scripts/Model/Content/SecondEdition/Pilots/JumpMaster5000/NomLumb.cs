6425885bd6bc9f42ce7a2b308e5b96b9948feefc
Sandrem <oogoeroo@gmail.com>
2020-02-21 12:38:06 +0200

    Finished

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/JumpMaster5000/NomLumb.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/JumpMaster5000/NomLumb.cs
@@ -1,6 +1,7 @@
 ﻿using Arcs;
 using BoardTools;
 using Ship;
+using SubPhases;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -35,7 +36,7 @@ namespace Abilities.SecondEdition
         // After you become the defender, if the attacker is not in your single turret arc, you must rotate your
         // single turret arc indicator to a standard arc the attacker is in.
 
-        // TODO: Allow to select arc if in 2
+        List<ArcFacing> FacingsToAttacker = new List<ArcFacing>();
 
         public override void ActivateAbility()
         {
@@ -53,35 +54,97 @@ namespace Abilities.SecondEdition
             if (!reverseShotInfo.InArcByType(ArcType.SingleTurret))
             {
                 Messages.ShowInfo(HostShip.PilotInfo.PilotName + ": Attacker is not in turret arc");
-
-                List<ArcType> standatdArcs = new List<ArcType>() { ArcType.Front, ArcType.Left, ArcType.Right, ArcType.Rear };
-                foreach (ArcType facing in standatdArcs)
+                
+                FacingsToAttacker = GetFacingsToAttacker();
+                if (FacingsToAttacker.Count == 1)
                 {
-                    if (HostShip.SectorsInfo.IsShipInSector(Combat.Attacker, facing))
-                    {
-                        ArcFacing direction = ArcFacing.Front;
-                        switch (facing)
-                        {
-                            case ArcType.Front:
-                                direction = ArcFacing.Front;
-                                break;
-                            case ArcType.Rear:
-                                direction = ArcFacing.Rear;
-                                break;
-                            case ArcType.Left:
-                                direction = ArcFacing.Left;
-                                break;
-                            case ArcType.Right:
-                                direction = ArcFacing.Right;
-                                break;
-                            default:
-                                break;
-                        }
-
-                        HostShip.ArcsInfo.GetArc<ArcSingleTurret>().RotateArc(direction);
-                    }
+                    HostShip.ArcsInfo.GetArc<ArcSingleTurret>().RotateArc(FacingsToAttacker.First());
+                    Messages.ShowInfo(HostShip.PilotInfo.PilotName + ": Arc indicator is rotated");
+                }
+                else if (FacingsToAttacker.Count > 1)
+                {
+                    RegisterAbilityTrigger(TriggerTypes.OnAttackStart, AskWhatArcToUse);
+                }
+                else
+                {
+                    Messages.ShowError(HostShip.PilotInfo.PilotName + ": Cannot find arc with attacker");
                 }
             }
         }
+
+        private void AskWhatArcToUse(object sender, EventArgs e)
+        {
+            NomLumbArcRotationSubphase subphase = Phases.StartTemporarySubPhaseNew<NomLumbArcRotationSubphase>(
+                "Nom Lumb: Arc Rotation Decision",
+                Triggers.FinishTrigger
+            );
+
+            subphase.DescriptionShort = HostShip.PilotInfo.PilotName;
+            subphase.DescriptionLong = "You must rotate arc indicator";
+            subphase.ImageSource = HostShip;
+
+            foreach (ArcFacing facing in FacingsToAttacker)
+            {
+                subphase.AddDecision(
+                    "Rotate to " + facing,
+                    delegate {
+                        HostShip.ArcsInfo.GetArc<ArcSingleTurret>().RotateArc(facing);
+                        DecisionSubPhase.ConfirmDecision();
+                    }
+                );
+            }
+
+            subphase.DecisionOwner = HostShip.Owner;
+
+            //TODO: Improve: count how many ships in each arc
+            subphase.DefaultDecisionName = subphase.GetDecisions().First().Name;
+
+            subphase.ShowSkipButton = false;
+
+            subphase.Start();
+        }
+
+        private List<ArcFacing> GetFacingsToAttacker()
+        {
+            List<ArcFacing> result = new List<ArcFacing>();
+
+            List<ArcType> standatdArcs = new List<ArcType>() { ArcType.Front, ArcType.Left, ArcType.Right, ArcType.Rear };
+            foreach (ArcType arcType in standatdArcs)
+            {
+                if (HostShip.SectorsInfo.IsShipInSector(Combat.Attacker, arcType))
+                {
+                    result.Add(ArcTypeToFacing(arcType));
+                }
+            }
+
+            return result;
+        }
+
+        private ArcFacing ArcTypeToFacing(ArcType type)
+        {
+            ArcFacing facing = ArcFacing.None;
+
+            switch (type)
+            {
+                case ArcType.Front:
+                    facing = ArcFacing.Front;
+                    break;
+                case ArcType.Rear:
+                    facing = ArcFacing.Rear;
+                    break;
+                case ArcType.Left:
+                    facing = ArcFacing.Left;
+                    break;
+                case ArcType.Right:
+                    facing = ArcFacing.Right;
+                    break;
+                default:
+                    break;
+            }
+
+            return facing;
+        }
+
+        public class NomLumbArcRotationSubphase : DecisionSubPhase { };
     }
 }

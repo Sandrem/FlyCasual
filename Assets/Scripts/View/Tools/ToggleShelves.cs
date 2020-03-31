de8c166676d3bf51579840a6a43bf0e77a9a545b
Sandrem <oogoeroo@gmail.com>
2020-03-31 14:34:13 +0300

    Shelves

---
+++ Assets/Scripts/View/Tools/ToggleShelves.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleShelves : MonoBehaviour
+{
+    public GameObject Room;
+
+    private bool ShelvesAreLoaded;
+    private GameObject Shelves;
+
+    void OnMouseDown()
+    {
+        if (!ShelvesAreLoaded)
+		{
+			GameObject prefab = Resources.Load<GameObject>("Prefabs/Scenes/Shelves");
+			Shelves = Instantiate(prefab, Room.transform);
+			ShelvesAreLoaded = true;
+		}
+		else
+		{	
+			GameObject.Destroy(Shelves);
+			ShelvesAreLoaded = false;
+		}
+    }
+}

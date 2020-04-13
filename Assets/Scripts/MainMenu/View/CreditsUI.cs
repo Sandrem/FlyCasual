344965b296a62fa56f9ab3c504830e11930887bf
Sandrem <oogoeroo@gmail.com>
2020-04-13 19:34:28 +0300

    Partisan skins by Davish_Krail

--- Assets/Scripts/MainMenu/View/CreditsUI.cs
+++ Assets/Scripts/MainMenu/View/CreditsUI.cs
@@ -14,7 +14,7 @@ public static class CreditsUI {
     {
         { "Programming", "Sandrem, Rune-b, Azrapse, Matthew Blanchard, GeneralVryth, bwakefield27, thordurk91, Conzar, deakolt, Galaxy613, TheChiefMoose, YannickBgn, Evan Lundell, Fabio Macal, SirHaplo, Nick Buonarota (NickyDaB), rats3g, tetrarchangel, Andrew Matheny, Jimmy5910, jamorbal, pappnase99, Stephen Wangner, jychuah, jtg15, vladamex, xNyer, Ryan Praska, savagerose, Krumok, Tron, mikethetike, tinydantser, belk, XPav, DejaFu, bitsai, joshhightower, Andrew Nickell, barraudl, randy-ja" },
         { "3D Art", "TerranCmdr, Izaak94, Darksaber, DTM, Warb Null, Rob, Gambler, Nim the Netrunner, Gremlyn, Morvana, FabioOtto, Grediel, Cherrybomb67, Smight, Voiceoftheforce, flipdark95, JonnieLP, Atherin, Captain Tributon, JackJohn2942, danielnavax90s, abdyla" },
-        { "2D Art", "xwingtmgphotography, Matthew Cohen, Andreas Bazylewsi, Andrew McCarthy" },
+        { "2D Art", "xwingtmgphotography, Matthew Cohen, Andreas Bazylewsi, Andrew McCarthy, Davish_Krail" },
         { "Music", "Daniel (Affordable Audio 4 Everyone), Robson Cozendey" },
         { "Fonts", "Pixel Sagas \"Rebellion\", \"Strike Fighter\" (Daniel Zadorozny, Iconian Fonts), xwing-miniatures-font (geordanr)" },
         { "Additional assets", "Skybox Volume 2: Nebula (Hedgehog Team), Simple Table Glass (Quadrante Studio), sFuture Modules PRO Space and Ground (Sami Lehtonen), Sci Fi Doors (MASH Virtual)" },

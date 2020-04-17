e7a54a57c3e53259a93c91fc5ab042e4a60f48d5
Sandrem <oogoeroo@gmail.com>
2020-04-17 16:57:11 +0300

    TIE SF are updated

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/TIESfFighter/ZetaSquadronSurvivor.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIESfFighter/ZetaSquadronSurvivor.cs
@@ -11,8 +11,7 @@ namespace Ship
                 PilotInfo = new PilotCardInfo(
                     "Zeta Squadron Survivor",
                     2,
-                    32 //,
-                      //seImageNumber: 120
+                    32
                 );
 
                 ImageUrl = "https://sb-cdn.fantasyflightgames.com/card_images/en/848db1993150bda19217e2c14b3c3df6.png";

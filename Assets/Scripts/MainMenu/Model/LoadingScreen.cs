2e5cb7578e8ba48171e72af0b30ac88cd8c5c89d
Sandrem <oogoeroo@gmail.com>
2020-04-21 19:27:58 +0300

    Loading tips are updated

--- Assets/Scripts/MainMenu/Model/LoadingScreen.cs
+++ Assets/Scripts/MainMenu/Model/LoadingScreen.cs
@@ -28,23 +28,25 @@ public static class LoadingScreen
         "Obstacle/ship placement is turn zero",
         "What is your desired endgame?",
         "Try to block opponent’s aces",
-        "In every matchup, there is a beatdown player, and a control player. Actions can be taken to change one's role in a situation...",
+        "In every matchup, there is a beatdown player, and a control player",
         "Stop blaming dice",
         "Think about the next turn when choosing your maneuver",
         "Sometimes bumping is good",
-        "Sometimes flight through an obstacle is better then flight out of combat zone for few turns",
-        "Sometimes the best idea is to fly in tight formation",
+        "Practice formation flying",
+        "Practice, practice, practice",
+        "Learn the rule of 11",
         "Don't install too many upgrades on a ship",
+        "Bring the right obstacles for your list and a plan on how to use them",
+        "Fly a ship according to its role: Jouster, Flanker, Tank, Support",
 
         // Sun Tzu
-        "If you know the enemy and know yourself, you need not fear the result of a hundred battles. (Sun Tzu)",
-        "All warfare is based on deception. (Sun Tzu)",
-        "In the midst of chaos, there is also opportunity. (Sun Tzu)",
-        "If your enemy is in superior strength, evade him. (Sun Tzu)",
-        "If your enemy's forces are united, separate them. (Sun Tzu)",
-        "To know your Enemy, you must become your Enemy. (Sun Tzu)",
-        "He will win who knows when to fight and when not to fight. (Sun Tzu)",
-        "Know yourself and you will win all battles. (Sun Tzu)",
+        "If you know the enemy and know yourself, you need not fear the result of a hundred battles",
+        "In the midst of chaos, there is also opportunity",
+        "If your enemy is in superior strength, evade him",
+        "If your enemy's forces are united, separate them",
+        "To know your Enemy, you must become your Enemy",
+        "He will win who knows when to fight and when not to fight",
+        "Know yourself and you will win all battles",
         "Tactics without strategy is the noise before defeat", // Author is unknown, but this is in style of Sun Tzu
 
         // Controls
@@ -55,19 +57,25 @@ public static class LoadingScreen
         "To change 3D/2D view: press TAB button",
         "To save/load squads: click SQUADS in squad builder",
         "To activate Skip button: press SPACE",
+        "You can import/export squads from popular squad builders in XWS format",
 
         // Development
         "Please report bugs on GitHub, with screenshots",
+        "Online multiplayer is in development",
 
         // The AI doesn't cheat!
         "I repeat again: The die rolls are fair",
         "No, the AI doesn't look at your dials",
+
+        // Specially for FFG
+        "You defenitely need to buy more miniatures",
 
         // Quotes
         "Try spinning - that's a good trick",
         "Do, or do not. There is no try.",
         "May the Force be with you!",
         "01110010 01101111 01100111 01100101 01110010\n01110010 01101111 01100111 01100101 01110010",  // "roger roger" in binary
+        "Permission to jump in an X-wing and blow something up?"
     };
 
     public static void Show()

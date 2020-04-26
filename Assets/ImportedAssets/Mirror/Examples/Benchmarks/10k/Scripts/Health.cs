5a98110fda6c4715b8d7c27f7aec7d4c31a3a0a9
Sandrem <oogoeroo@gmail.com>
2020-04-26 18:50:07 +0300

    Mirror library is imported

---
+++ Assets/ImportedAssets/Mirror/Examples/Benchmarks/10k/Scripts/Health.cs
@@ -0,0 +1,13 @@
+﻿namespace Mirror.Examples
+{
+    public class Health : NetworkBehaviour
+    {
+        [SyncVar] public int health = 10;
+
+        [ServerCallback]
+        public void Update()
+        {
+            health = (health + 1) % 10;
+        }
+    }
+}

913469d265a07c0073432bd4443555a195a8a496
Sandrem <oogoeroo@gmail.com>
2020-06-26 22:59:23 +0300

    Cost of Ensnare is fixed

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/Ensnare.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/Ensnare.cs
@@ -25,13 +25,13 @@ namespace UpgradesList.SecondEdition
         {
             Dictionary<int, int> initiativeToCost = new Dictionary<int, int>()
             {
-                {0, 10},
-                {1, 10},
-                {2, 10},
-                {3, 10},
-                {4, 10},
-                {5, 16},
-                {6, 24}
+                {0, 12},
+                {1, 12},
+                {2, 12},
+                {3, 12},
+                {4, 14},
+                {5, 21},
+                {6, 28}
             };
 
             UpgradeInfo.Cost = initiativeToCost[ship.PilotInfo.Initiative];

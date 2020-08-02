cd6dca9e015c95a073eb10cb357b9673758eaf1d
Sandrem <oogoeroo@gmail.com>
2020-08-02 17:34:26 +0300

    TIE rb Heavy model by Atherin

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIERbHeavy/Rampage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BoardTools;
+using Arcs;
+using Upgrade;
+using Ship;
+using System;
+
+namespace Ship
+{
+    namespace SecondEdition.TIERbHeavy
+    {
+        public class Rampage : TIERbHeavy
+        {
+            public Rampage() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "\"Rampage\"",
+                    4,
+                    60,
+                    isLimited: true
+                );
+
+                ImageUrl = "https://i.imgur.com/cPVLRxm.png";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/TIERbHeavy.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Actions;
+using ActionsList;
+using Arcs;
+using Movement;
+using Ship;
+using SubPhases;
+using Tokens;
+using UnityEngine;
+using Upgrade;
+
+namespace Ship.SecondEdition.TIERbHeavy
+{
+    public class TIERbHeavy : GenericShip
+    {
+        public TIERbHeavy() : base()
+        {
+            RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
+
+            ShipInfo = new ShipCardInfo
+            (
+                "TIE/rb Heavy",
+                BaseSize.Medium,
+                Faction.Imperial,
+                new ShipArcsInfo(ArcType.SingleTurret, 2), 1, 8, 0,
+                new ShipActionsInfo(
+                    new ActionInfo(typeof(FocusAction)),
+                    new ActionInfo(typeof(ReinforceAction)),
+                    new ActionInfo(typeof(TargetLockAction)),
+                    new ActionInfo(typeof(BarrelRollAction), ActionColor.Red),
+                    new ActionInfo(typeof(RotateArcAction))
+                ),
+                new ShipUpgradesInfo(
+                    UpgradeType.Title,
+                    UpgradeType.Crew,
+                    UpgradeType.Illicit,
+                    UpgradeType.Modification
+                )
+            );
+
+            ShipInfo.ActionIcons.AddLinkedAction(new LinkedActionInfo(typeof(RotateArcAction), typeof(CalculateAction), ActionColor.Red));
+
+            IconicPilots = new Dictionary<Faction, System.Type> {
+                { Faction.Imperial, typeof(Rampage) }
+            };
+
+            ModelInfo = new ShipModelInfo(
+                "TIE Heavy",
+                "Blue",
+                new Vector3(-3.73f, 7.9f, 5.55f),
+                2.3f
+            );
+
+            DialInfo = new ShipDialInfo(
+                new ManeuverInfo(ManeuverSpeed.Speed0, ManeuverDirection.Stationary, ManeuverBearing.Stationary, MovementComplexity.Complex),
+
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Easy),
+
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Normal),
+
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Complex),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Complex),
+
+                new ManeuverInfo(ManeuverSpeed.Speed4, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Normal)
+            );
+
+            SoundInfo = new ShipSoundInfo(
+                new List<string>()
+                {
+                    "TIE-Fly1",
+                    "TIE-Fly2",
+                    "TIE-Fly3",
+                    "TIE-Fly4",
+                    "TIE-Fly5",
+                    "TIE-Fly6",
+                    "TIE-Fly7"
+                },
+                "TIE-Fire", 2
+            );
+        }
+    }
+}

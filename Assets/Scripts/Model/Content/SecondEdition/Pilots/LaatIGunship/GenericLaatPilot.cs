70be08c973f91826ddf892d4cfeef18b2049ff24
Sandrem <oogoeroo@gmail.com>
2020-08-03 06:09:32 +0300

    LAAT/i Gunship model

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/LaatIGunship/GenericLaatPilot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Ship
+{
+    namespace SecondEdition.LaatIGunship
+    {
+        public class GenericLaatPilot : LaatIGunship
+        {
+            public GenericLaatPilot() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Generic Laat Pilot",
+                    2,
+                    50
+                );
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/LaatIGunship.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Actions;
+using ActionsList;
+using Arcs;
+using Movement;
+using Ship;
+using SubPhases;
+using Tokens;
+using UnityEngine;
+using Upgrade;
+
+namespace Ship.SecondEdition.LaatIGunship
+{
+    public class LaatIGunship : GenericShip
+    {
+        public LaatIGunship() : base()
+        {
+            RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
+
+            ShipInfo = new ShipCardInfo
+            (
+                "LAAT/i Gunship",
+                BaseSize.Medium,
+                Faction.Republic,
+                new ShipArcsInfo(ArcType.DoubleTurret, 2), 1, 8, 2,
+                new ShipActionsInfo(
+                    new ActionInfo(typeof(FocusAction)),
+                    new ActionInfo(typeof(ReinforceAction), ActionColor.Red),
+                    new ActionInfo(typeof(TargetLockAction)),
+                    new ActionInfo(typeof(RotateArcAction)),
+                    new ActionInfo(typeof(ReloadAction))
+                ),
+                new ShipUpgradesInfo(
+                    UpgradeType.Title,
+                    UpgradeType.Crew,
+                    UpgradeType.Missile,
+                    UpgradeType.Modification
+                )
+            );
+
+            IconicPilots = new Dictionary<Faction, System.Type> {
+                { Faction.Republic, typeof(GenericLaatPilot) }
+            };
+
+            ModelInfo = new ShipModelInfo(
+                "LAAT Gunship",
+                "Default",
+                new Vector3(-4f, 8.65f, 5.55f),
+                2.3f
+            );
+
+            DialInfo = new ShipDialInfo(
+                new ManeuverInfo(ManeuverSpeed.Speed0, ManeuverDirection.Stationary, ManeuverBearing.Stationary, MovementComplexity.Complex),
+
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Normal),
+
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Complex),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Complex),
+
+                new ManeuverInfo(ManeuverSpeed.Speed4, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Complex)
+            );
+
+            SoundInfo = new ShipSoundInfo(
+                new List<string>()
+                {
+                    "Falcon-Fly1",
+                    "Falcon-Fly2",
+                    "Falcon-Fly3"
+                },
+                "Falcon-Fire", 2
+            );
+        }
+    }
+}

f1faff7c227e83f1304769a3124ea4ed12c836af
Sandrem <oogoeroo@gmail.com>
2020-08-03 06:32:44 +0300

    HMP Droid Gunship by CrazyVulcan

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/HMPDroidGunship/SeparatistPredator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Ship
+{
+    namespace SecondEdition.HMPDroidGunship
+    {
+        public class SeparatistPredator : HMPDroidGunship
+        {
+            public SeparatistPredator() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Separatist Predator",
+                    4,
+                    50
+                );
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/HMPDroidGunship.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Actions;
+using ActionsList;
+using Arcs;
+using Movement;
+using UnityEngine;
+using Upgrade;
+
+namespace Ship.SecondEdition.HMPDroidGunship
+{
+    public class HMPDroidGunship : GenericShip
+    {
+        public HMPDroidGunship() : base()
+        {
+            RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
+
+            ShipInfo = new ShipCardInfo
+            (
+                "HMP Droid Gunship",
+                BaseSize.Small,
+                Faction.Separatists,
+                new ShipArcsInfo(ArcType.FullFront, 2), 1, 5, 3,
+                new ShipActionsInfo(
+                    new ActionInfo(typeof(CalculateAction)),
+                    new ActionInfo(typeof(TargetLockAction)),
+                    new ActionInfo(typeof(BarrelRollAction), ActionColor.Red),
+                    new ActionInfo(typeof(ReloadAction))
+                ),
+                new ShipUpgradesInfo(
+                    UpgradeType.Title,
+                    UpgradeType.Cannon,
+                    UpgradeType.Missile,
+                    UpgradeType.Modification,
+                    UpgradeType.Configuration
+                )
+            );
+
+            ShipInfo.ActionIcons.AddLinkedAction(new LinkedActionInfo(typeof(ReloadAction), typeof(CalculateAction), ActionColor.Red));
+
+            IconicPilots = new Dictionary<Faction, System.Type> {
+                { Faction.Separatists, typeof(SeparatistPredator) }
+            };
+
+            ModelInfo = new ShipModelInfo(
+                "HMP Droid Gunship",
+                "Default",
+                new Vector3(-3.62f, 7.62f, 5.55f),
+                1.5f
+            );
+
+            DialInfo = new ShipDialInfo(
+                new ManeuverInfo(ManeuverSpeed.Speed0, ManeuverDirection.Stationary, ManeuverBearing.Stationary, MovementComplexity.Complex),
+
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Complex),
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Complex),
+
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Easy),
+
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Complex),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Normal),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Complex),
+                new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Normal),
+
+                new ManeuverInfo(ManeuverSpeed.Speed4, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Complex),
+
+                new ManeuverInfo(ManeuverSpeed.Speed5, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Complex)
+            );
+
+            SoundInfo = new ShipSoundInfo(
+                new List<string>()
+                {
+                    "XWing-Fly1",
+                    "XWing-Fly2",
+                    "XWing-Fly3"
+                },
+                "XWing-Laser", 3
+            );
+        }
+    }
+}

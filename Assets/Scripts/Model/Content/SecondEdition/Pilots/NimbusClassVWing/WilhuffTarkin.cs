937a85803a317d259c90994d8bf474822dad0f73
Sandrem <oogoeroo@gmail.com>
2020-08-03 06:55:58 +0300

    V-Wing by CrazyVulcan is added

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/NimbusClassVWing/WilhuffTarkin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Ship;
+using SubPhases;
+using Tokens;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.NimbusClassVWing
+    {
+        public class WilhuffTarkin : NimbusClassVWing
+        {
+            public WilhuffTarkin() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Wilhuff Tarkin",
+                    3,
+                    40,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.WilhuffTarkinAbility),
+                    extraUpgradeIcons: new List<UpgradeType> { UpgradeType.Talent } 
+                );
+
+                ImageUrl = "https://i.imgur.com/IMMW1ey.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class WilhuffTarkinAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            
+        }
+
+        public override void DeactivateAbility()
+        {
+            
+        }
+    }
+}

49412f2868c4eb45e0377518e6d9d1d5590759e4
Sandrem <oogoeroo@gmail.com>
2020-08-08 20:03:53 +0300

    TIE Heavy generics

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIERbHeavy/CardiaAcademyPilot.cs
@@ -0,0 +1,22 @@
+﻿using Arcs;
+using Movement;
+using Tokens;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIERbHeavy
+    {
+        public class CardiaAcademyPilot : TIERbHeavy
+        {
+            public CardiaAcademyPilot() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Cardia Academy Pilot",
+                    1,
+                    30
+                );
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIERbHeavy/OnyxSquadronSentry.cs
@@ -0,0 +1,22 @@
+﻿using Arcs;
+using Movement;
+using Tokens;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIERbHeavy
+    {
+        public class OnyxSquadronSentry : TIERbHeavy
+        {
+            public OnyxSquadronSentry() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Onyx Squadron Sentry",
+                    3,
+                    33
+                );
+            }
+        }
+    }
+}

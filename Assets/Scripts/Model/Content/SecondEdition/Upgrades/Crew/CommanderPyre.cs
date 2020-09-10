9b932dc893f73dff99ddd83b611c46ecb8eadf02
rune-b <rune-b@users.noreply.github.com>
2020-09-10 09:01:59 +0200

    Commander Pyre (#2898)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/CommanderPyre.cs
@@ -0,0 +1,107 @@
+﻿using Ship;
+using Upgrade;
+using System.Linq;
+using Tokens;
+using SubPhases;
+using System;
+
+namespace UpgradesList.SecondEdition
+{
+    public class CommanderPyre : GenericUpgrade
+    {
+        public CommanderPyre() : base()
+        {
+            FromMod = typeof(Mods.ModsList.UnreleasedContentMod);
+
+            UpgradeInfo = new UpgradeCardInfo(
+                "Commander Pyre",
+                UpgradeType.Crew,
+                cost: 5, //TODO
+                isLimited: true,
+                restriction: new FactionRestriction(Faction.FirstOrder),
+                abilityType: typeof(Abilities.SecondEdition.CommanderPyreAbility)
+            );
+
+            ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/10/5d/105d386c-ff1b-44be-9b9c-a2c1dc2877ec/swz69_pyre_card.png";
+        }        
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //Setup: After placing forces, choose an enemy ship. It gains 2 stress tokens.
+    //While you defend, if the attacker is stressed, you may reroll 1 defense die.
+    public class CommanderPyreAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            Phases.Events.OnSetupEnd += RegisterAbility;
+            AddDiceModification(
+                HostName,
+                IsAvailable,
+                GetAiPriority,
+                DiceModificationType.Reroll,
+                1
+                );
+        }
+
+        public override void DeactivateAbility()
+        {
+            Phases.Events.OnSetupEnd -= RegisterAbility;
+            RemoveDiceModification();
+        }
+
+        private int GetAiPriority()
+        {
+            return 90;
+        }
+
+        private bool IsAvailable()
+        {
+            return Combat.Defender == HostShip
+                && Combat.AttackStep == CombatStep.Defence
+                && Combat.Attacker.IsStressed;
+        }
+
+        protected void RegisterAbility()
+        {
+            Triggers.RegisterTrigger(new Trigger()
+            {
+                Name = HostShip.PilotInfo.PilotName,
+                TriggerType = TriggerTypes.OnSetupEnd,
+                TriggerOwner = HostShip.Owner.PlayerNo,
+                EventHandler = SelectTarget
+            });
+        }
+
+
+        private void SelectTarget(object Sender, System.EventArgs e)
+        {
+            SelectTargetForAbility(
+                  AssignStress,
+                  CheckRequirements,
+                  GetAiPriority,
+                  HostShip.Owner.PlayerNo,
+                  HostName,
+                  "Assign 2 stress tokens to 1 enemy ship",
+                  HostUpgrade,
+                  showSkipButton: false
+            );
+        }
+
+        protected virtual void AssignStress()
+        {
+            TargetShip.Tokens.AssignTokens(() => new StressToken(TargetShip), 2, SelectShipSubPhase.FinishSelection);
+        }
+
+        protected virtual bool CheckRequirements(GenericShip ship)
+        {
+            return ship.Owner != HostShip.Owner;
+        }
+
+        private int GetAiPriority(GenericShip ship)
+        {
+            return ship.PilotInfo.Cost + ship.UpgradeBar.GetUpgradesOnlyFaceup().Sum(n => n.UpgradeInfo.Cost);
+        }
+    }
+}

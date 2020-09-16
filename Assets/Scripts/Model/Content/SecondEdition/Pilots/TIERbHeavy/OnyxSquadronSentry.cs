0b731b907e1f942d00efef8ddecfda978aaaa6ff
Sandrem <oogoeroo@gmail.com>
2020-09-16 20:00:45 +0300

    Talent slot is added to TIE/Rb second generic

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/TIERbHeavy/OnyxSquadronSentry.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIERbHeavy/OnyxSquadronSentry.cs
@@ -14,7 +14,8 @@ namespace Ship
                 PilotInfo = new PilotCardInfo(
                     "Onyx Squadron Sentry",
                     3,
-                    33
+                    33,
+                    extraUpgradeIcon: UpgradeType.Talent
                 );
 
                 ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/3d/7c/3d7ca1c1-9e57-4085-a9a6-e82a3d92c6df/swz67_onyx-sentry.png";

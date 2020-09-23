00e2f8093828c6dc457cb99fcae8fa308253474e
Sandrem <oogoeroo@gmail.com>
2020-09-23 20:39:16 +0300

    Nimi Chireen  (T-70 X-Wing)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/T70XWing/NimiChereen.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.T70XWing
+    {
+        public class NimiChereen : T70XWing
+        {
+            public NimiChereen() : base()
+            {
+                RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
+
+                PilotInfo = new PilotCardInfo(
+                    "Nimi Chereen",
+                    2,
+                    44,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.NimiChereenAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+
+                ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/c3/49/c349eef1-f13a-4c31-9b73-73dab884af41/swz68_nimi-chereen.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class NimiChereenAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            AddDiceModification(
+                "Nimi Chereen",
+                IsAvailable,
+                GetAiPriority,
+                DiceModificationType.Change,
+                1,
+                sidesCanBeSelected: new List<DieSide>() { DieSide.Blank },
+                sideCanBeChangedTo: DieSide.Focus
+            );
+        }
+
+        private bool IsAvailable()
+        {
+            return Combat.AttackStep == CombatStep.Attack
+                && Combat.Defender.State.Initiative > HostShip.State.Initiative
+                && Combat.CurrentDiceRoll.Blanks > 0;
+        }
+
+        private int GetAiPriority()
+        {
+            return 100;
+        }
+
+        public override void DeactivateAbility()
+        {
+            RemoveDiceModification();
+        }
+    }
+}

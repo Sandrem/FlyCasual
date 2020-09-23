0d5dfe6f8fcae3109e649e842b5b86a3b742b593
Sandrem <oogoeroo@gmail.com>
2020-09-24 01:32:22 +0300

    CombinedAbility cleanup

--- Assets/Scripts/Model/Content/Core/Ability/TemplateAbilities/TriggeredAbility/CombinedAbility.cs
+++ Assets/Scripts/Model/Content/Core/Ability/TemplateAbilities/TriggeredAbility/CombinedAbility.cs
@@ -1,7 +1,6 @@
 ﻿using Ship;
 using System;
 using System.Collections.Generic;
-using UnityEngine;
 using Upgrade;
 
 namespace Abilities
@@ -13,14 +12,10 @@ namespace Abilities
 
         public override void ActivateAbility()
         {
-            Debug.Log("Activate");
-
             foreach (GenericAbility ability in StoredAbilities)
             {
                 ability.ActivateAbility();
             }
-
-            Debug.Log("Activated: " + StoredAbilities.Count);
         }
 
         public override void DeactivateAbility()
@@ -33,8 +28,6 @@ namespace Abilities
 
         public override void Initialize(GenericShip hostShip)
         {
-            Debug.Log("Initialize");
-            
             foreach (Type ability in CombinedAbilities)
             {
                 StoredAbilities.Add((GenericAbility)Activator.CreateInstance(ability));
@@ -43,28 +36,22 @@ namespace Abilities
             {
                 ability.Initialize(hostShip);
             }
-
-            Debug.Log("Initialized: " + StoredAbilities.Count);
         }
 
         public override void InitializeForSquadBuilder(GenericShip hostShip)
         {
-            Debug.Log("Initialize SB");
             foreach (GenericAbility ability in StoredAbilities)
             {
                 ability.InitializeForSquadBuilder(hostShip);
             }
-            Debug.Log("Initialized SB: " + StoredAbilities.Count);
         }
 
         public override void InitializeForSquadBuilder(GenericUpgrade hostUpgrade)
         {
-            Debug.Log("Initialize SB");
             foreach (GenericAbility ability in StoredAbilities)
             {
                 ability.InitializeForSquadBuilder(hostUpgrade);
             }
-            Debug.Log("Initialized SB: " + StoredAbilities.Count);
         }
     }
 }

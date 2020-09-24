dd2abc9d6a5a3ac35fc1b56dc35566335e0c6fea
Sandrem <oogoeroo@gmail.com>
2020-09-24 03:00:07 +0300

    Ghost Company

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Gunner/GhostCompany.cs
@@ -0,0 +1,29 @@
+﻿using Upgrade;
+using System.Collections.Generic;
+using ActionsList;
+using Actions;
+
+namespace UpgradesList.SecondEdition
+{
+    public class GhostCompany : GenericUpgrade
+    {
+        public GhostCompany() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Ghost Company",
+                types: new List<UpgradeType>() { UpgradeType.Crew, UpgradeType.Gunner },
+                cost: 5,
+                isLimited: true,
+                abilityType: typeof(Abilities.SecondEdition.BistanGunnerAbility),
+                restrictions: new UpgradeCardRestrictions
+                (
+                    new FactionRestriction(Faction.Republic),
+                    new ActionBarRestriction(typeof(RotateArcAction))
+                ),
+                addActionLink: new LinkedActionInfo(typeof(RotateArcAction), typeof(FocusAction), ActionColor.Red)
+            );
+
+            ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/7e/83/7e836a20-1ed5-44c3-9f21-3e0e5e7f9347/swz70_a1_ghost-company_upgrade.png";
+        }
+    }
+}

76b9050f4d97337e5bb7379f7879115a93061aed
Sandrem <oogoeroo@gmail.com>
2020-09-25 18:46:40 +0300

    Eta2ActisGeneric is added

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Eta2Actis/Eta2ActisGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Upgrade;
+
+namespace Ship.SecondEdition.Eta2Actis
+{
+    public class Eta2ActisGeneric : Eta2Actis
+    {
+        public Eta2ActisGeneric()
+        {
+            PilotInfo = new PilotCardInfo(
+                "Eta-2 Actis Generic",
+                3,
+                37,
+                force: 1,
+                extraUpgradeIcon: UpgradeType.ForcePower
+            );
+        }
+    }
+}

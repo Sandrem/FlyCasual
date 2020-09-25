232a8dcc50fdfef7ea8de73f3ee51c03b88a5787
Sandrem <oogoeroo@gmail.com>
2020-09-25 19:34:07 +0300

    Firespray Generic for Separatists

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/FiresprayClassPatrolCraft/FiresprayGeneric.cs
@@ -0,0 +1,18 @@
+﻿namespace Ship
+{
+    namespace SecondEdition.FiresprayClassPatrolCraft
+    {
+        public class FiresprayGeneric : FiresprayClassPatrolCraft
+        {
+            public FiresprayGeneric() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Firespray Generic",
+                    2,
+                    62,
+                    factionOverride: Faction.Separatists
+                );
+            }
+        }
+    }
+}

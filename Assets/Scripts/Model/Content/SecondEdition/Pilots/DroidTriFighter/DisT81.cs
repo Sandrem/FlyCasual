ea4a080e728a8046edb6842d8ce390e5558e3ab8
Sandrem <oogoeroo@gmail.com>
2020-09-25 19:27:36 +0300

    Droid Tri-Fighter is added

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/DroidTriFighter/DisT81.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ship.SecondEdition.DroidTriFighter
+{
+    public class DisT81 : DroidTriFighter
+    {
+        public DisT81()
+        {
+            PilotInfo = new PilotCardInfo(
+                "DIS-T81",
+                3,
+                36,
+                true,
+                abilityType: typeof(Abilities.SecondEdition.DisT81Ability)
+            );
+
+            ImageUrl = "https://static.wikia.nocookie.net/xwing-miniatures-second-edition/images/c/c2/DIS-T81.png";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class DisT81Ability : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+
+        }
+
+        public override void DeactivateAbility()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/DroidTriFighter/DroidTriFighterGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ship.SecondEdition.DroidTriFighter
+{
+    public class DroidTriFighterGeneric : DroidTriFighter
+    {
+        public DroidTriFighterGeneric()
+        {
+            PilotInfo = new PilotCardInfo(
+                "Droid Tri-Fighter Generic",
+                1,
+                31
+            );
+        }
+    }
+}

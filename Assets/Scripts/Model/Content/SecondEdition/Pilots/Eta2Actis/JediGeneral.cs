af03e2aafe88d85af81633c08cdf2a9fa4454e93
Sandrem <oogoeroo@gmail.com>
2020-10-19 19:14:31 +0300

    Jedi General

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Eta2Actis/JediGeneral.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Upgrade;
+
+namespace Ship.SecondEdition.Eta2Actis
+{
+    public class JediGeneral : Eta2Actis
+    {
+        public JediGeneral()
+        {
+            PilotInfo = new PilotCardInfo(
+                "Jedi General",
+                4,
+                40,
+                force: 2,
+                extraUpgradeIcon: UpgradeType.ForcePower
+            );
+
+            ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/b4/8d/b48d787d-96db-4955-9a58-5c8aa3ab9035/swz79_jedi-general.png";
+        }
+    }
+}

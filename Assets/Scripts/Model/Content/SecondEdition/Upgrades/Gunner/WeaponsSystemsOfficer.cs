3f0e6c5ac73b45ba8d744648d0536682046b7c2b
Sandrem <oogoeroo@gmail.com>
2020-11-06 05:56:53 +0200

    Weapons Systems Officer

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Gunner/WeaponsSystemsOfficer.cs
@@ -0,0 +1,72 @@
+﻿using Upgrade;
+using Ship;
+using System;
+using SubPhases;
+
+namespace UpgradesList.SecondEdition
+{
+    public class WeaponsSystemsOfficer : GenericUpgrade
+    {
+        public WeaponsSystemsOfficer() : base()
+        {
+            FromMod = typeof(Mods.ModsList.UnreleasedContentMod);
+
+            UpgradeInfo = new UpgradeCardInfo(
+                "Weapons Systems Officer",
+                UpgradeType.Gunner,
+                cost: 1,
+                abilityType: typeof(Abilities.SecondEdition.WeaponsSystemsOfficerAbility)
+            );
+
+            ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/c8/de/c8de95b1-c5f8-467e-b9ef-0d81cc92f72e/swz82_a1_upgrade_weapon-systems.png";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class WeaponsSystemsOfficerAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnAttackFinishAsAttacker += CheckAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnAttackFinishAsAttacker -= CheckAbility;
+        }
+
+        private void CheckAbility(GenericShip ship)
+        {
+            if (Combat.ChosenWeapon.WeaponInfo.RequiresTokens.Contains(typeof(Tokens.BlueTargetLockToken)))
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnAttackFinish, AskToAquireLock);
+            }
+        }
+
+        private void AskToAquireLock(object sender, EventArgs e)
+        {
+            AskToUseAbility(
+                "Weapons Systems Officer",
+                AlwaysUseByDefault,
+                GainLockOnDefender,
+                descriptionLong: "Do you want to gain a acquire on the defender?",
+                imageHolder: HostUpgrade,
+                requiredPlayer: HostShip.Owner.PlayerNo
+            );
+        }
+
+        private void GainLockOnDefender(object sender, EventArgs e)
+        {
+            DecisionSubPhase.ConfirmDecisionNoCallback();
+
+            ActionsHolder.AcquireTargetLock(
+                HostShip,
+                Combat.Defender,
+                Triggers.FinishTrigger,
+                Triggers.FinishTrigger
+            );
+        }
+    }
+}

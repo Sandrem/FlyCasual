6c3ec5ae5ed652bd38307c4e729ee1d1a8ff0184
Sandrem <oogoeroo@gmail.com>
2020-11-07 19:08:54 +0200

    VERSION 1.6.1

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -11,9 +11,9 @@ public class Global : MonoBehaviour {
 
     private static bool isAlreadyInitialized;
 
-    public static string CurrentVersion = "1.6";
-    public static int CurrentVersionInt = 101060000;
-    public static int LatestVersionInt = 101060000;
+    public static string CurrentVersion = "1.6.1";
+    public static int CurrentVersionInt = 101060100;
+    public static int LatestVersionInt = 101060100;
 
     void Awake()
     {

d4109452eac68ab30962f092f17fb803489c4c81
Sandrem <oogoeroo@gmail.com>
2021-02-13 14:59:33 +0200

    VERSION 1.8.2.1

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -12,9 +12,9 @@ public class Global : MonoBehaviour {
 
     private static bool isAlreadyInitialized;
 
-    public static string CurrentVersion = "1.8.2";
-    public static int CurrentVersionInt = 101080200;
-    public static int LatestVersionInt = 101080200;
+    public static string CurrentVersion = "1.8.2.1";
+    public static int CurrentVersionInt = 101080201;
+    public static int LatestVersionInt = 101080201;
 
     public static SquadBuilder SquadBuilder { get; set;}
 

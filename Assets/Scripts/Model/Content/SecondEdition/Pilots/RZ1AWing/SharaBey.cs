b72df7fbe8cb3d813782e271a326bb907021f361
Sandrem <oogoeroo@gmail.com>
2021-02-20 20:12:33 +0200

    Shara Bey (A-Wing)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/RZ1AWing/SharaBey.cs
@@ -0,0 +1,31 @@
+﻿using Mods.ModsList;
+using System;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.RZ1AWing
+    {
+        public class SharaBey : RZ1AWing
+        {
+            public SharaBey() : base()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Shara Bey",
+                    4,
+                    34,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.SharaBeyAbility),
+                    extraUpgradeIcons: new List<UpgradeType>() { UpgradeType.Talent, UpgradeType.Talent },
+                    abilityText: "While you defend or perform a primary attack, you may spend 1 lock you have on the enemy ship to add 1 focus result to your dice results."
+                );
+
+                RequiredMods = new List<Type>() { typeof(UnreleasedContentMod) };
+                PilotNameCanonical = "sharabey-rz1awing";
+
+                ModelInfo.SkinName = "Phoenix Squadron";
+            }
+        }
+    }
+}

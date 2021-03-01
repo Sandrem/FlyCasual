a2d038fddd2fa16597c42d34bbbab7a94fbae2e9
Sandrem <oogoeroo@gmail.com>
2021-03-01 17:50:35 +0200

    Amaxine Warrior

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/BTLA4YWing/AmaxineWarrior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.BTLA4YWing
+    {
+        public class AmaxineWarrior : BTLA4YWing
+        {
+            public AmaxineWarrior() : base()
+            {
+                PilotInfo = new PilotCardInfo
+                (
+                    "Amaxine Warrior",
+                    3,
+                    33,
+                    extraUpgradeIcons: new List<UpgradeType>()
+                    {
+                        UpgradeType.Talent,
+                        UpgradeType.Tech,
+                        UpgradeType.Illicit
+                    },
+                    factionOverride: Faction.Scum
+                );
+
+                ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/52/7d/527dd63a-1c64-4d78-bac3-b7b999accaf9/swz85_pilot_amaxinewarrior.png";
+            }
+        }
+    }
+}

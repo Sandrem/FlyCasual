8c88d3a76c3d4d3c96d8e682b482fe9df2a92c0e
Sandrem <oogoeroo@gmail.com>
2021-03-01 17:50:43 +0200

    Jinata Security Officer

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/BTLA4YWing/JinataSecurityOfficer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.BTLA4YWing
+    {
+        public class JinataSecurityOfficer : BTLA4YWing
+        {
+            public JinataSecurityOfficer() : base()
+            {
+                PilotInfo = new PilotCardInfo
+                (
+                    "Jinata Security Officer",
+                    2,
+                    32,
+                    extraUpgradeIcons: new List<UpgradeType>()
+                    {
+                        UpgradeType.Tech,
+                        UpgradeType.Illicit
+                    },
+                    factionOverride: Faction.Scum
+                );
+
+                ImageUrl = "https://images-cdn.fantasyflightgames.com/filer_public/e6/7f/e67f3145-67ad-4175-8a48-b92d87e58c28/swz85_pilot_jinatasecurityofficer.png";
+            }
+        }
+    }
+}

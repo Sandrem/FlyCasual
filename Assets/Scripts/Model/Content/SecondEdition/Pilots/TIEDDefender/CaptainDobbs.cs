ed09d29739967ae0c61a650784a608460dc8e8a3
Sandrem <oogoeroo@gmail.com>
2021-03-25 13:21:47 +0200

    Captain Dobbs is upgrades

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEDDefender/CaptainDobbs.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIEDDefender/CaptainDobbs.cs
@@ -19,8 +19,7 @@ namespace Ship.SecondEdition.TIEDDefender
                 75,
                 isLimited: true,
                 abilityType: typeof(Abilities.SecondEdition.CaptainDobbsAbility),
-                extraUpgradeIcons: new List<UpgradeType>(){ UpgradeType.Talent, UpgradeType.Sensor },
-                abilityText: "When another friendly ship defends, before the Neutralize Results step, if you are in the attack arc and you are not ionized, you may gain 1 ion token to cancel 1 hit or crit result"
+                extraUpgradeIcons: new List<UpgradeType>(){ UpgradeType.Talent, UpgradeType.Sensor }
             );
 
             ImageUrl = "https://i.imgur.com/RfgdAPL.png";
@@ -30,6 +29,9 @@ namespace Ship.SecondEdition.TIEDDefender
 
 namespace Abilities.SecondEdition
 {
+    // While another friendly ship at range 0-1 defends, before the Neutralize Results step,
+    // if you are in the attack arc and are not ionized, you may gain 1 ion token to cancel 1 Hit result.
+
     public class CaptainDobbsAbility : GenericAbility
     {
         public override string Name => HostShip.PilotInfo.PilotName;
@@ -53,7 +55,7 @@ namespace Abilities.SecondEdition
             curDamageInfo = e;
 
             if (AbilityCanBeUsed())
-                RegisterAbilityTrigger(TriggerTypes.OnTryDamagePrevention, StartQuestionSubphase);
+                RegisterAbilityTrigger(TriggerTypes.OnTryDamagePrevention, AskToUseDobbsAbility);
         }
 
         protected virtual bool AbilityCanBeUsed()
@@ -66,9 +68,12 @@ namespace Abilities.SecondEdition
             if (curToDamage.Owner.PlayerNo != HostShip.Owner.PlayerNo || curToDamage.ShipId == HostShip.ShipId)
                 return false;
 
-            // Is the defender at range 1 and is there a hit/crit result?
-            if (!Board.IsShipAtRange(HostShip, curToDamage, 1) || curToDamage.AssignedDamageDiceroll.Successes < 1)
+            // Is the defender at range 1 and is there a hit result?
+            if (!Board.IsShipAtRange(HostShip, curToDamage, 1) || curToDamage.AssignedDamageDiceroll.RegularSuccesses < 1)
                 return false;
+
+            ShotInfoArc shotInfoArc = new ShotInfoArc(Combat.Attacker, HostShip, Combat.ArcForShot);
+            if (!shotInfoArc.InArc) return false;
 
             if (HostShip.Tokens.CountTokensByType<IonToken>() > 0)
                 return false;
@@ -78,62 +83,21 @@ namespace Abilities.SecondEdition
 
         protected class CaptainDobbsDecisionSubPhase : DecisionSubPhase { }
 
-        protected virtual void StartQuestionSubphase(object sender, EventArgs e)
+        protected virtual void AskToUseDobbsAbility(object sender, EventArgs e)
         {
-            CaptainDobbsDecisionSubPhase selectBiggsDarklighterSubPhase = (CaptainDobbsDecisionSubPhase)Phases.StartTemporarySubPhaseNew(
-                Name,
-                typeof(CaptainDobbsDecisionSubPhase),
-                Triggers.FinishTrigger
+            AskToUseAbility(
+                HostShip.PilotInfo.PilotName,
+                NeverUseByDefault,
+                CancelDamageAndGainIon,
+                descriptionLong: "Do you want to gain 1 Ion token to cancel 1 Hit result?",
+                imageHolder: HostShip,
+                requiredPlayer: HostShip.Owner.PlayerNo
             );
-
-            selectBiggsDarklighterSubPhase.DescriptionShort = Name;
-            selectBiggsDarklighterSubPhase.DescriptionLong = "You may gain 1 Ion token to cancel 1 die result";
-            selectBiggsDarklighterSubPhase.ImageSource = HostShip;
-
-            if (curToDamage.AssignedDamageDiceroll.RegularSuccesses > 0)
-            {
-                selectBiggsDarklighterSubPhase.AddDecision("Cancel Hit result", delegate { PreventDamage(DieSide.Success); });
-                selectBiggsDarklighterSubPhase.AddTooltip("Cancel Hit result", HostShip.ImageUrl);
-            }
-
-            if (curToDamage.AssignedDamageDiceroll.CriticalSuccesses > 0)
-            {
-                selectBiggsDarklighterSubPhase.AddDecision("Cancel Crit result", delegate { PreventDamage(DieSide.Crit); });
-                selectBiggsDarklighterSubPhase.AddTooltip("Cancel Crit result", HostShip.ImageUrl);
-            }
-
-            selectBiggsDarklighterSubPhase.AddDecision("Don't cancel anything", delegate { DecisionSubPhase.ConfirmDecision(); });
-            selectBiggsDarklighterSubPhase.DefaultDecisionName = GetDefaultDecision();
-            selectBiggsDarklighterSubPhase.ShowSkipButton = true;
-            selectBiggsDarklighterSubPhase.DecisionOwner = HostShip.Owner;
-            selectBiggsDarklighterSubPhase.Start();
         }
 
-        protected string GetDefaultDecision()
+        private void CancelDamageAndGainIon(object sender, System.EventArgs e)
         {
-            string result = "Don't cancel anything";
-
-            if (HostShip.State.HullCurrent > 1)
-            {
-                if (curToDamage.AssignedDamageDiceroll.RegularSuccesses > 0)
-                {
-                    result = "Cancel Hit result";
-                }
-
-                if (curToDamage.AssignedDamageDiceroll.CriticalSuccesses > 0)
-                {
-                    result = "Cancel Crit result";
-                }
-
-            }
-
-            return result;
-        }
-
-        private void PreventDamage(DieSide type)
-        {
-            // Find a crit and remove it from the ship we're protecting's assigned damage.
-            Die dieToRemove = curToDamage.AssignedDamageDiceroll.DiceList.Find(n => n.Side == type);
+            Die dieToRemove = curToDamage.AssignedDamageDiceroll.DiceList.Find(n => n.Side == DieSide.Success);
             curToDamage.AssignedDamageDiceroll.DiceList.Remove(dieToRemove);
 
             HostShip.Tokens.AssignToken(typeof(IonToken), DecisionSubPhase.ConfirmDecision);

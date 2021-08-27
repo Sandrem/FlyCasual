56789ee4e8d562ebd70745a6da519bcfcae31ea1
Sandrem <oogoeroo@gmail.com>
2021-08-28 02:11:26 +0300

    C'ai Threnalli (BTA-NR2 Y-Wing)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/BTANR2YWing/CaiThrenalli.cs
@@ -0,0 +1,27 @@
+﻿using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.BTANR2YWing
+    {
+        public class CaiThrenalli : BTANR2YWing
+        {
+            public CaiThrenalli() : base()
+            {
+                PilotInfo = new PilotCardInfo
+                (
+                    "C'ai Threnalli",
+                    2,
+                    28,
+                    extraUpgradeIcon: UpgradeType.Talent,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.CaiThrenalliAbility)
+                );
+
+                PilotNameCanonical = "caithrenalli-btanr2ywing";
+
+                ImageUrl = "https://i.imgur.com/quuZGXf.png";
+            }
+        }
+    }
+}

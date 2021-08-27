efb57a965162e352f2fe0a6564d229bcba8c4624
Sandrem <oogoeroo@gmail.com>
2021-08-28 01:29:41 +0300

    First Order Ordnance Tech

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Gunner/FirstOrderOrdnanceTech.cs
@@ -0,0 +1,26 @@
+﻿using Upgrade;
+using Actions;
+using ActionsList;
+
+namespace UpgradesList.SecondEdition
+{
+    public class FirstOrderOrdnanceTech : GenericUpgrade
+    {
+        public FirstOrderOrdnanceTech() : base()
+        {
+            FromMod = typeof(Mods.ModsList.UnreleasedContentMod);
+
+            UpgradeInfo = new UpgradeCardInfo
+            (
+                "First Order Ordnance Tech",
+                UpgradeType.Gunner,
+                cost: 1,
+                restriction: new FactionRestriction(Faction.FirstOrder),
+                addAction: new ActionInfo(typeof(ReloadAction)),
+                addActionLink: new LinkedActionInfo(typeof(ReloadAction), typeof(TargetLockAction), linkedColor: ActionColor.White)
+            );
+
+            ImageUrl = "https://i.imgur.com/QNh8vcN.png";
+        }
+    }
+}

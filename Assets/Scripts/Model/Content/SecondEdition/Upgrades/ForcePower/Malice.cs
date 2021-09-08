6a0503f39b1c2ed74805dc7b73822d7a2f3350e6
Sandrem <oogoeroo@gmail.com>
2021-09-08 10:19:32 +0300

    Malice and Compassion (WIP)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/ForcePower/Malice.cs
@@ -0,0 +1,41 @@
+﻿using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class Malice : GenericUpgrade
+    {
+        public Malice() : base()
+        {
+            IsWIP = true;
+
+            FromMod = typeof(Mods.ModsList.UnreleasedContentMod);
+
+            UpgradeInfo = new UpgradeCardInfo
+            (
+                "Malice",
+                UpgradeType.ForcePower,
+                cost: 1,
+                restriction: new ForceAlignmentRestriction(ForceAlignment.Dark),
+                abilityType: typeof(Abilities.SecondEdition.MaliceAbility)       
+            );
+
+            ImageUrl = "https://i.imgur.com/mRyM1s0.png";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class MaliceAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            
+        }
+
+        public override void DeactivateAbility()
+        {
+            
+        }
+    }
+}

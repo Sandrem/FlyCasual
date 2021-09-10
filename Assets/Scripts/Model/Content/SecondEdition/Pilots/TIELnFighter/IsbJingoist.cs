c9f09b74ae36e774d95fe5007ca4574482c31a6e
Sandrem <oogoeroo@gmail.com>
2021-09-10 11:40:06 +0300

    ISB Jingoist (WIP)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/TIELnFighter/IsbJingoist.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Upgrade;
+using System;
+
+namespace Ship
+{
+    namespace SecondEdition.TIELnFighter
+    {
+        public class IsbJingoist : TIELnFighter
+        {
+            public IsbJingoist() : base()
+            {
+                IsWIP = true;
+
+                RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
+
+                PilotInfo = new PilotCardInfo
+                (
+                    "ISB Jingoist",
+                    4,
+                    28,
+                    limited: 2,
+                    abilityType: typeof(Abilities.SecondEdition.IsbJingoistAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+
+                ImageUrl = "https://i.imgur.com/6rxtxtb.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class IsbJingoistAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            
+        }
+
+        public override void DeactivateAbility()
+        {
+            
+        }
+    }
+}
+

6cfa34da707e25c562c4f53e7fd704d6d40ceb83
Sandrem <oogoeroo@gmail.com>
2021-09-10 12:27:35 +0300

    The Child (WIP)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/TheChild.cs
@@ -0,0 +1,44 @@
+﻿using Upgrade;
+using Ship;
+using ActionsList;
+
+namespace UpgradesList.SecondEdition
+{
+    public class TheChild : GenericUpgrade
+    {
+        public TheChild() : base()
+        {
+            IsWIP = true;
+
+            FromMod = typeof(Mods.ModsList.UnreleasedContentMod);
+
+            UpgradeInfo = new UpgradeCardInfo
+            (
+                "The Child",
+                UpgradeType.Crew,
+                cost: 5,
+                abilityType: typeof(Abilities.SecondEdition.TheChildAbility),
+                restriction: new FactionRestriction(Faction.Imperial, Faction.Rebel, Faction.Scum),
+                addForce: 2
+            );
+
+            ImageUrl = "https://i.imgur.com/8pqkhJr.png";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class TheChildAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+
+        }
+
+        public override void DeactivateAbility()
+        {
+
+        }
+    }
+}

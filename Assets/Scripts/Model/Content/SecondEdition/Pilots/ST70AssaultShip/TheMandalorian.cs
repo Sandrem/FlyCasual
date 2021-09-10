f0eaec6e9006fb090c1b0cc7b8ce20247aba6c89
Sandrem <oogoeroo@gmail.com>
2021-09-10 11:39:56 +0300

    ST-70 Assault Ship + Mandalorian (WIP)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/ST70AssaultShip/TheMandalorian.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.ST70AssaultShip
+    {
+        public class TheMandalorian : ST70AssaultShip
+        {
+            public TheMandalorian() : base()
+            {
+                IsWIP = true;
+
+                RequiredMods = new List<Type>() { typeof(Mods.ModsList.UnreleasedContentMod) };
+
+                PilotInfo = new PilotCardInfo
+                (
+                    "TheMandalorian",
+                    5,
+                    55,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.TheMandalorianAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+
+                ImageUrl = "https://i.imgur.com/Ncx2wka.png";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class TheMandalorianAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            
+        }
+
+        public override void DeactivateAbility()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/ST70AssaultShip.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Movement;
+using ActionsList;
+using Upgrade;
+using Actions;
+using Arcs;
+
+namespace Ship
+{
+    namespace SecondEdition.ST70AssaultShip
+    {
+        public class ST70AssaultShip : GenericShip
+        {
+            public ST70AssaultShip() : base()
+            {
+                ShipInfo = new ShipCardInfo
+                (
+                    "ST-70 AssaultShip",
+                    BaseSize.Medium,
+                    Faction.Scum,
+                    new ShipArcsInfo(ArcType.Front, 3), 2, 7, 2,
+                    new ShipActionsInfo
+                    (
+                        new ActionInfo(typeof(FocusAction)),
+                        new ActionInfo(typeof(EvadeAction)),
+                        new ActionInfo(typeof(TargetLockAction)),
+                        new ActionInfo(typeof(BarrelRollAction), ActionColor.Red)
+                    ),
+                    new ShipUpgradesInfo
+                    (
+                        UpgradeType.Modification,
+                        UpgradeType.Crew,
+                        UpgradeType.Illicit
+                    )
+                );
+
+                IconicPilots = new Dictionary<Faction, System.Type> {
+                    { Faction.Scum, typeof(TheMandalorian) }
+                };
+
+                ModelInfo = new ShipModelInfo(
+                    "ST-70 AssaultShip",
+                    "ST-70 AssaultShip",
+                    new Vector3(-3.7f, 7.84f, 5.55f),
+                    1.75f
+                );
+
+                DialInfo = new ShipDialInfo
+                (
+                    new ManeuverInfo(ManeuverSpeed.Speed0, ManeuverDirection.Stationary, ManeuverBearing.Stationary, MovementComplexity.Complex),
+
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Complex),
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Complex),
+
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.TallonRoll, MovementComplexity.Complex),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.TallonRoll, MovementComplexity.Complex),
+
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Complex),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Complex),
+
+                    new ManeuverInfo(ManeuverSpeed.Speed4, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+
+                    new ManeuverInfo(ManeuverSpeed.Speed5, ManeuverDirection.Forward, ManeuverBearing.KoiogranTurn, MovementComplexity.Complex)
+                );
+
+                SoundInfo = new ShipSoundInfo(
+                    new List<string>()
+                    {
+                        "Slave1-Fly1",
+                        "Slave1-Fly2"
+                    },
+                    "Slave1-Fire", 3
+                );
+            }
+        }
+    }
+}

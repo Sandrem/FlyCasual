5d059b4f7cc27849de7c531637546b05feb244e9
Sandrem <oogoeroo@gmail.com>
2022-03-02 16:06:30 +0200

    Outer Rim Enforcer

---
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Scum/ST70AssaultShip/OuterRimEnforcer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.ST70AssaultShip
+    {
+        public class OuterRimEnforcer : ST70AssaultShip
+        {
+            public OuterRimEnforcer() : base()
+            {
+                PilotInfo = new PilotCardInfo25
+                (
+                    "Outer Rim Enforcer",
+                    "",
+                    Faction.Scum,
+                    2,
+                    6,
+                    10,
+                    extraUpgradeIcons: new List<UpgradeType>
+                    {
+                        UpgradeType.Modification
+                    }
+                );
+
+                ImageUrl = "https://infinitearenas.com/xw2/images/pilots/outerrimenforcer.png";
+            }
+        }
+    }
+}

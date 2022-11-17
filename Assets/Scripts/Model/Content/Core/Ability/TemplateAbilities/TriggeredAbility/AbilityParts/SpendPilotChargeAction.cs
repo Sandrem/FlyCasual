ad202dc3826a1132947849891d462be0120032d1
Sandrem <oogoeroo@gmail.com>
2022-11-17 20:10:07 +0200

    Wes Janson (Unreleased Content)

---
+++ Assets/Scripts/Model/Content/Core/Ability/TemplateAbilities/TriggeredAbility/AbilityParts/SpendPilotChargeAction.cs
@@ -0,0 +1,32 @@
+﻿namespace Abilities
+{
+    public class SpendPilotChargeAction : AbilityPart
+    {
+        private GenericAbility Ability;
+        private AbilityPart Next = null;
+
+        public SpendPilotChargeAction(AbilityPart next = null)
+        {
+            Next = next;
+        }
+
+        public override void DoAction(GenericAbility ability)
+        {
+            Ability = ability;
+
+            Ability.HostShip.SpendCharge();
+            FinishAbilityPart();
+        }
+        private void FinishAbilityPart()
+        {
+            if (Next != null)
+            {
+                Next.DoAction(Ability);
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/Core/Ability/TemplateAbilities/TriggeredAbility/Conditions/HasPilotChargesAbility.cs
@@ -0,0 +1,17 @@
+﻿namespace Abilities
+{
+    public class HasPilotChargesAbility : Condition
+    {
+        private int PilotChargesCount;
+
+        public HasPilotChargesAbility(int count)
+        {
+            PilotChargesCount = count;
+        }
+
+        public override bool Passed(ConditionArgs args)
+        {
+            return args.ShipAbilityHost.State.Charges >= PilotChargesCount;
+        }
+    }
+}

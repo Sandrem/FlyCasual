abcfa17b09a267e2e2dff69f9484e4e342e7e344
Sandrem <oogoeroo@gmail.com>
2022-11-18 20:30:02 +0200

    Lapin (Unreleased Content)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Scum/ModifiedTIELnFighter/Lapin.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Scum/ModifiedTIELnFighter/Lapin.cs
@@ -1,7 +1,6 @@
-﻿using Content;
+﻿using ActionsList;
+using Content;
 using Ship;
-using SubPhases;
-using System;
 using System.Collections.Generic;
 using Tokens;
 using Upgrade;
@@ -47,15 +46,33 @@ namespace Abilities.SecondEdition
 {
     public class LapinAbility : GenericAbility
     {
-
         public override void ActivateAbility()
         {
-            
+            GenericShip.OnTryAddAvailableDiceModificationGlobal += UseLapinRestriction;
         }
 
         public override void DeactivateAbility()
         {
-            
+            GenericShip.OnTryAddAvailableDiceModificationGlobal -= UseLapinRestriction;
+        }
+
+        private void UseLapinRestriction(GenericShip ship, GenericAction diceModification, ref bool canBeUsed)
+        {
+            if (!diceModification.IsNotRealDiceModification
+                && ship.Tokens.HasToken(typeof(StressToken))
+                && (IsShipIsFightingAgainstLapin(ship))
+            )
+            {
+                Messages.ShowErrorToHuman($"{HostShip.PilotInfo.PilotName}: {ship.PilotInfo.PilotName} is unable to modify dice");
+                canBeUsed = false;
+            }
+        }
+
+        private bool IsShipIsFightingAgainstLapin(GenericShip ship)
+        {
+            if (Tools.IsSameShip(Combat.Attacker, HostShip) && Tools.IsSameShip(Combat.Defender, ship)) return true;
+            else if (Tools.IsSameShip(Combat.Defender, HostShip) && Tools.IsSameShip(Combat.Attacker, ship)) return true;
+            else return false;
         }
     }
 }

266675a7dd0656d481f56d37658af16156a8eeff
Sandrem <oogoeroo@gmail.com>
2022-11-19 01:36:22 +0200

    Durge (Scum) (Unreleased Content)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Scum/RogueClassStarfighter/Durge.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Scum/RogueClassStarfighter/Durge.cs
@@ -1,4 +1,5 @@
 ﻿using Content;
+using Ship;
 using System.Collections.Generic;
 using Upgrade;
 
@@ -51,12 +52,70 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
-            
+            HostShip.OnGenerateDiceModificationsCompareResults += DurgeActionEffect;
         }
 
         public override void DeactivateAbility()
         {
-            
+            HostShip.OnGenerateDiceModificationsCompareResults -= DurgeActionEffect;
+        }
+
+        private void DurgeActionEffect(GenericShip host)
+        {
+            ActionsList.GenericAction newAction = new ActionsList.DurgeActionEffect()
+            {
+                ImageUrl = HostShip.ImageUrl,
+                HostShip = host
+            };
+            host.AddAvailableDiceModificationOwn(newAction);
         }
     }
+}
+
+namespace ActionsList
+{
+    public class DurgeActionEffect : GenericAction
+    {
+
+        public DurgeActionEffect()
+        {
+            Name = DiceModificationName = "Durge";
+            DiceModificationTiming = DiceModificationTimingType.CompareResults;
+        }
+
+        public override int GetDiceModificationPriority()
+        {
+            int result = 0;
+
+            //If shields cancel crit
+            if (Combat.DiceRollAttack.Successes - Combat.DiceRollDefence.Successes - 1 >= HostShip.State.ShieldsCurrent) result = 100;
+            //If crit is better than to destroyed
+            if (Combat.DiceRollAttack.Successes - Combat.DiceRollDefence.Successes - HostShip.State.HullCurrent == 0) result = 100;
+
+            return result;
+        }
+
+        public override bool IsDiceModificationAvailable()
+        {
+            bool result = false;
+
+            if (Combat.AttackStep == CombatStep.CompareResults
+                && Tools.IsSameShip(HostShip, Combat.Defender)
+                && (Combat.DiceRollAttack.Successes - Combat.DiceRollDefence.Successes > HostShip.State.ShieldsCurrent))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        public override void ActionEffect(System.Action callBack)
+        {
+            Combat.DiceRollAttack.ChangeOne(DieSide.Success, DieSide.Crit);
+            Combat.DiceRollAttack.ChangeOne(DieSide.Success, DieSide.Blank);
+            
+            callBack();
+        }
+    }
+
 }

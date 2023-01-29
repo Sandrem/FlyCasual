0595f1096f9e0e2f0192844b69f412795b06950f
Sandrem <oogoeroo@gmail.com>
2023-01-29 14:21:02 +0200

    Jul Jerjerrod (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/FO/TIESeBomber/JulJerjerrod.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/FO/TIESeBomber/JulJerjerrod.cs
@@ -1,8 +1,11 @@
-﻿using BoardTools;
+﻿using ActionsList;
+using BoardTools;
 using Content;
 using Ship;
+using SubPhases;
 using System;
 using System.Collections.Generic;
+using Tokens;
 using Upgrade;
 
 namespace Ship
@@ -13,8 +16,6 @@ namespace Ship
         {
             public JulJerjerrod() : base()
             {
-                IsWIP = true;
-
                 PilotInfo = new PilotCardInfo25
                 (
                     "Jul Jerjerrod",
@@ -24,7 +25,7 @@ namespace Ship
                     4,
                     14,
                     isLimited: true,
-                    abilityType: typeof(Abilities.SecondEdition.JulJerjerrodAbility),
+                    abilityType: typeof(Abilities.SecondEdition.JulJerjerrodPilotAbility),
                     extraUpgradeIcons: new List<UpgradeType>()
                     {
                         UpgradeType.Talent,
@@ -47,16 +48,77 @@ namespace Ship
 
 namespace Abilities.SecondEdition
 {
-    public class JulJerjerrodAbility : GenericAbility
+    public class JulJerjerrodPilotAbility : GenericAbility
     {
         public override void ActivateAbility()
         {
-            
+            HostShip.OnActionIsPerformed += CheckAbilityAfterBoost;
         }
 
         public override void DeactivateAbility()
         {
-            
+            HostShip.OnActionIsPerformed -= CheckAbilityAfterBoost;
         }
+
+        private void CheckAbilityAfterBoost(GenericAction action)
+        {
+            if (action is BoostAction && HostShip.State.Charges > 0 && HostShip.Tokens.GetNonLockRedOrangeTokens().Count > 0)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnActionIsPerformed, AskUseAbility);
+            }
+        }
+        private void AskUseAbility(object sender, EventArgs e)
+        {
+            AskToUseAbility(
+                HostShip.PilotInfo.PilotName,
+                NeverUseByDefault,
+                UseAbility,
+                descriptionLong: "Do you want to spend 1 Charge to remove 1 non-lock red or orange token?",
+                imageHolder: HostShip
+            );
+        }
+        private void UseAbility(object sender, EventArgs e)
+        {
+            JulJerjerrodAbilitySubphase subphase = Phases.StartTemporarySubPhaseNew<JulJerjerrodAbilitySubphase>(
+                "Remove Token",
+                DecisionSubPhase.ConfirmDecision
+            );
+
+            subphase.Name = HostShip.PilotInfo.PilotName;
+            subphase.DescriptionShort = "Select a non-lock red or orange token to remove";
+            subphase.ImageSource = HostShip;
+
+            subphase.DecisionOwner = HostShip.Owner;
+            subphase.ShowSkipButton = true;
+
+            HostShip.SpendCharges(1);
+
+            List<GenericToken> tokensToRemove = HostShip.Tokens.GetNonLockRedOrangeTokens();
+
+            foreach (GenericToken token in tokensToRemove)
+            {
+                subphase.AddDecision(
+                    token.Name,
+                    delegate {
+                        tokensToRemove.Add(token);
+                        ActionsHolder.RemoveTokens(tokensToRemove, DecisionSubPhase.ConfirmDecision);
+                    }
+                );
+            }
+
+
+            if (subphase.GetDecisions().Count > 0)
+            {
+                subphase.Start();
+            }
+            else
+            {
+                Phases.GoBack();
+                Messages.ShowInfoToHuman("Jul Jerjerrod: No tokens to remove");
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private class JulJerjerrodAbilitySubphase : DecisionSubPhase { }
     }
 }

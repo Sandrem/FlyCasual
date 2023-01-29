86bc5a1f717c11098c63b0b38c4596979d161dd6
Sandrem <oogoeroo@gmail.com>
2023-01-29 13:00:51 +0200

    Boost (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Republic/CloneZ95Headhunter/Boost.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Republic/CloneZ95Headhunter/Boost.cs
@@ -1,4 +1,6 @@
-﻿using Content;
+﻿using ActionsList;
+using BoardTools;
+using Content;
 using System.Collections.Generic;
 using Upgrade;
 
@@ -10,8 +12,6 @@ namespace Ship
         {
             public Boost() : base()
             {
-                IsWIP = true;
-
                 PilotInfo = new PilotCardInfo25
                 (
                     "\"Boost\"",
@@ -47,12 +47,37 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
-
+            Phases.Events.OnCombatPhaseStart_Triggers += RegisterBoostAbility;
         }
 
         public override void DeactivateAbility()
         {
+            Phases.Events.OnCombatPhaseStart_Triggers -= RegisterBoostAbility;
+        }
 
+        private void RegisterBoostAbility()
+        {
+            RegisterAbilityTrigger(TriggerTypes.OnCombatPhaseStart, FreeBoostAbility);
+        }
+
+        private void FreeBoostAbility(object sender, System.EventArgs e)
+        {
+
+            if (Board.GetShipsAtRange(HostShip, new UnityEngine.Vector2(0, 1), Team.Type.Friendly).FindAll(n => n.RevealedManeuver.ColorComplexity == Movement.MovementComplexity.Easy).Count >= 1)
+            {
+                Selection.ThisShip = HostShip;
+                HostShip.AskPerformFreeAction(
+                    new BoostAction() { HostShip = HostShip },
+                    Triggers.FinishTrigger,
+                    HostShip.PilotInfo.PilotName,
+                    "At the start of the Engagement Phase, if there is a friendly ship at range 0-1 whose revealed maneuver is blue, you may perform a boost action.",
+                    HostShip
+                );
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
         }
     }
 }

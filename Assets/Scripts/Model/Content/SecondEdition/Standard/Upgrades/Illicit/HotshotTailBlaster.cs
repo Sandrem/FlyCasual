a6b13876cc0a946f08e6f721c5cced180b6b6325
Sandrem <oogoeroo@gmail.com>
2023-01-29 11:30:39 +0200

    Hotshot Tail Blaster (by sampson-matt)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Upgrades/Illicit/HotshotTailBlaster.cs
@@ -0,0 +1,32 @@
+﻿using BoardTools;
+using Ship;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tokens;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class HotshotTailBlaster : GenericSpecialWeapon
+    {
+        public HotshotTailBlaster() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Hotshot Tail Blaster",
+                UpgradeType.Illicit,
+                cost: 2,
+                restriction: new BaseSizeRestriction(BaseSize.Medium, BaseSize.Large),
+                weaponInfo: new SpecialWeaponInfo(
+                    attackValue: 2,
+                    minRange: 0,
+                    maxRange: 1,
+                    arc: Arcs.ArcType.Rear,
+                    charges: 2
+                )
+            );
+
+            ImageUrl = "https://infinitearenas.com/xw2/images/upgrades/hotshottailblaster.png";
+        }        
+    }
+}

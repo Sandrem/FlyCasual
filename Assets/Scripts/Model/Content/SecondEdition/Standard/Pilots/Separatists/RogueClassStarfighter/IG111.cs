f5ae61277ac252a52d1cfb0592786333434e56d8
Sandrem <oogoeroo@gmail.com>
2023-01-29 10:07:18 +0200

    IG-111 (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Separatists/RogueClassStarfighter/IG111.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Separatists/RogueClassStarfighter/IG111.cs
@@ -1,5 +1,9 @@
-﻿using Content;
+﻿using Abilities.SecondEdition;
+using Content;
+using Ship;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Upgrade;
 
 namespace Ship
@@ -10,8 +14,6 @@ namespace Ship
         {
             public IG111() : base()
             {
-                IsWIP = true;
-
                 PilotInfo = new PilotCardInfo25
                 (
                     "IG-111",
@@ -37,6 +39,11 @@ namespace Ship
 
                 ShipInfo.ActionIcons.SwitchToDroidActions();
 
+                DeadToRights oldAbility = (DeadToRights)ShipAbilities.First(n => n.GetType() == typeof(DeadToRights));
+                oldAbility.DeactivateAbility();
+                ShipAbilities.Remove(oldAbility);
+                ShipAbilities.Add(new NetworkedCalculationsAbility());
+
                 ImageUrl = "https://infinitearenas.com/xw2/images/pilots/ig111.png";
             }
         }
@@ -49,12 +56,68 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
-            
+            HostShip.OnAttackMissedAsAttacker += RegisterAbility;
         }
 
         public override void DeactivateAbility()
         {
-            
+            HostShip.OnAttackMissedAsAttacker -= RegisterAbility;
+        }
+
+        private void RegisterAbility()
+        {
+            if (TargetsForAbilityExist(FilterTargets))
+            {
+                HostShip.OnAttackFinish += RegisterTrigger;
+            }
+        }
+
+        private void RegisterTrigger(GenericShip ship)
+        {
+            RegisterAbilityTrigger(TriggerTypes.OnAttackFinish, StartSelectShipSubphase);
+            HostShip.OnAttackFinish -= RegisterTrigger;
+        }
+
+        private void StartSelectShipSubphase(object sender, EventArgs e)
+        {
+            SelectTargetForAbility(
+                SufferDamage,
+                FilterTargets,
+                GetAiPriority,
+                HostShip.Owner.PlayerNo,
+                HostShip.PilotInfo.PilotName,
+                "You may gain 1 deplete token to causee 1 enemy ship in your bullseye arc to suffer 1 damage",
+                imageSource: HostShip
+            );
+        }
+
+        private int GetAiPriority(GenericShip ship)
+        {
+            return (100) - (ship.State.ShieldsCurrent + ship.State.HullCurrent);
+        }
+
+        private void SufferDamage()
+        {
+            Messages.ShowInfo(HostShip.PilotInfo.PilotName + " assigned a damage to " + Combat.Defender.PilotInfo.PilotName);
+            SubPhases.SelectShipSubPhase.FinishSelectionNoCallback();
+            DamageSourceEventArgs ig111damage = new DamageSourceEventArgs()
+            {
+                Source = HostShip,
+                DamageType = DamageTypes.CardAbility
+            };
+
+            HostShip.Tokens.AssignToken(
+                new Tokens.DepleteToken(HostShip),
+                Triggers.FinishTrigger);
+
+            TargetShip.Damage.TryResolveDamage(1, ig111damage, delegate { });
+
+        }
+
+        private bool FilterTargets(GenericShip ship)
+        {
+            return ship.Owner.PlayerNo != HostShip.Owner.PlayerNo
+                && HostShip.SectorsInfo.IsShipInSector(ship, Arcs.ArcType.Bullseye);
         }
     }
 }

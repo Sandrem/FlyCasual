6fb04d3ceb8a10feaa24b9b2335cabd81bd220f1
Sandrem <oogoeroo@gmail.com>
2023-01-29 13:01:13 +0200

    Knack (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Republic/CloneZ95Headhunter/Knack.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Republic/CloneZ95Headhunter/Knack.cs
@@ -1,5 +1,9 @@
 ﻿using Content;
+using Ship;
+using SubPhases;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Upgrade;
 
 namespace Ship
@@ -10,8 +14,6 @@ namespace Ship
         {
             public Knack() : base()
             {
-                IsWIP = true;
-
                 PilotInfo = new PilotCardInfo25
                 (
                     "\"Knack\"",
@@ -47,12 +49,99 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
-
+            HostShip.OnShipIsDestroyed += TryRegisterDestructionAbility;
         }
 
         public override void DeactivateAbility()
         {
+            HostShip.OnShipIsDestroyed -= TryRegisterDestructionAbility;
+        }
 
+        private void TryRegisterDestructionAbility(GenericShip ship, bool isFled)
+        {
+            if (HostShip.UpgradeBar.GetInstalledUpgrades(UpgradeType.Talent).Count > 0
+                && BoardTools.Board.GetShipsAtRange(HostShip, new UnityEngine.Vector2(0, 1), Team.Type.Friendly).FindAll(s => s.PilotInfo.IsLimited).Count > 0)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnShipIsDestroyed, AskSelectFriendlyShip);
+            }
+        }
+
+        private void AskSelectFriendlyShip(object sender, System.EventArgs e)
+        {
+            SelectTargetForAbility(
+                ShipIsSelected,
+                FilterTargets,
+                GetAiPriority,
+                HostShip.Owner.PlayerNo,
+                name: HostShip.PilotInfo.PilotName,
+                description: "Select friendly non-limited ship at range 0-2",
+                imageSource: HostShip,
+                showSkipButton: true
+            );
+        }
+
+        private void ShipIsSelected()
+        {
+            SelectShipSubPhase.FinishSelectionNoCallback();
+            StartSelectIllicitUpgradeSubphase(
+                "Choose Talent upgrade to assign to " + TargetShip.PilotInfo.PilotName,
+                (GenericUpgrade talentUpgrade) => { UpgradeCardIsSelected(talentUpgrade); },
+                delegate { Triggers.FinishTrigger(); }
+            );
+        }
+
+        private void StartSelectIllicitUpgradeSubphase(string descriptionText, Action<GenericUpgrade> doWithIllicitUpgrade, Action subphaseCallback)
+        {
+            AssignTalentUpgradeDecisionSubphase subphase = Phases.StartTemporarySubPhaseNew<AssignTalentUpgradeDecisionSubphase>(
+                descriptionText,
+                subphaseCallback
+            );
+
+            subphase.DescriptionShort = "Knack";
+            subphase.DescriptionLong = descriptionText;
+            subphase.ImageSource = HostUpgrade;
+
+            subphase.RequiredPlayer = Selection.ThisShip.Owner.PlayerNo;
+            subphase.DecisionViewType = DecisionViewTypes.ImagesUpgrade;
+
+            foreach (GenericUpgrade talentUpgrade in HostShip.UpgradeBar.GetInstalledUpgrades(UpgradeType.Talent))
+            {
+                subphase.AddDecision(
+                    talentUpgrade.UpgradeInfo.Name,
+                    delegate { doWithIllicitUpgrade(talentUpgrade); },
+                    talentUpgrade.ImageUrl
+                );
+            }
+
+            subphase.DefaultDecisionName = subphase.GetDecisions().First().Name;
+
+            subphase.Start();
+        }
+
+        private void UpgradeCardIsSelected(GenericUpgrade talentUpgrade)
+        {
+            TargetShip.PilotInfo.ExtraUpgrades.Add(UpgradeType.Talent);
+
+            talentUpgrade.PreAttachToShip(TargetShip);
+            talentUpgrade.AttachToShip(TargetShip);
+            Roster.UpdateUpgradesPanel(TargetShip, TargetShip.InfoPanel);
+            Roster.SubscribeUpgradesPanel(TargetShip, TargetShip.InfoPanel);
+            DecisionSubPhase.ConfirmDecisionNoCallback();
+            Triggers.FinishTrigger();
+        }
+
+        public class AssignTalentUpgradeDecisionSubphase : DecisionSubPhase { }
+
+        private bool FilterTargets(GenericShip ship)
+        {
+            return FilterByTargetType(ship, TargetTypes.OtherFriendly)
+                && FilterTargetsByRange(ship, 0, 1)
+                && !ship.PilotInfo.IsLimited;
+        }
+
+        private int GetAiPriority(GenericShip ship)
+        {
+            return 0;
         }
     }
 }

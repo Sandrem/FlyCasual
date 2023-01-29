9c21b0c9b12e776d016ac51df36044cf30a3926c
Sandrem <oogoeroo@gmail.com>
2023-01-29 14:21:44 +0200

    Poe Dameron (Resistance YT-1300) (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Resistance/ScavengedYT1300/PoeDameron.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Resistance/ScavengedYT1300/PoeDameron.cs
@@ -1,9 +1,12 @@
-﻿using ActionsList;
+﻿using Actions;
+using ActionsList;
 using BoardTools;
 using Content;
 using Ship;
+using System;
 using System.Collections.Generic;
 using Upgrade;
+using SubPhases;
 
 namespace Ship
 {
@@ -13,8 +16,6 @@ namespace Ship
         {
             public PoeDameron() : base()
             {
-                IsWIP = true;
-
                 PilotInfo = new PilotCardInfo25
                 (
                     "Poe Dameron",
@@ -24,7 +25,9 @@ namespace Ship
                     7,
                     25,
                     isLimited: true,
-                    abilityType: typeof(Abilities.SecondEdition.PoeDameronResistanceAbility),
+                    charges: 2,
+                    regensCharges: 1,
+                    abilityType: typeof(Abilities.SecondEdition.PoeDameronYT1300PilotAbility),
                     extraUpgradeIcons: new List<UpgradeType>
                     {
                         UpgradeType.Talent,
@@ -52,16 +55,115 @@ namespace Ship
 
 namespace Abilities.SecondEdition
 {
-    public class PoeDameronResistanceAbility : GenericAbility
+    public class PoeDameronYT1300PilotAbility : GenericAbility
     {
         public override void ActivateAbility()
         {
-            
+            HostShip.OnMovementActivationStart += RegisterTriggerActivationPhase;
+            HostShip.OnMovementFinishSuccessfully += RegisterAbility;
         }
 
         public override void DeactivateAbility()
         {
-            
+            HostShip.OnMovementActivationStart -= RegisterTriggerActivationPhase;
+            HostShip.OnMovementFinishSuccessfully -= RegisterAbility;
         }
+
+        private void RegisterAbility(GenericShip ship)
+        {
+            if (HostShip.State.Charges > 1)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnMovementFinish, PerformFreeFocusAction);
+            }
+        }
+
+        private void PerformFreeFocusAction(object sender, System.EventArgs e)
+        {
+            HostShip.BeforeActionIsPerformed += RegisterSpendChargeTrigger;
+            HostShip.OnActionIsPerformed += RegisterExposeDamageCardTrigger;
+            HostShip.AskPerformFreeAction(
+                new List<GenericAction>()
+                {
+                    new BoostAction(),
+                    new BarrelRollAction() { Color = ActionColor.Red }
+                },
+                CleanUp,
+                HostShip.PilotInfo.PilotName,
+                "After you fully execute a maneuver you may spend 2 Charges to perform a white Boost or a red Barrel Roll action. If you perform a red Barrel Roll action, expose 1 damage card if able.",
+                HostShip
+            );
+        }
+
+        private void RegisterExposeDamageCardTrigger(GenericAction action)
+        {
+            HostShip.OnActionIsPerformed -= RegisterExposeDamageCardTrigger;
+            if (action is BarrelRollAction && HostShip.Damage.GetFacedownCards().Count > 0)
+            {
+                RegisterAbilityTrigger(
+                    TriggerTypes.OnFreeAction,
+                    delegate
+                    {
+                        Messages.ShowInfo(HostShip.PilotInfo.PilotName + " has performed a red Barrel Roll action and must expose one damage card.");
+                        HostShip.Damage.ExposeRandomFacedownCard(Triggers.FinishTrigger);
+                    }
+                );
+            }
+        }
+
+        private void RegisterSpendChargeTrigger(GenericAction action, ref bool isFreeAction)
+        {
+            HostShip.BeforeActionIsPerformed -= RegisterSpendChargeTrigger;
+            RegisterAbilityTrigger(
+                TriggerTypes.OnFreeAction,
+                delegate
+                {
+                    HostShip.SpendCharges(2);
+                    Triggers.FinishTrigger();
+                }
+            );
+        }
+
+        private void CleanUp()
+        {
+            HostShip.BeforeActionIsPerformed -= RegisterSpendChargeTrigger;
+            HostShip.OnActionIsPerformed -= RegisterExposeDamageCardTrigger;
+            Triggers.FinishTrigger();
+        }
+
+        public void RegisterTriggerActivationPhase(GenericShip host)
+        {
+            if (HostShip.State.Charges > 0)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnMovementActivationStart, AskToUseAbilityActivationPhase);
+            }
+        }
+
+        private void AskToUseAbilityActivationPhase(object sender, EventArgs e)
+        {
+            AskToUseAbility(
+                HostShip.PilotInfo.PilotName,
+                NeverUseByDefault,
+                TurnOnIgnoreObstaclesActivationPhase,
+                descriptionLong: "Do you want to spend 1 Charge to ignore obstacles during this maneuver?",
+                imageHolder: HostShip
+            );
+        }
+
+        private void TurnOnIgnoreObstaclesActivationPhase(object sender, EventArgs e)
+        {
+            HostShip.SpendCharge();
+            HostShip.IsIgnoreObstacles = true;
+            HostShip.IsIgnoreObstacleObstructionWhenAttacking = true;
+            Phases.Events.OnActivationPhaseEnd_NoTriggers += TurnOffIgnoreObstaclesActivationPhase;
+            DecisionSubPhase.ConfirmDecision();
+        }
+
+        private void TurnOffIgnoreObstaclesActivationPhase()
+        {
+            HostShip.IsIgnoreObstacles = false;
+            HostShip.IsIgnoreObstacleObstructionWhenAttacking = false;
+            Phases.Events.OnActivationPhaseEnd_NoTriggers -= TurnOffIgnoreObstaclesActivationPhase;
+        }
+
     }
 }

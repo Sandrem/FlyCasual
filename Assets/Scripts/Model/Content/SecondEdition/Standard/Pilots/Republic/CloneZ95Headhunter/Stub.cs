060aae6159de6b29c64edafe92fdd498d14d5f73
Sandrem <oogoeroo@gmail.com>
2023-01-29 13:01:23 +0200

    Stub (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Republic/CloneZ95Headhunter/Stub.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Republic/CloneZ95Headhunter/Stub.cs
@@ -10,8 +10,6 @@ namespace Ship
         {
             public Stub() : base()
             {
-                IsWIP = true;
-
                 PilotInfo = new PilotCardInfo25
                 (
                     "\"Stub\"",
@@ -46,12 +44,36 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
-
+            HostShip.AfterGotNumberOfPrimaryWeaponAttackDice += CheckAttackAbility;
+            HostShip.AfterGotNumberOfDefenceDice += CheckDefensebility;
         }
 
         public override void DeactivateAbility()
         {
+            HostShip.AfterGotNumberOfPrimaryWeaponAttackDice -= CheckAttackAbility;
+            HostShip.AfterGotNumberOfDefenceDice -= CheckDefensebility;
+        }
 
+        private void CheckAttackAbility(ref int count)
+        {
+            if (HostShip.RevealedManeuver == null) return;
+
+            if (HostShip.RevealedManeuver.Speed % 2 != 0)
+            {
+                Messages.ShowInfo(HostShip.PilotInfo.PilotName + ": +1 attack die");
+                count++;
+            }
+        }
+
+        private void CheckDefensebility(ref int count)
+        {
+            if (HostShip.RevealedManeuver == null) return;
+
+            if (HostShip.RevealedManeuver.Speed % 2 == 0)
+            {
+                Messages.ShowInfo(HostShip.PilotInfo.PilotName + ": +1 defense die");
+                count++;
+            }
         }
     }
 }

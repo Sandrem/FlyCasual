d2e807e439851ecb2204c15115f25f0ee9387a00
Sandrem <oogoeroo@gmail.com>
2023-01-29 13:53:35 +0200

    Durge (Separatists) (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Separatists/RogueClassStarfighter/DurgeSeparatist.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Separatists/RogueClassStarfighter/DurgeSeparatist.cs
@@ -1,4 +1,6 @@
 ﻿using Content;
+using Ship;
+using System;
 using System.Collections.Generic;
 using Upgrade;
 
@@ -10,8 +12,6 @@ namespace Ship
         {
             public DurgeSeparatist() : base()
             {
-                IsWIP = true;
-
                 PilotInfo = new PilotCardInfo25
                 (
                     "Durge",
@@ -21,6 +21,7 @@ namespace Ship
                     4,
                     10,
                     isLimited: true,
+                    charges: 1,
                     abilityType: typeof(Abilities.SecondEdition.DurgeSeparatistAbility),
                     extraUpgradeIcons: new List<UpgradeType>
                     {
@@ -53,12 +54,78 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
-            
+            HostShip.OnDamageWasSuccessfullyDealt += RegisterAbility;
         }
+
 
         public override void DeactivateAbility()
         {
-            
+            HostShip.OnDamageWasSuccessfullyDealt += RegisterAbility;
+        }
+
+        private void RegisterAbility(GenericShip ship, bool flag)
+        {
+            if (HostShip.State.Charges > 0 && HostShip.Damage.HasFacedownCards && HostShip.AssignedDamageDiceroll.Count == 0 && HostShip.Damage.CountAssignedDamage() >= HostShip.ShipInfo.Hull)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnDamageWasSuccessfullyDealt, CheckAbility);
+            }
+        }
+
+        private void CheckAbility(object sender, EventArgs e)
+        {
+            if (HostShip.State.Charges > 0 && HostShip.Damage.HasFacedownCards)
+            {
+                AskToUseAbility(HostShip.PilotInfo.PilotName,
+                    AlwaysUseByDefault,
+                    UseAbility,
+                    dontUseAbility: delegate { HostShip.DestroyShipForced(Triggers.FinishTrigger); },
+                    descriptionLong: "Do you want to spend 1 charge to Charge to reveal all of your facedown damage, discard each Direct Hit! and each of your damage cards with the Pilot trait, then repair all of your faceup damage cards?",
+                    imageHolder: HostShip,
+                    requiredPlayer: HostShip.Owner.PlayerNo
+                );
+            }
+        }
+
+
+        private void UseAbility(object sender, System.EventArgs e)
+        {
+            SubPhases.DecisionSubPhase.ConfirmDecisionNoCallback();
+            HostShip.SpendCharge();
+
+            HostShip.OnDamageWasSuccessfullyDealt -= RegisterAbility;
+
+            List<GenericDamageCard> cardsToDiscard = HostShip.Damage.GetFacedownCards().FindAll(d => d.Type == CriticalCardType.Pilot || d.Name == "Direct Hit");
+            //foreach (GenericDamageCard card in HostShip.Damage.GetFacedownCards())
+            //{
+            //    Messages.ShowInfo(HostShip.PilotInfo.PilotName + " revealed: " + card.Name + " type:" + card.Type);
+            //}
+            //Messages.ShowInfo(HostShip.PilotInfo.PilotName + " revealed " + cardsToDiscard.Count + " Pilot trait and Direct Hit! cards.");
+
+            cardsToDiscard.AddRange(HostShip.Damage.GetFaceupCrits().FindAll(d => d.Type == CriticalCardType.Pilot || d.Name == "Direct Hit"));
+
+            Messages.ShowInfo(HostShip.PilotInfo.PilotName + " discarded " + cardsToDiscard.Count + " cards.");
+
+            int remainingDamage = HostShip.Damage.GetFacedownCards().Count + HostShip.Damage.GetFaceupCrits().Count - cardsToDiscard.Count;
+
+            //Messages.ShowInfo(HostShip.PilotInfo.PilotName + " remaining damage: " + remainingDamage);
+
+            if (cardsToDiscard.Count > 0 && remainingDamage < HostShip.ShipInfo.Hull)
+            {
+                Messages.ShowInfo(HostShip.PilotInfo.PilotName + " has prevented his own destruction");
+                HostShip.IsDestroyed = false;
+                foreach (GenericDamageCard card in cardsToDiscard)
+                {
+                    HostShip.Damage.DamageCards.Remove(card);
+                    HostShip.CallAfterAssignedDamageIsChanged();
+                }
+
+                foreach (GenericDamageCard card in HostShip.Damage.GetFaceupCrits())
+                {
+                    HostShip.Damage.FlipFaceupCritFacedown(card);
+                }
+            }
+            Triggers.FinishTrigger();
+
         }
     }
 }

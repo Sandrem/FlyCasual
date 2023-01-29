d9783334006fc6cd462d27ddca993009d82b9acc
Sandrem <oogoeroo@gmail.com>
2023-01-29 14:21:11 +0200

    Taka Jamoreesa (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Resistance/ResistanceTransport/TakaJamoreesa.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Resistance/ResistanceTransport/TakaJamoreesa.cs
@@ -1,6 +1,8 @@
 ﻿using Abilities.SecondEdition;
 using Ship;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Upgrade;
 
 namespace Ship
@@ -11,8 +13,6 @@ namespace Ship
         {
             public TakaJamoreesa() : base()
             {
-                IsWIP = true;
-
                 PilotInfo = new PilotCardInfo25
                 (
                     "Taka Jamoreesa",
@@ -46,14 +46,59 @@ namespace Abilities.SecondEdition
 {
     public class TakaJamoreesaAbility : GenericAbility
     {
+        GenericShip JammedShip;
         public override void ActivateAbility()
         {
-
+            HostShip.OnJamTargetIsSelected += CheckAssignJam;
         }
 
         public override void DeactivateAbility()
         {
+            HostShip.OnJamTargetIsSelected -= CheckAssignJam;
+        }
 
+        private void CheckAssignJam(GenericShip ship)
+        {
+            if (BoardTools.Board.GetShipsAtRange(ship, new UnityEngine.Vector2(0, 1), Team.Type.Any).Count > 1)
+            {
+                JammedShip = ship;
+                RegisterAbilityTrigger(TriggerTypes.OnActionIsPerformed, AskToJam);
+            }
+        }
+        private void AskToJam(object sender, EventArgs e)
+        {
+            SelectTargetForAbility(
+                ShipIsSelected,
+                FilterTargets,
+                GetAiPriority,
+                HostShip.Owner.PlayerNo,
+                name: HostShip.PilotInfo.PilotName,
+                description: "You must to assign a Jam token to ship at range 0-1 of " + JammedShip.PilotInfo.PilotName,
+                imageSource: HostShip,
+                showSkipButton: false
+            );
+        }
+
+        private void ShipIsSelected()
+        {
+            SubPhases.SelectShipSubPhase.FinishSelectionNoCallback();
+            TargetShip.Tokens.AssignToken(
+                new Tokens.JamToken(TargetShip, HostShip.Owner),
+                Triggers.FinishTrigger
+            );
+        }
+
+        private bool FilterTargets(GenericShip ship)
+        {
+            return BoardTools.Board.GetShipsAtRange(JammedShip, new UnityEngine.Vector2(0, 1), Team.Type.Any).Contains(ship) && ship.ShipId != JammedShip.ShipId;
+        }
+
+        private int GetAiPriority(GenericShip ship)
+        {
+            int teamModifier = (Tools.IsSameTeam(ship, HostShip)) ? 1 : 10;
+            int tokensModifier = (teamModifier == 1) ? 0 : ship.Tokens.GetAllTokens().Count(n => n.TokenColor == Tokens.TokenColors.Green) * 10;
+            int shipCostPriority = (teamModifier == 1) ? 200 - ship.PilotInfo.Cost : 200 + ship.PilotInfo.Cost;
+            return (shipCostPriority + tokensModifier) * teamModifier;
         }
     }
 }

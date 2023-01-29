a546e6dc24de39117774022f2db51a3873cddaaa
Sandrem <oogoeroo@gmail.com>
2023-01-29 11:52:27 +0200

    Bodica Venj (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Rebel/FangFighter/BodicaVenj.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Rebel/FangFighter/BodicaVenj.cs
@@ -1,5 +1,8 @@
 ﻿using Content;
+using Ship;
+using System;
 using System.Collections.Generic;
+using Tokens;
 using Upgrade;
 
 namespace Ship
@@ -10,8 +13,6 @@ namespace Ship
         {
             public BodicaVenj() : base()
             {
-                IsWIP = true;
-
                 PilotInfo = new PilotCardInfo25
                 (
                     "Bodica Venj",
@@ -45,14 +46,83 @@ namespace Abilities.SecondEdition
 {
     public class BodicaVenjAbility : GenericAbility
     {
+        GenericShip bonusAttackTarget;
+
         public override void ActivateAbility()
         {
-            
+            GenericShip.OnAttackFinishGlobal += CheckBodicaVenjAbility;
         }
 
         public override void DeactivateAbility()
         {
-            
+            GenericShip.OnAttackFinishGlobal -= CheckBodicaVenjAbility;
+        }
+
+        private void CheckBodicaVenjAbility(GenericShip ship)
+        {
+
+            if (!HostShip.IsDepleted && !HostShip.IsCannotAttackSecondTime && Tools.IsSameTeam(Combat.Defender, HostShip) && Combat.Defender.ShipId != HostShip.ShipId)
+            {
+                bonusAttackTarget = Combat.Attacker;
+                bonusAttackTarget.OnCombatCheckExtraAttack += RegisterBodicaVenjAbility;
+            }
+        }
+
+        private void RegisterBodicaVenjAbility(GenericShip ship)
+        {
+            bonusAttackTarget.OnCombatCheckExtraAttack -= RegisterBodicaVenjAbility;
+
+            RegisterAbilityTrigger(TriggerTypes.OnCombatCheckExtraAttack, UseBodicaVenjAbility);
+        }
+
+        private void UseBodicaVenjAbility(object sender, EventArgs e)
+        {
+            Messages.ShowInfo(HostShip.PilotInfo.PilotName + " can perform a bonus attack against " + bonusAttackTarget.PilotInfo.PilotName + " and then gain 1 deplete token.");
+
+            Combat.StartSelectAttackTarget
+            (
+                HostShip,
+                Cleanup,
+                IsPrimaryWeaponShot,
+                HostShip.PilotInfo.PilotName,
+                "You may perform a bonus attack against " + bonusAttackTarget.PilotInfo.PilotName + " and then gain 1 deplete token.",
+                HostShip
+            );
+        }
+
+        private bool IsPrimaryWeaponShot(GenericShip ship, IShipWeapon weapon, bool isSilent)
+        {
+            if (weapon.WeaponType == WeaponTypes.PrimaryWeapon && ship == bonusAttackTarget)
+            {
+                return true;
+            }
+            else if (weapon.WeaponType != WeaponTypes.PrimaryWeapon)
+            {
+                Messages.ShowError(HostShip.PilotInfo.PilotName + "'s bonus attack must be performed using primary weapon");
+                return false;
+            }
+            else
+            {
+                Messages.ShowError(HostShip.PilotInfo.PilotName + "'s bonus attack must target " + bonusAttackTarget.PilotInfo.PilotName);
+                return false;
+            }
+        }
+
+        private void Cleanup()
+        {
+            bonusAttackTarget = null;
+            HostShip.IsAttackPerformed = true;
+            //if bonus attack was skipped, allow bonus attacks again
+            if (HostShip.IsAttackSkipped)
+            {
+                HostShip.IsCannotAttackSecondTime = false;
+                Triggers.FinishTrigger();
+            }
+            else
+            {
+                HostShip.IsCannotAttackSecondTime = true;
+                HostShip.Tokens.AssignToken(typeof(DepleteToken), Triggers.FinishTrigger);
+            }
         }
     }
 }

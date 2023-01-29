cda32b9fe26eb5f1c08ababede1bb5f109bdccc6
Sandrem <oogoeroo@gmail.com>
2023-01-29 13:53:02 +0200

    Slammer (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Republic/V19TorrentStarfighter/Slammer.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Republic/V19TorrentStarfighter/Slammer.cs
@@ -11,8 +11,6 @@ namespace Ship.SecondEdition.V19TorrentStarfighter
     {
         public Slammer()
         {
-            IsWIP = true;
-
             PilotInfo = new PilotCardInfo25
             (
                 "\"Slammer\"",
@@ -22,6 +20,8 @@ namespace Ship.SecondEdition.V19TorrentStarfighter
                 3,
                 7,
                 isLimited: true,
+                charges: 2,
+                regensCharges: 1,
                 abilityType: typeof(Abilities.SecondEdition.SlammerAbility),
                 extraUpgradeIcons: new List<UpgradeType>
                 {
@@ -42,16 +42,58 @@ namespace Ship.SecondEdition.V19TorrentStarfighter
 
 namespace Abilities.SecondEdition
 {
+    //After you fully execute a speed 3-5 maneuver you may spend 1 charge to perform a boost action, even while stressed.
     public class SlammerAbility : GenericAbility
     {
         public override void ActivateAbility()
         {
-            
+            HostShip.OnMovementFinishSuccessfully += CheckAbility;
         }
 
         public override void DeactivateAbility()
         {
-            
+            HostShip.OnMovementFinishSuccessfully -= CheckAbility;
+        }
+
+        private void CheckAbility(GenericShip ship)
+        {
+            //AI doesn't use ability
+            if (HostShip.Owner.UsesHotacAiRules) return;
+
+            if (HostShip.State.Charges >= 2)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnMovementFinish, AskUseAbility);
+            }
+        }
+
+        private void AskUseAbility(object sender, EventArgs e)
+        {
+            HostShip.BeforeActionIsPerformed += RegisterSpendChargeTrigger;
+            HostShip.AskPerformFreeAction(
+                new SlamAction(true) { CanBePerformedWhileStressed = true },
+                CleanUp,
+                HostShip.PilotInfo.PilotName,
+                "After you fully execute a maneuver you may spend 1 Charge to perform a Slam action, even while stressed.",
+                HostShip
+            );
+        }
+
+        private void RegisterSpendChargeTrigger(GenericAction action, ref bool isFreeAction)
+        {
+            HostShip.BeforeActionIsPerformed -= RegisterSpendChargeTrigger;
+            RegisterAbilityTrigger(
+                TriggerTypes.OnFreeAction,
+                delegate {
+                    HostShip.SpendCharges(2);
+                    Triggers.FinishTrigger();
+                }
+            );
+        }
+
+        private void CleanUp()
+        {
+            HostShip.BeforeActionIsPerformed -= RegisterSpendChargeTrigger;
+            Triggers.FinishTrigger();
         }
     }
 }

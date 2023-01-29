17041dfdc2ed3fd85517f7dd9acf1a69883c7609
Sandrem <oogoeroo@gmail.com>
2023-01-29 11:52:15 +0200

    Clan Wren Volunteer (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Rebel/FangFighter/ClanWrenVolunteer.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Rebel/FangFighter/ClanWrenVolunteer.cs
@@ -1,5 +1,7 @@
-﻿using Content;
+﻿using BoardTools;
+using Content;
 using System.Collections.Generic;
+using System.Linq;
 using Upgrade;
 
 namespace Ship
@@ -10,8 +12,6 @@ namespace Ship
         {
             public ClanWrenVolunteer() : base()
             {
-                IsWIP = true;
-
                 PilotInfo = new PilotCardInfo25
                 (
                     "Clan Wren Volunteer",
@@ -47,12 +47,36 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
-            
+            AddDiceModification(
+                HostName,
+                IsDiceModificationAvailable,
+                GetDiceModificationAiPriority,
+                DiceModificationType.Reroll,
+                1
+            );
         }
 
         public override void DeactivateAbility()
         {
-            
+            RemoveDiceModification();
+        }
+
+        public bool IsDiceModificationAvailable()
+        {
+            bool result = true;
+
+            if (Combat.AttackStep != CombatStep.Attack) return false;
+
+            if (Combat.ShotInfo.Range != 1) return false;
+
+            if (!Board.GetShipsAtRange(HostShip, new UnityEngine.Vector2(1, 1), Team.Type.Friendly).Any(s => s.RevealedManeuver.Speed == HostShip.RevealedManeuver.Speed)) return false;
+
+            return result;
+        }
+
+        private int GetDiceModificationAiPriority()
+        {
+            return 90;
         }
     }
 }

5b25eb160a0f278825fd5143df2679b45d3d81a7
Sandrem <oogoeroo@gmail.com>
2023-01-29 13:01:04 +0200

    Killer (by sampson-matt)

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Republic/CloneZ95Headhunter/Killer.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Republic/CloneZ95Headhunter/Killer.cs
@@ -1,5 +1,6 @@
 ﻿using Content;
 using System.Collections.Generic;
+using Tokens;
 using Upgrade;
 
 namespace Ship
@@ -10,8 +11,6 @@ namespace Ship
         {
             public Killer() : base()
             {
-                IsWIP = true;
-
                 PilotInfo = new PilotCardInfo25
                 (
                     "\"Killer\"",
@@ -48,12 +47,52 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
-
+            HostShip.OnAttackStartAsAttacker += RegisterKillerAbility;
         }
 
         public override void DeactivateAbility()
         {
+            HostShip.OnAttackStartAsAttacker -= RegisterKillerAbility;
+        }
 
+        private void RegisterKillerAbility()
+        {
+            if (Combat.Defender.State.HullCurrent <= 2)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnAttackStart, AskKillerAbility);
+            }
+        }
+
+        protected void AskKillerAbility(object sender, System.EventArgs e)
+        {
+            AskToUseAbility(
+                HostShip.PilotInfo.PilotName,
+                AlwaysUseByDefault,
+                UseKillerAbility,
+                descriptionLong: "Do you want to roll 1 additional attack die, and then gain 1 deplete token?",
+                imageHolder: HostShip
+            );
+        }
+
+        private void UseKillerAbility(object sender, System.EventArgs e)
+        {
+            HostShip.AfterGotNumberOfAttackDice += IncreaseByOne;
+            SubPhases.DecisionSubPhase.ConfirmDecision();
+
+        }
+
+        private void IncreaseByOne(ref int value)
+        {
+            value++;
+            HostShip.AfterGotNumberOfAttackDice -= IncreaseByOne;
+            HostShip.AfterAttackWindow += AssignDepelete;
+
+        }
+
+        private void AssignDepelete()
+        {
+            HostShip.AfterAttackWindow -= AssignDepelete;
+            HostShip.Tokens.AssignToken(typeof(DepleteToken), delegate { });
         }
     }
 }

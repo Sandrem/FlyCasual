1a306651e9aa129df90e8968cc5f6f4789635a96
Sandrem <oogoeroo@gmail.com>
2023-01-30 17:39:37 +0200

    Scurrg

--- Assets/Scripts/Model/Content/SecondEdition/Extended/Pilots/Scum/ScurrgH6Bomber/SolSixxa.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Extended/Pilots/Scum/ScurrgH6Bomber/SolSixxa.cs
@@ -1,5 +1,6 @@
 ﻿using BoardTools;
 using Bombs;
+using Content;
 using Movement;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +14,28 @@ namespace Ship
         {
             public SolSixxa() : base()
             {
-                PilotInfo = new PilotCardInfo(
+                PilotInfo = new PilotCardInfo25
+                (
                     "Sol Sixxa",
+                    "Cunning Commander",
+                    Faction.Scum,
                     3,
-                    44,
+                    5,
+                    12,
                     isLimited: true,
                     abilityType: typeof(Abilities.SecondEdition.SolSixxaAbility),
-                    extraUpgradeIcon: UpgradeType.Talent,
-                    seImageNumber: 205
+                    extraUpgradeIcons: new List<UpgradeType>()
+                    {
+                        UpgradeType.Talent,
+                        UpgradeType.Turret,
+                        UpgradeType.Crew,
+                        UpgradeType.Gunner,
+                        UpgradeType.Device,
+                        UpgradeType.Device,
+                        UpgradeType.Illicit
+                    },
+                    seImageNumber: 205,
+                    legality: new List<Legality>() { Legality.ExtendedLegal }
                 );
             }
         }
@@ -31,6 +46,8 @@ namespace Abilities.SecondEdition
 {
     public class SolSixxaAbility : Abilities.FirstEdition.SolSixxaAbiliity
     {
+
+
         protected override void SolSixxaTemplate(List<ManeuverTemplate> availableTemplates, GenericUpgrade upgrade)
         {
             base.SolSixxaTemplate(availableTemplates, upgrade);
@@ -46,6 +63,42 @@ namespace Abilities.SecondEdition
                 if (!availableTemplates.Any(t => t.Name == newTemplate.Name))
                 {
                     availableTemplates.Add(newTemplate);
+                }
+            }
+        }
+    }
+}
+
+namespace Abilities.FirstEdition
+{
+    public class SolSixxaAbiliity : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnGetAvailableBombDropTemplatesTwoConditions += SolSixxaTemplate;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnGetAvailableBombDropTemplatesTwoConditions -= SolSixxaTemplate;
+        }
+
+        protected virtual void SolSixxaTemplate(List<ManeuverTemplate> availableTemplates, GenericUpgrade upgrade)
+        {
+            if (availableTemplates.Any(n => n.Bearing == ManeuverBearing.Straight && n.Speed == ManeuverSpeed.Speed1))
+            {
+                List<ManeuverTemplate> newTemplates = new List<ManeuverTemplate>()
+                {
+                    new ManeuverTemplate(ManeuverBearing.Turn, ManeuverDirection.Right, ManeuverSpeed.Speed1, isBombTemplate: true),
+                    new ManeuverTemplate(ManeuverBearing.Turn, ManeuverDirection.Left, ManeuverSpeed.Speed1, isBombTemplate: true),
+                };
+
+                foreach (ManeuverTemplate newTemplate in newTemplates)
+                {
+                    if (!availableTemplates.Any(t => t.Name == newTemplate.Name))
+                    {
+                        availableTemplates.Add(newTemplate);
+                    }
                 }
             }
         }
--- Assets/Scripts/Model/Content/SecondEdition/Extended/Ships/Scum/ScurrgH6Bomber.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Extended/Ships/Scum/ScurrgH6Bomber.cs
@@ -4,39 +4,81 @@ using Movement;
 using ActionsList;
 using Upgrade;
 using Actions;
+using Ship.CardInfo;
+using Arcs;
+using UnityEngine;
 
 namespace Ship
 {
     namespace SecondEdition.ScurrgH6Bomber
     {
-        public class ScurrgH6Bomber : FirstEdition.ScurrgH6Bomber.ScurrgH6Bomber
+        public class ScurrgH6Bomber : GenericShip
         {
             public ScurrgH6Bomber() : base()
             {
-                ShipInfo.FactionsAll.Remove(Faction.Rebel);
+                ShipInfo = new ShipCardInfo25
+                (
+                    "Scurrg H-6 Bomber",
+                    BaseSize.Medium,
+                    new FactionData
+                    (
+                        new Dictionary<Faction, System.Type>
+                        {
+                            { Faction.Scum, typeof(CaptainNym) }
+                        }
+                    ),
+                    new ShipArcsInfo(ArcType.Front, 3), 1, 6, 4,
+                    new ShipActionsInfo
+                    (
+                        new ActionInfo(typeof(FocusAction)),
+                        new ActionInfo(typeof(TargetLockAction)),
+                        new ActionInfo(typeof(BarrelRollAction), ActionColor.Red)
+                    ),
+                    new ShipUpgradesInfo()
+                );
 
-                ShipInfo.Hull = 6;
-                ShipInfo.Shields = 4;
-                ShipInfo.BaseSize = BaseSize.Medium;
+                ModelInfo = new ShipModelInfo
+                (
+                    "Scurrg H-6 Bomber",
+                    "Lok Revenant",
+                    new Vector3(-3.7f, 7.8f, 5.55f),
+                    2f
+                );
 
-                ShipInfo.ActionIcons.RemoveActions(typeof(BarrelRollAction));
-                ShipInfo.ActionIcons.AddActions(new ActionInfo(typeof(BarrelRollAction), ActionColor.Red));
+                DialInfo = new ShipDialInfo
+                (
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Easy),
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                    new ManeuverInfo(ManeuverSpeed.Speed1, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Easy),
 
-                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Missile);
-                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Torpedo);
-                ShipInfo.UpgradeIcons.Upgrades.Add(UpgradeType.Gunner);
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Easy),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Normal),
 
-                DialInfo.ChangeManeuverComplexity(new ManeuverHolder(ManeuverSpeed.Speed1, ManeuverDirection.Left, ManeuverBearing.Bank), MovementComplexity.Easy);
-                DialInfo.ChangeManeuverComplexity(new ManeuverHolder(ManeuverSpeed.Speed1, ManeuverDirection.Right, ManeuverBearing.Bank), MovementComplexity.Easy);
-                DialInfo.ChangeManeuverComplexity(new ManeuverHolder(ManeuverSpeed.Speed2, ManeuverDirection.Left, ManeuverBearing.Bank), MovementComplexity.Normal);
-                DialInfo.ChangeManeuverComplexity(new ManeuverHolder(ManeuverSpeed.Speed2, ManeuverDirection.Right, ManeuverBearing.Bank), MovementComplexity.Normal);
-                DialInfo.ChangeManeuverComplexity(new ManeuverHolder(ManeuverSpeed.Speed3, ManeuverDirection.Forward, ManeuverBearing.Straight), MovementComplexity.Normal);
-                DialInfo.ChangeManeuverComplexity(new ManeuverHolder(ManeuverSpeed.Speed4, ManeuverDirection.Forward, ManeuverBearing.Straight), MovementComplexity.Complex);
-                DialInfo.RemoveManeuver(new ManeuverHolder(ManeuverSpeed.Speed5, ManeuverDirection.Forward, ManeuverBearing.Straight));
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Turn, MovementComplexity.Complex),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Bank, MovementComplexity.Normal),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Turn, MovementComplexity.Complex),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.TallonRoll, MovementComplexity.Complex),
+                    new ManeuverInfo(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.TallonRoll, MovementComplexity.Complex),
 
-                IconicPilots[Faction.Scum] = typeof(CaptainNym);
+                    new ManeuverInfo(ManeuverSpeed.Speed4, ManeuverDirection.Forward, ManeuverBearing.Straight, MovementComplexity.Complex)
+                );
 
-                ManeuversImageUrl = "https://vignette.wikia.nocookie.net/xwing-miniatures-second-edition/images/0/0a/Maneuver_scurrg.png";
+                SoundInfo = new ShipSoundInfo
+                (
+                    new List<string>()
+                    {
+                        "YWing-Fly1",
+                        "YWing-Fly2"
+                    },
+                    "XWing-Laser", 2
+                );
+
+                ShipIconLetter = 'H';
             }
         }
     }

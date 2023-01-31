f0b9b47d359d78fd0d7b3f675a409bdfee95ec48
Sandrem <oogoeroo@gmail.com>
2023-01-31 06:13:17 +0200

    FE Modifications are removed

--- Assets/Scripts/Model/Content/SecondEdition/Standard/Upgrades/Modification/StealthDevice.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Upgrades/Modification/StealthDevice.cs
@@ -54,3 +54,42 @@ namespace Abilities.SecondEdition
         }
     }
 }
+
+namespace Abilities.FirstEdition
+{
+    public class StealthDeviceAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.ShipInfo.Agility++;
+            HostShip.ChangeAgilityBy(1);
+
+            HostShip.OnAttackHitAsDefender += RegisterStealthDeviceCleanup;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.ShipInfo.Agility--;
+            HostShip.ChangeAgilityBy(-1);
+
+            HostShip.OnAttackHitAsDefender -= RegisterStealthDeviceCleanup;
+        }
+
+        private void RegisterStealthDeviceCleanup()
+        {
+            Triggers.RegisterTrigger(new Trigger
+            {
+                Name = "Discard Stealth Device",
+                TriggerType = TriggerTypes.OnAttackHit,
+                TriggerOwner = HostShip.Owner.PlayerNo,
+                EventHandler = StealthDeviceCleanup
+            });
+        }
+
+        protected void StealthDeviceCleanup(object sender, System.EventArgs e)
+        {
+            Messages.ShowInfo("Stealth Device: This ship has suffered a hit! Discarding Stealth Device");
+            HostUpgrade.Discard(Triggers.FinishTrigger);
+        }
+    }
+}

79f9ef154aea47b4a460bc9d409a342444f6308d
Sandrem <oogoeroo@gmail.com>
2023-03-29 21:09:50 +0300

    "Wampa" (Battle of Yavin)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Empire/TIELnFighter/WampaBoY.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Ship;
+using Abilities.SecondEdition;
+using Upgrade;
+using Content;
+
+namespace Ship
+{
+    namespace SecondEdition.TIELnFighter
+    {
+        public class WampaBoY : TIELnFighter
+        {
+            public WampaBoY() : base()
+            {
+                PilotInfo = new PilotCardInfo25
+                (
+                    "\"Wampa\"",
+                    "Battle of Yavin",
+                    Faction.Imperial,
+                    1,
+                    3,
+                    0,
+                    isLimited: true,
+                    abilityType: typeof(WampaAbility),
+                    charges: 1,
+                    regensCharges: 1,
+                    extraUpgradeIcons: new List<UpgradeType>
+                    {
+                        UpgradeType.Talent,
+                        UpgradeType.Talent
+                    },
+                    tags: new List<Tags>
+                    {
+                        Tags.Tie
+                    },
+                    isStandardLayout: true
+                );
+
+                ImageUrl = "https://static.wikia.nocookie.net/xwing-miniatures-second-edition/images/0/0c/Wampa-battleofyavin.png";
+
+                MustHaveUpgrades.Add(typeof(UpgradesList.SecondEdition.Elusive));
+                MustHaveUpgrades.Add(typeof(UpgradesList.SecondEdition.Vengeful));
+
+                ShipInfo.Hull++;
+
+                PilotNameCanonical = "wampa-battleofyavin";
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Upgrades/Talent/Vengeful.cs
@@ -0,0 +1,125 @@
+﻿using BoardTools;
+using Ship;
+using SubPhases;
+using System;
+using System.Linq;
+using Tokens;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class Vengeful : GenericUpgrade
+    {
+        public Vengeful() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Vengeful",
+                UpgradeType.Talent,
+                cost: 0,
+                abilityType: typeof(Abilities.SecondEdition.VengefulAbility)
+            );
+
+            IsHidden = true;
+
+            ImageUrl = "https://i.imgur.com/Y0aJSkT.jpg";
+        }        
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class VengefulAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            GenericShip.OnShipIsDestroyedGlobal += CheckAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            GenericShip.OnShipIsDestroyedGlobal -= CheckAbility;
+        }
+
+        private void CheckAbility(GenericShip ship, bool flag)
+        {
+            if (!Tools.IsSameTeam(HostShip, ship)) return;
+            
+            DistanceInfo distanceInfo = new DistanceInfo(HostShip, ship);
+            if (distanceInfo.Range > 3) return;
+
+            if (HasWhatToDo())
+            {
+                RegisterAbilityTrigger(
+                    TriggerTypes.OnShipIsDestroyed,
+                    AskWhatToDo,
+                    customTriggerName: $"{HostUpgrade.UpgradeInfo.Name} (ID: {HostShip.ShipId})"
+                );
+            }
+        }
+
+        private bool HasWhatToDo()
+        {
+            return HostShip.Tokens.HasTokenByColor(TokenColors.Red)
+                || (HostShip.State.MaxCharges > 0
+                    && HostShip.State.RegensCharges != 0
+                    && HostShip.State.Charges < HostShip.State.MaxCharges);
+        }
+
+        private void AskWhatToDo(object sender, EventArgs e)
+        {
+            VengefulDecisionSubphase subphase = Phases.StartTemporarySubPhaseNew<VengefulDecisionSubphase>("Vengeful Decision", Triggers.FinishTrigger);
+
+            subphase.DescriptionShort = HostUpgrade.UpgradeInfo.Name;
+            subphase.DescriptionLong = "Choose what to do:";
+
+            foreach (GenericToken token in HostShip.Tokens.GetTokensByColor(TokenColors.Red))
+            {
+                if (!subphase.GetDecisions().Any(n => n.Name == GetRemoveTokenDescription(token)))
+                {
+                    subphase.AddDecision(
+                        GetRemoveTokenDescription(token),
+                        delegate { RemoveToken(token); }
+                    );
+                }
+            }
+
+            if (HostShip.State.MaxCharges > 0
+                && HostShip.State.RegensCharges != 0
+                && HostShip.State.Charges < HostShip.State.MaxCharges
+            )
+            {
+                subphase.AddDecision(
+                    $"Recover charge on {HostShip.PilotInfo.PilotName}",
+                    RecoverPilotCharge
+                );
+            }
+
+            subphase.ShowSkipButton = true;
+            subphase.DecisionOwner = HostShip.Owner;
+            subphase.DefaultDecisionName = subphase.GetDecisions().First()?.Name;
+
+            subphase.Start();
+        }
+
+        private string GetRemoveTokenDescription(GenericToken token)
+        {
+            string lockLetter = (token is RedTargetLockToken) ? $" ({(token as RedTargetLockToken).Letter})" : "";
+            return $"Remove {token.Name}{lockLetter}";
+        }
+
+        private void RemoveToken(GenericToken token)
+        {
+            DecisionSubPhase.ConfirmDecisionNoCallback();
+            HostShip.Tokens.RemoveToken(token, Triggers.FinishTrigger);
+        }
+
+        private void RecoverPilotCharge(object sender, EventArgs e)
+        {
+            DecisionSubPhase.ConfirmDecisionNoCallback();
+            HostShip.RestoreCharges(1);
+            Triggers.FinishTrigger();
+        }
+
+        private class VengefulDecisionSubphase : DecisionSubPhase { }
+    }
+}

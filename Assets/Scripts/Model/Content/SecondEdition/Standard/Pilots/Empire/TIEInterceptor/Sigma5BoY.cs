c47983533241cdbb1be0b471eff69f40aa7a67c6
Sandrem <oogoeroo@gmail.com>
2023-03-31 00:28:01 +0300

    Sigma 5 (Battle of Yavin)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Empire/TIEInterceptor/Sigma5BoY.cs
@@ -0,0 +1,112 @@
+﻿using Abilities.SecondEdition;
+using ActionsList;
+using BoardTools;
+using Content;
+using Ship;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEInterceptor
+    {
+        public class Sigma5BoY : TIEInterceptor
+        {
+            public Sigma5BoY() : base()
+            {
+                PilotInfo = new PilotCardInfo25
+                (
+                    "Sigma 5",
+                    "Battle of Yavin",
+                    Faction.Imperial,
+                    4,
+                    4,
+                    0,
+                    charges: 2,
+                    isLimited: true,
+                    abilityType: typeof(Sigma5BoYAbility),
+                    extraUpgradeIcons: new List<UpgradeType>()
+                    {
+                        UpgradeType.Talent,
+                        UpgradeType.Sensor
+                    },
+                    tags: new List<Tags>
+                    {
+                        Tags.Tie
+                    },
+                    isStandardLayout: true
+                );
+
+                AutoThrustersAbility oldAbility = (AutoThrustersAbility) ShipAbilities.First(n => n.GetType() == typeof(AutoThrustersAbility));
+                //oldAbility.DeactivateAbility();
+                ShipAbilities.Remove(oldAbility);
+                ShipAbilities.Add(new SensitiveControlsBoYRealAbility());
+
+                MustHaveUpgrades.Add(typeof(UpgradesList.SecondEdition.SensorJammer));
+                MustHaveUpgrades.Add(typeof(UpgradesList.SecondEdition.Elusive));
+
+                ImageUrl = "https://static.wikia.nocookie.net/xwing-miniatures-second-edition/images/8/85/Sigma5-battleofyavin.png";
+
+                PilotNameCanonical = "sigma5-battleofyavin";
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class Sigma5BoYAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnAttackHitAsAttacker += RegisterSigma5Ability;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnAttackHitAsAttacker -= RegisterSigma5Ability;
+        }
+
+        public void RegisterSigma5Ability()
+        {
+            if (HostShip.State.Charges > 0)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnAttackHit, AskPerformEvadeAction);
+            }
+        }
+
+        private void AskPerformEvadeAction(object sender, System.EventArgs e)
+        {
+            if (HostShip.State.Charges > 0)
+            {
+                HostShip.BeforeActionIsPerformed += PayChargeCost;
+
+                HostShip.AskPerformFreeAction
+                (
+                    new EvadeAction() { HostShip = HostShip },
+                    CleanUp,
+                    HostShip.PilotInfo.PilotName,
+                    "You may spend 1 charge to perform an Evade action"
+                );
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private void PayChargeCost(GenericAction action, ref bool isFreeAction)
+        {
+            HostShip.SpendCharge();
+            HostShip.BeforeActionIsPerformed -= PayChargeCost;
+        }
+
+        private void CleanUp()
+        {
+            HostShip.BeforeActionIsPerformed -= PayChargeCost;
+            Triggers.FinishTrigger();
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Upgrades/Sensor/SensorJammer.cs
@@ -0,0 +1,111 @@
+﻿using Ship;
+using Upgrade;
+using ActionsList;
+using System;
+using Tokens;
+
+namespace UpgradesList.SecondEdition
+{
+    public class SensorJammer : GenericUpgrade
+    {
+        public SensorJammer() : base()
+        {
+            IsHidden = true;
+
+            UpgradeInfo = new UpgradeCardInfo
+            (
+                "Sensor Jammer",
+                UpgradeType.Sensor,
+                cost: 0,
+                abilityType: typeof(Abilities.SecondEdition.SensorJammerAbility)
+            );
+
+            ImageUrl = "https://i.imgur.com/hN4ZDsy.jpg";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class SensorJammerAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnGenerateDiceModificationsOpposite += SensorJammerActionEffect;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnGenerateDiceModificationsOpposite -= SensorJammerActionEffect;
+        }
+
+        private void SensorJammerActionEffect(GenericShip host)
+        {
+            GenericAction newAction = new SensorJammerActionEffect()
+            {
+                ImageUrl = HostUpgrade.ImageUrl,
+                HostShip = host
+            };
+            host.AddAvailableDiceModificationOwn(newAction);
+        }
+    }
+}
+
+namespace ActionsList
+{
+    public class SensorJammerActionEffect : GenericAction
+    {
+
+        public SensorJammerActionEffect()
+        {
+            Name = DiceModificationName = "Sensor Jammer";
+            DiceModificationTiming = DiceModificationTimingType.Opposite;
+        }
+
+        public override int GetDiceModificationPriority()
+        {
+            int result = 0;
+
+            result = 100;
+
+            return result;
+        }
+
+        public override bool IsDiceModificationAvailable()
+        {
+            bool result = false;
+
+            if (Combat.AttackStep == CombatStep.Attack
+                && Combat.DiceRollAttack.RegularSuccesses > 0
+                && HasLockOfAnotherFriendlyShip(Combat.Attacker))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+        private bool HasLockOfAnotherFriendlyShip(GenericShip attacker)
+        {
+            foreach (GenericToken token in attacker.Tokens.GetAllTokens())
+            {
+                if ((token is RedTargetLockToken)
+                    && ((token as RedTargetLockToken).OtherTargetLockTokenOwner is GenericShip)
+                    && ((token as RedTargetLockToken).OtherTargetLockTokenOwner as GenericShip).Owner == HostShip.Owner)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override void ActionEffect(Action callBack)
+        {
+            Combat.DiceRollAttack.ChangeOne(DieSide.Success, DieSide.Focus, true);
+            callBack();
+        }
+
+    }
+
+}

d284b82ce51fbb7c91d51cae02667deac5b5d7fb
Sandrem <oogoeroo@gmail.com>
2023-04-21 01:17:46 +0300

    Phlac-Arphocc Prototype (Siege of Coruscant)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Pilots/Separatists/DroidTriFighter/PhlacArphoccPrototypeSoC.cs
@@ -0,0 +1,81 @@
+﻿using Content;
+using Ship;
+using System;
+using System.Collections.Generic;
+using Tokens;
+using Upgrade;
+using UpgradesList.SecondEdition;
+
+namespace Ship.SecondEdition.DroidTriFighter
+{
+    public class PhlacArphoccPrototypeSoC : DroidTriFighter
+    {
+        public PhlacArphoccPrototypeSoC()
+        {
+            PilotInfo = new PilotCardInfo25
+            (
+                "Phlac-Arphocc Prototype",
+                "Siege of Coruscant",
+                Faction.Separatists,
+                5,
+                4,
+                0,
+                limited: 2,
+                abilityType: typeof(Abilities.SecondEdition.PhlacArphoccPrototypeSoCAbility),
+                extraUpgradeIcons: new List<UpgradeType>
+                {
+                    UpgradeType.Configuration,
+                    UpgradeType.Modification,
+                    UpgradeType.Modification
+                },
+                tags: new List<Tags>
+                {
+                    Tags.Droid
+                },
+                isStandardLayout: true
+            );
+
+            MustHaveUpgrades.Add(typeof(AfterBurners));
+            MustHaveUpgrades.Add(typeof(ContingencyProtocol));
+            MustHaveUpgrades.Add(typeof(EvasionSequence7));
+
+            ImageUrl = "https://static.wikia.nocookie.net/xwing-miniatures-second-edition/images/5/5a/SWZ95_PhlacArphoccPrototype.png";
+
+            PilotNameCanonical = "phlacarphoccprototype-siegeofcoruscant";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class PhlacArphoccPrototypeSoCAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            Phases.Events.OnCombatPhaseStart_Triggers += CheckAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            Phases.Events.OnCombatPhaseStart_Triggers -= CheckAbility;
+        }
+
+        private void CheckAbility()
+        {
+            foreach (GenericShip enemyShip in HostShip.Owner.EnemyShips.Values)
+            {
+                if (HostShip.SectorsInfo.IsShipInSector(enemyShip, Arcs.ArcType.Bullseye))
+                {
+                    RegisterAbilityTrigger(TriggerTypes.OnCombatPhaseStart, AssignCalculateToken);
+                    return;
+                }
+            }
+        }
+
+        private void AssignCalculateToken(object sender, EventArgs e)
+        {
+            Messages.ShowInfo($"{HostShip.PilotInfo.PilotName} (ID:{HostShip.ShipId}) gains calculate token");
+            HostShip.Tokens.AssignToken(typeof(CalculateToken), Triggers.FinishTrigger);
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Standard/Upgrades/Modification/EvasionSequence7.cs
@@ -0,0 +1,65 @@
+﻿using Upgrade;
+using ActionsList;
+using Obstacles;
+using BoardTools;
+using Actions;
+
+namespace UpgradesList.SecondEdition
+{
+    public class EvasionSequence7 : GenericUpgrade
+    {
+        public EvasionSequence7() : base()
+        {
+            IsHidden = true;
+
+            UpgradeInfo = new UpgradeCardInfo
+            (
+                "Evasion Sequence 7",
+                UpgradeType.Modification,
+                cost: 0,
+                abilityType: typeof(Abilities.SecondEdition.EvasionSequence7Ability)
+            );
+
+            ImageUrl = "https://i.imgur.com/a4AvvnZ.jpg";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class EvasionSequence7Ability : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnCheckActionComplexity += CheckDecreaseComplexity;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnCheckActionComplexity -= CheckDecreaseComplexity;
+        }
+
+        private void CheckDecreaseComplexity(GenericAction action, ref ActionColor color)
+        {
+            if (action is EvadeAction && color == ActionColor.Red)
+            {
+                if (IsNearObstacle()) color = ActionColor.White;
+            }
+        }
+
+        private bool IsNearObstacle()
+        {
+            foreach (GenericObstacle obstacle in ObstaclesManager.GetPlacedObstacles())
+            {
+                ShipObstacleDistance shipObstacleDist = new ShipObstacleDistance(HostShip, obstacle);
+                if (shipObstacleDist.Range == 1)
+                {
+                    Messages.ShowInfoToHuman(HostUpgrade.UpgradeInfo.Name + ": The Evade action is treated as a white action");
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
